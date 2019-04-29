134f7a64918ef64439da85b1a29071ca7f34b4e3
Alan Woo <public@alanwoo.ca>
2019-04-30 01:59:26 +0200

    Configure deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+echo 'www.ethereum.org' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,7 @@ module.exports = {
       { text: 'Build', link: '/build/' }
     ]
   },
+  base: "/<REPO>/",
   head: [
     ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,maximum-scale=1'}],
     ['link', {rel: 'icon', type: 'image/png', href: 'favicon.png'}]
