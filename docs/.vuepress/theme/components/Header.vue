b39730cc7b50d91f0a7e1036800a3ebd8359bca9
Alan Woo <studio@alanwoo.ca>
2019-05-01 10:29:04 +0200

    Fix github link for mobile

--- docs/.vuepress/theme/components/Header.vue
+++ docs/.vuepress/theme/components/Header.vue
@@ -6,9 +6,9 @@
     </div>
 
     <div class="menu inline flex flex-center">
-      <a href="https://github.com/ethereum/ethereum-org-website" target="_blank" class="sm-hide md-hide"><button>Fork This Page</button></a>
+      <a href="https://github.com/ethereum/ethereum-org-website" target="_blank" title="Fork This Page (Github)" class="sm-hide md-hide"><button>Fork This Page</button></a>
       <SearchBox v-if="$site.themeConfig.search !== false"/>
-      <a href="#" class="md-up-hide">
+      <a href="https://github.com/ethereum/ethereum-org-website" target="_blank" title="Fork This Page (Github)" class="md-up-hide">
         <img alt="Github" class="hide-dark" src="../images/icon-github.svg" />
         <img alt="Github" class="show-dark" src="../images/icon-github-white.svg" />
       </a>
