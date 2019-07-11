084e50027fd30bd0beb2ccaa7ed6a6005d5f1edb
Anuj Gupta <mail@anuj.me>
2019-07-11 22:20:22 +0530

    Added Matomo to footer

--- docs/.vuepress/theme/components/Footer.vue
+++ docs/.vuepress/theme/components/Footer.vue
@@ -1,4 +1,4 @@
-<template>
+><template>
   <footer class="footer">
     <ul>
       <li><a href="https://github.com/ethereum" target="_blank">GitHub</a></li>
@@ -10,7 +10,25 @@
       <li><router-link to="/cookie-policy/">Cookie Policy</router-link></li>
       <li><a href="mailto:press@ethereum.org">Contact</a></li>
     </ul>
-  </footer>
+    <!-- Matomo -->
+    <script type="text/javascript">
+      var _paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(["setCookieDomain", "*.ethereum.org"]);
+      _paq.push(["setDomains", ["*.ethereum.org","*.ethereum.org"]]);
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//matomo.ethereum.org/piwik/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '4']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="//matomo.ethereum.org/piwik/matomo.php?idsite=4&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Matomo Code -->
+  </footer>  
 </template>
 
 <style lang="stylus" scoped>
