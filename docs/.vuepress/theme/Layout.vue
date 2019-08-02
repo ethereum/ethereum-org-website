3859e9825532f3982ff48b28fe3374458d3f60d8
Alan Woo <public@alanwoo.ca>
2019-08-02 13:36:07 +0200

    Update link to artwork post

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -6,7 +6,7 @@
     <Sidebar :items="sidebarItems" @close-sidebar="closeSidebar" />
     <Footer :class="{ 'home': isLanding }" />
 
-    <a href="https://blog.ethereum.org/2019/04/30/beginning-a-new-ethereum-org/" target="_blank">
+    <a href="https://hackernoon.com/rethinking-the-identity-of-ethereumorg-l718w347l" target="_blank">
       <button v-if="!isRelaunch" class="announcement">
         Read about the new artwork!  <span class="accent">→  More</span>
       </button>
