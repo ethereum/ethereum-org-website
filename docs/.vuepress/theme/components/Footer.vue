e5e25482afe78562bbafb2a4e2bdc09a8ba038c2
Alan Woo <public@alanwoo.ca>
2019-08-06 20:01:17 +0200

    Fix quotes, fix dupe domain

--- docs/.vuepress/theme/components/Footer.vue
+++ docs/.vuepress/theme/components/Footer.vue
@@ -17,12 +17,12 @@
     <script type="text/javascript">
       var _paq = window._paq || [];
       /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-      _paq.push(["setCookieDomain", "*.ethereum.org"]);
-      _paq.push(["setDomains", ["*.ethereum.org","*.ethereum.org"]]);
+      _paq.push(['setCookieDomain', '*.ethereum.org']);
+      _paq.push(['setDomains', ['*.ethereum.org']]);
       _paq.push(['trackPageView']);
       _paq.push(['enableLinkTracking']);
       (function() {
-        var u="//matomo.ethereum.org/piwik/";
+        var u='//matomo.ethereum.org/piwik/';
         _paq.push(['setTrackerUrl', u+'matomo.php']);
         _paq.push(['setSiteId', '4']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
