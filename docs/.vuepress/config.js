bfafb459197ad711bc45c8ce36c1715a5e7254c5
Sam Richards <sbrichards@gmail.com>
2019-08-19 16:59:05 -0700

    Matomo fix (#159)
    * gs
    
    * Move motomo to config
    
    * Take 2
    
    * Remove noscript tag
    
    * Add noscript to config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,24 +11,71 @@ module.exports = {
     ]
   },
   head: [
-    ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,maximum-scale=1'}],
-    ['link', {rel: 'icon', type: 'image/png', href: '/favicon.png'}],
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content:
+          'width=device-width,initial-scale=1,maximum-scale=1,maximum-scale=1'
+      }
+    ],
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
     ['meta', { name: 'twitter:site', content: '@Ethereum' }],
     ['meta', { name: 'twitter:creator', content: '@Ethereum' }],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:title', content: 'Ethereum' }],
     ['meta', { property: 'og:site_name', content: 'ethereum.org' }],
-    ['meta', { property: 'og:description', content: 'Ethereum is a global, decentralized platform for money and new kinds of applications. On Ethereum, you can write code that controls money, and build applications accessible anywhere in the world.'}],
+    [
+      'meta',
+      {
+        property: 'og:description',
+        content:
+          'Ethereum is a global, decentralized platform for money and new kinds of applications. On Ethereum, you can write code that controls money, and build applications accessible anywhere in the world.'
+      }
+    ],
     ['meta', { property: 'og:url', content: 'https://ethereum.org' }],
-    ['meta', { property: 'og:image', content: '/og-image.png'}],
-    ['meta', { property: 'twitter:card', content: 'summary_large_image'}],
-    ['meta', { property: 'twitter:site', content: '@ethereum'}],
-    ['meta', { property: 'twitter:creator', content: '@ethereum'}],
-    ['meta', { property: 'twitter:title', content: 'Ethereum'}],
-    ['meta', { property: 'twitter:description', content: 'Ethereum is a global, decentralized platform for money and new kinds of applications. On Ethereum, you can write code that controls money, and build applications accessible anywhere in the world.'}],
-    ['meta', { property: 'twitter:image', content: '/og-image-twitter.png'}]
+    ['meta', { property: 'og:image', content: '/og-image.png' }],
+    ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { property: 'twitter:site', content: '@ethereum' }],
+    ['meta', { property: 'twitter:creator', content: '@ethereum' }],
+    ['meta', { property: 'twitter:title', content: 'Ethereum' }],
+    [
+      'meta',
+      {
+        property: 'twitter:description',
+        content:
+          'Ethereum is a global, decentralized platform for money and new kinds of applications. On Ethereum, you can write code that controls money, and build applications accessible anywhere in the world.'
+      }
+    ],
+    ['meta', { property: 'twitter:image', content: '/og-image-twitter.png' }],
+    // Matomo tracking
+    // see https://github.com/vuejs/vuepress/issues/790
+    [
+      'script',
+      {},
+      `
+        var _paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['setCookieDomain', '*.ethereum.org']);
+        _paq.push(['setDomains', ['*.ethereum.org']]);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u='//matomo.ethereum.org/piwik/';
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '4']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+        `
+    ],
+    [
+      'noscript',
+      {},
+      `<p><img src="//matomo.ethereum.org/piwik/matomo.php?idsite=4&amp;rec=1" style="border:0;" alt="" /></p>`
+    ]
   ],
   markdown: {
     anchor: { permalinkSymbol: '↳' }
   }
-}
+};
--- docs/.vuepress/theme/components/Footer.vue
+++ docs/.vuepress/theme/components/Footer.vue
@@ -1,4 +1,4 @@
-><template>
+<template>
   <footer class="footer">
     <div class="credits">
       Artwork by <a href="https://impermanence.co" target="_blank">Lili Feyerabend</a> feat. <a href="https://ilankatin.com" target="_blank">ilan katin</a>, <a href="https://linktr.ee/mattiacprodukt" target="_blank">Mattia Cuttini</a>, <a href="https://oficinastk.github.io" target="_blank">Oficinas TK</a>, <a href="https://xcopyart.com" target="_blank">XCOPY</a>.
@@ -7,31 +7,11 @@
       <li><a href="https://github.com/ethereum" target="_blank">GitHub</a></li>
       <li><a href="https://twitter.com/ethereum" target="_blank">Twitter</a></li>
       <li><a href="https://blog.ethereum.org/">Blog</a></li>
-      <!-- <li><a href="#">Branding</a></li> -->
       <li><router-link to="/privacy-policy/">Privacy Policy</router-link></li>
       <li><router-link to="/terms-of-use/">Terms of Use</router-link></li>
       <li><router-link to="/cookie-policy/">Cookie Policy</router-link></li>
       <li><a href="mailto:press@ethereum.org">Contact</a></li>
     </ul>
-    <!-- Matomo -->
-    <script type="text/javascript">
-      var _paq = window._paq || [];
-      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-      _paq.push(['setCookieDomain', '*.ethereum.org']);
-      _paq.push(['setDomains', ['*.ethereum.org']]);
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u='//matomo.ethereum.org/piwik/';
-        _paq.push(['setTrackerUrl', u+'matomo.php']);
-        _paq.push(['setSiteId', '4']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-      })();
-    </script>
-    <noscript><p><img src="//matomo.ethereum.org/piwik/matomo.php?idsite=4&amp;rec=1" style="border:0;" alt="" /></p></noscript>
-    <!-- End Matomo Code -->
-
   </footer>
 </template>
 
