f284291ca886a5602d6c287239b6200d7a24fbcd
Sam Richards <sbrichards@gmail.com>
2019-09-18 21:24:59 -0700

    Only display artwork announcement on homepage [Fixes #186]

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -7,7 +7,7 @@
     <Footer :class="{ 'home': isLanding }" />
 
     <a href="https://hackernoon.com/rethinking-the-identity-of-ethereumorg-l718w347l" target="_blank">
-      <button v-if="!isRelaunch" class="announcement">
+      <button v-if="isLanding" class="announcement">
         {{linkText}} <span class="accent">{{linkTextMore}}</span>
       </button>
       
@@ -55,9 +55,6 @@
     computed: {
       isLanding() {
         return this.$page.frontmatter && this.$page.frontmatter.layout === "home"
-      },
-      isRelaunch() {
-        return this.$page.path === "/relaunch.html"
       },
       posts() {
         return this.$site.pages
