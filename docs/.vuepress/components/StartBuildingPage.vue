a559e1db87608d7edcb1a324d17231d0ca798a19
Sam Richards <sbrichards@gmail.com>
2019-10-22 11:31:00 -0700

    Update web-based learning section

--- docs/.vuepress/components/StartBuildingPage.vue
+++ docs/.vuepress/components/StartBuildingPage.vue
@@ -3,7 +3,9 @@
     <div class="center">
       <div class="headline-text-container">
         <h1 class="headline-title">Get started building on Ethereum</h1>
-        <div class="headline-subtitle">Learn the basics of Ethereum with Ethereum Studio, our web-based IDE where you can create and test smart contracts and their front end.</div>
+        <div
+          class="headline-subtitle"
+        >Learn the basics of Ethereum with Ethereum Studio, our web-based IDE where you can create and test smart contracts and their front end.</div>
       </div>
       <a href="https://studio.ethereum.org" target="blank">
         <button class="try-button">Try it now</button>
@@ -11,10 +13,13 @@
       <div class="terminal-gif">
         <img src="/ethereum-studio.gif" />
       </div>
-      <div class="gif-caption">Powered by <a href="https://superblocks.com" target="blank">Superblocks</a></div>
-      <h2 class="bold">
-        Start coding right away, saving many hours of development setup. Try the following templates to see what you can build with Ethereum.
-      </h2>
+      <div class="gif-caption">
+        Powered by
+        <a href="https://superblocks.com" target="blank">Superblocks</a>
+      </div>
+      <h2
+        class="bold"
+      >Start coding right away, saving many hours of development setup. Try the following templates to see what you can build with Ethereum.</h2>
       <div class="features">
         <div class="feature">
           <div class="icon">👋</div>
@@ -74,14 +79,14 @@
             </div>
             <div class="logo-description">Complete levels by hacking smart contracts</div>
           </div>
-            <div class="logo-container">
-              <div class="logo-title">0x Instant</div>
-              <div class="logo">
-                <a href="https://0x.org/instant" target="blank">
-                  <img src="/ecosystem/0x.png" class="zero" alt="0x Instant" />
-                </a>
-              </div>
-            <div class="logo-description">Offer simple crypto purchasing in any app or website</div>
+          <div class="logo-container">
+            <div class="logo-title">Remix</div>
+            <div class="logo">
+              <a href="https://remix.ethereum.org" target="blank">
+                <img src="/ecosystem/remix.png" alt="Remix" />
+              </a>
+            </div>
+            <div class="logo-description">Ethereum IDE and tools for the web</div>
           </div>
           <div class="logo-container">
             <div class="logo-title">ChainShot</div>
@@ -101,15 +106,6 @@
             </div>
             <div class="logo-description">Online Ethereum developer bootcamp</div>
           </div>
-            <div class="logo-container">
-              <div class="logo-title">Remix</div>
-              <div class="logo">
-                <a href="https://remix.ethereum.org" target="blank">
-                  <img src="/ecosystem/remix.png" alt="Remix" />
-                </a>
-              </div>
-            <div class="logo-description">Ethereum IDE and tools for the web</div>
-          </div>
         </div>
       </div>
       <div class="learn">
@@ -119,7 +115,10 @@
           <button class="learn-button">Learn More</button>
         </a>
       </div>
-      <p class="collaboration"> <a href="https://studio.ethereum.org" target="blank">Ethereum Studio</a> is a collaboration between <a href="https://superblocks.com" target="blank">Superblocks</a> and Ethereum.org.</p>
+      <p class="collaboration">
+        <a href="https://studio.ethereum.org" target="blank">Ethereum Studio</a> is a collaboration between
+        <a href="https://superblocks.com" target="blank">Superblocks</a> and Ethereum.org.
+      </p>
     </div>
   </div>
 </template>
@@ -128,128 +127,128 @@
 </script>
 
 <style lang="stylus" scoped>
-@import '../theme/styles/config.styl';
+  @import '../theme/styles/config.styl';
   
-.start-building
-  margin-top 80px
-  padding 4em 0 0
+  .start-building
+    margin-top 80px
+    padding 4em 0 0
 
-  .center
-    display flex
-    flex-flow column nowrap
-    align-items center
-    h2
-      border-bottom none
-      padding-top 0
-      line-height 1.4em
-      max-width 85%
-      &.bold
-        margin-top 3em
-        font-weight 700
-        max-width 60%
-    .try-button
-      margin 2em
-      font-size $fsRegular
-      font-weight 700
-    .terminal-gif
-      margin-top 3em
-      border-radius 8px
-      border 2px solid #000
-      max-width 85%
-      img
-        max-width: 100%
-    .gif-caption
-      font-size $fsSmall
-      margin-top 0.5rem
-      color $subduedColor
-      a
-        color $subduedColor
-        &:hover
-          color $accentColor
-
-    .features
-      display flex
-      margin-top 6em
-      flex-flow row wrap
-      justify-content center
-      .feature
-        .icon
-          margin 0.5em 0
-          font-size 3em
-        .box
-          max-width 260px
-          padding 0.2em 1.2em
-          .box-title
-            font-weight 700
-            font-size $fsLarge
-            margin-bottom 1em
-          .box-description
-            font-size $fsMedium
-            min-height 6.25rem
-            line-height 1.6em
-            font-weight 400
-          .box-link
-            margin-top 1em
-            font-size $fsRegular
-            text-transform uppercase
-            &:hover
-              text-decoration underline
-    .resources
-      padding 2em 0
-      margin-top 5em
-      font-weight 700
-      width 100%
-      .title
-        text-transform uppercase
-        font-size $fsLarge
-      .logos
-        margin-top 3em
-        display flex
-        flex-flow row wrap
-        justify-content center
-        .logo-container
-          display flex
-          flex-direction column
-          align-items center
-          flex 0 1 25%
-          margin 2rem
-        .logo-title
-          z-index 1
-        .logo-description
-          font-size $fsMedium
-          font-weight 400
-        .logo
-          cursor pointer
-          display flex
-          flex-direction column
-          align-items center
-          justify-content center
-          padding 0 2em
-          min-height 9rem
-          img
-            height 100px
-            &.oz
-              height 140px
-            &.zero
-              height 80px
-          &:hover
-            transform translateZ(0px) scale(1.1)
-            transition 0.25s ease-out 0s
-    .learn
-      margin-top 2em
+    .center
       display flex
       flex-flow column nowrap
       align-items center
       h2
+        border-bottom none
+        padding-top 0
+        line-height 1.4em
+        max-width 85%
+        &.bold
+          margin-top 3em
+          font-weight 700
+          max-width 60%
+      .try-button
+        margin 2em
+        font-size $fsRegular
         font-weight 700
-        font-size $fsLarge
-      p
-        margin-top 1em
-        font-size $fsRegular
-        max-width 60%
-      .learn-button
-        margin-top 1em
-        font-size $fsRegular
-    .collaboration
-      margin-top 4rem
+      .terminal-gif
+        margin-top 3em
+        border-radius 8px
+        border 2px solid #000
+        max-width 85%
+        img
+          max-width: 100%
+      .gif-caption
+        font-size $fsSmall
+        margin-top 0.5rem
+        color $subduedColor
+        a
+          color $subduedColor
+          &:hover
+            color $accentColor
+
+      .features
+        display flex
+        margin-top 6em
+        flex-flow row wrap
+        justify-content center
+        .feature
+          .icon
+            margin 0.5em 0
+            font-size 3em
+          .box
+            max-width 260px
+            padding 0.2em 1.2em
+            .box-title
+              font-weight 700
+              font-size $fsLarge
+              margin-bottom 1em
+            .box-description
+              font-size $fsMedium
+              min-height 6.25rem
+              line-height 1.6em
+              font-weight 400
+            .box-link
+              margin-top 1em
+              font-size $fsRegular
+              text-transform uppercase
+              &:hover
+                text-decoration underline
+      .resources
+        padding 2em 0
+        margin-top 5em
+        font-weight 700
+        width 100%
+        .title
+          text-transform uppercase
+          font-size $fsLarge
+        .logos
+          margin-top 3em
+          display flex
+          flex-flow row wrap
+          justify-content center
+          .logo-container
+            display flex
+            flex-direction column
+            align-items center
+            flex 0 1 25%
+            margin 2rem
+          .logo-title
+            z-index 1
+          .logo-description
+            font-size $fsMedium
+            font-weight 400
+          .logo
+            cursor pointer
+            display flex
+            flex-direction column
+            align-items center
+            justify-content center
+            padding 0 2em
+            min-height 9rem
+            img
+              height 100px
+              &.oz
+                height 140px
+              &.zero
+                height 80px
+            &:hover
+              transform translateZ(0px) scale(1.1)
+              transition 0.25s ease-out 0s
+      .learn
+        margin-top 2em
+        display flex
+        flex-flow column nowrap
+        align-items center
+        h2
+          font-weight 700
+          font-size $fsLarge
+        p
+          margin-top 1em
+          font-size $fsRegular
+          max-width 60%
+        .learn-button
+          margin-top 1em
+          font-size $fsRegular
+      .collaboration
+        margin-top 4rem
 </style>
