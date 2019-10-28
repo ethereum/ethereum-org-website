160450bc8bb7d051e7c516ef06b66f0a17cd5b02
Antonio Della Porta <antoniodellaporta.13@protonmail.com>
2019-10-28 15:39:52 +0100

    Web3 World Hackaton - Create a component that randomly displays X of N list items [#352]

---
+++ docs/.vuepress/components/RandomAppList.vue
@@ -0,0 +1,54 @@
+<template>
+        <div>
+                <ul>
+                        <li v-for="item in this.selected">
+                                <a :href="item.url" target="_blank" rel="noopener noreferrer">{{item.name}}</a>, {{item.description}}
+                        </li>
+                </ul>
+        </div>
+</template>
+
+<script>
+
+/*
+* Using RandomList as a Mixin we can achieve the random list functionality
+*/
+
+import RandomList from './RandomList.vue'
+
+export default {
+	mixins: [RandomList],	
+	data() {
+		const items = [
+			{
+				name: "Gitcoin",
+				url: "https://gitcoin.co",
+				description: "a network of incentivized open-source developers"
+			},
+			{
+				name: "Cent",
+				url: "https://beta.cent.co",
+				description: "a social network where you can earn money by posting"
+			},
+			{
+				name: "CryptoKitties",
+				url: "https://www.cryptokitties.co",			
+				description: "a game where you collect and breed digital collectible cats"
+			},
+			{
+				name: "DAI",
+				url: "https://makerdao.com/en/",
+				description: "a stable cryptocurrency that holds value at $1 USD"	
+			}
+		];
+
+		let selected = [];		
+
+		return { items: items, selected: selected }
+	},
+	
+	created() {
+		this.selected = this.selectItems(this.items, 4);	
+	}
+} 
+</script>
---
+++ docs/.vuepress/components/RandomList.vue
@@ -0,0 +1,24 @@
+<script>
+/* 
+*  Implements a component showing an <ul> with `numberOfItems` items took from 
+*  the array `items` after shuffling it using random.js APIs. The component can be
+*  used as a child component (e.g. RandomAppList.vue) to implement a random selection
+*  of items. 
+*/
+
+import {MersenneTwister19937, shuffle} from "random-js"
+
+export default {
+	data() {
+		const random = MersenneTwister19937.autoSeed();
+		return {random}
+	},
+
+	methods: {
+		selectItems(items, numberOfItems) {
+			shuffle(this.random, items);
+			return items.slice(0, numberOfItems);
+		}
+	}
+} 
+</script>
