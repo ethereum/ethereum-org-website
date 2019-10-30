dffe2106caefb474aa097c02ccf78147fb0f4d45
think-in-universe <mstr.raphael@gmail.com>
2019-10-31 02:39:55 +0800

    update the script for generating markdown heading IDs

--- scripts/generateHeadingIDs.js
+++ scripts/generateHeadingIDs.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const GithubSlugger = require('github-slugger');
+const toc = {};
+let curLevel = [0, 0, 0];
 
-function walk(dir) {
+function walk(dir, doc) {
   let results = [];
   const list = fs.readdirSync(dir);
   list.forEach(function (file) {
@@ -9,10 +11,12 @@ function walk(dir) {
     const stat = fs.statSync(file);
     if (stat && stat.isDirectory()) {
       /* Recurse into a subdirectory */
-      results = results.concat(walk(file));
+      results = results.concat(walk(file, doc));
     } else {
       /* Is a file */
-      results.push(file);
+      if (!doc || doc.length == 0 || file.indexOf(doc) != -1 && !file.startsWith(`docs/${doc}`)) {
+        results.push(file);
+      }
     }
   });
   return results;
@@ -22,23 +26,42 @@ function stripLinks(line) {
   return line.replace(/\[([^\]]+)\]\([^)]+\)/, (match, p1) => p1);
 }
 
-function addHeaderID(line, slugger) {
+function addHeaderID(line, slugger, write = false) {
   // check if we're a header at all
   if (!line.startsWith('#')) {
     return line;
   }
   // check if it already has an id
-  if (/\{#[^}]+\}/.test(line)) {
+  if (/\{#[^}]+\}/.test(line) && !write) {
     return line;
   }
-  const headingText = line.slice(line.indexOf(' ')).trim();
+  const headingText = line.slice(line.indexOf(' ')).replace(/\{#[^}]+\}/, '').trim();
   const headingLevel = line.slice(0, line.indexOf(' '));
-  return `${headingLevel} ${headingText} {#${slugger.slug(
-    stripLinks(headingText),
-  )}}`;
+  curLevel[headingLevel.length - 1] ++;
+  for (let l = headingLevel.length; l < 3; l++) {
+    curLevel[l] = 0
+  }
+  const headerNumber = curLevel.join(".");
+  let slug = null;
+  if (!write) {
+    slug = slugger.slug(stripLinks(headingText));
+    toc[headerNumber] = {
+      text: headingText,
+      slug
+    }
+  } else {
+    if (headerNumber in toc) {
+      slug = toc[headerNumber].slug;
+      console.log("write heading ID", headerNumber, headingText, "==>", slug);
+      return `${headingLevel} ${headingText} {#${slug}}`;
+    } else {
+      console.log("headerNumber not found", headerNumber, headingText, "==>", slug);
+      return line;
+    }
+  }
 }
 
-function addHeaderIDs(lines) {
+function addHeaderIDs(lines, write = false) {
   // Sluggers should be per file
   const slugger = new GithubSlugger();
   let inCode = false;
@@ -55,21 +78,35 @@ function addHeaderIDs(lines) {
       return;
     }
 
-    results.push(addHeaderID(line, slugger));
+    results.push(addHeaderID(line, slugger, write));
   });
   return results;
 }
 
-const [path] = process.argv.slice(2);
+function traverseHeaders(path, doc = "", write = false) {
+  const files = walk(path, doc);
+  files.forEach(file => {
+    if (!file.endsWith('.md')) {
+      return;
+    }
 
-const files = walk(path);
-files.forEach(file => {
-  if (!file.endsWith('.md')) {
-    return;
+    curLevel = [0, 0, 0];
+    const content = fs.readFileSync(file, 'utf8');
+    const lines = content.split('\n');
+    const updatedLines = addHeaderIDs(lines, write);
+    if (write) {
+      fs.writeFileSync(file, updatedLines.join('\n'));
+    }
+  });
+  if (!write) {
+    console.log(toc);
   }
+}
 
-  const content = fs.readFileSync(file, 'utf8');
-  const lines = content.split('\n');
-  const updatedLines = addHeaderIDs(lines);
-  fs.writeFileSync(file, updatedLines.join('\n'));
-});
+// const [path] = process.argv.slice(2);
+
+for (let doc of ["beginners", "developers", "learn", "use"]) {
+  traverseHeaders(`docs/${doc}`); // build toc
+  traverseHeaders("docs", doc, true); // write heading anchors
+}
+
