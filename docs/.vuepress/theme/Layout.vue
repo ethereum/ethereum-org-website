591a3e7efdccc47386f98a70ded487d2f7eaf3d0
Sam Richards <sbrichards@gmail.com>
2019-10-31 13:47:51 -0700

    Clean up NotFound component

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,113 +1,127 @@
 <template>
   <div id="wrapper" :class="pageClasses">
-    <Header :class="{ 'home': isLanding }" @toggle-sidebar="toggleSidebar" @toggle-mode="toggleMode" />
+    <Header
+      :shouldShowSidebarButton="true"
+      :class="{ 'home': isLanding }"
+      @toggle-sidebar="toggleSidebar"
+      @toggle-mode="toggleMode"
+    />
     <Hero v-if="showHero" :dark="darkMode" />
-    <main :class="contentClasses"><Content/></main>
+    <main :class="contentClasses">
+      <Content />
+    </main>
     <Sidebar :items="sidebarItems" @close-sidebar="closeSidebar" />
     <Footer :class="{ 'home': isLanding }" />
   </div>
 </template>
 
 <script>
-  import Footer from '@theme/components/Footer'
-  import Header from '@theme/components/Header'
-  import Hero from '@theme/components/Hero'
-  import Sidebar from '@theme/components/Sidebar'
-  import { resolveSidebarItems } from './utils/util'
-  import { translate } from './utils/translations'
+import Footer from "@theme/components/Footer";
+import Header from "@theme/components/Header";
+import Hero from "@theme/components/Hero";
+import Sidebar from "@theme/components/Sidebar";
+import { resolveSidebarItems } from "./utils/util";
+import { translate } from "./utils/translations";
 
-  export default {
-    data () {
+export default {
+  data() {
+    return {
+      isSidebarOpen: false,
+      darkMode: false
+    };
+  },
+  components: {
+    Footer,
+    Header,
+    Hero,
+    Sidebar
+  },
+  beforeMount() {
+    if (localStorage && localStorage.getItem("dark-mode") !== null) {
+      this.darkMode = localStorage.getItem("dark-mode") === "true";
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+    if (localStorage && localStorage.getItem("dark-mode") === null) {
+      this.darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addListener(({ matches }) => {
+        if (localStorage && localStorage.getItem("dark-mode") === null) {
+          this.darkMode = matches;
+        }
+      });
+  },
+  computed: {
+    isLanding() {
+      return this.$page.frontmatter && this.$page.frontmatter.layout === "home";
+    },
+    showHero() {
+      return (
+        this.$page.frontmatter &&
+        this.$page.frontmatter.layout === "home" &&
+        !this.$page.frontmatter.hideHero
+      );
+    },
+    posts() {
+      return this.$site.pages.filter(
+        page =>
+          page.path.endsWith(".html") && page.path.startsWith(this.$page.path)
+      );
+    },
+    showSidebar() {
+      return this.$page.frontmatter.sidebar;
+    },
+    sidebarItems() {
+      return resolveSidebarItems(
+        this.$page,
+        this.$route,
+        this.$site,
+        this.$localePath
+      );
+    },
+    contentClasses() {
       return {
-        isSidebarOpen: false,
-        darkMode: false
-      }
+        "content-block": this.isLanding,
+        page: !this.isLanding
+      };
     },
-    components: {
-      Footer,
-      Header,
-      Hero,
-      Sidebar
-    },
-    beforeMount () {
-      if (localStorage && localStorage.getItem('dark-mode') !== null) {
-        this.darkMode = localStorage.getItem('dark-mode') === "true"
-      }
-    },
-    mounted () {
-      window.addEventListener('scroll', this.onScroll)
-      if (localStorage && localStorage.getItem('dark-mode') === null) {
-        this.darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
-      }
-      window.matchMedia('(prefers-color-scheme: dark)').addListener(({ matches }) => {
-        if (localStorage && localStorage.getItem('dark-mode') === null) {
-          this.darkMode = matches
-        }
-      })
-    },
-    computed: {
-      isLanding() {
-        return this.$page.frontmatter && this.$page.frontmatter.layout === "home"
-      },
-      showHero() {
-        return (this.$page.frontmatter && this.$page.frontmatter.layout === "home")
-          && !this.$page.frontmatter.hideHero
-      },
-      posts() {
-        return this.$site.pages
-        .filter(page => page.path.endsWith(".html") && page.path.startsWith(this.$page.path))
-      },
-      showSidebar () {
-        return this.$page.frontmatter.sidebar
-      },
-      sidebarItems () {
-        return resolveSidebarItems(
-          this.$page,
-          this.$route,
-          this.$site,
-          this.$localePath
-        )
-      },
-      contentClasses () {
-        return {
-          'content-block': this.isLanding,
-          'page': !this.isLanding
-        }
-      },
-      pageClasses () {
-      const userPageClass = this.$page.frontmatter.pageClass
+    pageClasses() {
+      const userPageClass = this.$page.frontmatter.pageClass;
       return [
         {
-          'home': this.isLanding,
-          'has-sidebar': this.showSidebar,
-          'sidebar-open': this.isSidebarOpen,
-          'dark-mode': this.darkMode,
-          'right-to-left-text': this.$lang === 'fa'
+          home: this.isLanding,
+          "has-sidebar": this.showSidebar,
+          "sidebar-open": this.isSidebarOpen,
+          "dark-mode": this.darkMode,
+          "right-to-left-text": this.$lang === "fa"
         },
         userPageClass
-        ]
-      }
+      ];
+    }
+  },
+  methods: {
+    toggleSidebar(to) {
+      this.isSidebarOpen = typeof to === "boolean" ? to : !this.isSidebarOpen;
     },
-    methods: {
-      toggleSidebar (to) {
-        this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-      },
-      closeSidebar () {
-        this.isSidebarOpen = false
-      },
-      toggleMode () {
-        this.darkMode = this.darkMode ? false : true
-        if (localStorage) {
-          localStorage.setItem('dark-mode', this.darkMode)
-        }
-      }
+    closeSidebar() {
+      this.isSidebarOpen = false;
     },
-    watch: {
-      '$route' () {
-        this.closeSidebar()
+    toggleMode() {
+      this.darkMode = this.darkMode ? false : true;
+      if (localStorage) {
+        localStorage.setItem("dark-mode", this.darkMode);
       }
-    },
+    }
+  },
+  watch: {
+    $route() {
+      this.closeSidebar();
+    }
   }
+};
 </script>
 
 <style lang="stylus" scoped>
@@ -123,7 +137,5 @@
     right 3em
     border-radius 25px
     padding 1em 2em
-
 </style>
-
 <style src="./styles/theme.styl" lang="stylus"></style>
