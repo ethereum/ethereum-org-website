e8eb439382ffb5e5575f4e85ac781c6676b7e859
think-in-universe <mstr.raphael@gmail.com>
2019-11-07 10:35:59 +0800

    mock util and fix #387

--- docs/.vuepress/theme/utils/__mocks__/util.js
+++ docs/.vuepress/theme/utils/__mocks__/util.js
@@ -6,3 +6,8 @@ export function groupHeaders(headers) {
   // mock function that return all headers
   return headers;
 }
+
+export function resolveHeaderTitle(title) {
+  // mock header title processing
+  return title;
+}
