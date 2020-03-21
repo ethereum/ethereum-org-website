18cb3bcb3b1f578a075d562f5210975aac2b748a
Carl Fairclough <hi@carlfairclough.me>
2020-03-21 10:20:37 -0300

    updating markdown text handling, isolated text styles [fixes #858 #832]

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -71,13 +71,62 @@ module.exports = {
   ],
   markdown: {
     anchor: {
-      permalinkSymbol: '↳',
-      renderPermalink: renderHeaderWithExplicitAnchor
+      permalink: false
     }
   },
   extendMarkdown: md => {
     md.use(require('markdown-it-attrs'))
+    const r = md.renderer.rules
+
+    const buildTag = (token, slf, classname) => {
+      // We need to add our md classes
+      const mdClasses = token.attrGet('class') || ''
+      token.attrSet('class', classname + ' ' + mdClasses)
+      let attrs = slf.renderAttrs(token)
+      return `<${token.tag} ${attrs}>`
+    }
+
+    r.heading_open = (tokens, idx, options, env, slf) => {
+      tkn = tokens[idx]
+      const anchor =
+        tkn.tag == 'h2' || tkn.tag == 'h3'
+          ? `<a href="#${tkn.attrGet('id')}" class="header-anchor">↳</a>`
+          : ''
+      const classes = [
+        'l' + tkn.tag.substr(-1),
+        anchor && 'markdown-heading',
+        // Add classes here
+        'tc-tect-500'
+      ].join(' ')
+
+      const tag = buildTag(tkn, slf, classes)
+      return tag + anchor
+    }
+    r.paragraph_open = (tokens, idx, options, env, slf) =>
+      buildTag(
+        tokens[idx],
+        slf,
+        // Add classes here
+        'l7 tc-text300'
+      )
+    r.paragraph_close = () => '</p>'
+
+    r.bullet_list_open = (tokens, idx, options, env, slf) =>
+      buildTag(
+        tokens[idx],
+        slf,
+        // Add classes here
+        'l7 tc-text300'
+      )
+    r.ordered_list_open = (tokens, idx, options, env, slf) =>
+      buildTag(
+        tokens[idx],
+        slf,
+        // Add classes here
+        'l7 tc-text300'
+      )
   },
+
   extendPageData($page) {
     if ($page.path.split('/')[1] == 'translations') {
       $page.path = $page.path.replace('/translations/', '/')
