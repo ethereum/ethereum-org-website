c7598fecb647169064061ee1f2b66b45df73cdfd
Josh Stark <joshstark@gmail.com>
2020-03-26 16:29:05 -0700

    Changes in response to individuals content audit
    - Remove Cryptokitties for now, as it's a relatively old app
    - Add Gods Unchained
    - Add Decentraland
    - Add Dharma
    - Add Augur

--- docs/.vuepress/components/RandomAppList.vue
+++ docs/.vuepress/components/RandomAppList.vue
@@ -26,14 +26,29 @@ export default {
 				description: "a social network where you can earn money by posting"
 			},
 			{
-				name: "CryptoKitties",
-				url: "https://www.cryptokitties.co",
-				description: "a game where you collect and breed digital collectible cats"
+				name: "Gods Unchained",
+				url: "https://godsunchained.com/",
+				description: "a competitive collectible card game built on Ethereum"
 			},
 			{
 				name: "DAI",
 				url: "https://makerdao.com/en/",
 				description: "a stable cryptocurrency that holds value at $1 USD"
+			}
+			{
+				name: "Decentraland",
+				url: "https://decentraland.org/",
+				description: "a virtual world owned and built by its users"
+			}
+			{
+				name: "Dharma",
+				url: "dharma.io",
+				description: "the easiest way to earn interest on your cryptocurrency"
+			}
+			{
+				name: "Augur",
+				url: "https://www.augur.net/",
+				description: "The world’s most accessible, no-limit betting exchange."
 			}
 		];
 		return {items}
