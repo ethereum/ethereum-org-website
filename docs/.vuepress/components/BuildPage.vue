dab79cd8ab91c6dfcc26e670a4b62e72306688cc
Carl Fairclough <hi@carlfairclough.me>
2020-03-26 13:40:56 -0300

    updated build page (wip)

--- docs/.vuepress/components/BuildPage.vue
+++ docs/.vuepress/components/BuildPage.vue
@@ -1,122 +1,75 @@
 <template>
-  <div class="start-building center flex flex-column flex-center page">
-    <header class="center flex flex-column flex-center">
-      <div class="headline-text-container">
-        <h1 class="l2 tc-text500">
-          {{ translateString('page-build-title') }}
-        </h1>
-        <p class="l4 tc-text300 subtitle max-w-55ch">
-          {{ translateString('page-build-subtitle') }}
-        </p>
-        <Button
-          isExternal
-          to="https://studio.ethereum.org"
-          class="inline-block mt-05"
-        >
-          {{ translateString('page-build-try-button') }}
-        </Button>
-      </div>
-      <div class="mt-4">
-        <img src="/ethereum-studio.gif" class="w-100" />
-      </div>
-      <p class="l5 tc-text100 center">
-        <!-- TODO how to include links within translations? -->
-        Powered by
-        <a
-          href="https://superblocks.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Superblocks
-        </a>
+  <div class="start-buildingflex flex-column flex-center page">
+    <header class="md-up-pl-3 md-up-pr-3 pb-5 pt-3">
+      <h1 class="l1 tc-text500">
+        {{ translateString('page-build-title') }}
+      </h1>
+      <p class="l4 tc-text300 subtitle max-w-55ch">
+        {{ translateString('page-build-subtitle') }}
       </p>
     </header>
 
-    <h2 class="l3 tc-text500 mt-8 mb-4 max-w-35ch">
-      {{ translateString('page-build-h2') }}
-    </h2>
+    <section class="pb-4">
+      <div class="md-up-pl-3 md-up-pr-3 pb-2 md-up-pb-4">
+        <h2 class="l2">Ethereum Studio Templates</h2>
+        <p class="l4">
+          {{ translateString('page-build-h2') }}
+        </p>
+      </div>
+      <CardList :items="templates" desktopColumns="3" tabletColumns="3" />
+    </section>
 
-    <section
-      class="features flex flex-row-wrap justify-center mb-8 max-w-1140px"
-    >
-      <div
-        v-for="template in templates"
-        class="feature mt-4 mr-1 ml-1 flex flex-column"
-      >
-        <span class="l1 ma-0">{{ template.icon }}</span>
-        <div class="box">
-          <h3 class="l4 ma-0 tc-text400">{{ template.title }}</h3>
-          <p class="l7 tc-text200">{{ template.description }}</p>
-        </div>
-        <div class="mt-auto mb-0">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            class="hide-icon"
-            :href="template.to.url"
-            >{{ template.to.text }}</a
+    <section class="pb-4">
+      <div class="md-up-pl-3 md-up-pr-3 pb-2 md-up-pb-4">
+        <h2 class="l2">Learning Resources</h2>
+        <p class="l4">{{ translateString('page-build-h2') }}</p>
+      </div>
+      <CardList
+        :items="learningPlatforms"
+        desktopColumns="3"
+        tabletColumns="3"
+      />
+    </section>
+
+    <section class="pb-4 flex flex-wrap">
+      <div class="a-m-w-50 overflow-hidden pt-2 pb-2 studio-image-wrapper">
+        <img class="studio-img" src="/ethereum-studio.gif" />
+      </div>
+      <div class="a-m-w-50 md-up-pl-2 flex flex-center">
+        <div>
+          <h2 class="l2 tc-text400 w-100 mt-0">
+            Ethereum Studio
+          </h2>
+          <p class="l4 tc-text200">
+            {{ translateString('page-build-subtitle') }}
+          </p>
+          <Button
+            isExternal
+            secondary
+            to="https://studio.ethereum.org"
+            class="inline-block mt-05"
           >
+            {{ translateString('page-build-try-button') }}
+          </Button>
         </div>
       </div>
     </section>
 
     <section
-      class="resources flex flex-row-wrap justify-center mb-8 max-w-1140px"
+      class="learn-more relative bg-primary500 md-up-pl-3 md-up-pr-3 pt-4 pb-4"
     >
-      <h2 class="l3 tc-text400 min-w-100">
-        {{ translateString('page-build-more-learning-title') }}
-      </h2>
-      <div
-        v-for="resource in learningPlatforms"
-        class="resource mt-4 mr-2 ml-2"
-      >
-        <h3 class="l4 tc-text400 ma-0">{{ resource.title }}</h3>
-        <div class="logo flex flex-column flex-center justify-center pl-2 pr-2">
-          <a
-            :href="resource.to"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="hide-icon"
-          >
-            <img
-              :src="resource.img.src"
-              :alt="resource.img.alt || resource.title"
-            />
-          </a>
-        </div>
-        <p class="l7 tc-text200 ma-0">{{ resource.description }}</p>
+      <div class="relative w-100 learn-content">
+        <h2 class="l2 mt-0 tc-onPrimary">
+          {{ translateString('page-build-learn-more-cta') }}
+        </h2>
+        <p class="l4 tc-onPrimary o-08 max-w-45ch mb-2">
+          {{ translateString('page-build-learn-more-description') }}
+        </p>
+        <Button secondary :to="langPath() + 'learn/'">
+          {{ translateString('learn-more') }}
+        </Button>
       </div>
     </section>
-
-    <section class="learn center flex flex-column flex-center mb-8">
-      <h2 class="l2 tc-text400 w-100">
-        {{ translateString('page-build-learn-more-cta') }}
-      </h2>
-      <p class="l4 tc-text200 max-w-45ch mb-2">
-        {{ translateString('page-build-learn-more-description') }}
-      </p>
-      <Button secondary :to="langPath() + 'learn/'">
-        {{ translateString('learn-more') }}
-      </Button>
-    </section>
-
-    <!-- TODO how to include links within translations? -->
-    <p class="l5">
-      <a
-        href="https://studio.ethereum.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Ethereum Studio</a
-      >
-      is a collaboration between
-      <a
-        href="https://superblocks.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Superblocks</a
-      >
-      and <router-link to="/">Ethereum.org</router-link>.
-    </p>
   </div>
 </template>
 
@@ -138,36 +91,30 @@ export default {
       return [
         {
           title: this.translateString('page-build-hello-world-title'),
-          description: this.translateString(
-            'page-build-hello-world-description'
-          ),
-          to: {
-            url: 'https://studio.ethereum.org/1',
+          content: this.translateString('page-build-hello-world-description'),
+          link: {
+            to: 'https://studio.ethereum.org/1',
             text: this.translateString('page-build-hello-world-link-text')
           },
-          icon: '👋'
+          header: '👋'
         },
         {
           title: this.translateString('page-build-coin-contract-title'),
-          description: this.translateString(
-            'page-build-coin-contract-description'
-          ),
-          to: {
-            url: 'https://studio.ethereum.org/2',
+          content: this.translateString('page-build-coin-contract-description'),
+          link: {
+            to: 'https://studio.ethereum.org/2',
             text: this.translateString('page-build-coin-contract-link-text')
           },
-          icon: '🗝️'
+          header: '🗝️'
         },
         {
           title: this.translateString('page-build-crypto-pizza-title'),
-          description: this.translateString(
-            'page-build-crypto-pizza-description'
-          ),
-          to: {
-            url: 'https://studio.ethereum.org/3',
+          content: this.translateString('page-build-crypto-pizza-description'),
+          link: {
+            to: 'https://studio.ethereum.org/3',
             text: this.translateString('page-build-crypto-pizza-link-text')
           },
-          icon: '🍕'
+          header: '🍕'
         }
       ]
     },
@@ -175,54 +122,45 @@ export default {
       return [
         {
           title: 'CryptoZombies',
-          description: this.translateString(
-            'page-build-cryptozombies-description'
-          ),
-          to: 'https://cryptozombies.io/',
-          img: {
-            src: '/ecosystem/crypto-zombie.png',
-            alt: 'CryptoZombies'
-          }
+          content: this.translateString('page-build-cryptozombies-description'),
+          link: {
+            to: 'https://cryptozombies.io/'
+          },
+          header: '/ecosystem/crypto-zombie.png'
         },
         {
           title: 'Ethernauts',
-          description: this.translateString(
-            'page-build-ethernauts-description'
-          ),
-          to: 'https://ethernaut.openzeppelin.com/',
-          img: {
-            src: '/ecosystem/oz.png',
-            alt: 'Open Zeppelin Ethernaut'
-          }
+          content: this.translateString('page-build-ethernauts-description'),
+          link: {
+            to: 'https://ethernaut.openzeppelin.com/'
+          },
+          header: '/ecosystem/oz.png'
         },
         {
           title: 'Remix',
-          description: this.translateString('page-build-remix-description'),
-          to: 'https://remix.ethereum.org',
-          img: {
-            src: '/ecosystem/remix.png',
-            alt: 'Remix'
-          }
+          content: this.translateString('page-build-remix-description'),
+          link: {
+            to: 'https://remix.ethereum.org'
+          },
+          header: '/ecosystem/remix.png'
         },
         {
           title: 'ChainShot',
-          description: this.translateString('page-build-chainshot-description'),
-          to: 'https://www.chainshot.com',
-          img: {
-            src: '/ecosystem/chainshot.png',
-            alt: 'ChainShot'
-          }
+          content: this.translateString('page-build-chainshot-description'),
+          link: {
+            to: 'https://www.chainshot.com'
+          },
+          header: '/ecosystem/chainshot.png'
         },
         {
           title: 'ConsenSys Academy',
-          description: this.translateString(
+          content: this.translateString(
             'page-build-consensys-academy-description'
           ),
-          to: 'https://consensys.net/academy/bootcamp/',
-          img: {
-            src: '/ecosystem/consensys.png',
-            alt: 'ConsenSys Academy'
-          }
+          link: {
+            to: 'https://consensys.net/academy/bootcamp/'
+          },
+          header: '/ecosystem/consensys.png'
         }
       ]
     }
@@ -232,6 +170,13 @@ export default {
 
 <style lang="stylus" scoped>
 @import '../theme/styles/config.styl';
+
+
+.studio-img
+  border-radius 8px
+  width 100%
+  @media (min-width: $breakM)
+    width 180%
 
 .feature, .resource
   flex: 1;
@@ -247,6 +192,24 @@ export default {
   min-width 200px
   @media (min-width: $breakS)
     flex 0 1 25%
+
+.learn-more
+  z-index 2
+  & > *
+    z-index 2
+  &:before
+    position absolute
+    content ''
+    background $colorPrimary500
+    width: 100vw
+    height: 100%
+    left: calc(50% - 50vw);
+    top 0
+    z-index 1
+
+.dark-mode
+  .learn-more:before
+    background $colorPrimaryDark500
 
 .logo
   cursor pointer
