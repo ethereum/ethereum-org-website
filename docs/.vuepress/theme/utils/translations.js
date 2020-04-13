fed367dd4f6dc9029ae94f97a3f2896432168299
Sebastian T F <sebastian.tharakan97@gmail.com>
2020-04-14 00:37:56 +0530

    update translations.js

--- docs/.vuepress/theme/utils/translations.js
+++ docs/.vuepress/theme/utils/translations.js
@@ -6,6 +6,7 @@
 // This is to make the files compatible with CrowdIn & easily exportable/uploadable.
 const english = require('../../../strings.json')
 const ar = require('../../../translations/ar/strings.json')
+const bn = require('../../../translations/bn/strings.json')
 const cs = require('../../../translations/cs/strings.json')
 const de = require('../../../translations/de/strings.json')
 const el = require('../../../translations/el/strings.json')
@@ -33,6 +34,7 @@ const simplifiedChinese = require('../../../translations/zh/strings.json')
 // e.g. `nl` --> https://crowdin.com/project/ethereumfoundation/nl
 const stringTranslations = {
   'en-US': english,
+  'bn_BD': bn,
   'ar-AR': ar,
   cs,
   'de-DE': de,
@@ -71,6 +73,12 @@ const languageMetaData = {
     language: 'العربية',
     'language-english': 'Arabic',
     path: '/ar/'
+  },
+  bn: {
+    version: 1.1,
+    language: 'বাংলা',
+    'language-english': 'Bengali',
+    path: '/bn/'
   },
   cs: {
     version: 1.0,
