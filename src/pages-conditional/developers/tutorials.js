77ea222b0c038ef41334306eee8b7e831e0a42a4
Sam Richards <sbrichards@gmail.com>
2020-09-10 18:34:49 -0700

    Add tutorial tag filters

--- src/pages-conditional/developers/tutorials.js
+++ src/pages-conditional/developers/tutorials.js
@@ -1,40 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
-import { graphql, Link } from "gatsby"
-import { Twemoji } from "react-emoji-render"
+import { graphql } from "gatsby"
 
-import CardList from "../../components/CardList"
-import { EdnPage } from "../../components/SharedStyledComponents"
+import Link from "../../components/Link"
 import Pill from "../../components/Pill"
-
-// const Table = styled.div`
-//   background-color: ${(props) => props.theme.colors.background};
-//   box-shadow: ${(props) => props.theme.colors.tableBoxShadow};
-//   width: 100%;
-//   margin-bottom: 0rem;
-// `
-
-// const StyledCardList = styled(CardList)`
-//   margin-bottom: -2rem;
-// `
-
-// const Item = styled.div`
-//   text-decoration: none;
-//   display: flex;
-//   font-family: "SFMono-Regular", monospace;
-//   text-transform: uppercase;
-//   font-weight: 600;
-//   color: ${(props) => props.theme.colors.text} !important;
-//   box-shadow: 0 1px 1px ${(props) => props.theme.colors.tableItemBoxShadow};
-//   background: ${(props) => props.theme.colors.cardGradient};
-//   margin-bottom: 1px;
-//   margin-top: 0rem;
-//   padding: 1rem;
-//   width: 100%;
-// `
-// const Emoji = styled(Twemoji)`
-//   margin-right: 1rem;
-// `
+import Tag from "../../components/Tag"
+import { Content, EdnPage } from "../../components/SharedStyledComponents"
 
 const TutorialCard = styled(Link)`
   text-decoration: none;
@@ -54,8 +25,7 @@ const TutorialCard = styled(Link)`
   }
 `
 
-const Background = styled.div`
-  background-color: ${(props) => props.theme.colors.background};
+const TutorialContainer = styled.div`
   box-shadow: ${(props) => props.theme.colors.tableBoxShadow};
   margin-bottom: 2rem;
   width: 66%;
@@ -115,6 +85,32 @@ const TitleContainer = styled.div`
     margin-bottom: 2rem;
   }
 `
+const TagsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-bottom: 2rem;
+  @media (max-width: ${(props) => props.theme.breakpoints.s}) {
+    flex-direction: column;
+  }
+`
+const TagContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 80%;
+  @media (max-width: ${(props) => props.theme.breakpoints.s}) {
+    max-width: 100%;
+    margin-bottom: 1rem;
+  }
+`
+const ClearLink = styled.button`
+  color: ${(props) => props.theme.colors.primary};
+  text-decoration: underline;
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+`
 
 const TutorialsPage = ({ data }) => {
   const allTutorials = data.allTutorials.nodes.map((tutorial) => {
@@ -125,51 +121,105 @@ const TutorialsPage = ({ data }) => {
       author: tutorial.frontmatter.author,
       tags: tutorial.frontmatter.tags,
       skill: tutorial.frontmatter.skill,
+      timeToRead: tutorial.timeToRead,
     }
   })
+  const allTags = data.allTags.group
+
+  const [state, setState] = useState({
+    activeTagNames: [],
+    filteredTutorials: allTutorials,
+  })
+
+  const clearActiveTags = () => {
+    setState({ activeTagNames: [], filteredTutorials: allTutorials })
+  }
+
+  const handleTagSelect = (tagName) => {
+    const activeTagNames = state.activeTagNames
+
+    // Add or remove the selected tag
+    const index = activeTagNames.indexOf(tagName)
+    if (index > -1) {
+      activeTagNames.splice(index, 1)
+    } else {
+      activeTagNames.push(tagName)
+    }
+
+    // If no tags are active, show all tutorials, otherwise filter by active tag
+    let filteredTutorials = allTutorials
+    if (activeTagNames.length > 0) {
+      filteredTutorials = allTutorials.filter((tutorial) => {
+        for (const tag of activeTagNames) {
+          if (!tutorial.tags.includes(tag)) {
+            return false
+          }
+        }
+        return true
+      })
+    }
+    setState({ activeTagNames, filteredTutorials })
+  }
+
+  const hasActiveTags = state.activeTagNames.length > 0
+  const hasNoTutorials = state.filteredTutorials.length === 0
 
   return (
     <StyledEdnPage>
-      <PageTitle>Tutorials</PageTitle>
-      <h2>All</h2>
-      <Background>
-        {allTutorials.map((tutorial) => {
-          return (
-            <TutorialCard key={tutorial.to} to={tutorial.to}>
-              <TitleContainer>
-                <Title>{tutorial.title}</Title>
-                <StyledPill>{tutorial.skill}</StyledPill>
-              </TitleContainer>
-              <Author>{tutorial.author}</Author>
-              <About>{tutorial.description}</About>
-              <PillContainer>
-                {tutorial.tags.map((tag, idx) => {
-                  return <StyledPill key={idx}>{tag}</StyledPill>
-                })}
-              </PillContainer>
-            </TutorialCard>
-          )
-        })}
-      </Background>
-      {/* TODO group & display tutorials by tag */}
-      {/* <Table>
-        <Item>
-          <Emoji svg text=":page_with_curl:" /> Smart contracts
-        </Item>
-        <StyledCardList content={smartContractTutorials} />
-      </Table>
-      <Table>
-        <Item>
-          <Emoji svg text=":computer:" /> Nodes and clients
-        </Item>
-        <StyledCardList content={nodesTutorials} />
-      </Table>
-      <Table>
-        <Item>
-          <Emoji svg text=":shield:" /> Security
-        </Item>
-        <StyledCardList content={securityTutorials} />
-      </Table> */}
+      {/* TODO add PageMetaData */}
+      <Content>
+        <PageTitle>Ethereum Development Tutorials</PageTitle>
+        <p>Welcome to our curated list of community tutorials.</p>
+        <p>Filter tutorials by tag:</p>
+        <TagsContainer>
+          <TagContainer>
+            {allTags.map((tag) => {
+              const name = `${tag.name} (${tag.totalCount})`
+              const isActive = state.activeTagNames.includes(tag.name)
+              return (
+                <Tag
+                  name={name}
+                  key={name}
+                  isActive={isActive}
+                  shouldShowIcon={false}
+                  onSelect={handleTagSelect}
+                  value={tag.name}
+                />
+              )
+            })}
+          </TagContainer>
+          {hasActiveTags && (
+            <ClearLink onClick={clearActiveTags}>Clear filters</ClearLink>
+          )}
+        </TagsContainer>
+
+        <TutorialContainer>
+          {hasNoTutorials && (
+            <p>
+              Sorry, no tutorial has all of these tags. Try removing a tag or
+              two.
+            </p>
+          )}
+          {state.filteredTutorials.map((tutorial) => {
+            return (
+              <TutorialCard key={tutorial.to} to={tutorial.to}>
+                <TitleContainer>
+                  <Title>{tutorial.title}</Title>
+                  <StyledPill>{tutorial.skill}</StyledPill>
+                </TitleContainer>
+                <Author>{tutorial.author}</Author>
+                <About>{tutorial.description}</About>
+                <About>{tutorial.timeToRead} minutes to read</About>
+                <PillContainer>
+                  {tutorial.tags.map((tag, idx) => {
+                    return <StyledPill key={idx}>{tag}</StyledPill>
+                  })}
+                </PillContainer>
+              </TutorialCard>
+            )
+          })}
+        </TutorialContainer>
+      </Content>
     </StyledEdnPage>
   )
 }
@@ -190,6 +240,12 @@ export const query = graphql`
           skill
         }
         timeToRead
+      }
+    }
+    allTags: allMdx {
+      group(field: frontmatter___tags) {
+        name: fieldValue
+        totalCount
       }
     }
   }
