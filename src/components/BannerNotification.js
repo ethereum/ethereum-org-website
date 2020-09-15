3189b8572ca878162a3af013a38d2bd4b01289f9
Sam Richards <sbrichards@gmail.com>
2020-09-15 09:04:22 -0700

    Remove docs breadcrumbs on mobile

--- src/components/BannerNotification.js
+++ src/components/BannerNotification.js
@@ -3,12 +3,14 @@ import styled from "styled-components"
 
 const Banner = styled.div`
   position: absolute;
-  width: 100%;
+  /* width: 100%; */
   background-color: ${(props) => props.theme.colors.primary};
   color: ${(props) => props.theme.colors.background};
   padding: 1rem;
 `
 
+// TODO width bleeds over
+// Take away page padding? Make flex-direction column & add padding to ContentContainer?
 const BannerNotification = ({ children }) => {
   return <Banner>{children}</Banner>
 }
