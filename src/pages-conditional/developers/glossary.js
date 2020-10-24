93ff773e59dcc70a57b364cd403736a8208201ee
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2020-10-23 22:46:28 -0700

    working glossary
    needs to be linked to

--- src/pages-conditional/developers/glossary.js
+++ src/pages-conditional/developers/glossary.js
@@ -1,13 +1,19 @@
 import React from "react"
 import styled from "styled-components"
 import { graphql } from "gatsby"
-
 import PageMetadata from "../../components/PageMetadata"
-// import Translation from "../../components/Translation"
 import Button from "../../components/Button"
 import { Mixins } from "../../components/Theme"
 import { Content, EdnPage } from "../../components/SharedStyledComponents"
 import CalloutBanner from "../../components/CalloutBanner"
+import glossary from "../../data/developer-glossary.yaml"
+import Link from "../../components/Link"
+
+const StyledContent = styled(Content)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const Header = styled.header`
   display: flex;
@@ -39,6 +45,53 @@ const Subtitle = styled.h2`
   margin-top: 1rem;
 `
 
+const GlossaryContainer = styled.div`
+  box-shadow: ${(props) => props.theme.colors.tableBoxShadow};
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+  align-self: center;
+  width: 66%;
+  @media (max-width: ${(props) => props.theme.breakpoints.m}) {
+    width: 100%;
+  }
+`
+
+const GlossaryCard = styled.div`
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: ${(props) => props.theme.colors.text};
+  box-shadow: 0 1px 1px ${(props) => props.theme.colors.tableItemBoxShadow};
+  margin-bottom: 1px;
+  padding: 2rem;
+  width: 100%;
+  color: #000;
+  &:hover {
+    border-radius: 4px;
+    box-shadow: 0 0 1px ${(props) => props.theme.colors.primary};
+    background: ${(props) => props.theme.colors.tableBackgroundHover};
+  }
+`
+
+const Name = styled.p`
+  color: ${(props) => props.theme.colors.text};
+  font-weight: 600;
+  font-size: 24px;
+  @media (max-width: ${(props) => props.theme.breakpoints.m}) {
+    margin-right: 0rem;
+  }
+`
+
+const Definition = styled.p`
+  color: ${(props) => props.theme.colors.text200};
+`
+
+const Citation = styled.p`
+  color: ${(props) => props.theme.colors.text200};
+  font-style: italic;
+`
+
 const Glossary = ({ data }) => {
   return (
     <EdnPage>
@@ -52,8 +105,29 @@ const Glossary = ({ data }) => {
           A glossary of technical and non-technical terms related to Ethereum.
         </Subtitle>
       </Header>
+      <StyledContent>
+        <GlossaryContainer>
+          {glossary.map(({ id, name, definition }) => {
+            return (
+              <GlossaryCard key={id}>
+                <Name>{name}</Name>
+                <Definition>{definition}</Definition>
+              </GlossaryCard>
+            )
+          })}
+        </GlossaryContainer>
+        <Citation>
+          Page content provided in part from{" "}
+          <Link to="https://github.com/ethereumbook/ethereumbook">
+            Mastering Ethereum
+          </Link>{" "}
+          by{" "}
+          <Link to="https://ethereumbook.info">
+            Andreas M. Antonopoulos, Gavin Wood
+          </Link>
+        </Citation>
+      </StyledContent>
       <Content>
-        {/* TODO: Content */}
         <CalloutBanner
           image={data.learn.childImageSharp.fluid}
           title="Learn with documentation"
