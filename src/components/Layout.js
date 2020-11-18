0940cea777237762118054686eca395208c1660d
Sam Richards <sbrichards@gmail.com>
2020-11-18 13:52:08 -0800

    Refactor Layout to function component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,4 @@
-// TODO figure out case, Netlify is case sensitive
-// https://community.netlify.com/t/support-guide-netlify-app-builds-locally-but-fails-on-deploy-case-sensitivity/10754
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { ThemeProvider } from "styled-components"
 import { IntlProvider, IntlContextProvider } from "gatsby-plugin-intl"
 import styled from "styled-components"
@@ -60,106 +58,92 @@ const StyledBannerNotification = styled(BannerNotification)`
 `
 
 // TODO `Layout` renders twice on page load - why?
-// TODO refactor into function component
-class Layout extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isDarkTheme: false,
-    }
-  }
+const Layout = (props) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
 
   // set isDarkTheme based on browser/app user preferences
-  componentDidMount = () => {
+  useEffect(() => {
     if (localStorage && localStorage.getItem("dark-theme") !== null) {
-      this.setState({
-        isDarkTheme: localStorage.getItem("dark-theme") === "true",
-      })
+      setIsDarkTheme(localStorage.getItem("dark-theme") === "true")
     } else {
-      this.setState({
-        isDarkTheme: window.matchMedia("(prefers-color-scheme: dark)").matches,
-      })
+      setIsDarkTheme(window.matchMedia("(prefers-color-scheme: dark)").matches)
     }
     window
       .matchMedia("(prefers-color-scheme: dark)")
       .addListener(({ matches }) => {
         if (localStorage && localStorage.getItem("dark-theme") === null) {
-          this.setState({ isDarkTheme: matches })
+          setIsDarkTheme(matches)
         }
       })
-  }
+    return () => {
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .removeListener(({ matches }) => {
+          if (localStorage && localStorage.getItem("dark-theme") === null) {
+            setIsDarkTheme(matches)
+          }
+        })
+    }
+  }, [])
 
-  componentWillUnmount = () => {
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .removeListener(({ matches }) => {
-        if (localStorage && localStorage.getItem("dark-theme") === null) {
-          this.setState({ isDarkTheme: matches })
-        }
-      })
-  }
-
-  handleThemeChange = () => {
-    const isDarkTheme = !this.state.isDarkTheme
-    this.setState({ isDarkTheme: isDarkTheme })
+  const handleThemeChange = () => {
+    setIsDarkTheme(!isDarkTheme)
     if (localStorage) {
-      localStorage.setItem("dark-theme", isDarkTheme)
+      localStorage.setItem("dark-theme", !isDarkTheme)
     }
   }
 
-  render() {
-    // IntlProvider & IntlContextProvider appear to be necessary in order to pass context
-    // into components that live outside page components (e.g. Nav & Footer).
-    // https://github.com/wiziple/gatsby-plugin-intl/issues/116
-    const intl = this.props.pageContext.intl
-    const theme = this.state.isDarkTheme ? darkTheme : lightTheme
+  // IntlProvider & IntlContextProvider appear to be necessary in order to pass context
+  // into components that live outside page components (e.g. Nav & Footer).
+  // https://github.com/wiziple/gatsby-plugin-intl/issues/116
+  const intl = props.pageContext.intl
+  const theme = isDarkTheme ? darkTheme : lightTheme
 
-    const path = this.props.path
-    const shouldShowSideNav = path.includes("/docs/")
-    const shouldShowSubNav = path.includes("/developers/")
-    const shouldShowBanner =
-      path.includes("/eth2/") && !path.includes("/eth2/deposit-contract/")
+  const path = props.path
+  const shouldShowSideNav = path.includes("/docs/")
+  const shouldShowSubNav = path.includes("/developers/")
+  const shouldShowBanner =
+    path.includes("/eth2/") && !path.includes("/eth2/deposit-contract/")
 
-    return (
-      <IntlProvider
-        locale={intl.language}
-        defaultLocale={intl.defaultLocale}
-        messages={intl.messages}
-      >
-        <IntlContextProvider value={intl}>
-          <ThemeProvider theme={theme}>
-            <GlobalStyle isDarkTheme={this.state.isDarkTheme} />
-            <ContentContainer>
-              <Nav
-                handleThemeChange={this.handleThemeChange}
-                isDarkTheme={this.state.isDarkTheme}
-                path={path}
-              />
+  return (
+    <IntlProvider
+      locale={intl.language}
+      defaultLocale={intl.defaultLocale}
+      messages={intl.messages}
+    >
+      <IntlContextProvider value={intl}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle isDarkTheme={isDarkTheme} />
+          <ContentContainer>
+            <Nav
+              handleThemeChange={handleThemeChange}
+              isDarkTheme={isDarkTheme}
+              path={path}
+            />
 
-              <MainContainer
-                shouldShowBanner={shouldShowBanner}
-                shouldShowSubNav={shouldShowSubNav}
-                shouldShowSideNav={shouldShowSideNav}
-              >
-                {shouldShowSideNav && <SideNav path={path} />}
-                {shouldShowSideNav && <SideNavMobile path={path} />}
-                <MainContent>
-                  <StyledBannerNotification shouldShow={shouldShowBanner}>
-                    Staking has arrived! If you're looking to stake your ETH,{" "}
-                    <Link to="/eth2/deposit-contract/">
-                      confirm the deposit contract address
-                    </Link>
-                    .
-                  </StyledBannerNotification>
-                  <Main>{this.props.children}</Main>
-                </MainContent>
-              </MainContainer>
-              <Footer />
-            </ContentContainer>
-          </ThemeProvider>
-        </IntlContextProvider>
-      </IntlProvider>
-    )
-  }
+            <MainContainer
+              shouldShowBanner={shouldShowBanner}
+              shouldShowSubNav={shouldShowSubNav}
+              shouldShowSideNav={shouldShowSideNav}
+            >
+              {shouldShowSideNav && <SideNav path={path} />}
+              {shouldShowSideNav && <SideNavMobile path={path} />}
+              <MainContent>
+                <StyledBannerNotification shouldShow={shouldShowBanner}>
+                  Staking has arrived! If you're looking to stake your ETH,{" "}
+                  <Link to="/eth2/deposit-contract/">
+                    confirm the deposit contract address
+                  </Link>
+                  .
+                </StyledBannerNotification>
+                <Main>{props.children}</Main>
+              </MainContent>
+            </MainContainer>
+            <Footer />
+          </ContentContainer>
+        </ThemeProvider>
+      </IntlContextProvider>
+    </IntlProvider>
+  )
 }
 export default Layout
