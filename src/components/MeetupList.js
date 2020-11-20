4dd5e27caaa41371a72ca995823b6b12ba21541f
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2020-11-20 14:58:03 -0800

    meetups list always visible
    pre-set results box size that shows 3-5 results, and overflow is scrollable. Defaults to unfiltered list when no query entered.

--- src/components/MeetupList.js
+++ src/components/MeetupList.js
@@ -1,10 +1,9 @@
-import React, { useState, createRef } from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import Emoji from "./Emoji"
 import Link from "./Link"
 import Icon from "./Icon"
 import meetups from "../data/community/meetups.json"
-import { useOnClickOutside } from "../hooks/useOnClickOutside"
 
 const Container = styled.div`
   width: 100%;
@@ -41,11 +40,12 @@ const SearchIcon = styled(Icon)`
 const Table = styled.div`
   box-shadow: ${(props) => props.theme.colors.tableBoxShadow};
   display: ${(props) => (props.show ? `grid` : `none`)};
-  max-height: 50vh;
+  grid-template-rows: repeat(4, 2fr) 1fr;
   overflow: scroll;
   z-index: 2;
-  position: absolute;
-  width: 85%;
+  margin-bottom: 2rem;
+  width: 100%;
+  height: 260px;
   background: ${(props) => props.theme.colors.background};
   border-radius: 4px;
 `
@@ -70,7 +70,6 @@ const Item = styled(Link)`
 
 const RightContainer = styled.div`
   display: flex;
-  align-items: right;
   align-content: flex-start;
   flex: 1 1 25%;
   margin-right: 1rem;
@@ -79,20 +78,16 @@ const RightContainer = styled.div`
 
 const LeftContainer = styled.div`
   display: flex;
-  flex: 1 1 75%;
+  flex: 1 1 60%;
   margin-right: 1rem;
 `
 
 const MeetupList = () => {
-  const ref = createRef()
   const [query, setQuery] = useState(``)
-  const [focus, setFocus] = useState(false)
-  const searchString = "Search for Meetups..."
-
-  useOnClickOutside(ref, () => setFocus(false))
+  const searchString = "Filter Meetups..."
 
   return (
-    <Container ref={ref}>
+    <Container>
       <Form>
         <StyledInput
           type="text"
@@ -102,35 +97,33 @@ const MeetupList = () => {
           onChange={(event) =>
             setQuery(event.target.value.replace(/[^0-9\w\s]/g, ""))
           }
-          onFocus={() => setFocus(true)}
           onSubmit={(e) => e.preventDefault()}
         />
         <SearchIcon name="search" />
       </Form>
       {/* TODO: Place tags/pills of each available region/flag, clickable to filter */}
-      {query && (
-        <Table show={query && query.length > 0 && focus}>
-          {meetups
-            .filter(({ title, emoji, location }) => {
-              return (
-                title.match(RegExp(query, "gi")) ||
-                emoji.match(RegExp(query, "gi")) ||
-                location.match(RegExp(query, "gi"))
-              )
-            })
-            .map(({ title, emoji, location, link }, idx) => {
-              return (
-                <Item key={idx} to={link}>
-                  <LeftContainer>{title}</LeftContainer>
-                  <RightContainer>
-                    <Emoji text={emoji} size={1} mr={`0.5em`} />
-                    {location}
-                  </RightContainer>
-                </Item>
-              )
-            })}
-        </Table>
-      )}
+      <Table show={true /* query && query.length > 0 && focus */}>
+        {meetups
+          .filter(({ title, emoji, location }) => {
+            return (
+              title.match(RegExp(query, "gi")) ||
+              emoji.match(RegExp(query, "gi")) ||
+              location.match(RegExp(query, "gi")) ||
+              !query
+            )
+          })
+          .map(({ title, emoji, location, link }, idx) => {
+            return (
+              <Item key={idx} to={link}>
+                <LeftContainer>{title}</LeftContainer>
+                <RightContainer>
+                  <Emoji text={emoji} size={1} mr={`0.5em`} />
+                  {location}
+                </RightContainer>
+              </Item>
+            )
+          })}
+      </Table>
     </Container>
   )
 }
