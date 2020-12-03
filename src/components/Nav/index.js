27feaf6e287890e3f08c57e5b93f63ff9534576b
Ryan Cordell <ryancordell@virginmedia.com>
2020-12-03 10:09:38 +0000

    Updated nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -198,16 +198,6 @@ const Nav = ({ handleThemeChange, isDarkTheme, path }) => {
       shouldDisplay: contentVersion > 1.1,
       items: [
         {
-          text: "page-home-section-individuals-item-one",
-          to: "/what-is-ethereum/",
-          shouldDisplay: contentVersion > 1,
-        },
-        {
-          text: "page-home-section-individuals-item-four",
-          to: "/eth/",
-          shouldDisplay: contentVersion > 1,
-        },
-        {
           text: "page-home-section-individuals-item-six",
           to: "/get-eth/",
           shouldDisplay: contentVersion > 1.1,
@@ -222,6 +212,16 @@ const Nav = ({ handleThemeChange, isDarkTheme, path }) => {
           to: "/wallets/",
           shouldDisplay: contentVersion > 1,
         },
+        {
+          text: "page-stablecoins",
+          to: "/stablecoins/",
+          shouldDisplay: contentVersion > 1.1,
+        },
+        {
+          text: "page-stake-eth",
+          to: "/eth2/staking/",
+          shouldDisplay: contentVersion > 1.1,
+        },
       ],
     },
     {
@@ -230,6 +230,16 @@ const Nav = ({ handleThemeChange, isDarkTheme, path }) => {
       shouldDisplay: contentVersion > 1.1,
       items: [
         {
+          text: "page-home-section-individuals-item-one",
+          to: "/what-is-ethereum/",
+          shouldDisplay: contentVersion > 1,
+        },
+        {
+          text: "page-home-section-individuals-item-four",
+          to: "/eth/",
+          shouldDisplay: contentVersion > 1,
+        },
+        {
           text: "page-home-section-individuals-item-three",
           to: "/learn/",
           shouldDisplay: contentVersion > 1.1,
@@ -237,11 +247,6 @@ const Nav = ({ handleThemeChange, isDarkTheme, path }) => {
         {
           text: "footer-ethereum-whitepaper",
           to: "/whitepaper/",
-          shouldDisplay: contentVersion > 1.1,
-        },
-        {
-          text: "page-stablecoins",
-          to: "/stablecoins/",
           shouldDisplay: contentVersion > 1.1,
         },
         {
