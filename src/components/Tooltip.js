68b8225e7a2823facfc93f9bdb89f184b3d2afda
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-01-25 17:03:03 -0800

    Enabled hiding tooltip by clicking away anywhere
    ({destructuring refactor})

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -11,21 +11,21 @@ const Container = styled.div`
 const Content = styled.div`
   text-align: center;
   width: 200px;
-  color: ${(props) => props.theme.colors.text};
-  background-color: ${(props) => props.theme.colors.background};
-  box-shadow: ${(props) => props.theme.colors.tableBoxShadow};
+  color: ${({ theme }) => theme.colors.text};
+  background-color: ${({ theme }) => theme.colors.background};
+  box-shadow: ${({ theme }) => theme.colors.tableBoxShadow};
   position: absolute;
   z-index: 10;
   padding: 1rem 0.5rem;
   text-transform: none;
-  font-size: ${(props) => props.theme.fontSizes.s};
+  font-size: ${({ theme }) => theme.fontSizes.s};
   font-weight: 500;
   cursor: default;
   border-radius: 4px;
   bottom: calc(100% + 1rem);
   left: 50%;
   transform: translateX(-50%);
-  @media (max-width: ${(props) => props.theme.breakpoints.m}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
     width: 140px;
   }
 `
@@ -35,8 +35,19 @@ const Arrow = styled.span`
   bottom: -0.5rem;
   left: calc(50% - 6px);
   border-right: 10px solid transparent;
-  border-top: 10px solid ${(props) => props.theme.colors.background};
+  border-top: 10px solid ${({ theme }) => theme.colors.background};
   border-left: 10px solid transparent;
+`
+
+// Invisible full screen div "below" the clickable link
+// Added so clicking away anywhere will hide Tooltip
+const ModalReturn = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
 `
 
 // TODO add `position` prop
@@ -47,15 +58,18 @@ const Tooltip = ({ content, children }) => {
   // onMouseLeave={() => setIsVisible(false)}
   // onMouseEnter={() => setIsVisible(true)}
   return (
-    <Container onClick={() => setIsVisible(!isVisible)}>
-      {children}
-      {isVisible && (
-        <Content>
-          <Arrow />
-          {content}
-        </Content>
-      )}
-    </Container>
+    <>
+      {isVisible && <ModalReturn onClick={() => setIsVisible(false)} />}
+      <Container onClick={() => setIsVisible(!isVisible)}>
+        {children}
+        {isVisible && (
+          <Content>
+            <Arrow />
+            {content}
+          </Content>
+        )}
+      </Container>
+    </>
   )
 }
 
