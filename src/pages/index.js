2f17d09b8ba1f6a286a78e264a2ea9c3ce5c5e2c
Sam Richards <sbrichards@gmail.com>
2021-01-27 10:22:13 -0800

    Add SimpleWallet code example

--- src/pages/index.js
+++ src/pages/index.js
@@ -479,6 +479,50 @@ const HomePage = ({ data }) => {
   const codeExamples = [
     {
       title: translateMessageId(
+        "page-index-developers-code-example-title-0",
+        intl
+      ),
+      description: translateMessageId(
+        "page-index-developers-code-example-description-0",
+        intl
+      ),
+      codeLanguage: "language-solidity",
+      code: `// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+// This is a smart contract - a program that can be deployed to the Ethereum blockchain.
+contract SimpleWallet {
+    // An 'address' is comparable to an email address - it's used to identify an account on Ethereum.
+    address payable private owner;
+
+    // Events allow for logging of activity on the blockchain.
+    // Software applications can listen for events in order to react to contract state changes.
+    event LogDeposit(uint amount, address indexed sender);
+    event LogWithdrawal(uint amount, address indexed recipient);
+
+  // When this contract is deployed, set the deploying address as the owner of the contract.
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Send ETH from the function caller to the SimpleWallet contract
+    function deposit() public payable {
+        require(msg.value > 0, "Must send ETH.");
+        emit LogDeposit(msg.value, msg.sender);
+    }
+
+    // Send ETH from the SimpleWallet contract to a chosen recipient
+    function withdraw(uint amount, address payable recipient) public {
+        require(msg.sender == owner, "Only the owner of this wallet can withdraw.");
+        require(address(this).balance >= amount, "Not enough funds.");
+        emit LogWithdrawal(amount, recipient);
+        recipient.transfer(amount);
+    }
+}
+      `,
+    },
+    {
+      title: translateMessageId(
         "page-index-developers-code-example-title-1",
         intl
       ),
