619a246e3efbbd94b1bb44ac041f54f43cdcd202
Kristiyan96 <kristiyan.c@gmail.com>
2021-02-26 14:55:51 +0200

    Simplify Icon component render

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -19,29 +19,27 @@ import { BsQuestionSquareFill } from "react-icons/bs"
 import { IoCodeDownload } from "react-icons/io5"
 import { IoCodeOutline } from "react-icons/io5"
 
-const Icon = ({ name, size, className }) => {
-  return (
-    <IconContext.Provider value={{ size: size, className: className }}>
-      {name === "add" && <MdAdd />}
-      {name === "chevronDown" && <MdExpandMore />}
-      {name === "arrowRight" && <MdArrowForward />}
-      {name === "close" && <MdClose />}
-      {name === "darkTheme" && <MdBrightness2 />}
-      {name === "github" && <FaGithub />}
-      {name === "info" && <MdInfoOutline />}
-      {name === "language" && <MdLanguage />}
-      {name === "lightTheme" && <MdWbSunny />}
-      {name === "menu" && <MdMenu />}
-      {name === "twitter" && <FaTwitter />}
-      {name === "search" && <MdSearch />}
-      {name === "youtube" && <FaYoutube />}
-      {name === "discord" && <FaDiscord />}
-      {name === "glossary" && <BsQuestionSquareFill />}
-      {name === "codeDownload" && <IoCodeDownload />}
-      {name === "code" && <IoCodeOutline />}
-    </IconContext.Provider>
-  )
-}
+const Icon = ({ name, size, className }) => (
+  <IconContext.Provider value={{ size: size, className: className }}>
+    {name === "add" && <MdAdd />}
+    {name === "chevronDown" && <MdExpandMore />}
+    {name === "arrowRight" && <MdArrowForward />}
+    {name === "close" && <MdClose />}
+    {name === "darkTheme" && <MdBrightness2 />}
+    {name === "github" && <FaGithub />}
+    {name === "info" && <MdInfoOutline />}
+    {name === "language" && <MdLanguage />}
+    {name === "lightTheme" && <MdWbSunny />}
+    {name === "menu" && <MdMenu />}
+    {name === "twitter" && <FaTwitter />}
+    {name === "search" && <MdSearch />}
+    {name === "youtube" && <FaYoutube />}
+    {name === "discord" && <FaDiscord />}
+    {name === "glossary" && <BsQuestionSquareFill />}
+    {name === "codeDownload" && <IoCodeDownload />}
+    {name === "code" && <IoCodeOutline />}
+  </IconContext.Provider>
+)
 
 Icon.defaultProps = {
   name: ``,
