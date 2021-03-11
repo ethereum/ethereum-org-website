54dc81a174d32aa64b3d975536ca72e24491c363
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-03-10 16:16:36 -0800

    draft wallet card flip

--- src/components/WalletCard.js
+++ src/components/WalletCard.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import Img from "gatsby-image"
+import { motion } from "framer-motion"
 
 import Link from "./Link"
 
@@ -24,7 +25,30 @@ const Image = styled(Img)`
   }
 `
 
+const Container = styled(motion.div)``
+
 const Card = styled(Link)`
+  color: ${(props) => props.theme.colors.text};
+  box-shadow: 0px 14px 66px rgba(0, 0, 0, 0.07),
+    0px 10px 17px rgba(0, 0, 0, 0.03), 0px 4px 7px rgba(0, 0, 0, 0.05);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: ${(props) => props.theme.colors.searchBackground};
+  border-radius: 4px;
+  border: 1px solid ${(props) => props.theme.colors.lightBorder};
+  text-decoration: none;
+
+  &:hover {
+    box-shadow: 0px 8px 17px rgba(0, 0, 0, 0.15);
+    background: ${(props) => props.theme.colors.tableBackgroundHover};
+    transition: transform 0.1s;
+    transform: scale(1.02);
+  }
+`
+
+const CardDiv = styled.div`
   color: ${(props) => props.theme.colors.text};
   box-shadow: 0px 14px 66px rgba(0, 0, 0, 0.07),
     0px 10px 17px rgba(0, 0, 0, 0.03), 0px 4px 7px rgba(0, 0, 0, 0.05);
@@ -63,16 +87,112 @@ const Description = styled.p`
   line-height: 140%;
 `
 
-const WalletCard = ({ wallet }) => (
-  <Card to={wallet.url} hideArrow={true}>
-    <ImageWrapper background={wallet.brand_color}>
-      <Image fixed={wallet.image.childImageSharp.fixed} alt={wallet.alt} />
-    </ImageWrapper>
-    <Content>
-      <Title>{wallet.name}</Title>
-      <Description>{wallet.description}</Description>
-    </Content>
-  </Card>
+const WalletCardBack = ({ wallet, isFlipped, setIsFlipped }) => (
+  <Container
+    variants={flipVariants}
+    animate={isFlipped ? "backShown" : "backFlipped"}
+    onClick={() => setIsFlipped(!isFlipped)}
+  >
+    <CardDiv>
+      <Content>
+        <Title>Wallet Facts</Title>
+        <ul>
+          <li>
+            bank_withdrawals:{" "}
+            {wallet.has_bank_withdrawals === "TRUE" ? "✅" : "❌"}
+          </li>
+          <li>
+            card_deposits: {wallet.has_card_deposits === "TRUE" ? "✅" : "❌"}
+          </li>
+          <li>
+            defi_integrations:{" "}
+            {wallet.has_defi_integrations === "TRUE" ? "✅" : "❌"}
+          </li>
+          <li>desktop: {wallet.has_desktop === "TRUE" ? "✅" : "❌"}</li>
+          <li>
+            dex_integrations:{" "}
+            {wallet.has_dex_integrations === "TRUE" ? "✅" : "❌"}
+          </li>
+          <li>
+            explore_dapps: {wallet.has_explore_dapps === "TRUE" ? "✅" : "❌"}
+          </li>
+          <li>hardware: {wallet.has_hardware === "TRUE" ? "✅" : "❌"}</li>
+          <li>
+            high_volume_purchases:{" "}
+            {wallet.has_high_volume_purchases === "TRUE" ? "✅" : "❌"}
+          </li>
+          <li>
+            limits_protection:{" "}
+            {wallet.has_limits_protection === "TRUE" ? "✅" : "❌"}
+          </li>
+          <li>mobile: {wallet.has_mobile === "TRUE" ? "✅" : "❌"}</li>
+          <li>multisig: {wallet.has_multisig === "TRUE" ? "✅" : "❌"}</li>
+          <li>web: {wallet.has_web === "TRUE" ? "✅" : "❌"}</li>
+        </ul>
+      </Content>
+    </CardDiv>
+  </Container>
 )
+
+const flipVariants = {
+  frontShown: {
+    rotateY: [180, 90, 90, 0],
+    // opacity: [0, 0, 1, 1],
+    transition: {
+      duration: 0.2,
+    },
+  },
+  frontFlipped: {
+    rotateY: [0, 90, 90, 180],
+    // opacity: [1, 1, 0, 0],
+    transition: {
+      duration: 0.2,
+    },
+  },
+  backShown: {
+    rotateY: [-180, -90, -90, 0],
+    // opacity: [0, 0, 1, 1],
+    transition: {
+      duration: 0.2,
+    },
+  },
+  backFlipped: {
+    rotateY: [0, -90, -90, -180],
+    // opacity: [1, 1, 0, 0],
+    transition: {
+      duration: 0.2,
+    },
+  },
+}
+
+const WalletCard = ({ wallet }) => {
+  const [isFlipped, setIsFlipped] = useState(false)
+  console.log({ wallet })
+  return isFlipped ? (
+    <WalletCardBack
+      wallet={wallet}
+      isFlipped={isFlipped}
+      setIsFlipped={setIsFlipped}
+    />
+  ) : (
+    <Container
+      variants={flipVariants}
+      animate={isFlipped ? "frontFlipped" : "frontShown"}
+      onClick={() => setIsFlipped(!isFlipped)}
+    >
+      {/* <Card to={wallet.url} hideArrow={true}> */}
+      <CardDiv>
+        <ImageWrapper background={wallet.brand_color}>
+          <Image fixed={wallet.image.childImageSharp.fixed} alt={wallet.alt} />
+        </ImageWrapper>
+        <Content>
+          <Title>{wallet.name}</Title>
+          <Description>{wallet.description}</Description>
+        </Content>
+      </CardDiv>
+      {/* </Card> */}
+    </Container>
+  )
+}
 
 export default WalletCard
