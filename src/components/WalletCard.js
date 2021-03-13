53d6e26d0b939ae53bc2ab960133956065bd351f
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-03-12 17:28:43 -0800

    second draft card flip
    todo: add buttons for link to project and to flip card (can't make whole card clickable to flip and also embed link to project)

--- src/components/WalletCard.js
+++ src/components/WalletCard.js
@@ -10,7 +10,7 @@ const ImageWrapper = styled.div`
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  background: ${(props) => props.background};
+  background: ${({ background }) => background};
   box-shadow: inset 0px -1px 0px rgba(0, 0, 0, 0.1);
   min-height: 200px;
 `
@@ -20,52 +20,8 @@ const Image = styled(Img)`
   align-self: center;
   max-width: 372px;
   max-height: 257px;
-  @media (max-width: ${(props) => props.theme.breakpoints.s}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
     max-width: 311px;
-  }
-`
-
-const Container = styled(motion.div)``
-
-const Card = styled(Link)`
-  color: ${(props) => props.theme.colors.text};
-  box-shadow: 0px 14px 66px rgba(0, 0, 0, 0.07),
-    0px 10px 17px rgba(0, 0, 0, 0.03), 0px 4px 7px rgba(0, 0, 0, 0.05);
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  background: ${(props) => props.theme.colors.searchBackground};
-  border-radius: 4px;
-  border: 1px solid ${(props) => props.theme.colors.lightBorder};
-  text-decoration: none;
-
-  &:hover {
-    box-shadow: 0px 8px 17px rgba(0, 0, 0, 0.15);
-    background: ${(props) => props.theme.colors.tableBackgroundHover};
-    transition: transform 0.1s;
-    transform: scale(1.02);
-  }
-`
-
-const CardDiv = styled.div`
-  color: ${(props) => props.theme.colors.text};
-  box-shadow: 0px 14px 66px rgba(0, 0, 0, 0.07),
-    0px 10px 17px rgba(0, 0, 0, 0.03), 0px 4px 7px rgba(0, 0, 0, 0.05);
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  background: ${(props) => props.theme.colors.searchBackground};
-  border-radius: 4px;
-  border: 1px solid ${(props) => props.theme.colors.lightBorder};
-  text-decoration: none;
-
-  &:hover {
-    box-shadow: 0px 8px 17px rgba(0, 0, 0, 0.15);
-    background: ${(props) => props.theme.colors.tableBackgroundHover};
-    transition: transform 0.1s;
-    transform: scale(1.02);
   }
 `
 
@@ -82,117 +38,169 @@ const Title = styled.h3`
 
 const Description = styled.p`
   opacity: 0.8;
-  font-size: ${(props) => props.theme.fontSizes.s};
+  font-size: ${({ theme }) => theme.fontSizes.s};
   margin-bottom: 0.5rem;
   line-height: 140%;
 `
 
-const WalletCardBack = ({ wallet, isFlipped, setIsFlipped }) => (
-  <Container
-    variants={flipVariants}
-    animate={isFlipped ? "backShown" : "backFlipped"}
-    onClick={() => setIsFlipped(!isFlipped)}
-  >
-    <CardDiv>
-      <Content>
-        <Title>Wallet Facts</Title>
-        <ul>
-          <li>
-            bank_withdrawals:{" "}
-            {wallet.has_bank_withdrawals === "TRUE" ? "✅" : "❌"}
-          </li>
-          <li>
-            card_deposits: {wallet.has_card_deposits === "TRUE" ? "✅" : "❌"}
-          </li>
-          <li>
-            defi_integrations:{" "}
-            {wallet.has_defi_integrations === "TRUE" ? "✅" : "❌"}
-          </li>
-          <li>desktop: {wallet.has_desktop === "TRUE" ? "✅" : "❌"}</li>
-          <li>
-            dex_integrations:{" "}
-            {wallet.has_dex_integrations === "TRUE" ? "✅" : "❌"}
-          </li>
-          <li>
-            explore_dapps: {wallet.has_explore_dapps === "TRUE" ? "✅" : "❌"}
-          </li>
-          <li>hardware: {wallet.has_hardware === "TRUE" ? "✅" : "❌"}</li>
-          <li>
-            high_volume_purchases:{" "}
-            {wallet.has_high_volume_purchases === "TRUE" ? "✅" : "❌"}
-          </li>
-          <li>
-            limits_protection:{" "}
-            {wallet.has_limits_protection === "TRUE" ? "✅" : "❌"}
-          </li>
-          <li>mobile: {wallet.has_mobile === "TRUE" ? "✅" : "❌"}</li>
-          <li>multisig: {wallet.has_multisig === "TRUE" ? "✅" : "❌"}</li>
-          <li>web: {wallet.has_web === "TRUE" ? "✅" : "❌"}</li>
-        </ul>
-      </Content>
-    </CardDiv>
-  </Container>
-)
+const Container = styled.div`
+  /* TODO: Fix dimensions */
+  height: 460px;
+  perspective: 1000px;
+`
 
-const flipVariants = {
-  frontShown: {
-    rotateY: [180, 90, 90, 0],
-    // opacity: [0, 0, 1, 1],
-    transition: {
-      duration: 0.2,
-    },
-  },
-  frontFlipped: {
-    rotateY: [0, 90, 90, 180],
-    // opacity: [1, 1, 0, 0],
-    transition: {
-      duration: 0.2,
-    },
-  },
-  backShown: {
-    rotateY: [-180, -90, -90, 0],
-    // opacity: [0, 0, 1, 1],
-    transition: {
-      duration: 0.2,
-    },
-  },
-  backFlipped: {
-    rotateY: [0, -90, -90, -180],
-    // opacity: [1, 1, 0, 0],
-    transition: {
-      duration: 0.2,
-    },
-  },
-}
+const Card = styled.div`
+  position: relative;
+  transform-style: preserve-3d;
+  height: 100%;
+  width: 100%;
+  &:hover {
+    transition: transform 0.1s;
+    transform: scale(1.02);
+  }
+`
+
+const CardFace = styled(motion.div)`
+  position: absolute;
+  backface-visibility: hidden;
+  height: 100%;
+  width: 100%;
+
+  color: ${({ theme }) => theme.colors.text};
+  box-shadow: 0px 14px 66px rgba(0, 0, 0, 0.07),
+    0px 10px 17px rgba(0, 0, 0, 0.03), 0px 4px 7px rgba(0, 0, 0, 0.05);
+  background: ${({ theme }) => theme.colors.searchBackground};
+  &:hover {
+    /* box-shadow: 0px 8px 17px rgba(0, 0, 0, 0.15); */
+    background: ${(props) => props.theme.colors.tableBackgroundHover};
+  }
+`
+
+const CardFront = styled(CardFace)`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.colors.lightBorder};
+  text-decoration: none;
+`
+
+const CardBack = styled(CardFace)``
 
 const WalletCard = ({ wallet }) => {
-  const [isFlipped, setIsFlipped] = useState(false)
-  console.log({ wallet })
-  return isFlipped ? (
-    <WalletCardBack
-      wallet={wallet}
-      isFlipped={isFlipped}
-      setIsFlipped={setIsFlipped}
-    />
-  ) : (
-    <Container
-      variants={flipVariants}
-      animate={isFlipped ? "frontFlipped" : "frontShown"}
-      onClick={() => setIsFlipped(!isFlipped)}
-    >
-      {/* <Card to={wallet.url} hideArrow={true}> */}
-      <CardDiv>
-        <ImageWrapper background={wallet.brand_color}>
-          <Image fixed={wallet.image.childImageSharp.fixed} alt={wallet.alt} />
-        </ImageWrapper>
-        <Content>
-          <Title>{wallet.name}</Title>
-          <Description>{wallet.description}</Description>
-        </Content>
-      </CardDiv>
-      {/* </Card> */}
+  const [isCardFlipped, setIsCardFlipped] = useState(false)
+
+  const duration = 0.3
+  const flipVariants = {
+    shown: {
+      rotateY: 0,
+      transition: {
+        duration,
+      },
+    },
+    frontFlipped: {
+      rotateY: 180,
+      transition: {
+        duration,
+      },
+    },
+    backFlipped: {
+      rotateY: -180,
+      transition: {
+        duration,
+      },
+    },
+  }
+
+  return (
+    <Container onClick={() => setIsCardFlipped(!isCardFlipped)}>
+      <Card>
+        <CardFront
+          wallet={wallet}
+          variants={flipVariants}
+          animate={isCardFlipped ? "frontFlipped" : "shown"}
+        >
+          <ImageWrapper background={wallet.brand_color}>
+            <Image
+              fixed={wallet.image.childImageSharp.fixed}
+              alt={wallet.alt}
+            />
+          </ImageWrapper>
+          <Content>
+            <Title>{wallet.name}</Title>
+            <Description>{wallet.description}</Description>
+          </Content>
+        </CardFront>
+        <CardBack
+          wallet={wallet}
+          variants={flipVariants}
+          initial={{ rotateY: -180 }}
+          animate={isCardFlipped ? "shown" : "backFlipped"}
+        >
+          <Content>
+            <Title>Wallet Facts</Title>
+            <table>
+              <tr>
+                <td>Bank Withdrawals:</td>
+                <td>{wallet.has_bank_withdrawals === "TRUE" ? "✅" : "❌"}</td>
+              </tr>
+              <tr>
+                <td>Card Deposits:</td>
+                <td>{wallet.has_card_deposits === "TRUE" ? "✅" : "❌"}</td>
+              </tr>
+              <tr>
+                <td>DeFi Integrations:</td>
+                <td>{wallet.has_defi_integrations === "TRUE" ? "✅" : "❌"}</td>
+              </tr>
+              {/* <tr>desktop: {wallet.has_desktop === "TRUE" ? "✅" : "❌"}</tr> */}
+              <tr>
+                <td>DEX Integrations:</td>
+                <td>{wallet.has_dex_integrations === "TRUE" ? "✅" : "❌"}</td>
+              </tr>
+              <tr>
+                <td>Explore Dapps:</td>
+                <td>{wallet.has_explore_dapps === "TRUE" ? "✅" : "❌"}</td>
+              </tr>
+              {/* <tr>
+                <td>Hardware:</td>
+                <td>{wallet.has_hardware === "TRUE" ? "✅" : "❌"}</td>
+              </tr> */}
+              <tr>
+                <td>High Volume Purchases:</td>
+                <td>
+                  {wallet.has_high_volume_purchases === "TRUE" ? "✅" : "❌"}
+                </td>
+              </tr>
+              <tr>
+                <td>Limits Protection:</td>
+                <td>{wallet.has_limits_protection === "TRUE" ? "✅" : "❌"}</td>
+              </tr>
+              {/* <tr>mobile: {wallet.has_mobile === "TRUE" ? "✅" : "❌"}</tr> */}
+              <tr>
+                <td>MultiSig:</td>
+                <td>{wallet.has_multisig === "TRUE" ? "✅" : "❌"}</td>
+              </tr>
+              {/* <tr>web: {wallet.has_web === "TRUE" ? "✅" : "❌"}</tr> */}
+            </table>
+          </Content>
+        </CardBack>
+      </Card>
     </Container>
   )
+  //     {/* <Card to={wallet.url} hideArrow={true}> */}
+  //     <CardDiv>
+  //       <ImageWrapper background={wallet.brand_color}>
+  //         <Image fixed={wallet.image.childImageSharp.fixed} alt={wallet.alt} />
+  //       </ImageWrapper>
+  //       <Content>
+  //         <Title>{wallet.name}</Title>
+  //         <Description>{wallet.description}</Description>
+  //       </Content>
+  //     </CardDiv>
+  //     {/* </Card> */}
+  // )
 }
 
 export default WalletCard
