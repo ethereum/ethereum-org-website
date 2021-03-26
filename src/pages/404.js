893ede46e81f056cb573b3080d54283a6f626d0a
Kevin Ziechmann <kziechmann@gmail.com>
2021-03-26 12:30:47 -0700

    404 page translations added

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 
+import Translation from "../components/Translation"
 import Link from "../components/Link"
 import { Page, Content } from "../components/SharedStyledComponents"
 
@@ -8,14 +9,19 @@ const StyledPage = styled(Page)`
   margin-top: 4rem;
 `
 
-// TODO is there a way to translate this page?
 const NotFoundPage = () => (
   <StyledPage>
     <Content>
-      <h1>Resource not found.</h1>
+      <h1>
+        <Translation id="page-404-not-found" />
+      </h1>
       <p>
-        Try using search to find what you're looking for or{" "}
-        <Link to="/en/">return home</Link>.
+        <Translation id="page-404-copy" />
+      </p>
+      <p>
+        <Link to="/">
+          <Translation id="home" />
+        </Link>
       </p>
     </Content>
   </StyledPage>
