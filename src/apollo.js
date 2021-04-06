bbadddcdf9f2933ea72052fbe818f7c6dfd30495
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-04-06 15:24:40 -0700

    GATSBY_GITHUB_TOKEN_READ_ONLY

--- src/apollo.js
+++ src/apollo.js
@@ -1,19 +1,12 @@
 import fetch from "cross-fetch"
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
 
-const GITHUB_TOKEN_READ_ONLY =
-  process.env.CONTEXT === "production"
-    ? process.env.GITHUB_TOKEN_READ_ONLY_PROD
-    : process.env.CONTEXT === "deploy-preview"
-    ? process.env.GITHUB_TOKEN_READ_ONLY_STAGING
-    : process.env.GITHUB_TOKEN_READ_ONLY_DEV
-
 const client = new ApolloClient({
   link: new HttpLink({
     uri: `https://api.github.com/graphql`,
     fetch,
     headers: {
-      Authorization: `Bearer ${GITHUB_TOKEN_READ_ONLY}`,
+      Authorization: `Bearer ${process.env.GATSBY_GITHUB_TOKEN_READ_ONLY}`,
     },
   }),
   cache: new InMemoryCache(),
