070d00cba4f4c0982b8c4545fe3cfcd3638a79cb
Andreas Sofos <sofos_andreas@yahoo.gr>
2021-06-19 00:16:44 +0300

    build charts for homepage [Fixes #2319]

--- src/components/StatsBoxGrid.js
+++ src/components/StatsBoxGrid.js
@@ -3,12 +3,24 @@ import styled from "styled-components"
 import { useIntl } from "gatsby-plugin-intl"
 import axios from "axios"
 
+import {
+  AreaChart,
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  CartesianGrid,
+  Area,
+  XAxis,
+  YAxis,
+} from "recharts"
 import Translation from "./Translation"
 import Tooltip from "./Tooltip"
 import Link from "./Link"
 import Icon from "./Icon"
 
 import { getData } from "../utils/cache"
+import { range } from "lodash"
+import { act } from "react-test-renderer"
 
 const Value = styled.h3`
   font-size: min(4.4vw, 64px);
@@ -49,6 +61,7 @@ const Grid = styled.div`
 `
 
 const Box = styled.div`
+  position: relative;
   color: ${({ theme }) => theme.colors.text};
   height: 20rem;
   background: ${({ theme, color }) => theme.colors[color]};
@@ -58,6 +71,7 @@ const Box = styled.div`
   align-items: flex-start;
   border: 1px solid ${({ theme }) => theme.colors.color};
   padding: 1.5rem;
+  z-index: 1;
   @media (max-width: ${({ theme }) => theme.breakpoints.l}) {
     border-left: 0px solid #000000;
     border-right: 0px solid #000000;
@@ -104,8 +118,57 @@ const LoadingMessage = () => (
   </IndicatorSpan>
 )
 
+const Lines = styled.div`
+  position: absolute;
+  margin-left: -5px;
+  left: 0;
+  bottom: 0;
+  width: 101%;
+  height: 200px;
+  z-index: -1;
+`
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+  font-family: "SFMono-Regular", monospace;
+  // background: ${({ theme, color }) => theme.colors[color]};
+`
+
+const Button = styled.button`
+  background: ${(props) => props.theme.colors.background};
+  font-family: "SFMono-Regular", monospace;
+  color: ${({ theme }) => theme.colors.text};
+  padding: 5px 15px;
+  border-radius: 5px;
+  border: 1px solid ${({ theme, color }) => theme.colors[color]};
+  outline: none;
+  // text-transform: uppercase;
+  // margin: 10px 0px;
+  cursor: pointer;
+  // box-shadow: 0px 2px 2px lightgray;
+  // transition: ease background-color 250ms;
+  &:hover {
+    background-color: blue;
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`
+
+const ButtonToggle = styled(Button)`
+  opacity: 0.7;
+  ${({ active }) =>
+    active &&
+    `
+    opacity: 1; 
+  `}
+`
+
 const GridItem = ({ metric }) => {
-  const { title, description, state } = metric
+  const { title, description, line, buttonContainer, state } = metric
   const isLoading = !state.value
   const value = state.hasError ? (
     <ErrorMessage />
@@ -127,7 +190,10 @@ const GridItem = ({ metric }) => {
       <div>
         <Title>{title}</Title>
         <p>{description}</p>
+        <Lines>{line}</Lines>
+        <ButtonContainer>{buttonContainer}</ButtonContainer>
       </div>
+
       <Value>{value}</Value>
     </Box>
   )
@@ -290,6 +356,136 @@ const StatsBoxGrid = () => {
     }
   }, [])
 
+  var today = new Date(),
+    date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      (today.getDate() - 1),
+    oneMonthAgo =
+      today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate()
+  const start = "2019-10-30"
+
+  // let url =
+  //   "https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=30&interval=hour"
+
+  // const url1 =
+  //   "https://api.etherscan.io/api?module=stats&action=nodecounthistory&startdate=" +
+  //   oneMonthAgo +
+  //   "&enddate=" +
+  //   date +
+  //   "&sort=asc&apikey=2JD9ZCGGPST7VHY8FHW3NZKI1D34VQR4I5"
+
+  // const url1 =
+  //   "https://api.etherscan.io/api?module=stats&action=nodecounthistory&startdate=2019-10-30&enddate=2021-06-14&clienttype=geth&syncmode=default&sort=asc&apikey=2JD9ZCGGPST7VHY8FHW3NZKI1D34VQR4I5"
+
+  // const url2 =
+  //   "https://data-api.defipulse.com/api/v1/defipulse/api/GetHistory?api-key={{DeFi_Pulse_Key}}&period=1y&length=hours"
+
+  const [App, setApp] = useState(null)
+  useEffect(() => {
+    coinGeckoData("30")
+  }, [])
+
+  function coinGeckoData(mode) {
+    let url = `https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=${mode}&interval=hour`
+    axios.get(url).then((response) => {
+      setApp(response.data)
+    })
+  }
+
+  const [App1, setApp1] = useState(null)
+  useEffect(() => {
+    etherscanData(oneMonthAgo)
+  }, [])
+
+  let type
+  function etherscanData(mode1) {
+    let url1 = `https://api.etherscan.io/api?module=stats&action=nodecounthistory&startdate=${mode1}&enddate=${date}&sort=asc&apikey=2JD9ZCGGPST7VHY8FHW3NZKI1D34VQR4I5`
+    axios.get(url1).then((response) => {
+      setApp1(response.data)
+    })
+  }
+
+  const [App2, setApp2] = useState(null)
+  useEffect(() => {
+    defipalseData("1m")
+  }, [])
+
+  function defipalseData(mode2) {
+    let url2 = `https://data-api.defipulse.com/api/v1/defipulse/api/GetHistory?api-key=d68cfcfe749887913f720b22df372feb1b0f69e06e3e0c76eb25c592065a&period=${mode2}&length=days`
+    axios.get(url2).then((response) => {
+      setApp2(response.data)
+    })
+  }
+
+  // console.log(App2)
+
+  // let [data, setData] = useState(null)
+  let data = []
+  let data2 = []
+  let data1 = []
+
+  if (App) {
+    for (const i in App.prices) {
+      data.push({
+        name: "Page A",
+        uv: App.prices[i][1],
+        pv: i,
+        amt: 2400,
+      })
+    }
+  }
+
+  if (App1) {
+    for (const i in App1.result) {
+      data1.push({
+        name: "Page A",
+        uv: App1.result[i]["TotalNodeCount"],
+        pv: App1.result[i]["UTCDate"],
+        amt: 2400,
+      })
+    }
+  }
+
+  if (App2) {
+    console.log(Object.keys(App2).length)
+    for (let i = 1; i <= Object.keys(App2).length; i++) {
+      if (i != "error") {
+        data2.push({
+          name: " Page A",
+          uv: App2[Object.keys(App2).length - i]["tvlUSD"] / 1000000000,
+          pv: Object.keys(App2).length - i,
+          amt: 2400,
+        })
+      }
+    }
+  }
+
+  const types = ["0", "1"]
+  const coingeckoTypes = ["30", "max"]
+  const defaultTypes = ["30d", "All"]
+
+  function ToggleGroup() {
+    const [active, setActive] = useState(types[0])
+    return (
+      <div>
+        {types.map((type) => (
+          <ButtonToggle
+            active={active === type}
+            onClick={() => {
+              coinGeckoData(coingeckoTypes[type])
+              setActive(type)
+            }}
+          >
+            {defaultTypes[type]}
+          </ButtonToggle>
+        ))}
+      </div>
+    )
+  }
+
   const metrics = [
     {
       apiProvider: "CoinGecko",
@@ -300,6 +496,37 @@ const StatsBoxGrid = () => {
       description: (
         <Translation id="page-index-network-stats-eth-price-explainer" />
       ),
+      line: (
+        <ResponsiveContainer>
+          <AreaChart height={200} data={data}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={1} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fillOpacity={0.3}
+              fill="url(#colorUv)"
+              fillOpacity="0.2"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      ),
+      buttonContainer: (
+        // <div>
+        //   <button onClick={() => coinGeckoData("30")}>30d</button>
+        //   <button onClick={() => coinGeckoData("max")}>ALL</button>
+        // </div>
+        <ToggleGroup />
+      ),
       state: ethPrice,
     },
     {
@@ -308,6 +535,36 @@ const StatsBoxGrid = () => {
       title: <Translation id="page-index-network-stats-tx-day-description" />,
       description: (
         <Translation id="page-index-network-stats-tx-day-explainer" />
+      ),
+      line: (
+        <ResponsiveContainer>
+          <AreaChart height={200} data={data}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={1} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fillOpacity={0.3}
+              fill="url(#colorUv)"
+              fillOpacity="0.2"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      ),
+      buttonContainer: (
+        <div>
+          <button>30d</button>
+          <button>ALL</button>
+        </div>
       ),
       state: txs,
     },
@@ -320,6 +577,36 @@ const StatsBoxGrid = () => {
       description: (
         <Translation id="page-index-network-stats-value-defi-explainer" />
       ),
+      line: (
+        <ResponsiveContainer>
+          <AreaChart data={data2}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={1} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fillOpacity={0.3}
+              fill="url(#colorUv)"
+              fillOpacity="0.2"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      ),
+      buttonContainer: (
+        <div>
+          <button onClick={() => defipalseData("1m")}>30d</button>
+          <button onClick={() => defipalseData("all")}>ALL</button>
+        </div>
+      ),
       state: valueLocked,
     },
     {
@@ -328,6 +615,36 @@ const StatsBoxGrid = () => {
       title: <Translation id="page-index-network-stats-nodes-description" />,
       description: (
         <Translation id="page-index-network-stats-nodes-explainer" />
+      ),
+      line: (
+        <ResponsiveContainer>
+          <AreaChart height={200} data={data1}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={1} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fillOpacity={0.3}
+              fill="url(#colorUv)"
+              fillOpacity="0.2"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      ),
+      buttonContainer: (
+        <div>
+          <button onClick={() => etherscanData(oneMonthAgo)}>30d</button>
+          <button onClick={() => etherscanData(start)}>ALL</button>
+        </div>
       ),
       state: nodes,
     },
