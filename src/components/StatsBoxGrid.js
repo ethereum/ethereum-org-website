029745c10e73511f8364c22fc07453752a8bf617
Andreas Sofos <sofos_andreas@yahoo.gr>
2021-06-21 17:46:49 +0300

    minor changes for testing

--- src/components/StatsBoxGrid.js
+++ src/components/StatsBoxGrid.js
@@ -552,9 +552,28 @@ const StatsBoxGrid = () => {
         <Translation id="page-index-network-stats-tx-day-explainer" />
       ),
       line: (
-        <LineChart width={600} height={300} data={pricesData}>
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        </LineChart>
+        <ResponsiveContainer>
+          <AreaChart height={200} data={pricesData}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={1} />
+                <stop offset="100%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fillOpacity={0.3}
+              fill="url(#colorUv)"
+              fillOpacity="0.2"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
       ),
       buttonContainer: <ToggleGroupPrice />,
       state: txs,
