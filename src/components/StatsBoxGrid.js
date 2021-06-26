2333abd649205b105910949fe756fff2649eb179
Andreas Sofos <sofos_andreas@yahoo.gr>
2021-06-27 01:40:14 +0300

    homepage charts testing

--- src/components/StatsBoxGrid.js
+++ src/components/StatsBoxGrid.js
@@ -387,7 +387,20 @@ const StatsBoxGrid = () => {
   //   defipalseData("1m")
   // }, [])
 
-  const [coingecko, setCoingecko] = useState([])
+  const [coingecko, setCoingecko] = useState([
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+  ])
   useEffect(() => {
     coinGeckoData("30")
   }, [])
