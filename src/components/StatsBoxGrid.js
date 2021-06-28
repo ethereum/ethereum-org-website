eca4518e29a087f9b2c1250789dcefec437cf9f1
Andreas Sofos <sofos_andreas@yahoo.gr>
2021-06-28 13:53:51 +0300

    Add charts at homepage [Fixes #2319]

--- src/components/StatsBoxGrid.js
+++ src/components/StatsBoxGrid.js
@@ -621,50 +621,6 @@ const StatsBoxGrid = () => {
       </div>
     )
   }
-  // const transactionsData = [
-  //   {
-  //     name: "Page A",
-  //     uv: 4000,
-  //     pv: 2400,
-  //     amt: 2400,
-  //   },
-  //   {
-  //     name: "Page B",
-  //     uv: 3000,
-  //     pv: 1398,
-  //     amt: 2210,
-  //   },
-  //   {
-  //     name: "Page C",
-  //     uv: 2000,
-  //     pv: 9800,
-  //     amt: 2290,
-  //   },
-  //   {
-  //     name: "Page D",
-  //     uv: 2780,
-  //     pv: 3908,
-  //     amt: 2000,
-  //   },
-  //   {
-  //     name: "Page E",
-  //     uv: 1890,
-  //     pv: 4800,
-  //     amt: 2181,
-  //   },
-  //   {
-  //     name: "Page F",
-  //     uv: 2390,
-  //     pv: 3800,
-  //     amt: 2500,
-  //   },
-  //   {
-  //     name: "Page G",
-  //     uv: 3490,
-  //     pv: 4300,
-  //     amt: 2100,
-  //   },
-  // ]
   const [transactionsActive, setTransactionsActive] = useState(types[0])
   function ToggleGroupTransactions() {
     return (
