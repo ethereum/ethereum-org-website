5771f875f3f013a240eecfc07b71269c0f71be05
Andreas Sofos <sofos_andreas@yahoo.gr>
2021-06-28 04:47:33 +0300

    homepage charts testing

--- src/components/StatsBoxGrid.js
+++ src/components/StatsBoxGrid.js
@@ -192,7 +192,34 @@ const GridItem = ({ metric }) => {
   ) : isLoading1 ? (
     <LoadingMessage />
   ) : (
-    <div></div>
+    <div>
+      {line.value.length > 0 && (
+        <AreaChart width={700} height={200} data={line.value}>
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={1} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+
+          <Area
+            type="monotone"
+            dataKey="uv"
+            stroke="#8884d8"
+            fillOpacity={0.3}
+            fill="url(#colorUv)"
+            fillOpacity="0.2"
+            connectNulls={true}
+          />
+
+          <XAxis dataKey="pv" />
+        </AreaChart>
+      )}
+    </div>
   )
   console.log(line.value, "line")
 
