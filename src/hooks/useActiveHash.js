b51e37e6cb80a5f2e1e34dd58e1c598151e7cf07
Vaibhav <vaibhavchanana@yahoo.co.in>
2021-07-22 01:04:39 +0530

    feat: highlight table of content on scroll

---
+++ src/hooks/useActiveHash.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react"
+
+/**
+ * A hook to determine which section of the page is currently in the viewport.
+ * @param {*} itemIds Array of document ids to observe
+ * @param {*} rootMargin
+ * @returns id of the element currently in viewport
+ */
+export const useActiveHash = (itemIds, rootMargin = undefined) => {
+  const [activeHash, setActiveHash] = useState(``)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveHash(`#${entry.target.id}`)
+          }
+        })
+      },
+      { rootMargin: rootMargin || `0% 0% -80% 0%` }
+    )
+
+    itemIds?.forEach((id) => {
+      if (document.getElementById(id) !== null) {
+        observer.observe(document.getElementById(id))
+      }
+    })
+
+    return () => {
+      itemIds?.forEach((id) => {
+        if (document.getElementById(id) !== null) {
+          observer.unobserve(document.getElementById(id))
+        }
+      })
+    }
+  }, [])
+
+  return activeHash
+}
