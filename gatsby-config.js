3d8ff0d95fb312c943ce42d3fabd18f822c051df
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-09-28 08:37:09 -0700

    ignore queryParams on canonical URLs

--- gatsby-config.js
+++ gatsby-config.js
@@ -176,6 +176,7 @@ module.exports = {
       resolve: `gatsby-plugin-react-helmet-canonical-urls`,
       options: {
         siteUrl,
+        noQueryString: true,
       },
     },
     // Needed for `gatsby-image`
