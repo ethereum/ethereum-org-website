91a3607bcb9d2c8c0bce9ad14a31ee948ab3d966
SNikhill <snikhill@protonmail.com>
2021-10-26 18:00:01 +0530

    Reduce SearchSlash Padding
    Co-authored-by: Joshua <62268199+minimalsm@users.noreply.github.com>

--- src/components/Search/Input.js
+++ src/components/Search/Input.js
@@ -50,7 +50,7 @@ const SearchSlash = styled.p`
   color: ${(props) => props.theme.colors.text};
   display: none;
   margin-bottom: 0;
-  padding: 0 7px;
+  padding: 0 6px;
   position: absolute;
   right: 6px;
   top: 20%;
