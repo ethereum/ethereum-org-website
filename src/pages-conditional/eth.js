eec1793ac84ee3e4bbdc064693ef6c21e758fcc6
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-11-27 14:30:57 +0530

    Update to relative links

--- src/pages-conditional/eth.js
+++ src/pages-conditional/eth.js
@@ -221,25 +221,25 @@ const benefits = [
 
 const tokenLinks = [
   {
-    link: "https://ethereum.org/en/stablecoins/",
+    link: "/stablecoins/",
     caption: "",
     title: <Translation id="page-eth-tokens-stablecoins" />,
     description: <Translation id="page-eth-tokens-stablecoins-description" />,
   },
   {
-    link: "https://ethereum.org/en/defi/",
+    link: "/defi/",
     caption: "",
     title: <Translation id="page-eth-tokens-defi" />,
     description: <Translation id="page-eth-tokens-defi-description" />,
   },
   {
-    link: "https://ethereum.org/en/nft/",
+    link: "/nft/",
     caption: "",
     title: <Translation id="page-eth-tokens-nft" />,
     description: <Translation id="page-eth-tokens-nft-description" />,
   },
   {
-    link: "https://ethereum.org/en/dao/",
+    link: "/dao/",
     caption: "",
     title: <Translation id="page-eth-tokens-dao" />,
     description: <Translation id="page-eth-tokens-dao-description" />,
