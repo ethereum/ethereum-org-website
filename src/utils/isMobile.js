862afe423fdc3501f7f3ae3478d2b7d949445f66
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2021-12-02 17:42:00 +0530

    Update function

--- src/utils/isMobile.js
+++ src/utils/isMobile.js
@@ -1,6 +1,6 @@
 export const isMobile = () => {
   let isMobile = false
-  if (typeof window !== undefined) {
+  if (typeof window !== "undefined") {
     isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       window.navigator.userAgent
     )
