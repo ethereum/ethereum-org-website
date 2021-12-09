ca5447b06ca75eb9aeb9b4821d9535068aed0a0b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-12-09 11:43:45 -0800

    remove `cp netlify.toml public` from `onPostBuild`

--- gatsby-node.js
+++ gatsby-node.js
@@ -413,5 +413,5 @@ exports.onPostBuild = async (gatsbyNodeHelpers) => {
     if (stdout) reporter.info(stdout)
   }
 
-  reportOut(await exec("npm run build:lambda && cp netlify.toml public"))
+  reportOut(await exec("npm run build:lambda"))
 }
