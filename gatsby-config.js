f00a576f864aaa2aee9bec73b50a1802c2b748c6
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-12-09 11:19:39 -0800

    `redirect: false` in `gatsby-plugin-intl`

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         defaultLanguage,
         // redirect to `/${lang}/` when connecting to `/`
         // based on user's browser language preference
-        redirect: true,
+        redirect: false,
       },
     },
     // Web app manifest
