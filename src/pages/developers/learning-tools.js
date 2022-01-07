e46c3410c5df2a5c674a3e08eb703a5fd1835907
Justin Shaw <realjustinshaw@gmail.com>
2022-01-07 10:33:28 -0800

    Revert "remove remix promo banner"
    This reverts commit 1b21fc5fffab490f2b18cbd9379e45f37274ce57.

--- src/pages/developers/learning-tools.js
+++ src/pages/developers/learning-tools.js
@@ -247,6 +247,9 @@ const LearningToolsPage = ({ data }) => {
             </ProductCard>
           ))}
         </StyledCardGrid>
+        <InfoBanner emoji=":point_up:" shouldCenter={true}>
+          <Translation id="page-learning-tools-remix-description-2" />
+        </InfoBanner>
       </StackContainer>
       <StackContainer>
         <SubtitleTwo>
