cebc11db751c2c283bd7a14c23f02a314882dcea
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-01-10 19:34:01 -0800

    Add ExpandableInfo component

---
+++ src/components/ExpandableInfo.js
@@ -0,0 +1,195 @@
+import React, { useState } from "react"
+import styled from "styled-components"
+import { motion } from "framer-motion"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+import { FakeLink } from "./SharedStyledComponents"
+import Translation from "./Translation"
+import Icon from "./Icon"
+
+const Card = styled.div`
+  border: 1px solid ${(props) => props.theme.colors.border};
+  border-radius: 2px;
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+  background: ${({ background, theme }) =>
+    background ? theme.colors[background] : theme.colors.background};
+  &:hover {
+    transform: scale(1.02);
+    transition: 0.1s;
+    img {
+      transform: scale(1.08);
+      transition: 0.1s;
+    }
+    background: ${({ background, theme }) =>
+      background ? theme.colors[background] + "77" : theme.colors.background};
+  }
+`
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 3rem;
+  cursor: pointer;
+  @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
+    gap: 2rem;
+    flex-direction: column;
+  }
+`
+
+const TitleContent = styled.div`
+  display: flex;
+  align-items: center;
+  /* justify-content: space-between; */
+  gap: 3rem;
+  width: 100%;
+`
+
+const Title = styled.h2`
+  margin-top: 0rem;
+  margin-bottom: 0.5rem;
+`
+
+const TextPreview = styled.p`
+  font-weight: 400;
+  color: ${(props) => props.theme.colors.text200};
+  margin-bottom: 0rem;
+`
+
+const Text = styled(motion.div)`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${(props) => props.theme.colors.text};
+  margin-top: 2rem;
+  border-top: 1px solid ${(props) => props.theme.colors.border};
+  padding-top: 1.5rem;
+`
+
+const Question = styled.div`
+  margin-right: 1rem;
+`
+
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 1rem 0;
+  align-items: center;
+  img {
+    margin-right: 1.5rem;
+  }
+`
+
+const ButtonContainer = styled(motion.div)`
+  margin-left: auto;
+`
+
+const StyledIcon = styled(Icon)`
+  transform: translateY(10px);
+`
+
+const ExpandableInfo = ({
+  image,
+  title,
+  contentPreview,
+  children,
+  background,
+  className,
+}) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const expandCollapse = {
+    collapsed: {
+      height: 0,
+      transition: {
+        when: "afterChildren",
+      },
+    },
+    expanded: {
+      height: "100%",
+      transition: {
+        when: "beforeChildren",
+      },
+    },
+  }
+  const chevronFlip = {
+    collapsed: {
+      rotate: 0,
+      transition: {
+        duration: 0.1,
+      },
+    },
+    expanded: {
+      rotate: 180,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  }
+  const showHide = {
+    collapsed: {
+      display: "none",
+    },
+    expanded: {
+      display: "inline-block",
+    },
+  }
+  const fadeInOut = {
+    collapsed: {
+      opacity: 0,
+      transition: {
+        duration: 0.1,
+      },
+    },
+    expanded: {
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  }
+  return (
+    <Card background={background} className={className}>
+      <Content onClick={() => setIsVisible(!isVisible)}>
+        {image && <GatsbyImage image={image} />}
+        <TitleContent>
+          <Question>
+            <Header>
+              <Title>{title}</Title>
+            </Header>
+            <TextPreview>{contentPreview}</TextPreview>
+          </Question>
+          <ButtonContainer
+            variants={chevronFlip}
+            animate={isVisible ? "expanded" : "collapsed"}
+            initial={false}
+          >
+            <StyledIcon name="chevronDown" size="36" />
+          </ButtonContainer>
+        </TitleContent>
+      </Content>
+      <motion.div
+        variants={expandCollapse}
+        animate={isVisible ? "expanded" : "collapsed"}
+        initial={false}
+      >
+        <motion.div
+          variants={showHide}
+          animate={isVisible ? "expanded" : "collapsed"}
+          initial={false}
+        >
+          <Text
+            variants={fadeInOut}
+            animate={isVisible ? "expanded" : "collapsed"}
+            initial={false}
+          >
+            {children}
+          </Text>
+        </motion.div>
+      </motion.div>
+    </Card>
+  )
+}
+
+export default ExpandableInfo
