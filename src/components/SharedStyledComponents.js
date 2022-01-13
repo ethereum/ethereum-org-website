9ab267fda1658fca4cd01576297d23aa37c4a1b0
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-01-13 14:20:33 -0800

    Removes unused deprecated components

--- src/components/SharedStyledComponents.js
+++ src/components/SharedStyledComponents.js
@@ -229,31 +229,6 @@ export const ButtonSecondary = styled(Button)`
   }
 `
 
-export const Eth2Header = styled.h2`
-  font-style: normal;
-  font-weight: normal;
-  font-weight: 800;
-  font-size: 48px;
-  line-height: 120%;
-  max-width: 640px; // TODO refactor to remove this
-  margin-bottom: 0rem;
-  color: ${(props) => props.theme.colors.text00};
-  @media (max-width: ${(props) => props.theme.breakpoints.l}) {
-    font-size: 40px;
-  }
-`
-
-export const Eth2HeaderGradient = styled.span`
-  background-clip: text;
-  background-image: ${(props) => props.theme.colors.upgradesGradient};
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  @media (max-width: ${(props) => props.theme.breakpoints.s}) {
-    /* Avoid cutoff on mobile */
-    display: inline-block;
-  }
-`
-
 // MDX components
 
 export const Paragraph = styled.p`
