0796904e7fe860d01d21cc1668397cbc5b44ce99
Corwin Smith <cssmittys@gmail.com>
2022-02-27 17:38:28 -0700

    feat: RollupProductDevDoc component

--- src/components/RollupProductDevDoc.js
+++ src/components/RollupProductDevDoc.js
@@ -1,9 +1,30 @@
 // Libraries
 import React from "react"
+import styled from "styled-components"
+
+// Components
+import Link from "./Link"
 
 // Data
 import optimisticRollupList from "../data/layer-2-data-files/optimistic-rollups.json"
 import zkRollupList from "../data/layer-2-data-files/zk-rollups.json"
+
+// Styles
+const H3 = styled.h3`
+  margin-top: 0;
+  margin-bottom: 1rem;
+`
+
+const ProductCard = styled.div`
+  margin: 1rem 0;
+  background: ${(props) => props.theme.colors.searchBackground};
+  display: flex;
+  flex-direction: row;
+`
+
+const Content = styled.div`
+  padding: 1rem;
+`
 
 const RollupProductDevDoc = ({ rollupType }) => {
   const data = {
@@ -11,7 +32,33 @@ const RollupProductDevDoc = ({ rollupType }) => {
     zk: zkRollupList,
   }
 
-  return <p>Hello World</p>
+  return (
+    <div>
+      {data[rollupType].map((rollup) => {
+        return (
+          <ProductCard>
+            <Content>
+              <div>
+                <H3>
+                  {rollup.name}
+                  {rollup.note.length > 0 && ` (${rollup.note})`}
+                </H3>
+                <div>
+                  <Link to={rollup.website}>Website</Link>
+                </div>
+                <div>
+                  <Link to={rollup.developerDocs}>Developer docs</Link>
+                </div>
+                <div>
+                  <Link to={rollup.l2beat}>L2BEAT</Link>
+                </div>
+              </div>
+            </Content>
+          </ProductCard>
+        )
+      })}
+    </div>
+  )
 }
 
 export default RollupProductDevDoc
