53c1512909f8a30a823b2afa9501cfde6c33c2d7
Joshua <30259508@cityofglacol.ac.uk>
2022-03-23 17:26:17 +0000

    Test 404 translation

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,8 @@ import React from "react"
 import styled from "styled-components"
 
 import Link from "../components/Link"
+import Translation from "../components/Translation"
+
 import { Page, Content } from "../components/SharedStyledComponents"
 
 const StyledPage = styled(Page)`
@@ -12,10 +14,12 @@ const StyledPage = styled(Page)`
 const NotFoundPage = () => (
   <StyledPage>
     <Content>
-      <h1>Resource not found.</h1>
+      <h1>
+        <Translation id="about-us" />
+      </h1>
       <p>
         Try using search to find what you're looking for or{" "}
-        <Link to="/en/">return home</Link>.
+        <Link to="/">return home</Link>.
       </p>
     </Content>
   </StyledPage>
