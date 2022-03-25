c4ebf4ba504f50476a77b87e7b196ea5a7539fbf
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-03-24 17:57:56 -0700

    Create LaunchpadWidget.js

---
+++ src/components/LaunchpadWidget.js
@@ -0,0 +1,59 @@
+import React from "react"
+import styled from "styled-components"
+
+import Link from "./Link"
+import ButtonLink from "./ButtonLink"
+import Emoji from "./Emoji"
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const LaunchpadWidget = () => {
+  return (
+    <Container>
+      <h2>Get started on the Staking Launchpad</h2>
+      <p>
+        The Staking Launchpad is an open source application that will help you
+        become a validator. It walks you through a preparation checklist, how to
+        choose clients, generate your keys and assists with depositing your ETH
+        to the staking deposit contract.
+      </p>
+      <div>
+        <p>Choose network</p>
+        <select>
+          <option value="prater">Goerli/Prater Testnet</option>
+          <option value="mainnet">Mainnet</option>
+        </select>
+        <p>
+          <Emoji text="🧪" mr="1rem" /> Solo validators are expected to{" "}
+          <strong>test their setup</strong> and operational skills on the prater
+          testnet before risking funds. Remember it is important to choose a{" "}
+          <Link to="/client-diversity">minority client</Link> as it improves the
+          security of the network, and limits your risk.
+        </p>
+        <p>
+          <code>
+            If you're comfortable with it, you can set up everything needed from
+            the command line using the Staking Launchpad alone.
+          </code>
+        </p>
+        <p>
+          <Emoji text="🛠" mr="1rem" /> To make things easier, check out some of
+          the tools and guides below that can help you alongside the Staking
+          Launchpad to get your clients set up with ease.
+        </p>
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
+          <ButtonLink to="https://prater.launchpad.ethereum.org">
+            Start staking
+          </ButtonLink>
+
+          <ButtonLink to="#tools">Software tools and guide</ButtonLink>
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+export default LaunchpadWidget
