1a8d51c9f0f37e9026f6e5e20b6b93ee36f67294
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-03-31 20:32:42 -0700

    Fix desktop alignment

--- src/components/StakingHierarchy.js
+++ src/components/StakingHierarchy.js
@@ -133,16 +133,17 @@ const Header = styled.div`
   gap: 0.5rem;
   h2 {
     margin: 0;
-    text-align: center;
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
     align-items: center;
+    h2 {
+      text-align: center;
+    }
   }
 `
 
 const Pills = styled.div`
   display: flex;
-  justify-content: center;
   flex-wrap: wrap;
   gap: 0.25rem;
   p {
