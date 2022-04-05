0448394f6a94e24e41cbbb174343fa3e234bb833
Joshua <30259508@cityofglacol.ac.uk>
2022-04-05 20:22:06 +0100

    Rename function, move into translations utils

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -69,7 +69,7 @@ const translateMessageId = (id, intl) => {
 
 // Overwrites the default Persian numbering of the Farsi language to use Hindu-Arabic numerals (0-9)
 // Context: https://github.com/ethereum/ethereum-org-website/pull/5490#pullrequestreview-892596553
-export const getLocaleForNumberFormat = (locale) => {
+const getLocaleForNumberFormat = (locale) => {
   if (locale === "fa") {
     return "en"
   }
