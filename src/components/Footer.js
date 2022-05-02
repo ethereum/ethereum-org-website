5d63a8f6eafe543a9cf06b9de53c3f5e83f34e89
Joshua <30259508@cityofglacol.ac.uk>
2022-05-02 10:33:16 +0100

    Add link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -260,6 +260,10 @@ const Footer = () => {
           text: "esp",
         },
         {
+          to: "/upgrades/get-involved/bug-bounty/",
+          text: "ethereum-bug-bounty",
+        },
+        {
           to: "/community/grants",
           text: "grant-programs",
         },
