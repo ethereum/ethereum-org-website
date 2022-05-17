5f777862844a887227a54b24f8971c8706e9215c
Pablo Pettinari <pettinarip@gmail.com>
2022-05-17 11:34:25 -0300

    avoid loading matomo in preview deploys

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,13 @@ const ignoreContent = (process.env.IGNORE_CONTENT || "")
   .split(",")
   .filter(Boolean)
 
+const isPreviewDelpoy = process.env.GATSBY_IS_PREVIEW === "true"
+
 const ignoreTranslations = Object.keys(allLanguages)
   .filter((lang) => !supportedLanguages.includes(lang))
   .map((lang) => `**/translations\/${lang}`)
 
-module.exports = {
+const config = {
   siteMetadata: {
     // `title` & `description` pulls from respective ${lang}.json files in PageMetadata.js
     title: `ethereum.org`,
@@ -52,19 +54,6 @@ module.exports = {
         theme_color: `#222222`,
         display: `standalone`,
         icon: `src/assets/favicon.png`,
-      },
-    },
-    // Matomo analtyics
-    {
-      resolve: "gatsby-plugin-matomo",
-      options: {
-        siteId: "4",
-        matomoUrl: "https://matomo.ethereum.org",
-        siteUrl,
-        matomoPhpScript: "matomo.php",
-        matomoJsScript: "matomo.js",
-        trackLoad: false,
-        // dev: true,
       },
     },
     // Sitemap generator (ethereum.org/sitemap.xml)
@@ -240,3 +229,25 @@ module.exports = {
     FAST_DEV: true, // DEV_SSR, QUERY_ON_DEMAND & LAZY_IMAGES
   },
 }
+
+// Avoid loading Matomo in preview deploys since NODE_ENV is `production` in
+// there and it will send testing data as production otherwise
+if (!isPreviewDelpoy) {
+  config.plugins = [
+    ...config.plugins,
+    // Matomo analtyics
+    {
+      resolve: "gatsby-plugin-matomo",
+      options: {
+        siteId: "4",
+        matomoUrl: "https://matomo.ethereum.org",
+        siteUrl,
+        matomoPhpScript: "matomo.php",
+        matomoJsScript: "matomo.js",
+        trackLoad: false,
+      },
+    },
+  ]
+}
+
+module.exports = config
