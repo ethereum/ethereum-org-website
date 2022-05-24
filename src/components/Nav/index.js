099cb9a3bb030e5cf56327e111e086ef804b1ad5
Sam Richards <sbrichards@gmail.com>
2022-05-24 10:11:13 +0200

    Add nav link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -185,6 +185,10 @@ const Nav = ({ handleThemeChange, isDarkTheme, path }) => {
           to: "/dao/",
         },
         {
+          text: "desci-page",
+          to: "/desci/",
+        },
+        {
           text: "page-stablecoins-title",
           to: "/stablecoins/",
         },
