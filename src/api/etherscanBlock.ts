e5914c4bc430b7970bde47befd9fdfbd934886f4
William <WilliamWelsh@users.noreply.github.com>
2022-05-29 19:56:04 -0400

    Migrate src/api js files to ts

--- src/api/etherscanBlock.ts
+++ src/api/etherscanBlock.ts
@@ -1,6 +1,11 @@
-import { lambda } from "../lambda/etherscan"
+import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby"
 
-async function handler(__req, res) {
+import { lambda } from "../lambda/etherscanBlock"
+
+async function handler(
+  __req: GatsbyFunctionRequest,
+  res: GatsbyFunctionResponse
+) {
   // passing env vars as arguments due to a bug on GC functions where env vars
   // can not be accessed by imported functions
   const { statusCode, body } = await lambda(process.env.ETHERSCAN_API_KEY)
