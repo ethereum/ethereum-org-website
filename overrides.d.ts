4686b17df311a302ca801afd7a10278caf08ff41
dusartvict <dusartvict@eisti.eu>
2022-06-02 23:54:19 +0200

    Convert DeveloperDocsLinks file

--- overrides.d.ts
+++ overrides.d.ts
@@ -2,3 +2,8 @@
 // errors.
 // TODO: create issue in Gatsby repo
 declare module "gatsby-plugin-image"
+
+declare module "*.yaml" {
+  const content: { [key: string]: any }
+  export default content
+}
