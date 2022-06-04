9c6cce04c00ce2aad76c010c48b3cc5d0d65db22
Pablo Pettinari <pettinarip@gmail.com>
2022-06-04 15:51:27 -0300

    install react-icons/all-files to improve performance

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,32 +1,34 @@
 import React from "react"
-import PropTypes from "prop-types"
 import styled from "styled-components"
-import { IconContext } from "react-icons"
-import {
-  FaGithub,
-  FaTwitter,
-  FaYoutube,
-  FaDiscord,
-  FaRedditAlien,
-  FaStackExchange,
-  FaGlobe,
-} from "react-icons/fa"
-import {
-  MdAdd,
-  MdBrightness2,
-  MdClose,
-  MdExpandMore,
-  MdArrowForward,
-  MdInfoOutline,
-  MdLanguage,
-  MdMenu,
-  MdSearch,
-  MdWbSunny,
-  MdFlip,
-  MdLiveHelp,
-} from "react-icons/md"
-import { BsQuestionSquareFill, BsToggleOff, BsToggleOn } from "react-icons/bs"
-import { IoCodeOutline, IoCodeDownload } from "react-icons/io5"
+import { IconContext } from "@react-icons/all-files"
+
+import { FaGithub } from "@react-icons/all-files/fa/FaGithub"
+import { FaTwitter } from "@react-icons/all-files/fa/FaTwitter"
+import { FaYoutube } from "@react-icons/all-files/fa/FaYoutube"
+import { FaDiscord } from "@react-icons/all-files/fa/FaDiscord"
+import { FaRedditAlien } from "@react-icons/all-files/fa/FaRedditAlien"
+import { FaStackExchange } from "@react-icons/all-files/fa/FaStackExchange"
+import { FaGlobe } from "@react-icons/all-files/fa/FaGlobe"
+
+import { MdAdd } from "@react-icons/all-files/md/MdAdd"
+import { MdBrightness2 } from "@react-icons/all-files/md/MdBrightness2"
+import { MdClose } from "@react-icons/all-files/md/MdClose"
+import { MdExpandMore } from "@react-icons/all-files/md/MdExpandMore"
+import { MdArrowForward } from "@react-icons/all-files/md/MdArrowForward"
+import { MdInfoOutline } from "@react-icons/all-files/md/MdInfoOutline"
+import { MdLanguage } from "@react-icons/all-files/md/MdLanguage"
+import { MdMenu } from "@react-icons/all-files/md/MdMenu"
+import { MdSearch } from "@react-icons/all-files/md/MdSearch"
+import { MdWbSunny } from "@react-icons/all-files/md/MdWbSunny"
+import { MdFlip } from "@react-icons/all-files/md/MdFlip"
+import { MdLiveHelp } from "@react-icons/all-files/md/MdLiveHelp"
+
+import { BsQuestionSquareFill } from "@react-icons/all-files/bs/BsQuestionSquareFill"
+import { BsToggleOff } from "@react-icons/all-files/bs/BsToggleOff"
+import { BsToggleOn } from "@react-icons/all-files/bs/BsToggleOn"
+
+import { IoCodeOutline } from "@react-icons/all-files/io5/IoCodeOutline"
+import { IoCodeDownload } from "@react-icons/all-files/io5/IoCodeDownload"
 
 const socialColors = {
   reddit: "#ff4301",
