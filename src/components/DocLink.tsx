688e5fdb9863100e1dddcbd16851231eccb4cdee
Pablo Pettinari <pettinarip@gmail.com>
2022-06-30 13:42:44 -0300

    refactor doclink

--- src/components/DocLink.tsx
+++ src/components/DocLink.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { useTheme } from "styled-components"
 import Icon from "./Icon"
 import Link from "./Link"
 import Emoji from "./Emoji"
@@ -10,7 +10,7 @@ export interface IProps {
   isExternal?: boolean
 }
 
-const Container = styled(Link)`
+const Container = styled(Link)<{ isExternal: boolean }>`
   position: relative;
   z-index: 1;
   text-decoration: none;
@@ -52,7 +52,7 @@ const Title = styled.p`
   margin: 0;
 `
 
-const Arrow = styled(Icon)`
+const Arrow = styled(Icon)<{ isExternal: boolean }>`
   margin: 0rem 1.5rem;
   align-self: center;
   min-width: 2rem;
@@ -64,20 +64,29 @@ const EmojiCell = styled.div`
   align-items: center;
 `
 
-const DocLink: React.FC<IProps> = ({ to, children, className, isExternal }) => (
-  <Container to={to} className={className} isExternal={isExternal} hideArrow>
-    <EmojiCell>
-      <Emoji size={1} text=":page_with_curl:" mr={`1rem`} />
-    </EmojiCell>
-    <TextCell>
-      <Title>{children}</Title>
-    </TextCell>
-    <Arrow
-      isExternal={isExternal}
-      name="arrowRight"
-      color={({ theme }) => theme.colors.text}
-    />
-  </Container>
-)
+const DocLink: React.FC<IProps> = ({
+  to,
+  children,
+  className,
+  isExternal = false,
+}) => {
+  const theme = useTheme()
+
+  return (
+    <Container to={to} className={className} isExternal={isExternal} hideArrow>
+      <EmojiCell>
+        <Emoji size={1} text=":page_with_curl:" mr={`1rem`} />
+      </EmojiCell>
+      <TextCell>
+        <Title>{children}</Title>
+      </TextCell>
+      <Arrow
+        isExternal={isExternal}
+        color={theme.colors.text}
+        name="arrowRight"
+      />
+    </Container>
+  )
+}
 
 export default DocLink
