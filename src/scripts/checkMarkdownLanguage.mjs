ef5b2b51926fde3664315392b5f98ff727905f53
SNikhill <snikhill@protonmail.com>
2022-07-02 15:58:47 +0530

    chore: get MD files with language
    Add a method to
    get all the markdown
    files with language

--- src/scripts/checkMarkdownLanguage.mjs
+++ src/scripts/checkMarkdownLanguage.mjs
@@ -1,22 +1,49 @@
 import { spawn } from "child_process"
 
-let allMarkdownFilesBuffer = ""
+const commonDiffCommand = [
+  "diff",
+  "--cached",
+  "--name-only",
+  "--diff-filter",
+  "AM",
+]
+
+const getMarkdownFilesWithLanguage = () => {
+  return new Promise((resolve, reject) => {
+    const markdownFilesWithLanguageCommand = spawn("git", [
+      ...commonDiffCommand,
+      "-G",
+      "lang:",
+      "--",
+      "*.md",
+    ])
+
+    markdownFilesWithLanguageCommand.stdout.on("data", (data) => {
+      console.log(data.toString("utf8"))
+      resolve(data)
+    })
+
+    markdownFilesWithLanguageCommand.stderr.on("data", (data) => {
+      reject(data)
+    })
+
+    markdownFilesWithLanguageCommand.on("error", (error) => {
+      reject(error)
+    })
+  })
+}
 
 const getAllMarkdownFiles = () => {
   return new Promise((resolve, reject) => {
     const allMarkdownFilesCommand = spawn("git", [
-      "diff",
-      "--cached",
-      "--name-only",
-      "--diff-filter",
-      "AM",
+      ...commonDiffCommand,
       "--",
       "*.md",
     ])
 
     allMarkdownFilesCommand.stdout.on("data", (data) => {
       console.log(data.toString("utf8"))
-      allMarkdownFilesBuffer = data
+      resolve(data)
     })
 
     allMarkdownFilesCommand.stderr.on("data", (data) => {
