d381e8418bebcf24d875823273b0c21eb1fc00d7
SNikhill <snikhill@protonmail.com>
2022-07-02 15:54:23 +0530

    chore(allMDFiles): use promises
    Resolve getAllMarkdownFiles
    as promise with
    output from STDOUT

--- src/scripts/checkMarkdownLanguage.mjs
+++ src/scripts/checkMarkdownLanguage.mjs
@@ -3,28 +3,30 @@ import { spawn } from "child_process"
 let allMarkdownFilesBuffer = ""
 
 const getAllMarkdownFiles = () => {
-  const allMarkdownFilesCommand = spawn("git", [
-    "diff",
-    "--cached",
-    "--name-only",
-    "--diff-filter",
-    "AM",
-    "--",
-    "*.md",
-  ])
+  return new Promise((resolve, reject) => {
+    const allMarkdownFilesCommand = spawn("git", [
+      "diff",
+      "--cached",
+      "--name-only",
+      "--diff-filter",
+      "AM",
+      "--",
+      "*.md",
+    ])
 
-  allMarkdownFilesCommand.stdout.on("data", (data) => {
-    console.log(data.toString("utf8"))
-    allMarkdownFilesBuffer = data
-  })
+    allMarkdownFilesCommand.stdout.on("data", (data) => {
+      console.log(data.toString("utf8"))
+      allMarkdownFilesBuffer = data
+    })
 
-  allMarkdownFilesCommand.stderr.on("data", (data) => {
-    console.log(data.toString("utf8"))
-  })
+    allMarkdownFilesCommand.stderr.on("data", (data) => {
+      reject(data)
+    })
 
-  allMarkdownFilesCommand.on("error", (error) => {
-    console.error("SHITT", error)
+    allMarkdownFilesCommand.on("error", (error) => {
+      reject(error)
+    })
   })
 }
 
-getAllMarkdownFiles()
+await getAllMarkdownFiles()
