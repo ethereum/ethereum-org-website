b01902b80a7a9b93b4c83a4f8f76f8485b0ba54b
Pablo Pettinari <pettinarip@gmail.com>
2022-07-14 14:20:44 -0300

    revert changes on Translation component since we are not upgrading react-intl

--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-import { FormattedMessage } from "react-intl"
+import { FormattedHTMLMessage } from "react-intl"
 import { getDefaultMessage, TranslationKey } from "../utils/translations"
 
-// Wrapper on <FormattedMessage /> to always fallback to English
+// Wrapper on <FormattedHTMLMessage /> to always fallback to English
 // Use this component for any user-facing string
 const Translation = ({ id }: { id: TranslationKey }) => (
-  <FormattedMessage id={id} defaultMessage={getDefaultMessage(id)} />
+  <FormattedHTMLMessage id={id} defaultMessage={getDefaultMessage(id)} />
 )
 
 export default Translation
