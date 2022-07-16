57f6412e5012ba8ded9fd0f6847d76a89fc4164c
Joshua <30259508@cityofglacol.ac.uk>
2022-07-16 21:39:38 +0100

    Refactor functions

--- src/scripts/markdown-checker.js
+++ src/scripts/markdown-checker.js
@@ -9,6 +9,26 @@ const TUTORIAL_DATE_REGEX = new RegExp("\\d{4}-\\d{2}-\\d{2}")
 const langsArray = fs.readdirSync(PATH_TO_INTL_MARKDOWN)
 langsArray.push("en")
 
+function getAllMarkdownPaths(dirPath, arrayOfFiles = []) {
+  let files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllMarkdownPaths(dirPath + "/" + file, arrayOfFiles)
+    } else {
+      const fileToPush = path.join(dirPath, "/", file)
+
+      if (fileToPush.includes(".md")) {
+        arrayOfFiles.push(fileToPush)
+      }
+    }
+  })
+
+  return arrayOfFiles
+}
+
 function sortMarkdownPathsIntoLanguages(files) {
   const languages = langsArray.reduce((accumulator, value) => {
     return { ...accumulator, [value]: [] }
@@ -30,119 +50,55 @@ function sortMarkdownPathsIntoLanguages(files) {
   return languages
 }
 
-function getAllMarkdownPaths(dirPath, arrayOfFiles = []) {
-  let files = fs.readdirSync(dirPath)
-
-  arrayOfFiles = arrayOfFiles || []
-
-  files.forEach(function (file) {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllMarkdownPaths(dirPath + "/" + file, arrayOfFiles)
-    } else {
-      const fileToPush = path.join(dirPath, "/", file)
-
-      if (fileToPush.includes(".md")) {
-        arrayOfFiles.push(fileToPush)
-      }
-    }
-  })
-
-  return arrayOfFiles
-}
-
-function processFrontmatter(md, lang) {
-  const file = fs.readFileSync(md, "utf-8")
+function processFrontmatterAndLogErrors(path, lang) {
+  const file = fs.readFileSync(path, "utf-8")
   const frontmatter = matter(file).data
 
-  // console.log(frontmatter)
-  // if (lang === "fr") console.log(frontmatter.title)
-
-  if (frontmatter.lang !== lang) {
-    console.log(md)
+  if (!frontmatter.title) {
+    console.warn(`Missing 'title' frontmatter at ${path}.`)
   }
-}
-
-function processFrontmatter(md, lang) {
-  const file = fs.readFileSync(md, "utf-8")
-  const frontmatter = matter(file).data
-
-  if (!frontMatterHasTitle(frontmatter)) {
-    console.warn(
-      `Missing title: the file at ${md} does not have the frontmatter prop 'title'`
-    )
-  }
-
-  // Commented out as there are many 'blanks' we need to fix
-  // if (!frontMatterHasDescription(frontmatter)) {
-  //   console.warn(
-  //     `Missing description: the file at ${md} does not have the frontmatter prop 'description'`
-  //   )
+  // Description commented out as there are a lot of them missing :-)!
+  // if (!frontmatter.description) {
+  //   console.warn(`Missing 'description' frontmatter at ${path}.`)
   // }
-
-  if (!frontMatterHasLang(frontmatter)) {
+  if (!frontmatter.lang) {
+    console.log(`Missing 'lang' frontmatter at ${path}. Expected: ${lang}.'`)
+  } else if (!(frontmatter.lang === lang)) {
     console.warn(
-      `Missing lang: the file at ${md} does not have the frontmatter prop of 'lang: ${lang}'`
+      `Invalid 'lang' frontmatter at ${path}. Expected: ${lang}'. Received: ${frontmatter.lang}.`
     )
   }
 
-  if (
-    frontMatterHasLang(frontmatter) &&
-    !frontMatterHasCorrectLang(frontmatter, lang)
-  ) {
-    console.warn(
-      `Incorrect lang: ${lang} the file at ${md} does not have the expected frontmatter prop of 'lang: ${lang}'. Lang provided: ${frontmatter.lang}`
-    )
-  }
+  if (path.includes("/tutorials/")) {
+    if (!frontmatter.published) {
+      console.warn(`Missing 'published' frontmatter at ${path}.`)
+    } else {
+      try {
+        let stringDate = frontmatter.published.toISOString().slice(0, 10)
+        const dateIsFormattedCorrectly = TUTORIAL_DATE_REGEX.test(stringDate)
 
-  if (md.includes("/tutorials/")) {
-    if (!tutorialFrontmatterHasPublished(frontmatter)) {
-      console.warn(
-        `Missing date: the file at ${md} does not have the frontmatter prop of 'published:'`
-      )
-    }
-    try {
-      let stringDate = frontmatter.published.toISOString().slice(0, 10)
-      const dateIsFormattedCorrectly = TUTORIAL_DATE_REGEX.test(stringDate)
-
-      if (!dateIsFormattedCorrectly) {
+        if (!dateIsFormattedCorrectly) {
+          console.log(
+            `Invalid 'published' frontmatter at ${path}. Expected: 'YYYY-MM-DD' Received: ${frontmatter.published}`
+          )
+        }
+      } catch (e) {
         console.log(
-          `Incorrect date format: the file at ${md}, does not have a correctly formatted frontmatter prop of 'published'. Date provided: ${frontmatter.published}`
+          `Invalid 'published' frontmatter at ${path}. Expected: 'YYYY-MM-DD' Received: ${frontmatter.published}`
         )
       }
-    } catch (e) {
-      console.log(
-        `Incorrect date format: the file at ${md}, does not have a correctly formatted frontmatter prop of 'published'. Date provided: ${frontmatter.published}`
-      )
     }
   }
 }
+function checkMarkdown() {
+  const markdownPaths = getAllMarkdownPaths(PATH_TO_ALL_CONTENT)
+  const markdownPathsByLang = sortMarkdownPathsIntoLanguages(markdownPaths)
 
-const markdownPaths = getAllMarkdownPaths(PATH_TO_ALL_CONTENT)
-const markdownPathsByLang = sortMarkdownPathsIntoLanguages(markdownPaths)
-
-for (const lang in markdownPathsByLang) {
-  markdownPathsByLang[lang].forEach((file) => {
-    processFrontmatter(file, lang)
-  })
+  for (const lang in markdownPathsByLang) {
+    markdownPathsByLang[lang].forEach((file) => {
+      processFrontmatterAndLogErrors(file, lang)
+    })
+  }
 }
 
-/// Would it be useful to Luca to quickly get all page titles for a lang?
-
-function frontMatterHasTitle(frontmatter) {
-  return frontmatter.title
-}
-
-function frontMatterHasDescription(frontmatter) {
-  return frontmatter.description
-}
-
-function frontMatterHasLang(frontmatter) {
-  return frontmatter.lang
-}
-function frontMatterHasCorrectLang(frontmatter, lang) {
-  return frontmatter.lang === lang
-}
-
-function tutorialFrontmatterHasPublished(frontmatter) {
-  return frontmatter.published
-}
+checkMarkdown()
