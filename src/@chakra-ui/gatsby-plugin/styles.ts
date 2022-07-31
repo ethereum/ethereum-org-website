000263dd3da0b227e3bf97282c022b3b28c85721
Pablo Pettinari <pettinarip@gmail.com>
2022-07-31 13:00:50 -0300

    override chakra global styles to fix conflicts with old styles

--- src/@chakra-ui/gatsby-plugin/styles.ts
+++ src/@chakra-ui/gatsby-plugin/styles.ts
@@ -1,5 +1,12 @@
 const styles = {
-  global: (props) => ({}),
+  global: (_props) => ({
+    // TODO: remove these overrides as we adopt the new Design System
+    // override default global Chakra styles to keep compatibility with old theme
+    // https://github.com/chakra-ui/chakra-ui/blob/main/packages/theme/src/styles.ts
+    body: {
+      lineHeight: "1.6rem",
+    },
+  }),
 }
 
 export default styles
