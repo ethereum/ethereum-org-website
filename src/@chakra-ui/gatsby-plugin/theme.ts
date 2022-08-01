ae31f43e48c70aa77f5973fcf8d681294dc0473d
Pablo Pettinari <pettinarip@gmail.com>
2022-08-01 11:00:49 -0300

    migrate Link to use chakra

--- src/@chakra-ui/gatsby-plugin/theme.ts
+++ src/@chakra-ui/gatsby-plugin/theme.ts
@@ -10,6 +10,9 @@ import styles from "./styles"
 // Foundational style overrides
 import foundations from "./foundations"
 
+// Component style overrides
+import Link from "./components/Link"
+
 const config: ThemeConfig = {
   cssVarPrefix: "eth",
   initialColorMode: "light",
@@ -20,6 +23,9 @@ const theme: ThemeOverride = {
   config,
   styles,
   ...foundations,
+  components: {
+    Link,
+  },
 }
 
 export default extendTheme(theme)
--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,133 +1,48 @@
 import React from "react"
-import { Link as GatsbyLink, navigate as gatsbyNavigate } from "gatsby"
+import { Icon, Link, LinkProps } from "@chakra-ui/react"
+import { navigate as gatsbyNavigate } from "gatsby"
 import { LocalizedLink as IntlLink } from "gatsby-theme-i18n"
 import { NavigateOptions } from "@reach/router"
 import { IntlShape } from "react-intl"
-import styled from "@emotion/styled"
 
-import Icon from "./Icon"
+import { BsQuestionSquareFill } from "react-icons/bs"
 
-import { isLang, Lang } from "../utils/languages"
+import { Lang } from "../utils/languages"
 import { trackCustomEvent, EventOptions } from "../utils/matomo"
 import { Direction } from "../types"
 
 const HASH_PATTERN = /^#.*/
-// const DOMAIN_PATTERN = /^(?:https?:)?[/]{2,}([^/]+)/
-// const INTERNAL_PATTERN = /^\/(?!\/)/
-// const FILE_PATTERN = /.*[/](.+\.[^/]+?)([/].*?)?([#?].*)?$/
 
 const isHashLink = (to: string): boolean => HASH_PATTERN.test(to)
 
-const ExternalLink = styled.a`
-  &:after {
-    margin-left: 0.125em;
-    margin-right: 0.3em;
-    display: inline;
-    content: "↗";
-    transition: all 0.1s ease-in-out;
-    font-style: normal;
-  }
-  &:hover {
-    &:after {
-      transform: translate(0.15em, -0.2em);
-    }
-  }
-`
-
-const InternalLink = styled(IntlLink)`
-  .is-glossary {
-    white-space: nowrap;
-  }
-  &.active {
-    color: ${(props) => props.theme.colors.primary};
-  }
-  &:hover {
-    svg {
-      fill: ${(props) => props.theme.colors.primary};
-      transition: transform 0.1s;
-      transform: scale(1.2);
-    }
-  }
-`
-
-const ExplicitLangInternalLink = styled(GatsbyLink)`
-  &.active {
-    color: ${(props) => props.theme.colors.primary};
-  }
-`
-
-const GlossaryIcon = styled(Icon)`
-  margin: 0 0.25rem 0 0.35rem;
-  fill: ${(props) => props.theme.colors.primary400};
-  text-decoration: underline;
-  &:hover {
-    transition: transform 0.1s;
-    transform: scale(1.2);
-  }
-`
-
-export interface IProps {
-  to?: string
-  href?: string
+export interface IProps extends LinkProps {
+  to: string
   dir?: Direction
   hideArrow?: boolean
-  className?: string
   isPartiallyActive?: boolean
-  ariaLabel?: string
   customEventOptions?: EventOptions
   onClick?: () => void
 }
 
-const Link: React.FC<IProps> = ({
-  to,
+const LinkWrapper: React.FC<IProps> = ({
   dir = "ltr",
-  href,
+  to,
   children,
   hideArrow = false,
-  className,
   isPartiallyActive = true,
-  ariaLabel,
   customEventOptions,
   onClick = () => {},
+  ...restProps
 }) => {
-  // markdown pages pass `href`, not `to`
-  to = to || href
-
-  if (!to) {
-    throw new Error("Either 'to' or 'href' props must be provided")
-  }
-
   const isExternal = to.includes("http") || to.includes("mailto:")
   const isHash = isHashLink(to)
   const isGlossary = to.includes("glossary") && to.includes("#")
   const isStatic = to.includes("static")
   const isPdf = to.includes(".pdf")
 
-  // Must use <a> tags for anchor links
-  // Otherwise <Link> functionality will navigate to homepage
-  // See https://github.com/gatsbyjs/gatsby/issues/21909
-  if (isHash) {
-    return (
-      <a dir={dir} className={className} href={to} aria-label={ariaLabel}>
-        {children}
-      </a>
-    )
-  }
-
-  // Links to static image assets must use <a> to avoid
-  // <Link> redirection. Opens in separate window.
-  if (isStatic) {
-    return (
-      <a
-        className={className}
-        href={to}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label={ariaLabel}
-      >
-        {children}
-      </a>
-    )
+  const commonProps = {
+    dir,
+    ...restProps,
   }
 
   const eventOptions: EventOptions = {
@@ -136,91 +51,67 @@ const Link: React.FC<IProps> = ({
     eventName: to,
   }
 
-  if (isExternal) {
-    return hideArrow ? (
-      <a
-        dir={dir}
-        className={className}
-        href={to}
-        target="_blank"
-        rel="noopener noreferrer"
-        onClick={(e) => {
-          e.stopPropagation()
-          trackCustomEvent(
-            customEventOptions ? customEventOptions : eventOptions
-          )
-        }}
-        aria-label={ariaLabel}
-      >
-        {children}
-      </a>
-    ) : (
-      <ExternalLink
-        dir={dir}
-        className={className}
-        href={to}
-        target="_blank"
-        rel="noopener noreferrer"
-        onClick={(e) => {
-          e.stopPropagation()
-          trackCustomEvent(
-            customEventOptions ? customEventOptions : eventOptions
-          )
-        }}
-        aria-label={ariaLabel}
-      >
-        {children}
-      </ExternalLink>
-    )
-  }
-
-  // If lang path has been explicitly set, use `gatsby` Link
-  const langPath = to.split("/")[1]
-  if (isLang(langPath)) {
+  // Must use <a> tags for anchor links
+  // Otherwise <Link> functionality will navigate to homepage
+  // See https://github.com/gatsbyjs/gatsby/issues/21909
+  if (isHash) {
     return (
-      <ExplicitLangInternalLink
-        dir={dir}
-        className={className}
-        to={to}
-        activeClassName="active"
-        partiallyActive={isPartiallyActive}
-        onClick={onClick}
-      >
+      <Link href={to} {...commonProps}>
         {children}
-      </ExplicitLangInternalLink>
+      </Link>
     )
   }
 
   // Download link for internally hosted PDF's (ex: whitepaper)
-  if (isPdf && !isExternal) {
+  // Links to static image assets must use <a> to avoid
+  // <Link> redirection. Opens in separate window.
+  if (isExternal || isPdf || isStatic) {
     return (
-      <a
-        dir={dir}
+      <Link
         href={to}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label={ariaLabel}
+        isExternal
+        _after={{
+          content: '"↗"',
+        }}
+        onClick={(e) => {
+          // only track events on external links
+          if (!isExternal) {
+            return
+          }
+
+          e.stopPropagation()
+          trackCustomEvent(
+            customEventOptions ? customEventOptions : eventOptions
+          )
+        }}
+        {...commonProps}
       >
         {children}
-      </a>
+      </Link>
     )
   }
 
   // Use `gatsby-theme-i18n` Link (which prepends lang path)
   return (
-    <InternalLink
-      dir={dir}
-      className={isGlossary ? `is-glossary ${className}` : className}
+    <Link
       to={to}
+      as={IntlLink}
       activeClassName="active"
       partiallyActive={isPartiallyActive}
       onClick={onClick}
+      whiteSpace={isGlossary ? "nowrap" : "normal"}
+      {...commonProps}
     >
       {children}
       {isGlossary && (
-        <GlossaryIcon aria-label="See definition" size="12px" name="glossary" />
+        <Icon
+          as={BsQuestionSquareFill}
+          aria-label="See definition"
+          fontSize="12px"
+          margin="0 0.25rem 0 0.35rem"
+        />
       )}
-    </InternalLink>
+    </Link>
   )
 }
 
@@ -237,4 +128,4 @@ export function navigate(
   gatsbyNavigate(link, options)
 }
 
-export default Link
+export default LinkWrapper
