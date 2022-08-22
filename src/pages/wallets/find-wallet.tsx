8ab8a9c4e587b78f4c8b57863adc7fcf545b177f
Corwin Smith <cssmittys@gmail.com>
2022-08-22 15:02:40 -0600

    add bitcoin.com wallet info

--- src/pages/wallets/find-wallet.tsx
+++ src/pages/wallets/find-wallet.tsx
@@ -732,5 +732,10 @@ export const query = graphql`
         gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, quality: 100)
       }
     }
+    bitcoindotcom: file(relativePath: { eq: "wallets/bitcoindotcom.png" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, quality: 100)
+      }
+    }
   }
 `
