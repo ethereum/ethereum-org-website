c62caa5e907629764ba07bc58276306702a72da0
Pablo Pettinari <pettinarip@gmail.com>
2022-08-25 16:46:44 -0300

    fix red color name

--- src/@chakra-ui/gatsby-plugin/semanticTokens.ts
+++ src/@chakra-ui/gatsby-plugin/semanticTokens.ts
@@ -39,7 +39,7 @@ const semanticTokens = {
     disabled: { _light: "gray.400", _dark: "gray.500" },
     background: { _light: "white", _dark: "gray.700" },
     success: "green.500",
-    error: "red.600",
+    error: "red.500",
     attention: "yellow.200",
   },
 }
