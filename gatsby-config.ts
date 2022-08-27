b0550d73d1185462a151f7841f4f1ce8d7d265bb
Pablo Pettinari <pettinarip@gmail.com>
2022-08-27 01:08:24 -0300

    upgrade react 18

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -258,7 +258,8 @@ const config: GatsbyConfig = {
   ],
   // https://www.gatsbyjs.com/docs/reference/release-notes/v2.28/#feature-flags-in-gatsby-configjs
   flags: {
-    FAST_DEV: true, // DEV_SSR, QUERY_ON_DEMAND & LAZY_IMAGES
+    // FAST_DEV: true, // DEV_SSR, QUERY_ON_DEMAND & LAZY_IMAGES
+    // DEV_SSR: true,
   },
 }
 
