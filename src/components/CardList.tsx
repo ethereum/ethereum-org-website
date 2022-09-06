523ae6254057d4ea6b344b32c8a35974b5c68229
Pablo Pettinari <pettinarip@gmail.com>
2022-09-06 00:11:43 -0300

    use link overlay for a11y

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react"
 import { GatsbyImage } from "gatsby-plugin-image"
-import { Box, Flex } from "@chakra-ui/react"
+import { Box, Flex, LinkBox, LinkOverlay } from "@chakra-ui/react"
 
 import Link from "./Link"
 import { ImageProp } from "../types"
@@ -18,70 +18,60 @@ export interface IProps {
   clickHandler?: (idx: string | number) => void
 }
 
+const CardContainer = (props) => {
+  return (
+    <Flex
+      p={4}
+      justify="space-between"
+      color="text"
+      border="1px solid"
+      borderColor="border"
+      _hover={{
+        borderRadius: "base",
+        boxShadow: "0 0 1px var(--eth-colors-primary)",
+        background: "tableBackgroundHover",
+      }}
+      {...props}
+    />
+  )
+}
+
 const CardList: React.FC<IProps> = ({ content, clickHandler = () => null }) => (
   <Box bg="background" width="full">
     {content.map((listItem, idx) => {
       const { title, description, caption, link, image, alt, id } = listItem
       const isLink = !!link
       return isLink ? (
-        <Link
-          key={id || idx}
-          to={link}
-          textDecoration="none"
-          display="flex"
-          justifyContent="space-between"
-          p={4}
-          color="text"
-          border="1px solid"
-          borderColor="border"
-          _hover={{
-            borderRadius: "base",
-            boxShadow: "0 0 1px var(--eth-colors-primary)",
-            background: "tableBackgroundHover",
-          }}
-        >
-          {image && (
-            <Box
-              as={GatsbyImage}
-              image={image}
-              alt={alt}
-              minW="20px"
-              mr={4}
-              mt={1}
-            />
-          )}
-          <Flex flex="1 1 75%" direction="column" mr={8}>
-            <Box>{title}</Box>
+        <LinkBox key={id || idx}>
+          <CardContainer>
+            {image && (
+              <Box
+                as={GatsbyImage}
+                image={image}
+                alt={alt}
+                minW="20px"
+                mr={4}
+                mt={1}
+              />
+            )}
+            <Flex flex="1 1 75%" direction="column" mr={8}>
+              <LinkOverlay href={link}>{title}</LinkOverlay>
 
-            <Box fontSize="sm" mb={0} opacity={0.6}>
-              {description}
-            </Box>
-          </Flex>
-          {caption && (
-            <Flex flex="1 0 25%" align="center" wrap="wrap" mr={4}>
               <Box fontSize="sm" mb={0} opacity={0.6}>
-                {caption}
+                {description}
               </Box>
             </Flex>
-          )}
-        </Link>
+            {caption && (
+              <Flex flex="1 0 25%" align="center" wrap="wrap" mr={4}>
+                <Box fontSize="sm" mb={0} opacity={0.6}>
+                  {caption}
+                </Box>
+              </Flex>
+            )}
+          </CardContainer>
+        </LinkBox>
       ) : (
-        <Flex
-          key={idx}
-          onClick={() => clickHandler(idx)}
-          textDecoration="none"
-          justify="space-between"
-          p={4}
-          mb={4}
-          color="text"
-          border="1px solid"
-          borderColor="border"
-          _hover={{
-            borderRadius: "base",
-            boxShadow: "0 0 1px var(--eth-colors-primary)",
-            background: "tableBackgroundHover",
-          }}
-        >
+        <CardContainer key={idx} onClick={() => clickHandler(idx)} mb={4}>
           {image && (
             <Box
               as={GatsbyImage}
@@ -106,7 +96,7 @@ const CardList: React.FC<IProps> = ({ content, clickHandler = () => null }) => (
               </Box>
             </Flex>
           )}
-        </Flex>
+        </CardContainer>
       )
     })}
   </Box>
