5dfac6a582a80401fe3c18344ba2f0880c6eaa0c
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2022-09-10 15:56:55 +0530

    Refactor HorizontalCard component to ChakraUI

--- src/components/HorizontalCard.tsx
+++ src/components/HorizontalCard.tsx
@@ -2,26 +2,6 @@ import React, { ReactNode } from "react"
 import Emoji from "./OldEmoji"
 import { Text, Box } from "@chakra-ui/react"
 
-// const StyledCard = styled.div`
-//   border-radius: 4px;
-//   display: flex;
-// `
-
-// const Content = styled.div`
-//   flex: 0 1 75%;
-//   margin-left: 2rem;
-// `
-
-// const Description = styled.p`
-//   opacity: 0.8;
-//   margin-top: -1rem;
-//   margin-bottom: 0.5rem;
-// `
-
-// const Title = styled.p`
-//   font-size: 1.25rem;
-// `
-
 export interface IProps {
   children?: React.ReactNode
   emoji: string
