c3edd766c55e83e4dae0d774af6ac187757dbc81
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-09-13 17:24:09 +0200

    remove unused useEffect import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import { useIntl } from "react-intl"
 import { graphql, PageProps } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
