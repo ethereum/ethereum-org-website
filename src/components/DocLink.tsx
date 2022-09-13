2534b5e966856353a41f8418309a554982cdee10
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-09-13 06:53:33 -0500

    Swapped Link component with Chakra components

--- src/components/DocLink.tsx
+++ src/components/DocLink.tsx
@@ -1,8 +1,15 @@
 import React from "react"
 import { useTheme } from "@emotion/react"
-import { Icon, Box, Container, Text, Flex } from "@chakra-ui/react"
+import {
+  Icon,
+  Box,
+  Container,
+  Text,
+  Flex,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react"
 import { AiOutlineArrowRight } from "react-icons/ai"
-import Link from "./Link"
 import Emoji from "./Emoji"
 
 export interface IProps {
@@ -15,52 +22,55 @@ const DocLink: React.FC<IProps> = ({ to, children, isExternal = false }) => {
   const theme = useTheme()
 
   return (
-    <Link to={to} isExternal={isExternal} textDecoration="none" hideArrow>
-      <Container
-        position="relative"
-        zIndex={1}
-        textDecoration="none"
-        display="flex"
-        flexDirection="row"
-        flex={1}
-        width="100%"
-        justifyContent="space-between"
-        padding={4}
-        borderRadius="sm"
-        color="text"
-        border="1px"
-        borderStyle="solid"
-        borderColor="border"
-        _hover={{
-          textDecoration: "none",
-          background: "tableBackgroundHover",
-          borderRadius: "base",
-        }}
-        role="group"
-      >
-        <Flex align="center">
-          <Emoji fontSize="md" mr={4} text=":page_with_curl:" />
-        </Flex>
-        <Box flex={1} flexDirection="column" color="text">
-          <Text color="text300" fontWeight="semibold" margin={0}>
-            {children}
-          </Text>
-        </Box>
-        <Icon
-          as={AiOutlineArrowRight}
+    <LinkBox>
+      <LinkOverlay href={to} isExternal={isExternal}>
+        <Container
+          position="relative"
+          zIndex={1}
+          textDecoration="none"
+          display="flex"
+          flexDirection="row"
+          flex={1}
+          width="100%"
+          justifyContent="space-between"
+          padding={4}
+          borderRadius="sm"
           color="text"
-          alignSelf="center"
-          minWidth="2rem"
-          boxSize={6}
-          marginX={6}
-          _groupHover={{
-            fill: "primary",
-            transition: "transform 0.1s",
-            transform: "scale(1.2)",
+          border="1px"
+          borderStyle="solid"
+          borderColor="border"
+          _hover={{
+            textDecoration: "none",
+            background: "tableBackgroundHover",
+            borderRadius: "base",
           }}
-        />
-      </Container>
-    </Link>
+          role="group"
+        >
+          <Flex align="center">
+            <Emoji fontSize="md" mr={4} text=":page_with_curl:" />
+          </Flex>
+          <Box flex={1} flexDirection="column" color="text">
+            <Text color="text300" fontWeight="semibold" margin={0}>
+              {children}
+            </Text>
+          </Box>
+          <Icon
+            as={AiOutlineArrowRight}
+            color="text"
+            alignSelf="center"
+            minWidth="2rem"
+            boxSize={6}
+            marginX={6}
+            _groupHover={{
+              fill: "primary",
+              transition: "transform 0.1s",
+              transform: "scale(1.2)",
+              rotate: isExternal ? "-45deg" : "0",
+            }}
+          />
+        </Container>
+      </LinkOverlay>
+    </LinkBox>
   )
 }
 
