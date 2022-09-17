25c6c71442965205e964925e53ede74d6508f742
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2022-09-17 19:51:42 +0530

    Refactored HorizontalCard

--- src/components/HorizontalCard.tsx
+++ src/components/HorizontalCard.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react"
 import Emoji from "./Emoji"
-import { Text, Box } from "@chakra-ui/react"
+import { Text, Box, Flex } from "@chakra-ui/react"
 
 export interface IProps {
   children?: React.ReactNode
@@ -17,21 +17,14 @@ const HorizontalCard: React.FC<IProps> = ({
   className,
   emojiSize,
 }) => (
-  <Box
-    display="flex"
-    flexGrow="0"
-    flexShrink="1"
-    flexBasis="75%"
-    borderRadius="base"
-    className={className}
-  >
+  <Flex borderRadius="base" className={className}>
     <Emoji fontSize={emojiSize} text={emoji} />
-    <Box display="flex" ml="8">
+    <Text display="flex" flexGrow="0" flexShrink="1" flexBasis="75%" ml="8">
       <Text fontSize="lg" />
-      <Box as="p" p="0.8" mt="-4" mb="2" />
+      <Box as="p" opacity="0.8" mt="-4" mb="2" />
       {children}
-    </Box>
-  </Box>
+    </Text>
+  </Flex>
 )
 
 export default HorizontalCard
