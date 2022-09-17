80ac06c6ab58e312d2f92ab56bd84eb6adbacb13
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-09-17 19:31:02 +0200

    fix: NakedButton styling adjustments

--- src/components/NakedButton.tsx
+++ src/components/NakedButton.tsx
@@ -4,13 +4,17 @@ import { Button, ButtonProps } from "@chakra-ui/react"
 const NakedButton: React.FC<ButtonProps> = ({ children, ...props }) => (
   <Button
     appearance="none"
-    bg="none"
+    bg="inherit"
     border="none"
     color="inherit"
     display="inline-block"
     fontFamily="inherit"
     padding="initial"
     cursor="pointer"
+    _hover={{
+      bg: "inherit",
+      color: "inherit",
+    }}
     {...props}
   >
     {children}
