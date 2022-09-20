67733415d1a35aaad563637b3aa57038c078f2e4
ImLeugim <anarchxzx@gmail.com>
2022-09-19 21:14:08 -0700

    refactor: InfoBanner to chakra

--- src/components/InfoBanner.tsx
+++ src/components/InfoBanner.tsx
@@ -1,60 +1,6 @@
 import React from "react"
-import styled from "@emotion/styled"
-import Emoji from "./OldEmoji"
-import { margin } from "styled-system"
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-`
-
-const Banner = styled.div<{
-  shouldCenter: boolean
-  isWarning: boolean
-}>`
-  position: relative;
-  z-index: 1;
-  display: flex;
-  align-items: center;
-  padding: 1.5rem;
-  border-radius: 2px;
-  max-width: ${(props) => (props.shouldCenter ? `55rem` : `100%`)};
-  color: ${(props) => props.theme.colors.black300};
-  background: ${(props) =>
-    props.isWarning
-      ? props.theme.colors.warning
-      : props.theme.colors.infoBanner};
-
-  @media (max-width: ${(props) => props.theme.breakpoints.s}) {
-    flex-direction: column;
-  }
-
-  ${margin}
-`
-
-const StyledEmoji = styled(Emoji)`
-  flex-grow: 0;
-  flex-shrink: 0;
-  margin-right: 1.5rem;
-  @media (max-width: ${(props) => props.theme.breakpoints.s}) {
-    align-self: flex-start;
-    margin-right: 0;
-    margin-bottom: 0.5rem;
-  }
-`
-
-const Content = styled.div<{
-  shouldSpaceBetween: boolean
-}>`
-  display: ${(props) => (props.shouldSpaceBetween ? `flex` : `block`)};
-  align-items: ${(props) => (props.shouldSpaceBetween ? `center` : `auto`)};
-  width: ${(props) => (props.shouldSpaceBetween ? `100%` : `auto`)};
-  justify-content: ${(props) =>
-    props.shouldSpaceBetween ? `space-between` : `auto`};
-  @media (max-width: ${(props) => props.theme.breakpoints.s}) {
-    display: block;
-  }
-`
+import { Flex } from "@chakra-ui/react"
+import Emoji from "./Emoji"
 
 export interface IProps {
   children?: React.ReactNode
@@ -75,17 +21,38 @@ const InfoBanner: React.FC<IProps> = ({
   ...props
 }) => {
   const banner = (
-    <Banner
-      className={className}
-      isWarning={isWarning}
-      shouldCenter={shouldCenter}
+    <Flex
+      alignItems="center"
+      p={6}
+      borderRadius={0.5}
+      maxW={shouldCenter ? "55rem" : "100%"}
+      color="black300"
+      bg={isWarning ? "warning" : "infoBanner"}
+      direction={{ sm: "column" }}
       {...props}
     >
-      {emoji && <StyledEmoji text={emoji} size={2} />}
-      <Content shouldSpaceBetween={shouldSpaceBetween}>{children}</Content>
-    </Banner>
+      {emoji && (
+        <Emoji
+          flexGrow="0"
+          flexShrink="0"
+          mr={{base : 6, sm: 0}}
+          mb={{ sm: 2 }}
+          alignSelf={{ sm: "flex-start" }}
+          text={emoji}
+          fontSize={"2em"}
+        />
+      )}
+      <Flex
+        display={{ base: shouldSpaceBetween ? "flex" : "block", sm: "block" }}
+        alignItems={shouldSpaceBetween ? "center" : "auto"}
+        w={shouldSpaceBetween ? "100%" : "auto"}
+        justifyContent={shouldSpaceBetween ? "space-between" : "auto"}
+      >
+        {children}
+      </Flex>
+    </Flex>
   )
-  return shouldCenter ? <Container>{banner}</Container> : banner
+  return shouldCenter ? <Flex justify="center">{banner}</Flex> : banner
 }
 
 export default InfoBanner
