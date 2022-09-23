27a538a685115b08ceb1e2eb0ef64490cddeb9da
Pablo Pettinari <pettinarip@gmail.com>
2022-09-23 09:53:07 -0300

    fix export of chakra theme components

--- src/@chakra-ui/gatsby-plugin/components/index.ts
+++ src/@chakra-ui/gatsby-plugin/components/index.ts
@@ -1,3 +1,9 @@
-export * from "./Link"
-export * from "./Button"
-export * from "./Tag"
+import { Button } from "./Button"
+import { Link } from "./Link"
+import { Tag } from "./Tag"
+
+export default {
+  Button,
+  Link,
+  Tag,
+}
