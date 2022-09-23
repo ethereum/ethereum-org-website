4b4cb31567bc6aea3779dd4d317737c2e0cb04fb
GiFTED! <gkenny896@gmail.com>
2022-09-23 10:29:21 +0100

    Update: Migrate SocialListItem to Chakra

--- src/components/SocialListItem.tsx
+++ src/components/SocialListItem.tsx
@@ -1,29 +1,12 @@
 // Libraries
 import React from "react"
-import styled from "@emotion/styled"
+import { Flex, Box, chakra } from "@chakra-ui/react"
 
 // Components
 import Icon from "./Icon"
 
 // Styles
-const Item = styled.div`
-  display: flex;
-  width: 100%;
-  padding: 0.5rem 0;
-  align-items: center;
-`
-
-const StyledIcon = styled(Icon)`
-  padding-right: 0.75rem;
-`
-
-const ChildrenContainer = styled.div`
-  font-style: italic;
-
-  > a {
-    font-style: normal;
-  }
-`
+const StyledIcon = chakra(Icon)
 
 export interface IProps {
   children?: React.ReactNode
@@ -31,12 +14,26 @@ export interface IProps {
 }
 const SocialListItem: React.FC<IProps> = ({ children, socialIcon }) => {
   return (
-    <Item>
+    <Flex w="100%" py="0.5rem" px="0" alignItems="center">
       <div>
-        <StyledIcon name={socialIcon} size={"2.5rem"} color={true} />
+        <StyledIcon
+          pr={"0.75em"}
+          name={socialIcon}
+          size={"2.5rem"}
+          color={socialIcon}
+        />
       </div>
-      <ChildrenContainer>{children}</ChildrenContainer>
-    </Item>
+      <Box
+        fontStyle={"italic"}
+        sx={{
+          "> a": {
+            fontStyle: "normal",
+          },
+        }}
+      >
+        {children}
+      </Box>
+    </Flex>
   )
 }
 
