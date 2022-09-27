4dbe4f0e46ee7f4f7fbd9ae65245fb783c0102be
ImLeugim <anarchxzx@gmail.com>
2022-09-26 22:43:59 -0700

    refactor: leaderboard to chakra

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,65 +1,9 @@
 import React from "react"
-import styled from "@emotion/styled"
 import Emoji from "./OldEmoji"
 import Link from "./Link"
 
 import Translation from "./Translation"
-
-const Table = styled.div`
-  background-color: ${(props) => props.theme.colors.background};
-  box-shadow: ${(props) => props.theme.colors.tableBoxShadow};
-  width: 100%;
-  margin-bottom: 2rem;
-`
-
-const Item = styled(Link)`
-  text-decoration: none;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: ${(props) => props.theme.colors.text} !important;
-  box-shadow: 0 1px 1px ${(props) => props.theme.colors.tableItemBoxShadow};
-  margin-bottom: 1px;
-  padding: 1rem;
-  width: 100%;
-  color: #000000;
-  &:hover {
-    text-decoration: none;
-    border-radius: 4px;
-    box-shadow: 0 0 1px ${(props) => props.theme.colors.primary};
-    background: ${(props) => props.theme.colors.tableBackgroundHover};
-  }
-`
-
-const ItemTitle = styled.div``
-
-const ItemDesc = styled.div`
-  font-size: ${(props) => props.theme.fontSizes.s};
-  margin-bottom: 0;
-  opacity: 0.6;
-`
-
-const TextContainer = styled.div`
-  flex: 1 1 75%;
-  display: flex;
-  flex-direction: column;
-  margin-right: 2rem;
-`
-
-const Avatar = styled.img`
-  margin-right: 1rem;
-  height: 40px;
-  width: 40px;
-  border-radius: 50%;
-  @media (max-width: ${(props) => props.theme.breakpoints.xs}) {
-    display: none;
-  }
-`
-
-const ItemNumber = styled.div`
-  margin-right: 1rem;
-  opacity: 0.4;
-`
+import { Box, Avatar, Flex } from "@chakra-ui/react"
 
 const githubUrl = `https://github.com/`
 
@@ -75,7 +19,7 @@ export interface IProps {
 }
 
 const Leaderboard: React.FC<IProps> = ({ content, limit = 100 }) => (
-  <Table>
+  <Box bgColor="background" boxShadow="tableBoxShadow" w="100%" mb={8}>
     {content
       .filter((_, idx) => idx < limit)
       .map((item, idx) => {
@@ -94,21 +38,49 @@ const Leaderboard: React.FC<IProps> = ({ content, limit = 100 }) => (
           emoji = ":3rd_place_medal:"
         }
         return (
-          <Item key={idx} to={hasGitHub ? `${githubUrl}${username}` : "#"}>
-            <ItemNumber>{idx + 1}</ItemNumber>
-            <Avatar src={avatarImg} alt={avatarAlt} />
-            <TextContainer>
-              <ItemTitle>{name}</ItemTitle>
-              <ItemDesc>
+          <Link
+            key={idx}
+            textDecor="none"
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            color="text"
+            boxShadow="0 1px 1px tableItemBoxShadow"
+            mb={0.25}
+            p={4}
+            w="100%"
+            _hover={{
+              textDecor: "none",
+              borderRadius: 0.5,
+              boxShadow: "0 0 1px primary",
+              background: "tableBackgroundHover",
+            }}
+            to={hasGitHub ? `${githubUrl}${username}` : "#"}
+          >
+            <Box mr={4} opacity="0.4">
+              {idx + 1}
+            </Box>
+            <Avatar
+              src={avatarImg}
+              name={avatarAlt}
+              mr={4}
+              h={10}
+              w={10}
+              borderRadius="50%"
+              display={{ base: "none", xs: "block" }}
+            />
+            <Flex flex="1 1 75%" direction="column" mr={8}>
+              <Box>{name}</Box>
+              <Box fontSize="sm" mb={0} opacity="0.6">
                 {score}{" "}
                 <Translation id="page-upgrades-bug-bounty-leaderboard-points" />
-              </ItemDesc>
-            </TextContainer>
+              </Box>
+            </Flex>
             {emoji && <Emoji mr={`2rem`} text={emoji} />}
-          </Item>
+          </Link>
         )
       })}
-  </Table>
+  </Box>
 )
 
 export default Leaderboard
