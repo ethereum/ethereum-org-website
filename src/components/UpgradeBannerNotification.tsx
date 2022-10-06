d510e09e89ceff96091724804b22d5c471ed4468
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2022-10-06 11:46:53 +0500

    migrate component to chakra

--- src/components/UpgradeBannerNotification.tsx
+++ src/components/UpgradeBannerNotification.tsx
@@ -1,33 +1,38 @@
 import React from "react"
 import BannerNotification from "./BannerNotification"
-import Emoji from "./OldEmoji"
+import Emoji from "src/components/Emoji"
 import Link from "./Link"
-import styled from "@emotion/styled"
+import { Flex, chakra, Center } from "@chakra-ui/react"
 
-const StyledBannerNotification = styled(BannerNotification)`
-  display: flex;
-  justify-content: center;
-`
+const StyledBannerNotification = chakra(BannerNotification)
 
+const StyledEmoji = chakra(Emoji)
+
+/*
 const StyledEmoji = styled(Emoji)`
   align-self: center;
   margin-right: 1rem;
   flex-shrink: 0;
 `
 
+const StyledBannerNotification = styled(BannerNotification)`
+  display: flex;
+  justify-content: center;
+`
+
 const StyledBannerContent = styled.div`
   align-self: center;
 `
-
+*/
 const UpgradeBannerNotification: React.FC = () => (
-  <StyledBannerNotification shouldShow>
-    <StyledEmoji text=":megaphone:" />
-    <StyledBannerContent>
+  <StyledBannerNotification display="flex" justifyContent="center" shouldShow>
+    <StyledEmoji text=":megaphone:"  alignSelf="center" mr="1rem" flexShrink="0"/>
+    <Center>
       <b>We've deprecated our use of 'Eth1' and 'Eth2' terms.</b>{" "}
       <Link to="https://blog.ethereum.org/2022/01/24/the-great-eth2-renaming/">
         Read the full announcement
       </Link>
-    </StyledBannerContent>
+    </Center>
   </StyledBannerNotification>
 )
 
