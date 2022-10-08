8950094e1d8aa71618e24d7e10b0cda6d3f163f9
Victor Patru <patruvictor.mihai@gmail.com>
2022-10-08 18:59:10 +0300

    chore: migrating the MergeInfographic component to ChakraUI

--- src/components/MergeInfographic.tsx
+++ src/components/MergeInfographic.tsx
@@ -1,6 +1,6 @@
 // Library imports
 import React from "react"
-import styled from "@emotion/styled"
+import { Box, chakra, Icon, Text } from "@chakra-ui/react"
 import { useIntl } from "react-intl"
 // Component imports
 import Translation from "./Translation"
@@ -9,60 +9,7 @@ import { translateMessageId, TranslationKey } from "../utils/translations"
 // SVG imports
 import InfographicBg from "../assets/upgrades/merge-infographic-bg.svg"
 
-const Container = styled.div`
-  position: relative;
-  width: 100%;
-  aspect-ratio: 25 / 11;
-  isolation: isolate;
-`
-
-const ExecutionLayer = styled.div`
-  position: absolute;
-  top: 40%;
-  left: 2%;
-  width: 81%;
-  height: 18%;
-  margin: 0;
-  padding: 0;
-  z-index: 2;
-
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  align-items: center;
-
-  line-height: 1em;
-  max-height: 2em;
-  color: ${({ theme }) => theme.colors.background};
-
-  font-size: 1.375em;
-  @media (max-width: ${({ theme }) => theme.breakpoints.l}) {
-    font-size: 1.125em;
-  }
-  @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
-    font-size: 0.875em;
-  }
-  @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
-    font-size: 0.625em;
-  }
-`
-
-const Background = styled(InfographicBg)`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  z-index: 0;
-`
-
-const StyledSvg = styled.svg`
-  position: absolute;
-  z-index: 1;
-`
-
-const Text = styled.text`
-  text-anchor: start;
-  fill: currentColor;
-`
+const Background = chakra(InfographicBg)
 
 export type StringGetter = (key: TranslationKey) => string
 
@@ -73,7 +20,9 @@ export interface SvgProps {
 const SvgText: React.FC<SvgProps> = ({ getString }) => {
   const [sm, lg] = ["7px", "8px"]
   return (
-    <StyledSvg
+    <Icon
+      position="absolute"
+      zIndex={1}
       width="100%"
       height="100%"
       viewBox="0 0 250 110"
@@ -82,22 +31,22 @@ const SvgText: React.FC<SvgProps> = ({ getString }) => {
       xmlnsXlink="http://www.w3.org/1999/xlink"
       aria-hidden="true"
     >
-      <Text x="2%" y="35%" fontSize={lg}>
+      <Text skewX={2} skewY="35%" fontSize={lg}>
         ⛏ {getString("docs-nav-proof-of-work")}
       </Text>
-      <Text x="47%" y="35%" fontSize={lg}>
+      <Text skewX="47%" skewY="35%" fontSize={lg}>
         🌱 {getString("docs-nav-proof-of-stake")}
       </Text>
-      <Text x="11%" y="70%" fontSize={sm}>
+      <Text skewX="11%" skewY="70%" fontSize={sm}>
         🚀 {getString("beacon-chain")}
       </Text>
-      <Text x="43%" y="12.5%" fontSize={sm}>
+      <Text skewX="43%" skewY="12.5%" fontSize={sm}>
         🐼 {getString("page-upgrades-get-involved-ethresearch-2")}
       </Text>
-      <Text x="63%" y="95%" fontSize={sm}>
+      <Text skewX="63%" skewY="95%" fontSize={sm}>
         🌳 {getString("page-upgrades-get-involved-ethresearch-1")}
       </Text>
-    </StyledSvg>
+    </Icon>
   )
 }
 
@@ -111,20 +60,54 @@ const MergeInfographic: React.FC<IProps> = ({ className }) => {
     translateMessageId(id, intl)
 
   return (
-    <Container
+    <Box
       className={className}
       role="img"
       aria-label={translateMessageId(
         "page-upgrades-merge-infographic-alt-text",
         intl
       )}
+      position="relative"
+      width="100%"
+      sx={{
+        isolation: "isolate",
+        aspectRatio: 25 / 11,
+      }}
     >
-      <ExecutionLayer aria-hidden="true">
+      <Box
+        position="relative"
+        top="40%"
+        left="2%"
+        width="81%"
+        height="18%"
+        margin={0}
+        padding={0}
+        zIndex={2}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        lineHeight="1em"
+        maxHeight="2em"
+        color="background"
+        fontSize={{
+          base: "1.375em",
+          lg: "1.125em",
+          md: "0.875",
+          sm: "0.625em",
+        }}
+        aria-hidden="true"
+      >
         <Translation id="page-upgrades-merge-infographic-el" />
-      </ExecutionLayer>
+      </Box>
       <SvgText getString={getString} />
-      <Background aria-hidden="true" />
-    </Container>
+      <Background
+        aria-hidden="true"
+        position="absolute"
+        width="100%"
+        height="100%"
+        zIndex={0}
+      />
+    </Box>
   )
 }
 
