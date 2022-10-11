23fe1365e97165e5b2dd9be1f16c9a0811fccccd
tylerapfledderer <tyler.pfledderer@gmail.com>
2022-10-10 21:58:17 -0400

    refactor(expandable-info): migrate to chakra

--- src/components/ExpandableInfo.tsx
+++ src/components/ExpandableInfo.tsx
@@ -1,216 +1,155 @@
-import React, { ReactNode, useState } from "react"
-import styled from "@emotion/styled"
-import { motion } from "framer-motion"
+import React, { ReactNode } from "react"
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image"
+import {
+  BackgroundProps,
+  Box,
+  Center,
+  ChakraProps,
+  Collapse,
+  forwardRef,
+  Heading,
+  HStack,
+  Icon,
+  Stack,
+  Text,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react"
+import { motion } from "framer-motion"
+import { MdExpandMore } from "react-icons/md"
 
-import Icon from "./Icon"
-
-const Card = styled.div<{
-  background: string
-}>`
-  border: 1px solid ${({ theme }) => theme.colors.border};
-  border-radius: 2px;
-  padding: 1.5rem;
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 1rem;
-  background: ${({ background, theme }) =>
-    background ? theme.colors[background] : theme.colors.background};
-  &:hover {
-    img {
-      transform: scale(1.08);
-      transition: transform 0.1s;
-    }
-  }
-`
-
-const Content = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 3rem;
-  @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
-    gap: 2rem;
-    flex-direction: column;
-  }
-`
-
-const TitleContent = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 3rem;
-  width: 100%;
-`
-
-const Title = styled.h2`
-  margin-top: 0rem;
-  margin-bottom: 0.5rem;
-`
-
-const TextPreview = styled.p`
-  font-weight: 400;
-  color: ${(props) => props.theme.colors.text200};
-  margin-bottom: 0rem;
-`
-
-const Text = styled(motion.div)`
-  font-size: 16px;
-  font-weight: 400;
-  color: ${(props) => props.theme.colors.text};
-  margin-top: 2rem;
-  border-top: 1px solid ${(props) => props.theme.colors.border};
-  padding-top: 1.5rem;
-`
-
-const Question = styled.div`
-  margin-right: 1rem;
-`
-
-const Header = styled.div`
-  display: flex;
-  width: 100%;
-  margin: 1rem 0;
-  align-items: center;
-  img {
-    margin-right: 1.5rem;
-  }
-`
-
-const ButtonContainer = styled(motion.div)`
-  display: flex;
-  width: 5rem;
-  justify-content: center;
-  align-items: center;
-  min-height: 10rem;
-  cursor: pointer;
-  @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
-    min-height: 100%;
-    height: 100%;
-    width: 100%;
-    margin: 0;
-  }
-  &:hover {
-    svg {
-      transform: scale(1.25);
-      transition: transform 0.1s;
-    }
-  }
-`
-
-export interface IProps {
+export interface IProps extends ChakraProps {
   children?: React.ReactNode
   image?: IGatsbyImageData
   title: ReactNode
   contentPreview: ReactNode
-  background: string
-  forceOpen: boolean
+  background: BackgroundProps["background"]
+  forceOpen?: boolean
   className?: string
 }
 
-const ExpandableInfo: React.FC<IProps> = ({
-  image,
-  title,
-  contentPreview,
-  children,
-  background,
-  forceOpen,
-  className,
-}) => {
-  const [isVisible, setIsVisible] = useState(forceOpen)
-  const expandCollapse = {
-    collapsed: {
-      height: 0,
-      transition: {
-        when: "afterChildren",
+const ExpandableInfo = forwardRef<IProps, "div">(
+  (
+    {
+      image,
+      title,
+      contentPreview,
+      children,
+      background,
+      forceOpen,
+      className,
+      ...rest
+    },
+    ref
+  ) => {
+    const { isOpen, getButtonProps, getDisclosureProps } = useDisclosure({
+      defaultIsOpen: forceOpen,
+    })
+
+    const chevronFlip = {
+      collapsed: {
+        rotate: 0,
+        transition: {
+          duration: 0.1,
+        },
       },
-    },
-    expanded: {
-      height: "100%",
-      transition: {
-        when: "beforeChildren",
+      expanded: {
+        rotate: 180,
+        transition: {
+          duration: 0.4,
+        },
       },
-    },
-  }
-  const chevronFlip = {
-    collapsed: {
-      rotate: 0,
-      transition: {
-        duration: 0.1,
-      },
-    },
-    expanded: {
-      rotate: 180,
-      transition: {
-        duration: 0.4,
-      },
-    },
-  }
-  const showHide = {
-    collapsed: {
-      display: "none",
-    },
-    expanded: {
-      display: "inline-block",
-    },
-  }
-  const fadeInOut = {
-    collapsed: {
-      opacity: 0,
-      transition: {
-        duration: 0.1,
-      },
-    },
-    expanded: {
-      opacity: 1,
-      transition: {
-        duration: 0.4,
-      },
-    },
-  }
-  return (
-    <Card background={background} className={className}>
-      <Content>
-        {image && <GatsbyImage image={image} alt="" />}
-        <TitleContent>
-          <Question>
-            <Header>
-              <Title>{title}</Title>
-            </Header>
-            <TextPreview>{contentPreview}</TextPreview>
-          </Question>
-        </TitleContent>
-        {!forceOpen && (
-          <ButtonContainer
-            variants={chevronFlip}
-            animate={isVisible ? "expanded" : "collapsed"}
-            initial={false}
-            onClick={() => setIsVisible(!isVisible)}
-          >
-            <Icon name="chevronDown" size="36" />
-          </ButtonContainer>
-        )}
-      </Content>
-      <motion.div
-        variants={expandCollapse}
-        animate={isVisible ? "expanded" : "collapsed"}
-        initial={false}
+    }
+
+    const animateToggle = isOpen ? "expanded" : "collapsed"
+
+    return (
+      <VStack
+        border="1px solid"
+        borderColor="border"
+        borderRadius="2px"
+        p="1.5rem"
+        mb="1rem"
+        spacing="0"
+        background={background ?? "background"}
+        position="relative"
+        _hover={{
+          "& img": {
+            transform: "scale(1.08)",
+            transition: "transform 0.1s",
+          },
+        }}
+        ref={ref}
+        {...rest}
       >
-        <motion.div
-          variants={showHide}
-          animate={isVisible ? "expanded" : "collapsed"}
+        <Stack
+          justify="space-between"
+          gap={{ base: "2rem", md: "3rem" }}
+          flexDirection={{ base: "column", md: "row" }}
+          width="full"
+        >
+          {image && <GatsbyImage image={image} alt="" />}
+          <HStack gap="3rem" width="full">
+            <Box mr="1rem">
+              <HStack
+                width="full"
+                m="1rem 0"
+                sx={{
+                  img: {
+                    mr: "1.5rem",
+                  },
+                }}
+              >
+                <Heading mt="0rem" mb="0.5rem" size="md" fontWeight="600">
+                  {title}
+                </Heading>
+              </HStack>
+              <Text color="text200" mb="0">
+                {contentPreview}
+              </Text>
+            </Box>
+          </HStack>
+          {!forceOpen && (
+            <Center
+              as={motion.div}
+              variants={chevronFlip}
+              animate={animateToggle}
+              initial={false}
+              {...getButtonProps()}
+              width={{ base: "full", md: "5rem" }}
+              minHeight={{ base: "full", md: "10rem" }}
+              cursor="pointer"
+              _hover={{
+                "& svg": {
+                  transform: "scale(1.25)",
+                  transition: "transform 0.1s",
+                },
+              }}
+            >
+              <Icon as={MdExpandMore} boxSize="initial" size="36" />
+            </Center>
+          )}
+        </Stack>
+        <Collapse
+          {...getDisclosureProps()}
+          in={isOpen}
+          startingHeight="0"
+          endingHeight="100%"
           initial={false}
         >
-          <Text
-            variants={fadeInOut}
-            animate={isVisible ? "expanded" : "collapsed"}
-            initial={false}
+          <Box
+            color="text"
+            mt="2rem"
+            borderTop="1px solid"
+            borderColor="border"
+            paddingTop="1.5rem"
           >
             {children}
-          </Text>
-        </motion.div>
-      </motion.div>
-    </Card>
-  )
-}
+          </Box>
+        </Collapse>
+      </VStack>
+    )
+  }
+)
 
 export default ExpandableInfo
