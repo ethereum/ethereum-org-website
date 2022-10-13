4c096e3e7082dece2a54a854cb7a34fc3c742d18
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2022-10-13 16:07:37 +0500

    fixed according to guidelines

--- src/components/UpgradeBannerNotification.tsx
+++ src/components/UpgradeBannerNotification.tsx
@@ -1,23 +1,19 @@
 import React from "react"
-import BannerNotification from "./BannerNotification"
-import Emoji from "src/components/Emoji"
-import Link from "./Link"
 import { chakra, Center } from "@chakra-ui/react"
-
-const StyledBannerNotification = chakra(BannerNotification)
-
-const StyledEmoji = chakra(Emoji)
+import BannerNotification from "./BannerNotification"
+import Emoji from "./Emoji"
+import Link from "./Link"
 
 const UpgradeBannerNotification: React.FC = () => (
-  <StyledBannerNotification display="flex" justifyContent="center" shouldShow>
-    <StyledEmoji text=":megaphone:"  alignSelf="center" mr="1rem" flexShrink="0"/>
+  <BannerNotification justifyContent="center" shouldShow>
+    <Emoji text=":megaphone:"  alignSelf="center" mr={4} flexShrink="0"/>
     <Center>
       <b>We've deprecated our use of 'Eth1' and 'Eth2' terms.</b>{" "}
       <Link to="https://blog.ethereum.org/2022/01/24/the-great-eth2-renaming/">
         Read the full announcement
       </Link>
     </Center>
-  </StyledBannerNotification>
+  </BannerNotification>
 )
 
 export default UpgradeBannerNotification
