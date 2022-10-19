b527cd29e80f04c5276bb845c88f76a47390a1a6
Pablo Pettinari <pettinarip@gmail.com>
2022-10-19 09:10:10 -0300

    spread props on custom icon & simplify custom checkbox definition

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import {
   Checkbox as ChakraCheckbox,
+  CheckboxIconProps,
   CheckboxProps,
   Icon,
 } from "@chakra-ui/react"
 
-const CustomIcon = () => {
+const CustomIcon = (props: CheckboxIconProps) => {
   return (
     <Icon
       viewBox="0 0 24 24"
@@ -14,20 +15,17 @@ const CustomIcon = () => {
       strokeWidth="2px"
       w="100%"
       h="100%"
+      {...props}
     >
       <polyline points="20 6 9 17 4 12" />
     </Icon>
   )
 }
 
-export interface IProps extends CheckboxProps {
-  className?: string
-}
+export interface IProps extends CheckboxProps {}
 
-const Checkbox: React.FC<IProps> = ({ className, ...rest }) => {
-  return (
-    <ChakraCheckbox className={className} icon={<CustomIcon />} {...rest} />
-  )
+const Checkbox: React.FC<IProps> = (props) => {
+  return <ChakraCheckbox icon={<CustomIcon />} {...props} />
 }
 
 export default Checkbox
