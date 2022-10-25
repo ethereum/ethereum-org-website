ed725875f3645f4be9477e18d8cb8fb299ea19b4
tylerapfledderer <tyler.pfledderer@gmail.com>
2022-10-25 19:54:04 -0400

    revert(expandable-info): revert from `forwardRef`

--- src/components/ExpandableInfo.tsx
+++ src/components/ExpandableInfo.tsx
@@ -6,7 +6,6 @@ import {
   Center,
   ChakraProps,
   Collapse,
-  forwardRef,
   Heading,
   HStack,
   Icon,
@@ -28,135 +27,129 @@ export interface IProps extends ChakraProps {
   className?: string
 }
 
-const ExpandableInfo = forwardRef<IProps, "div">(
-  (
-    {
-      image,
-      title,
-      contentPreview,
-      children,
-      background,
-      forceOpen,
-      className,
-      ...rest
+const ExpandableInfo: React.FC<IProps> = ({
+  image,
+  title,
+  contentPreview,
+  children,
+  background,
+  forceOpen,
+  className,
+  ...rest
+}) => {
+  const { isOpen, getButtonProps, getDisclosureProps } = useDisclosure({
+    defaultIsOpen: forceOpen,
+  })
+
+  const chevronFlip = {
+    collapsed: {
+      rotate: 0,
+      transition: {
+        duration: 0.1,
+      },
     },
-    ref
-  ) => {
-    const { isOpen, getButtonProps, getDisclosureProps } = useDisclosure({
-      defaultIsOpen: forceOpen,
-    })
-
-    const chevronFlip = {
-      collapsed: {
-        rotate: 0,
-        transition: {
-          duration: 0.1,
-        },
+    expanded: {
+      rotate: 180,
+      transition: {
+        duration: 0.4,
       },
-      expanded: {
-        rotate: 180,
-        transition: {
-          duration: 0.4,
+    },
+  }
+
+  const animateToggle = isOpen ? "expanded" : "collapsed"
+
+  return (
+    <VStack
+      border="1px solid"
+      borderColor="border"
+      borderRadius="2px"
+      p={6}
+      mb={4}
+      spacing="0"
+      background={background ?? "background"}
+      position="relative"
+      _hover={{
+        "& img": {
+          transform: "scale(1.08)",
+          transition: "transform 0.1s",
         },
-      },
-    }
-
-    const animateToggle = isOpen ? "expanded" : "collapsed"
-
-    return (
-      <VStack
-        border="1px solid"
-        borderColor="border"
-        borderRadius="2px"
-        p={6}
-        mb={4}
-        spacing="0"
-        background={background ?? "background"}
-        position="relative"
-        _hover={{
-          "& img": {
-            transform: "scale(1.08)",
-            transition: "transform 0.1s",
-          },
-        }}
-        ref={ref}
-        {...rest}
+      }}
+      {...rest}
+    >
+      <Stack
+        justify="space-between"
+        align="center"
+        gap={{ base: 8, md: 12 }}
+        flexDirection={{ base: "column", md: "row" }}
+        width="full"
       >
-        <Stack
-          justify="space-between"
-          align="center"
-          gap={{ base: 8, md: 12 }}
-          flexDirection={{ base: "column", md: "row" }}
-          width="full"
-        >
-          {image && <GatsbyImage image={image} alt="" />}
-          <HStack gap={12} width="full">
-            <Box mr={4}>
-              <HStack
-                width="full"
-                my={4}
-                sx={{
-                  img: {
-                    mr: 6,
-                  },
-                }}
-              >
-                <Heading
-                  mt="0"
-                  mb={1}
-                  fontSize={{ base: "2xl", md: "2rem" }}
-                  fontWeight="600"
-                  lineHeight="1.4"
-                >
-                  {title}
-                </Heading>
-              </HStack>
-              <Text color="text200" mb="0">
-                {contentPreview}
-              </Text>
-            </Box>
-          </HStack>
-          {!forceOpen && (
-            <Center
-              as={motion.div}
-              variants={chevronFlip}
-              animate={animateToggle}
-              initial={false}
-              {...getButtonProps()}
-              width={{ base: "full", md: "5rem" }}
-              minHeight={{ base: "full", md: "10rem" }}
-              cursor="pointer"
-              _hover={{
-                "& svg": {
-                  transform: "scale(1.25)",
-                  transition: "transform 0.1s",
+        {image && <GatsbyImage image={image} alt="" />}
+        <HStack gap={12} width="full">
+          <Box mr={4}>
+            <HStack
+              width="full"
+              my={4}
+              sx={{
+                img: {
+                  mr: 6,
                 },
               }}
             >
-              <Icon as={MdExpandMore} boxSize="initial" size="36" />
-            </Center>
-          )}
-        </Stack>
-        <Collapse
-          {...getDisclosureProps()}
-          in={isOpen}
-          startingHeight="0"
-          endingHeight="100%"
-          initial={false}
-        >
-          <Box
-            color="text"
-            mt={8}
-            borderTop="1px solid"
-            borderColor="border"
-            paddingTop={6}
-          >
-            {children}
+              <Heading
+                mt="0"
+                mb={1}
+                fontSize={{ base: "2xl", md: "2rem" }}
+                fontWeight="600"
+                lineHeight="1.4"
+              >
+                {title}
+              </Heading>
+            </HStack>
+            <Text color="text200" mb="0">
+              {contentPreview}
+            </Text>
           </Box>
-        </Collapse>
-      </VStack>
-    )
-  }
-)
+        </HStack>
+        {!forceOpen && (
+          <Center
+            as={motion.div}
+            variants={chevronFlip}
+            animate={animateToggle}
+            initial={false}
+            {...getButtonProps()}
+            width={{ base: "full", md: "5rem" }}
+            minHeight={{ base: "full", md: "10rem" }}
+            cursor="pointer"
+            _hover={{
+              "& svg": {
+                transform: "scale(1.25)",
+                transition: "transform 0.1s",
+              },
+            }}
+          >
+            <Icon as={MdExpandMore} boxSize="initial" size="36" />
+          </Center>
+        )}
+      </Stack>
+      <Collapse
+        {...getDisclosureProps()}
+        in={isOpen}
+        startingHeight="0"
+        endingHeight="100%"
+        initial={false}
+      >
+        <Box
+          color="text"
+          mt={8}
+          borderTop="1px solid"
+          borderColor="border"
+          paddingTop={6}
+        >
+          {children}
+        </Box>
+      </Collapse>
+    </VStack>
+  )
+}
 
 export default ExpandableInfo
