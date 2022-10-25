3e0e8479d76c202c6a2906035ab86e4adf4a7181
Pablo Pettinari <pettinarip@gmail.com>
2022-10-25 09:56:58 -0300

    fix styles

--- src/components/UpgradeBannerNotification.tsx
+++ src/components/UpgradeBannerNotification.tsx
@@ -1,18 +1,24 @@
 import React from "react"
-import { chakra, Center } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import BannerNotification from "./BannerNotification"
 import Emoji from "./Emoji"
 import Link from "./Link"
 
 const UpgradeBannerNotification: React.FC = () => (
   <BannerNotification justifyContent="center" shouldShow>
-    <Emoji text=":megaphone:"  alignSelf="center" mr={4} flexShrink="0"/>
-    <Center>
+    <Emoji
+      text=":megaphone:"
+      alignSelf="center"
+      mr={4}
+      fontSize="2xl"
+      flexShrink="0"
+    />
+    <Box>
       <b>We've deprecated our use of 'Eth1' and 'Eth2' terms.</b>{" "}
       <Link to="https://blog.ethereum.org/2022/01/24/the-great-eth2-renaming/">
         Read the full announcement
       </Link>
-    </Center>
+    </Box>
   </BannerNotification>
 )
 
