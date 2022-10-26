13a08d30a36f00139f2237569dfd50167c9ecc1a
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-10-26 13:06:00 -0700

    getLocaleForNumberFormat with numberToPercent

--- src/utils/numberToPercent.ts
+++ src/utils/numberToPercent.ts
@@ -1,5 +1,8 @@
-export const numberToPercent = (num: number, locale: string = "en"): string =>
-  new Intl.NumberFormat(locale, {
+import { getLocaleForNumberFormat } from "./translations"
+import type { Lang } from "./languages"
+
+export const numberToPercent = (num: number, locale: Lang = "en"): string =>
+  new Intl.NumberFormat(getLocaleForNumberFormat(locale), {
     style: "percent",
     maximumFractionDigits: 0,
   }).format(num)
