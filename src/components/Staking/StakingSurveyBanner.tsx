061f65d93bb44316bd6402f8065fa87e82e1e3af
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-12-01 15:31:24 -0800

    fx mobile styling

--- src/components/Staking/StakingSurveyBanner.tsx
+++ src/components/Staking/StakingSurveyBanner.tsx
@@ -1,14 +1,17 @@
 import React from "react"
 import BannerNotification from "../BannerNotification"
 import Link from "../Link"
+import { Text } from "@chakra-ui/react"
 
 const StakingSurveyBanner: React.FC = () => (
   <BannerNotification shouldShow>
-    Researchers with EthStaker and the Ethereum Foundation are looking for
-    anonymous feedback from stakers.
-    <Link ms={2} to="https://stakingsurvey.paperform.co/">
-      Take a quick survey here
-    </Link>
+    <Text m={0} textAlign="center">
+      Researchers with EthStaker and the Ethereum Foundation are looking for
+      anonymous feedback from stakers.{" "}
+      <Link to="https://stakingsurvey.paperform.co/">
+        Take a quick survey here
+      </Link>
+    </Text>
   </BannerNotification>
 )
 
