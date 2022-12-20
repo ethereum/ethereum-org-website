ca6083b30071d3e44b41d5a25f1f58083a555201
tylerapfledderer <tyler.pfledderer@gmail.com>
2022-12-20 17:36:37 -0500

    style(badge): reconsolidate styles

--- src/@chakra-ui/gatsby-plugin/components/Badge.ts
+++ src/@chakra-ui/gatsby-plugin/components/Badge.ts
@@ -1,29 +1,34 @@
 import { ComponentStyleConfig, SystemStyleObject } from "@chakra-ui/react"
 
-const variantPrimary: SystemStyleObject = {
-  background: "primary100",
-  color: "black300",
-}
-
 const variantSecondary: SystemStyleObject = {
   border: "1px solid",
   borderColor: "primary100",
+  color: "text",
 }
 
 export const Badge: ComponentStyleConfig = {
   baseStyle: {
-    background: "transparent",
     borderRadius: "base",
+    border: "1px solid",
+    color: "black300",
     fontWeight: "initial",
     py: 1,
     px: 2,
     textTransform: "uppercase",
   },
   variants: {
-    primary: variantPrimary,
     secondary: variantSecondary,
   },
+  sizes: {
+    sm: {
+      py: 0,
+    },
+    lg: {
+      px: 3,
+    },
+  },
   defaultProps: {
-    variant: "primary",
+    // Remove the default from Chakra
+    variant: "",
   },
 }
