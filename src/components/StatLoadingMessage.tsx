c2b84fa3d9f23f2fe16bbddff93f09894e158d37
Pablo Pettinari <pettinarip@gmail.com>
2023-01-25 10:50:51 -0300

    update types and migrate StatErrorMessage component

--- src/components/StatLoadingMessage.tsx
+++ src/components/StatLoadingMessage.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import { Text } from "@chakra-ui/react"
+import { Text, TextProps } from "@chakra-ui/react"
 
 import Translation from "./Translation"
 
-export interface IProps {}
+export interface IProps extends TextProps {}
 
 const StatLoadingMessage: React.FC<IProps> = (props) => (
   <Text as="span" fontSize="2rem" {...props}>
