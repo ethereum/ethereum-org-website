7b39575fa90765e643afd9985780b29e77fa395e
Pablo Pettinari <pettinarip@gmail.com>
2023-02-01 19:30:38 -0300

    add script to optimize images

--- src/scripts/optimizeImages.ts
+++ src/scripts/optimizeImages.ts
@@ -6,27 +6,29 @@ import util from "util"
 const rename = util.promisify(fs.rename)
 
 const matches = glob.sync(`src/assets/**/*.{png,jpg,jpeg}`)
-const MAX_WIDTH = 1800
-const QUALITY = 70
+const MAX_WIDTH = 800 * 2
 
 Promise.all(
   matches.map(async (match) => {
     const stream = sharp(match)
     const info = await stream.metadata()
 
-    console.log({ info })
-
     const width = info.width || 0
+
     if (width < MAX_WIDTH) {
       return
     }
 
     const optimizedName = match.replace(
       /(\..+)$/,
-      (match, ext) => `-optimized${ext}`
+      (_match, ext) => `-optimized${ext}`
     )
 
-    await stream.resize(MAX_WIDTH).toFile(optimizedName)
+    await stream
+      .resize({
+        width: MAX_WIDTH,
+      })
+      .toFile(optimizedName)
 
     return rename(optimizedName, match)
   })
