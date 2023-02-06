e41c0830ec6511a5f8194b72c2f89be5a3f2398a
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-02-05 21:03:11 -0500

    refactor(theme/semanticTokens): update values and notate deprecations

--- src/@chakra-ui/gatsby-plugin/semanticTokens.ts
+++ src/@chakra-ui/gatsby-plugin/semanticTokens.ts
@@ -3,11 +3,15 @@ import {
   darkTheme as oldDarkTheme,
 } from "../../theme"
 
+/** @deprecated */
 const oldLightThemeColors = oldLightTheme.colors
 const oldDarkThemeColors = oldDarkTheme.colors
-
-// define each of the old colors as a `semanticToken`:
-// `name: { _light: lightColor, _dark: darkColor }`
+/**
+ * @deprecated
+ *
+ * define each of the old colors as a `semanticToken`:
+ * `name: { _light: lightColor, _dark: darkColor }`
+ */
 const oldColors = Object.keys(oldLightThemeColors).reduce((colors, color) => {
   const lightColor = oldLightThemeColors[color]
   const darkColor = oldDarkThemeColors[color]
@@ -34,18 +38,29 @@ const semanticTokens = {
     primaryLight: { _light: "blue.100", _dark: "orange.100" },
     primaryDark: { _light: "blue.700", _dark: "orange.800" },
     primaryHover: { _light: "blue.300", _dark: "orange.300" },
+    primaryVisited: { _light: "blue.600", _dark: "orange.600" },
+    // ! Deprecating primaryPressed
     primaryPressed: { _light: "blue.300", _dark: "orange.800" },
     body: { _light: "gray.700", _dark: "gray.100" },
+    // ! Deprecating bodyInverted
     bodyInverted: { _light: "gray.100", _dark: "gray.700" },
     bodyLight: { _light: "gray.500", _dark: "gray.100" },
-    disabled: { _light: "gray.400", _dark: "gray.500" },
+    disabled: { _light: "gray.300", _dark: "gray.500" },
     background: { _light: "white", _dark: "gray.700" },
+    backgroundHighlight: { _light: "gray.100", _dark: "gray.900" },
+    // ! Deprecating neutral
     neutral: { _light: "white", _dark: "gray.900" },
-    success: { _light: "green.500", _dark: "green.400" },
+    success: "green.500",
+    successLight: "green.100",
+    // ! Deprecating successNeutral
     successNeutral: { _light: "green.100", _dark: "green.900" },
+    // ? Keep "error" or rename to "fail" ?
     error: "red.500",
+    errorLight: "red.100",
+    // ! Deprecating errorNeutral
     errorNeutral: { _light: "red.100", _dark: "red.900" },
-    attention: "yellow.200",
+    attention: "yellow.500",
+    attentionLight: "yellow.200",
   },
 }
 
