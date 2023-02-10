a2ecb2c491ec6a627d6cf4f03de0acdd4c030e55
Corwin Smith <cssmittys@gmail.com>
2023-02-09 23:09:00 -0700

    removed unnecessary css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -137,7 +137,6 @@ const Layout: React.FC<IProps> = ({
                 flexFlow="column"
                 maxW={{
                   lg: lightTheme.variables.maxPageWidth,
-                  xl: "100%",
                 }}
               >
                 <ZenMode>
