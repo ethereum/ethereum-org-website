9a4befc1387438272c9d7fd99ae37ed508fb2a42
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-02-16 21:17:21 -0500

    revert(theme/typography): remove unneeded font size set

--- src/@chakra-ui/gatsby-plugin/foundations/typography.ts
+++ src/@chakra-ui/gatsby-plugin/foundations/typography.ts
@@ -14,20 +14,6 @@ const typography = {
     sm: 1.5,
     base: 1.6,
   },
-
-  fontSizes: {
-    xs: "0.75rem",
-    sm: "0.875rem",
-    md: "1rem",
-    lg: "1.125rem",
-    xl: "1.25rem",
-    "2xl": "1.5rem",
-    "3xl": "1.875rem",
-    "4xl": "2.25rem",
-    "5xl": "3rem",
-    "6xl": "3.75rem",
-    "7xl": "4.5rem",
-  },
 }
 
 export default typography
