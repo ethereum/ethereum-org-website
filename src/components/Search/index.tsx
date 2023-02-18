510525de1358556a8a82a5def4902d149cad17ba
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-02-17 18:34:31 -0800

    refactor styles.css to Styles.tsx

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -6,7 +6,7 @@ import { DocSearchHit } from "@docsearch/react/dist/esm/types"
 import { translateMessageId } from "../../utils/translations"
 // Styles
 import "@docsearch/css"
-import "./styles.css"
+import DocSearchStyles from "./Styles"
 
 const Search: FC = () => {
   const intl = useIntl()
@@ -16,56 +16,62 @@ const Search: FC = () => {
     process.env.GATSBY_ALGOLIA_BASE_SEARCH_INDEX_NAME || "prod-ethereum-org"
 
   return (
-    <DocSearch
-      appId={appId}
-      apiKey={apiKey}
-      indexName={indexName}
-      transformItems={(items) =>
-        items.map((item: DocSearchHit) => {
-          const newItem: DocSearchHit = structuredClone(item)
-          ;(newItem.url = item.url
-            .replace(/^https?:\/\/[^\/]+(?=\/)/, "")
-            .replace("#main-content", "")
-            .replace("#content", "")
-            .replace("#top", "")),
-            (newItem._highlightResult.hierarchy.lvl0.value =
-              item._highlightResult.hierarchy.lvl0.value.replace(
-                " | ethereum.org",
-                ""
-              ))
-          return newItem
-        })
-      }
-      searchParameters={{
-        facetFilters: [`lang:${intl.locale}`],
-      }}
-      placeholder={translateMessageId("search-ethereum-org", intl)}
-      translations={{
-        button: {
-          buttonText: translateMessageId("search", intl),
-          buttonAriaLabel: translateMessageId("search", intl),
-        },
-        modal: {
-          searchBox: {
-            resetButtonTitle: translateMessageId("clear", intl),
-            resetButtonAriaLabel: translateMessageId("clear", intl),
-            cancelButtonText: translateMessageId("close", intl),
-            cancelButtonAriaLabel: translateMessageId("close", intl),
+    <>
+      <DocSearchStyles />
+      <DocSearch
+        appId={appId}
+        apiKey={apiKey}
+        indexName={indexName}
+        searchParameters={{
+          facetFilters: [`lang:${intl.locale}`],
+        }}
+        transformItems={(items) =>
+          items.map((item: DocSearchHit) => {
+            const newItem: DocSearchHit = structuredClone(item)
+            ;(newItem.url = item.url
+              .replace(/^https?:\/\/[^\/]+(?=\/)/, "")
+              .replace("#main-content", "")
+              .replace("#content", "")
+              .replace("#top", "")),
+              (newItem._highlightResult.hierarchy.lvl0.value =
+                item._highlightResult.hierarchy.lvl0.value.replace(
+                  " | ethereum.org",
+                  ""
+                ))
+            return newItem
+          })
+        }
+        placeholder={translateMessageId("search-ethereum-org", intl)}
+        translations={{
+          button: {
+            buttonText: translateMessageId("search", intl),
+            buttonAriaLabel: translateMessageId("search", intl),
           },
-          footer: {
-            selectText: translateMessageId("docsearch-to-select", intl),
-            selectKeyAriaLabel: translateMessageId("docsearch-to-select", intl),
-            navigateText: translateMessageId("docsearch-to-navigate", intl),
-            navigateUpKeyAriaLabel: translateMessageId("up", intl),
-            navigateDownKeyAriaLabel: translateMessageId("down", intl),
-            closeText: translateMessageId("docsearch-to-close", intl),
-            closeKeyAriaLabel: translateMessageId("docsearch-to-close", intl),
-            searchByText: translateMessageId("docsearch-search-by", intl),
+          modal: {
+            searchBox: {
+              resetButtonTitle: translateMessageId("clear", intl),
+              resetButtonAriaLabel: translateMessageId("clear", intl),
+              cancelButtonText: translateMessageId("close", intl),
+              cancelButtonAriaLabel: translateMessageId("close", intl),
+            },
+            footer: {
+              selectText: translateMessageId("docsearch-to-select", intl),
+              selectKeyAriaLabel: translateMessageId(
+                "docsearch-to-select",
+                intl
+              ),
+              navigateText: translateMessageId("docsearch-to-navigate", intl),
+              navigateUpKeyAriaLabel: translateMessageId("up", intl),
+              navigateDownKeyAriaLabel: translateMessageId("down", intl),
+              closeText: translateMessageId("docsearch-to-close", intl),
+              closeKeyAriaLabel: translateMessageId("docsearch-to-close", intl),
+              searchByText: translateMessageId("docsearch-search-by", intl),
+            },
           },
-        },
-        /* TODO: Setup remainder of translations */
-      }}
-    />
+          /* TODO: Setup remainder of translations */
+        }}
+      />
+    </>
   )
 }
 
