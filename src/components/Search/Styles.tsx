cacbd6ed12f25c62cd0783f45686d3e6602bdc6e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-02-17 18:48:47 -0800

    clean up styles
    use predefined css variables where possible, add comments, lint

--- src/components/Search/Styles.tsx
+++ src/components/Search/Styles.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Global, css } from "@emotion/react"
 
 const styles = css`
-  /* Search field on desktop nav bar */
+  /* Search button on desktop nav bar */
   .DocSearch-Button {
     border-radius: var(--eth-radii-base);
     border: var(--eth-borders-1px);
@@ -17,30 +17,12 @@ const styles = css`
       var(--eth-colors-primaryHighlight);
   }
 
-  .DocSearch-Button-Container {
-  }
-
-  .DocSearch-Button-Keys {
-    min-width: 44px;
-    gap: var(--eth-space-1);
-    display: flex;
-    align-items: center !important;
-    margin: 0;
-    margin-top: 2px;
-    padding: 0;
-    height: 100%;
-  }
-
-  .DocSearch-Button-Container svg {
-    --icon-size: 0.875rem;
-    width: var(--icon-size);
-    height: var(--icon-size);
-  }
   .DocSearch-Button:active,
   .DocSearch-Button:focus,
   .DocSearch-Button:hover {
     box-shadow: none;
-    outline: 2px solid var(--eth-colors-primary);
+    outline: var(--eth-borders-2px);
+    outline-color: var(--eth-colors-primary);
   }
 
   .DocSearch-Button:hover svg path,
@@ -50,9 +32,20 @@ const styles = css`
     border-color: var(--eth-colors-bg);
   }
 
+  .DocSearch-Button-Keys {
+    display: flex;
+    min-width: 44px;
+    gap: var(--eth-space-1);
+    align-items: center !important;
+    margin: 0;
+    margin-top: var(--eth-space-0-5);
+    padding: 0;
+    height: 100%;
+  }
+
   .DocSearch-Button-Keys kbd {
     background: none;
-    border: 1px solid;
+    border: var(--eth-borders-1px);
     border-color: var(--eth-colors-disabled);
     color: var(--eth-colors-disabled);
     box-shadow: none;
@@ -63,22 +56,28 @@ const styles = css`
     font-size: var(--eth-fontSizes-sm);
     text-transform: lowercase;
   }
+
   .DocSearch-Button-Keys kbd:first-of-type {
     font-size: var(--eth-fontSizes-md);
     padding-top: var(--eth-space-1);
   }
 
+  .DocSearch-Button-Container svg {
+    --icon-size: 0.875rem;
+    width: var(--icon-size);
+    height: var(--icon-size);
+  }
+
   .DocSearch-Button-Placeholder {
-    text-align: start;
     text-transform: lowercase;
     font-style: italic;
     color: var(--eth-colors-body);
-    font-weight: 400;
-    width: 100%;
-    flex: 1;
+    font-weight: var(--eth-fontWeights-normal);
   }
 
-  /* Algolia search modal styling */
+  /**
+  * Algolia search modal styling
+  */
   .DocSearch-Modal {
     --docsearch-modal-background: var(--eth-colors-background);
     --docsearch-searchbox-height: fit-content;
@@ -86,29 +85,35 @@ const styles = css`
     --docsearch-modal-width: 650px;
   }
 
+  /* Modal header */
   .DocSearch-SearchBar {
     padding: var(--eth-space-8);
     padding-bottom: var(--eth-space-4);
   }
+
   .DocSearch-SearchBar form {
     padding-block: var(--eth-space-3);
     --docsearch-searchbox-shadow: inset 0 0 0 1px
       var(--eth-colors-primaryHighlight);
     background: var(--eth-colors-neutral);
   }
+
   .DocSearch-SearchBar label {
     --size: var(--eth-space-4);
     height: var(--size);
     width: var(--size);
     color: var(--eth-colors-primaryHighlight);
   }
+
   .DocSearch-SearchBar input {
     font-size: var(--eth-fontSizes-lg);
     color: var(--eth-colors-primaryHighlight);
   }
+
   .DocSearch-SearchBar button {
     color: var(--eth-colors-primaryHighlight);
   }
+
   .DocSearch-SearchBar button:hover {
     color: var(--eth-colors-primary);
   }
@@ -118,31 +123,37 @@ const styles = css`
     color: var(--chakra-colors-primaryHighlight);
   }
 
+  /* Modal body */
   .DocSearch-Dropdown {
     padding-inline: var(--eth-space-8);
     max-height: calc(100vh - var(--eth-space-64));
     overflow-y: scroll;
   }
+
   .DocSearch-Hit-source {
     color: var(--eth-colors-body);
   }
+
   .DocSearch-Hit a {
     border-radius: 0;
     --docsearch-hit-shadow: none;
-    border-bottom: 1px solid var(--eth-colors-disabled);
+    border-bottom: var(--eth-borders-1px);
+    border-color: var(--eth-colors-disabled);
     background: none;
   }
+
   .DocSearch-Hit[aria-selected="true"] a {
     background: var(--eth-colors-primaryHover);
     box-shadow: 4px 4px 0 0 var(--eth-colors-primaryLight);
     --docsearch-hit-active-color: var(--eth-colors-background);
   }
+
   .DocSearch-Hit-action-button:focus path,
   .DocSearch-Hit-action-button:hover path {
     fill: var(--eth-colors-switchBackground);
   }
+
   .DocSearch-Hit-icon {
-    /* --docsearch-muted-color:  */
     color: var(--eth-colors-body);
   }
 
@@ -153,10 +164,11 @@ const styles = css`
   /* Modal footer */
   .DocSearch-Commands kbd {
     background: var(--eth-colors-button-background);
-    border: 1px solid var(--eth-colors-primary);
+    border: var(--eth-borders-1px);
+    border-color: var(--eth-colors-primary);
     color: var(--eth-colors-primary);
     box-shadow: none;
-    --size: 1rem;
+    --size: var(--eth-space-4);
     height: var(--size);
     width: var(--size);
     padding: 0;
@@ -169,14 +181,18 @@ const styles = css`
     background: var(--eth-colors-primaryBackground);
     --docsearch-logo-colors: var(--eth-colors-bodyLight);
     --docsearch-footer-shadow: none;
-    border-top: 2px solid var(--eth-colors-primary);
+    border-top: var(--eth-borders-2px);
+    border-color: var(--eth-colors-primary);
   }
+
   .DocSearch-Footer ul {
     gap: var(--eth-space-4);
   }
+
   .DocSearch-Footer ul li {
     margin: 0 !important;
   }
+
   .DocSearch-Footer span {
     color: var(--eth-colors-body);
     text-transform: lowercase;
@@ -186,6 +202,7 @@ const styles = css`
     color: var(--eth-colors-bodyLight);
     text-transform: uppercase;
   }
+
   svg[aria-label="Algolia"] * {
     fill: var(--eth-colors-bodyLight);
   }
