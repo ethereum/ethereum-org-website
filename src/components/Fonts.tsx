e7765662525f2e52238e8927005b05e1621bd046
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-03-07 10:49:48 -0500

    chore(fonts): add inter `woff2` variable file

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -12,6 +12,14 @@ const Fonts = () => (
         font-display: swap;
         src: url('/fonts/Inter.ttf') format('truetype');
       }
+      @font-face {
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 300 800;
+        font-display: swap;
+        font-named-instance: 'Regular';
+        src: url("/fonts/Inter-roman.var.woff2") format("woff2");
+      }
       `}
   />
 )
