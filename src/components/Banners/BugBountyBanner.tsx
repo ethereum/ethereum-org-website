0ef57052539ff52884696e1337fa52a269155e7b
Corwin Smith <cssmittys@gmail.com>
2023-03-20 16:22:03 +0000

    fix BugBountyBanner implementation

--- src/components/Banners/BugBountyBanner.tsx
+++ src/components/Banners/BugBountyBanner.tsx
@@ -1,33 +1,19 @@
 // Libraries
 import React from "react"
-import styled from "@emotion/styled"
+import { Center, Text } from "@chakra-ui/react"
 
 // Components
 import BannerNotification from "../BannerNotification"
-import Link from "../Link"
-
-const StyledBannerNotification = styled(BannerNotification)`
-  display: flex;
-  z-index: 1;
-  justify-content: center;
-  p {
-    max-width: 100ch;
-    margin: 0;
-    padding: 0;
-  }
-  a {
-    text-decoration: underline;
-  }
-  text-align: center;
-`
 
 const BugBountyBanner: React.FC = () => (
-  <StyledBannerNotification>
-    <p>
-      All Shapella-related bounties currently receive a 2x bonus multiplier (up to
-      500,000 USD) until 5th of April 2023.{" "}
-    </p>
-  </StyledBannerNotification>
+  <BannerNotification shouldShow={true}>
+    <Center>
+      <Text m={0} p={0}>
+        All Shapella-related bounties currently receive a 2x bonus multiplier
+        (up to 500,000 USD) until 5th of April 2023.
+      </Text>
+    </Center>
+  </BannerNotification>
 )
 
 export default BugBountyBanner
