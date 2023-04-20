a4fe556a6e45db557855ffcceb14edadb48c0a8e
Mahendra <mahendrabrowny@gmail.com>
2023-04-20 18:13:41 +0530

    migrate languages.tsx to chakra

--- src/pages/languages.tsx
+++ src/pages/languages.tsx
@@ -1,63 +1,17 @@
-import React, { useState } from "react"
-import { graphql } from "gatsby"
-import { IconButton } from "@chakra-ui/react"
-import styled from "@emotion/styled"
+import { Box, Flex, Heading, IconButton, Input, Text } from "@chakra-ui/react"
 import { useLocation } from "@reach/router"
+import { graphql } from "gatsby"
 import { useTranslation } from "gatsby-plugin-react-i18next"
+import React, { useState } from "react"
+import { MdClose } from "react-icons/md"
 
+import Link from "../components/Link"
 import PageMetadata from "../components/PageMetadata"
 import Translation from "../components/Translation"
-import Link from "../components/Link"
-import { Page, Content } from "../components/SharedStyledComponents"
 
+import { CardItem as LangItem } from "../components/SharedStyledComponents"
 import { Language, languageMetadata } from "../utils/languages"
 import { TranslationKey } from "../utils/translations"
-import { CardItem as LangItem } from "../components/SharedStyledComponents"
-import Icon from "../components/Icon"
-
-const StyledPage = styled(Page)`
-  margin-top: 4rem;
-`
-
-const ContentContainer = styled.div``
-
-const LangContainer = styled.div`
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-`
-
-const LangTitle = styled.div`
-  font-size: 0.875rem;
-  line-height: 1.6;
-  font-weight: 400;
-  letter-spacing: 0.04em;
-  margin: 1.14em 0;
-  text-transform: uppercase;
-`
-
-const Form = styled.form`
-  margin: 0;
-  position: relative;
-  border-radius: 0.25em;
-  width: clamp(min(400px, 100%), 50%, 600px);
-`
-
-const StyledInput = styled.input`
-  border: 1px solid ${(props) => props.theme.colors.searchBorder};
-  color: ${(props) => props.theme.colors.text};
-  background: ${(props) => props.theme.colors.searchBackground};
-  padding: 0.5rem;
-  padding-right: 2rem;
-  border-radius: 0.25em;
-  width: 100%;
-
-  &:focus {
-    outline: ${(props) => props.theme.colors.primary} auto 1px;
-  }
-`
 
 const LanguagesPage = () => {
   const { t } = useTranslation()
@@ -91,45 +45,62 @@ const LanguagesPage = () => {
   translationsCompleted.sort((a, b) => a["name"].localeCompare(b["name"]))
 
   return (
-    <StyledPage>
+    <Flex direction="column" align="center" w="full" mx="auto">
       <PageMetadata
         title={t("page-languages-meta-title")}
         description={t("page-languages-meta-desc")}
       />
-      <Content>
-        <ContentContainer>
-          <h1>
+      <Box py={4} px={8} w="full">
+        <Box>
+          <Heading as="h1" fontSize={{ base: "2rem", sm: "2.5rem" }}>
             <Translation id="page-languages-h1" />
-          </h1>
-          <p>
+          </Heading>
+          <Text>
             <Translation id="page-languages-p1" />
-          </p>
-          <p>
+          </Text>
+          <Text>
             <Translation id="page-languages-interested" />{" "}
             <Link to="/contributing/translation-program/">
               <Translation id="page-languages-learn-more" />
             </Link>
             .
-          </p>
-          <p>
+          </Text>
+          <Text>
             <Translation id="page-languages-resources-paragraph" />{" "}
             <Link to="/community/language-resources">
               <Translation id="page-languages-resources-link" />
             </Link>
             .
-          </p>
-          <h2>
+          </Text>
+          <Heading fontSize={{ base: "2xl", md: "2rem" }}>
             <Translation id="page-languages-translations-available" />:
-          </h2>
-          <Form>
-            <StyledInput
+          </Heading>
+          <Box
+            as="form"
+            position="relative"
+            borderRadius="0.25em"
+            w="clamp(min(400px, 100%), 50%, 600px)"
+          >
+            <Input
+              border="1px solid"
+              borderColor="searchBorder"
+              color="text"
+              bg="searchBackground"
+              p={2}
+              pr={8}
+              borderRadius="0.25em"
+              w="full"
+              _focus={{
+                outline: "auto 1px",
+                outlineColor: "primary",
+              }}
               value={keyword}
               placeholder={searchString}
               onChange={(e) => setKeyword(e.target.value)}
             />
             {keyword !== "" && (
               <IconButton
-                icon={<Icon name="close" />}
+                icon={<MdClose />}
                 onClick={resetKeyword}
                 position="absolute"
                 insetInlineEnd={1}
@@ -138,28 +109,39 @@ const LanguagesPage = () => {
                 _hover={{ svg: { fill: "primary" } }}
               />
             )}
-          </Form>
-          <LangContainer>
+          </Box>
+          <Flex my={8} wrap="wrap" w="full">
             {translationsCompleted.map((lang) => (
               <LangItem to={redirectTo} language={lang.code} key={lang["name"]}>
-                <LangTitle>{lang["name"]}</LangTitle>
-                <h4>{lang.localName}</h4>
+                <Box
+                  fontSize="sm"
+                  lineHeight={1.6}
+                  fontWeight="normal"
+                  letterSpacing="0.04em"
+                  my="1.14em"
+                  textTransform="uppercase"
+                >
+                  {lang["name"]}
+                </Box>
+                <Heading as="h4" fontSize={{ base: "md", md: "xl" }}>
+                  {lang.localName}
+                </Heading>
               </LangItem>
             ))}
-          </LangContainer>
-          <h2>
+          </Flex>
+          <Heading fontSize={{ base: "2xl", md: "2rem" }}>
             <Translation id="page-languages-want-more-header" />
-          </h2>
-          <p>
+          </Heading>
+          <Text>
             <Translation id="page-languages-want-more-paragraph" />{" "}
             <Link to="/contributing/translation-program/">
               <Translation id="page-languages-want-more-link" />
             </Link>
             .
-          </p>
-        </ContentContainer>
-      </Content>
-    </StyledPage>
+          </Text>
+        </Box>
+      </Box>
+    </Flex>
   )
 }
 
