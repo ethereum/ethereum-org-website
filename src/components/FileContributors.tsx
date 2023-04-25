d49a56d4134a14cf2445ff33215835ef51cf0418
Pablo Pettinari <pettinarip@gmail.com>
2023-04-25 17:39:44 -0300

    refactor code to be aligned with conventions and cleanup code

--- src/components/FileContributors.tsx
+++ src/components/FileContributors.tsx
@@ -5,13 +5,14 @@ import { useQuery, gql } from "@apollo/client"
 
 import {
   Avatar,
-  ButtonProps,
-  ChakraProps,
   Flex,
+  FlexProps,
+  Heading,
   Icon,
   ListItem,
   ModalBody,
   ModalHeader,
+  Show,
   Skeleton,
   SkeletonCircle,
   Text,
@@ -23,7 +24,7 @@ import { getLocaleTimestamp } from "../utils/time"
 import { Lang } from "../utils/languages"
 
 import ButtonLink from "./ButtonLink"
-import Link, { IProps as ILinkProps } from "./Link"
+import Link from "./Link"
 import Modal from "./Modal"
 import Translation from "./Translation"
 import Button from "./Button"
@@ -73,46 +74,9 @@ const COMMIT_HISTORY = gql`
   }
 `
 
-const ContributorsButton = ({
-  children,
-  ...props
-}: {
-  children: React.ReactNode
-} & ButtonProps) => {
-  return (
-    <Button
-      m={0}
-      variant="outline"
-      border={0}
-      color="text"
-      bgColor="background"
-      p="md"
-      mt="1rem"
-      mb="0.5rem"
-      justifyContent="center"
-      height="40px"
-      {...props}
-    >
-      {children}
-    </Button>
-  )
-}
-
-const GithubButton = ({ ...props }: ILinkProps & ButtonProps) => {
-  return (
-    <ButtonLink
-      mt={0}
-      p="0.5rem"
-      height="40%"
-      display={{ base: "block", lg: "none" }}
-      {...props}
-    />
-  )
-}
-
 const ContributorList = ({ children }: { children: React.ReactNode }) => {
   return (
-    <UnorderedList maxH="16rem" m={0} mt="1.5rem" overflowY="scroll">
+    <UnorderedList maxH="2xs" m={0} mt={6} overflowY="scroll">
       {children}
     </UnorderedList>
   )
@@ -120,13 +84,13 @@ const ContributorList = ({ children }: { children: React.ReactNode }) => {
 
 const Contributor = ({ contributor }: { contributor: Author }) => {
   return (
-    <ListItem p="0.5rem" display="flex" alignItems="center">
+    <ListItem p={2} display="flex" alignItems="center">
       <Avatar
         height="40px"
         width="40px"
         src={contributor.avatarUrl}
         name={contributor.name}
-        mr="0.5rem"
+        mr={2}
       />
       {contributor.user && (
         <Link to={contributor.user.url}>@{contributor.user.login}</Link>
@@ -136,27 +100,7 @@ const Contributor = ({ contributor }: { contributor: Author }) => {
   )
 }
 
-const LeftContent = ({
-  children,
-  ...props
-}: { children: React.ReactNode } & ChakraProps) => {
-  return (
-    <Flex
-      w="100%"
-      mr="1rem"
-      alignItems="center"
-      fontSize={{
-        base: "sm",
-        md: "unset",
-      }}
-      {...props}
-    >
-      {children}
-    </Flex>
-  )
-}
-
-export interface IProps {
+export interface IProps extends FlexProps {
   relativePath: string
   editPath?: string
 }
@@ -203,11 +147,11 @@ const FileContributors: React.FC<IProps> = ({
 
   return (
     <>
-      <Modal isOpen={isModalOpen} setIsOpen={setModalOpen} {...props}>
+      <Modal isOpen={isModalOpen} setIsOpen={setModalOpen}>
         <ModalHeader py={0}>
-          <Text as="h2" m={0}>
+          <Heading m={0}>
             <Translation id="contributors" />
-          </Text>
+          </Heading>
         </ModalHeader>
 
         <ModalBody>
@@ -222,34 +166,26 @@ const FileContributors: React.FC<IProps> = ({
       </Modal>
 
       <Flex
-        justifyContent="space-between"
         direction={{
           base: "column",
           md: "row",
         }}
-        pb={{
-          base: "2rem",
-          lg: 0,
-        }}
+        p={{ base: 0, md: 2 }}
+        {...props}
       >
-        <LeftContent>
-          <SkeletonCircle size="10" mr="0.5rem" isLoaded={!loading}>
+        <Flex mr={4} alignItems="center" flex="1">
+          <SkeletonCircle size="10" mr={2} isLoaded={!loading}>
             <Avatar
               height="40px"
               width="40px"
               src={lastContributor.avatarUrl}
               name={lastContributor.name}
-              mr="0.5rem"
+              mr={2}
             />
           </SkeletonCircle>
 
-          <Skeleton isLoaded={!loading} w="100%" alignSelf="stretch">
-            <Flex
-              h="100%"
-              alignItems="center"
-              lineHeight="130%"
-              color="text200"
-            >
+          <Skeleton isLoaded={!loading}>
+            <Text m={0} color="text200">
               <Translation id="last-edit" />:{" "}
               {lastContributor.user && (
                 <Link to={lastContributor.user.url}>
@@ -258,37 +194,48 @@ const FileContributors: React.FC<IProps> = ({
               )}
               {!lastContributor.user && <span>{lastContributor.name}</span>},{" "}
               {getLocaleTimestamp(language as Lang, lastCommit.committedDate)}
-            </Flex>
+            </Text>
           </Skeleton>
-        </LeftContent>
+        </Flex>
 
-        <Flex direction="column" alignContent="center">
+        <Flex
+          direction="column"
+          alignContent="center"
+          justifyContent="space-between"
+        >
           <Skeleton
             isLoaded={!loading}
-            my={{
-              base: "1rem",
-              md: 0,
-            }}
+            mt={{ base: 4, md: 0 }}
+            mb={{ base: 2, md: 0 }}
           >
-            <ContributorsButton onClick={() => setModalOpen(true)} w="100%">
+            <Button
+              variant="outline"
+              bg="background"
+              border={0}
+              onClick={() => setModalOpen(true)}
+              w={{ base: "full", md: "inherit" }}
+            >
               <Translation id="see-contributors" />
-            </ContributorsButton>
+            </Button>
           </Skeleton>
 
           {editPath && (
-            <GithubButton to={editPath} hideArrow variant="outline">
-              <Flex
-                h="100%"
-                alignItems="center"
-                justifyContent="center"
-                gap="2"
-              >
-                <Icon as={FaGithub} />
-                <span>
-                  <Translation id="edit-page" />
-                </span>
-              </Flex>
-            </GithubButton>
+            <Show below="l">
+              {/* TODO: switch `l` to `lg` after UI migration and use `hideBelow` prop */}
+              <ButtonLink to={editPath} hideArrow variant="outline">
+                <Flex
+                  h="100%"
+                  alignItems="center"
+                  justifyContent="center"
+                  gap={2}
+                >
+                  <Icon as={FaGithub} fontSize="2xl" />
+                  <span>
+                    <Translation id="edit-page" />
+                  </span>
+                </Flex>
+              </ButtonLink>
+            </Show>
           )}
         </Flex>
       </Flex>
