5d9a8667b2734fd24d5af3ba2cbc00f05347d6b1
Corwin Smith <cssmittys@gmail.com>
2023-05-17 17:48:57 -0600

    detail view

--- src/components/CommunityEvents.tsx
+++ src/components/CommunityEvents.tsx
@@ -1,5 +1,6 @@
 //Libraries
 import React, { useEffect, useState } from "react"
+import { useI18next } from "gatsby-plugin-react-i18next"
 import {
   Box,
   Center,
@@ -10,6 +11,7 @@ import {
   Text,
 } from "@chakra-ui/react"
 import { FaDiscord } from "react-icons/fa"
+import { DateTime } from "luxon"
 
 // Components
 import ButtonLink from "./ButtonLink"
@@ -20,6 +22,7 @@ interface Event {
   date: string
   title: string
   calenderLink: string
+  pastEventLink: string | undefined
 }
 
 interface State {
@@ -39,6 +42,7 @@ const DiscordButton = () => {
 }
 
 const CommunityEvents = () => {
+  const { language } = useI18next()
   const [state, setState] = useState<State>({
     pastEventData: [],
     upcomingEventData: [],
@@ -48,8 +52,18 @@ const CommunityEvents = () => {
 
   useEffect(() => {
     try {
+      const pastEventData: Array<Event> = []
+      const upcomingEventData: Array<Event> = []
+
       setTimeout(
-        () => setState({ ...state, loading: false, hasError: false }),
+        () =>
+          setState({
+            ...state,
+            pastEventData,
+            upcomingEventData,
+            loading: false,
+            hasError: false,
+          }),
         500
       )
     } catch {
@@ -59,7 +73,7 @@ const CommunityEvents = () => {
 
   return (
     <Flex w="full" flexDirection={{ base: "column", lg: "row" }}>
-      <Center w={{ base: "100%", lg: "40%" }} p={16}>
+      <Center w={{ base: "100%", lg: "40%" }} px={16}>
         <Box>
           <Heading>Join the ethereum.org community</Heading>
           <Text>
@@ -78,12 +92,11 @@ const CommunityEvents = () => {
         w={{ base: "100%", lg: "60%" }}
         flexDirection={{ base: "column", lg: "row" }}
       >
-        <Box
+        <Flex
           w={{ base: "100%", lg: "50%" }}
           bg="layer2Gradient"
           p={12}
           textAlign="center"
-          display="flex"
           flexDir="column"
         >
           <Text fontSize="md" fontWeight="bold">
@@ -101,9 +114,23 @@ const CommunityEvents = () => {
                 </Text>
               ) : state.upcomingEventData.length ? (
                 <>
-                  <Text>Test</Text>
-                  <Text>
+                  <Text m={0} fontSize="xl">
+                    {DateTime.fromISO(state.upcomingEventData[0].date)
+                      .setLocale(language)
+                      .toLocaleString({
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                        hour12: false,
+                        hour: "numeric",
+                        minute: "numeric",
+                      })}
+                  </Text>
+                  <Text color={"bodyLight"} fontSize="md">
                     ({Intl.DateTimeFormat().resolvedOptions().timeZone})
+                  </Text>
+                  <Text fontSize="3xl" fontWeight="bold" mb={10}>
+                    {state.upcomingEventData[0].title}
                   </Text>
                 </>
               ) : (
@@ -113,19 +140,28 @@ const CommunityEvents = () => {
                   </Text>
                 </>
               )}
-              <DiscordButton />
+              <Flex flexDirection="column" gap={6}>
+                <DiscordButton />
+                {state.upcomingEventData.length && (
+                  <Link
+                    to={state.upcomingEventData[0].calenderLink}
+                    fontWeight="700"
+                  >
+                    Add to calendar
+                  </Link>
+                )}
+              </Flex>
             </Box>
           )}
-        </Box>
-        <Box
+        </Flex>
+        <Flex
           w={{ base: "100%", lg: "50%" }}
           bg="backgroundHighlight"
           p={12}
-          display="flex"
           flexDir="column"
         >
           <Text fontSize="lg" fontWeight="bold" mb={2}>
-            Upcoming
+            Upcoming calls
           </Text>
           <Divider mb={4} />
           {state.loading ? (
@@ -136,10 +172,29 @@ const CommunityEvents = () => {
             <Text color="error">
               <Translation id="loading-error-try-again-later" />
             </Text>
+          ) : state.upcomingEventData.slice(1).length ? (
+            state.upcomingEventData.slice(1).map((item) => {
+              return (
+                <Flex gap={6}>
+                  <Text>
+                    {DateTime.fromISO(item.date)
+                      .setLocale(language)
+                      .toLocaleString({
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })}
+                  </Text>
+                  <Link to={item.calenderLink} fontWeight="700">
+                    {item.title}
+                  </Link>
+                </Flex>
+              )
+            })
           ) : (
             <Text mx="auto">No upcoming calls</Text>
           )}
-          <Text fontSize="lg" fontWeight="bold" mb={2} mt={18}>
+          <Text fontSize="lg" fontWeight="bold" mb={2}>
             Previous calls
           </Text>
           <Divider mb={4} />
@@ -151,10 +206,29 @@ const CommunityEvents = () => {
             <Text color="error">
               <Translation id="loading-error-try-again-later" />
             </Text>
+          ) : state.pastEventData.length ? (
+            state.pastEventData.map((item) => {
+              return (
+                <Flex gap={6}>
+                  <Text>
+                    {DateTime.fromISO(item.date)
+                      .setLocale(language)
+                      .toLocaleString({
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })}
+                  </Text>
+                  <Link to={item.pastEventLink} fontWeight="700">
+                    {item.title}
+                  </Link>
+                </Flex>
+              )
+            })
           ) : (
             <Text mx="auto">There are no past calls</Text>
           )}
-        </Box>
+        </Flex>
       </Flex>
     </Flex>
   )
