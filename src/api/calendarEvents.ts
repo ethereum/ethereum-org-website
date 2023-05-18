ac140fcc1c484447335766318132d86c43869640
Corwin Smith <cssmittys@gmail.com>
2023-05-18 11:50:38 -0600

    setup lambda functions to get past and future calendar events from google calendar

--- src/api/calendarEvents.ts
+++ src/api/calendarEvents.ts
@@ -1,14 +1,14 @@
 import type { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby"
-
 import { lambda } from "../lambda/calendarEvents"
 
 async function handler(
   __req: GatsbyFunctionRequest,
   res: GatsbyFunctionResponse
 ): Promise<void> {
+  const { GOOGLE_API_KEY, GOOGLE_CALENDAR_ID } = process.env
   // passing env vars as arguments due to a bug on GC functions where env vars
   // can not be accessed by imported functions
-  const { statusCode, body } = await lambda()
+  const { statusCode, body } = await lambda(GOOGLE_API_KEY, GOOGLE_CALENDAR_ID)
   res.status(statusCode).send(body)
 }
 
