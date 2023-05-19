815bec9290ee982fbdeb991d94601bf5df397fb9
Corwin Smith <cssmittys@gmail.com>
2023-05-18 21:26:55 -0600

    add commonIconDefaultProps to RockXGlyphIcon

--- src/components/icons/staking/RockXGlyphIcon.tsx
+++ src/components/icons/staking/RockXGlyphIcon.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { createIcon } from "@chakra-ui/react"
+import { commonIconDefaultProps } from "../utils"
 
 export const RockXGlyphIcon = createIcon({
   displayName: "RockXGlyphIcon",
@@ -9,6 +10,7 @@ export const RockXGlyphIcon = createIcon({
     height: "33px",
     fill: "currentColor",
     fillOpacity: "0.66",
+    ...commonIconDefaultProps,
   },
   path: [
     <path d="M13.0958 13.8588C11.4265 15.5274 11.4265 18.2329 13.0958 19.9016L24.8278 31.6289C26.4971 33.2976 29.2036 33.2975 30.873 31.6289C32.5423 29.9602 32.5424 27.2547 30.873 25.586L22.1637 16.8802L28.0579 10.9882C29.7273 9.31956 29.7273 6.61413 28.0579 4.94542C26.3886 3.27672 23.6821 3.27674 22.0127 4.94542L13.0958 13.8588Z" />,
