8855bc9a75e4ca7363aa5e26ae85dccf364ddc31
Corwin Smith <cssmittys@gmail.com>
2023-05-19 11:39:40 -0600

    remove destructuring

--- src/api/calendarEvents.ts
+++ src/api/calendarEvents.ts
@@ -5,10 +5,12 @@ async function handler(
   __req: GatsbyFunctionRequest,
   res: GatsbyFunctionResponse
 ): Promise<void> {
-  const { GOOGLE_API_KEY, GOOGLE_CALENDAR_ID } = process.env
   // passing env vars as arguments due to a bug on GC functions where env vars
   // can not be accessed by imported functions
-  const { statusCode, body } = await lambda(GOOGLE_API_KEY, GOOGLE_CALENDAR_ID)
+  const { statusCode, body } = await lambda(
+    process.env.GOOGLE_API_KEY!,
+    process.env.GOOGLE_CALENDAR_ID!
+  )
   res.status(statusCode).send(body)
 }
 
