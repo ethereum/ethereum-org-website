d8f109c7c1246e0f67e8208ab6a205a2e38bf52b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-05-19 17:39:31 -0700

    add hidden description for DocSearch crawler

--- src/components/PageMetadata.tsx
+++ src/components/PageMetadata.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Box } from "@chakra-ui/react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 import { useI18next, useTranslation } from "gatsby-plugin-react-i18next"
@@ -134,91 +135,93 @@ const PageMetadata: React.FC<IProps> = ({
   const ogImageUrl = `${siteUrl}${ogImage}`
 
   return (
-    <Helmet
-      htmlAttributes={{ lang: language }}
-      title={title}
-      titleTemplate={`%s | ${siteTitle}`}
-      link={[{ rel: "canonical", key: canonical, href: canonical }]}
-      meta={[
-        {
-          name: `description`,
-          content: desc,
-        },
-        {
-          name: `image`,
-          content: site.siteMetadata.image,
-        },
-        {
-          property: `og:title`,
-          content: `${title} | ${siteTitle}`,
-        },
-        {
-          property: `og:description`,
-          content: desc,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary_large_image`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:site`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: `${title} | ${siteTitle}`,
-        },
-        {
-          name: `twitter:description`,
-          content: desc,
-        },
-        {
-          name: `twitter:image`,
-          content: ogImageUrl,
-        },
-        {
-          property: `og:url`,
-          content: siteUrl,
-        },
-        {
-          property: `og:image`,
-          content: ogImageUrl,
-        },
-        {
-          property: `og:video`,
-          content: `https://www.youtube.com/channel/UCNOfzGXD_C9YMYmnefmPH0g`,
-        },
-        {
-          property: `og:site_name`,
-          content: `ethereum.org`,
-        },
-        {
-          name: `docsearch:description`,
-          content: desc,
-        },
-      ].concat(meta)}
-    >
-      <script type="application/ld+json">
-        {`
-        {
-          "@context": "https://schema.org",
-          "@type": "Organization",
-          "url": "https://ethereum.org",
-          "email": "press@ethereum.org",
-          "name": "Ethereum",
-          "logo": "https://ethereum.org/og-image.png"
-        }
-      `}
-      </script>
-    </Helmet>
+    <>
+      <Helmet
+        htmlAttributes={{ lang: language }}
+        title={title}
+        titleTemplate={`%s | ${siteTitle}`}
+        link={[{ rel: "canonical", key: canonical, href: canonical }]}
+        meta={[
+          {
+            name: `description`,
+            content: desc,
+          },
+          {
+            name: `image`,
+            content: site.siteMetadata.image,
+          },
+          {
+            property: `og:title`,
+            content: `${title} | ${siteTitle}`,
+          },
+          {
+            property: `og:description`,
+            content: desc,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary_large_image`,
+          },
+          {
+            name: `twitter:creator`,
+            content: site.siteMetadata.author,
+          },
+          {
+            name: `twitter:site`,
+            content: site.siteMetadata.author,
+          },
+          {
+            name: `twitter:title`,
+            content: `${title} | ${siteTitle}`,
+          },
+          {
+            name: `twitter:description`,
+            content: desc,
+          },
+          {
+            name: `twitter:image`,
+            content: ogImageUrl,
+          },
+          {
+            property: `og:url`,
+            content: siteUrl,
+          },
+          {
+            property: `og:image`,
+            content: ogImageUrl,
+          },
+          {
+            property: `og:video`,
+            content: `https://www.youtube.com/channel/UCNOfzGXD_C9YMYmnefmPH0g`,
+          },
+          {
+            property: `og:site_name`,
+            content: `ethereum.org`,
+          },
+        ].concat(meta)}
+      >
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "url": "https://ethereum.org",
+            "email": "press@ethereum.org",
+            "name": "Ethereum",
+            "logo": "https://ethereum.org/og-image.png"
+          }
+        `}
+        </script>
+      </Helmet>
+      {/* Add hidden element with page description for DocSearch crawler */}
+      <Box display="none" id="page-description">
+        {desc}
+      </Box>
+    </>
   )
 }
 
