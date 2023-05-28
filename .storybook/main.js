3657475e0aa98df82f0cef172d3e95eb770109ff
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-05-28 14:20:22 -0400

    fix(.storybook/main): simplify webpackFinal and load babel presets

--- .storybook/main.js
+++ .storybook/main.js
@@ -27,30 +27,24 @@ module.exports = {
   },
   features: {},
   webpackFinal: async (config) => {
-    const isRuleExist =
-      config.module && config.module.rules && config.module.rules.length
-    if (isRuleExist) {
-      // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-      config.module.rules[0].exclude = [
-        /node_modules\/(?!(gatsby|gatsby-script)\/)/,
-      ]
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+    config.module.rules[0].use = [
+      {
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [
+            // use @babel/preset-react for JSX and env (instead of staged presets)
+            require.resolve("@babel/preset-react"),
+            require.resolve("@babel/preset-env"),
+          ],
+          plugins: [
+            // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+            require.resolve("babel-plugin-remove-graphql-queries"),
+          ],
+        },
+      },
+    ]
 
-      // Remove core-js to prevent issues with Storybook
-      config.module.rules[0].exclude = [/core-js/]
-    }
-
-    if (
-      isRuleExist &&
-      config.module.rules[0].use &&
-      config.module.rules[0].use.length
-    ) {
-      // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-      config.module.rules[0].use[0].options.plugins.push(
-        require.resolve("babel-plugin-remove-graphql-queries")
-      )
-    }
-
-    config.resolve.mainFields = ["browser", "module", "main"]
     return config
   },
   typescript: {
