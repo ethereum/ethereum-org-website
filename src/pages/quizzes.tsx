8a0ee1f855c2b384fce030d501511c4daad68718
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-05-29 15:11:20 -0300

    fix: useLocalStorage

--- src/pages/quizzes.tsx
+++ src/pages/quizzes.tsx
@@ -52,41 +52,28 @@ const INITIAL_USER_STATS = {
 // Hook
 // TODO: move to other place
 const useLocalStorage = (key, initialValue) => {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
-  const [storedValue, setStoredValue] = useState(() => {
-    if (typeof window === "undefined") {
-      return initialValue
-    }
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key)
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      // If error also return initialValue
-      console.log(error)
-      return initialValue
-    }
-  })
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
+  const [storedValue, setStoredValue] = useState(initialValue)
   const setValue = (value) => {
     try {
-      // Allow value to be a function so we have same API as useState
       const valueToStore =
         value instanceof Function ? value(storedValue) : value
-      // Save state
       setStoredValue(valueToStore)
-      // Save to local storage
-      if (typeof window !== "undefined") {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore))
-      }
+      window.localStorage.setItem(key, JSON.stringify(valueToStore))
     } catch (error) {
-      // A more advanced implementation would handle the error case
       console.log(error)
     }
   }
+
+  useEffect(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      setStoredValue(item ? JSON.parse(item) : initialValue)
+    } catch (error) {
+      console.log(error)
+      return setStoredValue(initialValue)
+    }
+  }, [])
+
   return [storedValue, setValue]
 }
 
@@ -99,7 +86,10 @@ const QuizzesHubPage = ({ data }: PageProps<Queries.QuizzesHubPageQuery>) => {
   const [nextQuiz, setNextQuiz] = useState<string | undefined>(undefined)
   const [quizStatus, setQuizStatus] = useState<QuizStatus>("neutral")
   // TODO: read stats from local storage (useLocalStorage)
-  const [userStats, setUserStats] = useState<UserStats>(INITIAL_USER_STATS)
+  const [userStats, setUserStats] = useLocalStorage(
+    USER_STATS_KEY,
+    INITIAL_USER_STATS
+  )
   const [isModalOpen, setModalOpen] = useState(false)
 
   useEffect(() => {
