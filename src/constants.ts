af2905113eb5f74488457c0c613cb6f33952fa36
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-06-04 10:55:26 -0300

    chore: update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 export const GATSBY_FUNCTIONS_PATH = process.env.GATSBY_FUNCTIONS_PATH || "/api"
 
 // Quiz Hub
+export const PROGRESS_BAR_GAP = "4px"
 export const PASSING_QUIZ_SCORE = 65
 export const USER_STATS_KEY = "quizzes-stats"
 export const INITIAL_QUIZ = "what-is-ethereum"
