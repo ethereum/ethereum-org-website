de21b31741532f4260d17841c0b955b36683c9b0
amit-ksh <ksamit1110@gmail.com>
2023-06-04 15:09:20 +0530

    heading font family

--- src/templates/docs.tsx
+++ src/templates/docs.tsx
@@ -65,7 +65,7 @@ const Divider = () => (
   />
 )
 const baseHeadingStyle: HeadingProps = {
-  fontFamily: "monospace",
+  fontFamily: "mono",
   textTransform: "uppercase",
   fontWeight: "bold",
 }
