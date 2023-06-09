56e7f60df7f9d29247bacf6e58fce02b928fb409
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-08 17:54:46 -0700

    add origin ether to staking pool services

---
+++ src/components/icons/staking/OriginEtherGlyphIcon.tsx
@@ -0,0 +1,81 @@
+import React from "react"
+import { createIcon } from "@chakra-ui/react"
+import { commonIconDefaultProps } from "../utils"
+
+export const OriginEtherGlyphIcon = createIcon({
+  displayName: "OriginEtherGlyphIcon",
+  viewBox: "0 0 33 33",
+  defaultProps: {
+    width: "33px",
+    height: "33px",
+    ...commonIconDefaultProps,
+  },
+  path: [
+    <path
+      d="M32.8906 16.7402C32.8906 25.5768 25.7272 32.7402 16.8906 32.7402C8.05407 32.7402 0.890625 25.5768 0.890625 16.7402C0.890625 7.90368 8.05407 0.740234 16.8906 0.740234C25.7272 0.740234 32.8906 7.90368 32.8906 16.7402Z"
+      fill="white"
+      fillOpacity="0.15"
+    />,
+    <path
+      d="M26.8251 6.76781C32.3104 12.2531 32.3104 21.1466 26.8251 26.6319L6.96094 6.76781C12.4463 1.28248 21.3397 1.28248 26.8251 6.76781Z"
+      fill="white"
+      fillOpacity="0.15"
+    />,
+    <path
+      d="M16.2578 10.3336L17.8448 7.73486C22.3752 8.21128 25.9059 12.0433 25.9059 16.7C25.9059 21.6516 21.9138 25.6708 16.9725 25.7146L19.1043 22.7041C21.5467 21.8035 23.2883 19.4551 23.2883 16.7C23.2883 13.1668 20.4241 10.3026 16.891 10.3026C16.6773 10.3026 16.4661 10.3131 16.2578 10.3336Z"
+      fill="url(#paint0_linear_4902_1037)"
+    />,
+    <path
+      d="M16.7107 7.68652C11.8146 7.78202 7.875 11.7808 7.875 16.6998C7.875 21.3103 11.3361 25.1124 15.8017 25.6498L17.6401 23.0536C17.394 23.0824 17.1438 23.0971 16.89 23.0971C13.3568 23.0971 10.4926 20.2329 10.4926 16.6998C10.4926 13.8519 12.3535 11.4386 14.9257 10.6096L16.7107 7.68652Z"
+      fill="url(#paint1_linear_4902_1037)"
+    />,
+    <path
+      d="M16.8984 24.1082V20.3911L12.362 17.7021L16.8984 24.1082Z"
+      fill="white"
+    />,
+    <path
+      d="M16.8984 24.1082V20.3911L21.4349 17.7021L16.8984 24.1082Z"
+      fill="#F1F1F1"
+    />,
+    <path
+      d="M16.8971 19.4672V14.7231L12.3086 16.8058L16.8971 19.4672Z"
+      fill="#E5E5E5"
+    />,
+    <path
+      d="M16.8998 19.4672V14.7231L21.4883 16.8058L16.8998 19.4672Z"
+      fill="#F1F1F1"
+    />,
+    <path
+      d="M12.31 16.8058L16.8984 9.29248V14.7232L12.31 16.8058Z"
+      fill="#F1F1F1"
+    />,
+    <path
+      d="M21.4869 16.8058L16.8984 9.29248V14.7232L21.4869 16.8058Z"
+      fill="white"
+    />,
+    <defs>
+      <linearGradient
+        id="paint0_linear_4902_1037"
+        x1="21.0819"
+        y1="23.2413"
+        x2="18.4642"
+        y2="26.8113"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="white" />
+        <stop offset="1" stopColor="white" stopOpacity="0.66" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_4902_1037"
+        x1="13.3519"
+        y1="9.87997"
+        x2="15.4588"
+        y2="6.80384"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="white" />
+        <stop offset="1" stopColor="white" stopOpacity="0.66" />
+      </linearGradient>
+    </defs>,
+  ],
+})
