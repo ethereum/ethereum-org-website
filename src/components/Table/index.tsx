61c1a55b2691e3b268900de0585409cc9b49158f
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-06-19 23:03:26 -0400

    feat(Table): create mdx component object for the table components

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -2,8 +2,13 @@ import * as React from "react"
 import {
   Table as ChakraTable,
   TableContainer,
+  Td,
+  Th,
+  Tr,
   ThemingProps,
+  Tbody,
 } from "@chakra-ui/react"
+import { MDXProviderComponentsProp } from "@mdx-js/react"
 
 interface TableProps extends ThemingProps<"Table"> {
   children: React.ReactNode
@@ -16,6 +21,14 @@ const Table = (props: TableProps) => {
       <ChakraTable layout="fixed" variant={variant} {...rest} />
     </TableContainer>
   )
+}
+
+export const mdxTableComponents: MDXProviderComponentsProp = {
+  table: Table,
+  th: ({ align, ...rest }) => <Th textAlign={align} {...rest} />,
+  td: ({ align, ...rest }) => <Td textAlign={align} {...rest} />,
+  tr: (props) => <Tr {...props} />,
+  tbody: (props) => <Tbody {...props} />,
 }
 
 export default Table
