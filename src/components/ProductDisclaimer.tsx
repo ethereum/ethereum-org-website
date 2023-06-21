c7e7f3929739202478f96fdbfafe3a0d91ee4bfb
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-06-21 07:58:58 -0700

    extract for intl

--- src/components/ProductDisclaimer.tsx
+++ src/components/ProductDisclaimer.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import InfoBanner from "./InfoBanner"
+import Translation from "./Translation"
 
 const ProductDisclaimer = () => (
   <InfoBanner emoji="⚠️" isWarning>
-    Products and services are listed as a convenience for the Ethereum
-    community. Inclusion of a product or service{" "}
-    <strong>does not represent an endorsement</strong> from the ethereum.org
-    website team, or the Ethereum Foundation.
+    <Translation id="product-disclaimer" />
   </InfoBanner>
 )
 
