00738be8e5b9092d0233026c79dc5e6486e8ff06
Joshua <62268199+minimalsm@users.noreply.github.com>
2023-06-28 19:43:14 +0100

    Refactor markdown checker to optionally get only translated markdown paths

--- src/scripts/crowdin/crowdinMain.ts
+++ src/scripts/crowdin/crowdinMain.ts
@@ -1,11 +1,11 @@
 import getAndSaveDirectories from "./getAndSaveDirectories"
 import getDirectoryIds from "./getDirectoryIds"
+import { getTranslatedMarkdownPaths } from "../markdownChecker"
 
 async function main() {
   await getAndSaveDirectories()
   const directoryIds = getDirectoryIds()
-  console.log(directoryIds)
-  console.log("done")
+  const translatedMarkdownPaths = getTranslatedMarkdownPaths()
 }
 
 main()
