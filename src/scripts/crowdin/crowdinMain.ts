f8c30a7944a4608ba21c31e801bdcfa86b8b93d8
Joshua <62268199+minimalsm@users.noreply.github.com>
2023-06-28 22:05:42 +0100

    Fetching and saving all file IDs

--- src/scripts/crowdin/crowdinMain.ts
+++ src/scripts/crowdin/crowdinMain.ts
@@ -1,11 +1,13 @@
 import getAndSaveDirectories from "./getAndSaveDirectories"
 import getDirectoryIds from "./getDirectoryIds"
 import { getTranslatedMarkdownPaths } from "../markdownChecker"
+import fetchAndSaveFileIds from "./fetchAndSaveFileIds"
 
 async function main() {
   await getAndSaveDirectories()
   const directoryIds = getDirectoryIds()
-  const translatedMarkdownPaths = getTranslatedMarkdownPaths()
+  // const translatedMarkdownPaths = getTranslatedMarkdownPaths()
+  fetchAndSaveFileIds(directoryIds)
 }
 
 main()
