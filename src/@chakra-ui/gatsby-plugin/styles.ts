97ce437bc4d50a303e2c3f35ec849e908aba5aad
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-07-06 14:37:53 -0400

    refactor(theme/styles): remove global styles for body copy and headings

--- src/@chakra-ui/gatsby-plugin/styles.ts
+++ src/@chakra-ui/gatsby-plugin/styles.ts
@@ -1,7 +1,5 @@
 import { mode } from "@chakra-ui/theme-tools"
 
-import { lightTheme as oldTheme } from "../../theme"
-
 const styles = {
   global: (props) => ({
     /**
@@ -20,14 +18,15 @@ const styles = {
       lineHeight: "base",
       fontSize: ["sm", null, "lg"],
     },
+    p: {
+      _last: {
+        mb: 0,
+      },
+    },
     a: {
       color: "primary.base",
       textDecoration: "underline",
     },
-    // should be replace with https://chakra-ui.com/docs/components/text
-    // p: {
-    //   margin: "0px 0px 1.45rem",
-    // },
     // should be replace with https://chakra-ui.com/docs/components/list
     "ul, ol": {
       margin: "0px 0px 1.45rem 1.45rem",
@@ -60,53 +59,6 @@ const styles = {
     "li > p": {
       "margin-bottom": "calc(1.45rem / 2)",
     },
-    // should be replace by the usage of https://chakra-ui.com/docs/components/heading
-    // also, the media queries defined on each of these heading tags are bearly used
-    // "h1,h2,h3,h4,h5,h6": {
-    //   margin: "2rem 0",
-    //   lineHeight: 1.4,
-    //   scrollMarginTop: "navHeight",
-    //   scrollSnapMargin: "navHeight",
-    // },
-    // h1: {
-    //   fontSize: "3rem",
-    //   fontWeight: 700,
-    //   [`@media (max-width: ${oldTheme.breakpoints.m})`]: {
-    //     fontSize: "2.5rem",
-    //   },
-    // },
-    // h2: {
-    //   fontSize: "2rem",
-    //   marginTop: "3rem",
-    //   fontWeight: 600,
-    //   [`@media (max-width: ${oldTheme.breakpoints.m})`]: {
-    //     fontSize: "1.5rem",
-    //   },
-    // },
-    // h3: {
-    //   fontSize: "1.5rem",
-    //   marginTop: "2.5rem",
-    //   fontWeight: 600,
-    //   [`@media (max-width: ${oldTheme.breakpoints.m})`]: {
-    //     fontSize: "1.25rem",
-    //   },
-    // },
-    // h4: {
-    //   fontSize: "1.25rem",
-    //   fontWeight: 500,
-    //   [`@media (max-width: ${oldTheme.breakpoints.m})`]: {
-    //     fontSize: "1rem",
-    //   },
-    // },
-    // h5: {
-    //   fontSize: "1rem",
-    //   fontWeight: 450,
-    // },
-    // h6: {
-    //   fontSize: "0.9rem",
-    //   fontWeight: 400,
-    //   textTransform: "uppercase",
-    // },
     // Anchor tag styles
     // Selected specifically for mdx rendered side icon link
     ".header-anchor": {
