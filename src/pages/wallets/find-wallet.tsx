90ee92114a53b1c05af85145ab52bdd922e49337
vijayer <vijayakumar.er@gmail.com>
2023-07-11 22:20:08 +0530

    adding frontier

--- src/pages/wallets/find-wallet.tsx
+++ src/pages/wallets/find-wallet.tsx
@@ -784,5 +784,10 @@ export const query = graphql`
         gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, quality: 100)
       }
     }
+    frontier: file(relativePath: { eq: "wallets/frontier.png" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, quality: 100)
+      }
+    }
   }
 `
