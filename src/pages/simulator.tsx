134f32b7064b8995b02a20724ab7e19cd4304572
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-08-08 11:43:32 +0200

    use Simulator component on page

--- src/pages/simulator.tsx
+++ src/pages/simulator.tsx
@@ -1,35 +1,11 @@
-import { Flex } from "@chakra-ui/react"
 import React from "react"
-import { PathButton } from "../components/Simulator/"
-import type { PathOption } from "../interfaces"
+import { Simulator } from "../components/Simulator"
+import { Box } from "@chakra-ui/react"
 
-const pathOptions: Array<PathOption> = [
-  {
-    primaryText: "Create account",
-    secondaryText: "How to?",
-    iconName: "EthWalletIcon",
-    onClick: () => console.log("clicked Create Account"),
-  },
-  {
-    primaryText: "Send/receive",
-    secondaryText: "How to?",
-    iconName: "WalletAppIcon",
-    onClick: () => console.log("clicked Send/Receive"),
-  },
-  {
-    primaryText: "Connect to Web3",
-    secondaryText: "How to?",
-    iconName: "ConnectWeb3Icon",
-    onClick: () => console.log("clicked Connect to Web3"),
-  },
-]
 const SimulatorPage = () => (
-  /* Button stack for path options */
-  <Flex direction="column" gap={8} w="min(100%, 300px)">
-    {pathOptions.map((pathOption) => (
-      <PathButton pathOption={pathOption} />
-    ))}
-  </Flex>
+  <Box py={16} w="full">
+    <Simulator />
+  </Box>
 )
 
 export default SimulatorPage
