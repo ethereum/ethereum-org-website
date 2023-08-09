ac54716c6f80d85a91071cf3648945e1f49409c2
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-08-09 17:52:19 +0200

    refactor SimulatorModel to custom modal

--- src/components/Simulator/SimulatorModal.tsx
+++ src/components/Simulator/SimulatorModal.tsx
@@ -1,12 +1,53 @@
 import React from "react"
-import Modal from "../Modal"
 
-interface IProps {
-  isOpen: boolean
-  setIsOpen: (isOpen: boolean) => void
+import {
+  Modal as ChakraModal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  type ModalProps,
+  type ModalContentProps,
+  UseDisclosureReturn,
+} from "@chakra-ui/react"
+
+export interface IPropsOverlay {
+  isActive: boolean
 }
-export const SimulatorModal: React.FC<IProps> = ({ isOpen, setIsOpen }) => (
-  <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-    Hello world
-  </Modal>
-)
+
+export interface IProps extends ModalContentProps, Pick<ModalProps, "size"> {
+  disclosure: UseDisclosureReturn
+  children?: React.ReactNode
+}
+
+export const SimulatorModal: React.FC<IProps> = ({
+  disclosure: { isOpen, onClose },
+  children,
+  size,
+  ...restProps
+}) => {
+  return (
+    <ChakraModal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+      size="full"
+      scrollBehavior="inside"
+    >
+      <ModalOverlay bgColor="blackAlpha.700" />
+
+      <ModalContent
+        p={8}
+        shadow="md"
+        border="1px"
+        borderColor="border"
+        borderRadius="md"
+        maxW={{ base: "100%", md: "1000px" }}
+        mx={{ base: 4, md: "auto" }}
+        {...restProps}
+      >
+        <ModalCloseButton />
+        {children}
+      </ModalContent>
+    </ChakraModal>
+  )
+}
