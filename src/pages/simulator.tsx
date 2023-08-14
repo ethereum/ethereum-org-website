8e7f6b6bb9ff7ca3d273d50163073c00c90ad501
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-08-14 12:46:20 +0200

    Update simulator.tsx

--- src/pages/simulator.tsx
+++ src/pages/simulator.tsx
@@ -1,11 +1,38 @@
 import React from "react"
-import { Simulator } from "../components/Simulator"
-import { Box } from "@chakra-ui/react"
+import {
+  SimulatorModal,
+  StartingPoint,
+  Template,
+} from "../components/Simulator/index"
+import { Box, useDisclosure } from "@chakra-ui/react"
 
-const SimulatorPage = () => (
-  <Box py={16} w="full">
-    <Simulator />
-  </Box>
-)
+const SimulatorPage = () => {
+  const disclosure = useDisclosure()
+  return (
+    <Box py={16} w="full">
+      {/* Temporary button to trigger modal */}
+      <Box
+        borderRadius="xl"
+        color="salmon"
+        fontWeight="bold"
+        m={6}
+        onClick={disclosure.onOpen}
+        textAlign="center"
+        border="2px solid salmon"
+        py={4}
+        cursor="pointer"
+        _hover={{
+          bg: "body.light",
+        }}
+      >
+        Open
+      </Box>
+      <StartingPoint />
+      <SimulatorModal disclosure={disclosure}>
+        <Template />
+      </SimulatorModal>
+    </Box>
+  )
+}
 
 export default SimulatorPage
