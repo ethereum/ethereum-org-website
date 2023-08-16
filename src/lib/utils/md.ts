645f3795e9d9b073c05bde6f36a5d3b95af54fae
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-08-16 15:38:55 -0300

    feat: add /guides/how-to-swap-tokens content page

--- src/lib/utils/md.ts
+++ src/lib/utils/md.ts
@@ -16,6 +16,7 @@ const getPostSlugs = (dir: string, files: string[] = []) => {
     "/community/code-of-conduct",
     "/community/support",
     "/energy-consumption",
+    "/guides/how-to-swap-tokens",
     "/history/",
     "/smart-contracts",
     "/whitepaper",
