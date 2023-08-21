08ff3a348383680c79e14bf661567a4d5189b143
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-08-21 07:01:23 -0700

    Update Phone.tsx

--- src/components/Simulator/Explanation.tsx
+++ src/components/Simulator/Explanation.tsx
@@ -14,6 +14,7 @@ import Tooltip from "../Tooltip"
 import { SimulatorStateProps } from "../../interfaces"
 import Emoji from "../Emoji"
 
+// TODO: Relocate data
 const explanationData = {
   "create-account": [
     {
--- src/components/Simulator/Phone.tsx
+++ src/components/Simulator/Phone.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import {
   Box,
   Flex,
@@ -16,25 +16,51 @@ import type { SimulatorStateProps } from "../../interfaces"
 import { EthGlyphIcon, HomeScreen, ProgressCta } from "./"
 import { motion } from "framer-motion"
 import { WORDLISTS } from "../../data/bip39Words"
+import { PiCheckThin } from "react-icons/pi"
 
 const MotionFlex = motion(Flex)
 const FadeInFlex = (props) => (
   <MotionFlex initial={{ opacity: 0 }} animate={{ opacity: 1 }} {...props} />
 )
 const GeneratingKeysComponent: React.FC<SimulatorStateProps> = ({ state }) => {
+  const [loading, setLoading] = useState<boolean>(true)
   const { progressStepper } = state
+
+  // Show spinner for 2100ms, switching "loading" state to false when complete
   useEffect(() => {
-    const timeout = setTimeout(progressStepper, 2100)
+    const timeout = setTimeout(() => {
+      setLoading(false)
+    }, 2100)
     return () => {
       clearTimeout(timeout)
     }
   }, [])
+
+  // After loading is complete, wait another 1600ms before calling progressStepper
+  useEffect(() => {
+    if (loading) return
+    const timeout = setTimeout(progressStepper, 1600)
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [loading])
+
+  const ICON_SIZE = "4.5rem" as const
   return (
-    <Grid placeItems="center" h="full">
+    <Grid placeItems="center" h="full" bg="background.highlight">
       <Flex direction="column" alignItems="center" pt={8} gap={4}>
-        <Spinner w="4.5rem" h="4.5rem" />
+        {loading ? (
+          <Spinner w={ICON_SIZE} h={ICON_SIZE} />
+        ) : (
+          <Icon
+            as={PiCheckThin}
+            w={ICON_SIZE}
+            h={ICON_SIZE}
+            transform="rotate(-10deg)"
+          />
+        )}
         <Text textAlign="center" px={{ base: 4, md: 8 }}>
-          Generating your recovery phrase
+          {loading ? "Generating your recovery phrase" : "Account created"}
         </Text>
       </Flex>
     </Grid>
@@ -127,7 +153,13 @@ const CreateAccountScreens: React.FC<SimulatorStateProps> = ({ state }) => {
     <>
       {[0, 1].includes(step) && <HomeScreen state={state} />}
       {[2].includes(step) && (
-        <FadeInFlex direction="column" alignItems="center" pt={8}>
+        <FadeInFlex
+          direction="column"
+          alignItems="center"
+          pt={8}
+          h="full"
+          bg="background.highlight"
+        >
           <Icon
             as={EthGlyphIcon}
             color="body.base"
@@ -150,7 +182,7 @@ const CreateAccountScreens: React.FC<SimulatorStateProps> = ({ state }) => {
       )}
       {[3].includes(step) && <GeneratingKeysComponent state={state} />}
       {[4].includes(step) && (
-        <Box py={8} px={{ base: 4, md: 8 }}>
+        <Box py={8} px={{ base: 4, md: 8 }} h="full" bg="background.highlight">
           <Text fontSize="2xl" lineHeight={8} fontWeight="bold">
             Recovery phrase
           </Text>
@@ -187,6 +219,7 @@ const CreateAccountScreens: React.FC<SimulatorStateProps> = ({ state }) => {
 export const Phone: React.FC<SimulatorStateProps> = ({ state }) => {
   const { pathId } = state
 
+  // TODO: Relocate data: store component name, dynamically load component then pass state
   const screenData = {
     "create-account": <CreateAccountScreens state={state} />,
   }
