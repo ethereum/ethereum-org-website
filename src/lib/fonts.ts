ffc8494008461c8a1a38b2c3da1d04275afed146
Pablo Pettinari <pettinarip@gmail.com>
2023-08-23 14:27:52 +0200

    remove display swap since its the default value + remove weight from inter to use the variable font type

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,14 +3,11 @@ import { IBM_Plex_Mono } from "next/font/google"
 
 export const inter = Inter({
   subsets: ["latin"],
-  display: "swap",
-  weight: ["400", "700"],
   fallback: ["sans-serif"],
 })
 
 export const mono = IBM_Plex_Mono({
   subsets: ["latin"],
-  display: "swap",
   weight: ["400"],
   fallback: ["Courier", "monospace"],
 })
