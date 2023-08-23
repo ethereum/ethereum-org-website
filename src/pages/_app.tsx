23dd3a44561d28386f4d3430dbb4e5a1abc24002
Pablo Pettinari <pettinarip@gmail.com>
2023-08-23 14:21:58 +0200

    refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,25 +2,12 @@ import type { AppProps } from "next/app"
 // ChakraProvider import updated as recommended on https://github.com/chakra-ui/chakra-ui/issues/4975#issuecomment-1174234230
 // to reduce bundle size. Should be reverted to "@chakra-ui/react" in case on theme issues
 import { ChakraProvider } from "@chakra-ui/provider"
-import { Inter } from "next/font/google"
-import { IBM_Plex_Mono } from "next/font/google"
 
 // Chakra custom theme
 import theme from "@/@chakra-ui/theme"
 
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["400", "700"],
-  fallback: ["sans-serif"],
-})
-
-const mono = IBM_Plex_Mono({
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["400"],
-  fallback: ["Courier", "monospace"],
-})
+// Fonts
+import { inter, mono } from "@/lib/fonts"
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
