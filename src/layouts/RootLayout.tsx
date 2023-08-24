5a6289320398a4ad9050a3da658071eed06f75b8
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-08-24 20:15:35 -0300

    chore: update Root layout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer"
 
 export const RootLayout = ({ children }) => (
   <Box px={20}>
-    {/* TODO: get proper path value */}
+    {/* TODO: get proper path value after setting i18n */}
     <Nav path="" />
 
     {children}
