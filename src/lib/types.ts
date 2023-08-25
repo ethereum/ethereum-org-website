cf50b5055cae64a5b5cd4d73b1d59dd3caf90ad6
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-08-24 21:19:19 -0300

    chore: update /lib/types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,13 @@
-import { ReactNode } from "react"
+import { ReactElement, ReactNode } from "react"
+import { NextPage } from "next"
+import { AppProps } from "next/app"
 
 export type ChildOnlyProp = { children?: ReactNode }
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+export type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
