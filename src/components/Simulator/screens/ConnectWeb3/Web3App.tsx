aa1e7cc27e2e2c0992f06d88499e380036653b92
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-08-28 18:51:38 -0700

    Add Web3App.tsx

---
+++ src/components/Simulator/screens/ConnectWeb3/Web3App.tsx
@@ -0,0 +1,27 @@
+import {
+  Box,
+  type BoxProps,
+  Icon,
+  Text,
+  Flex,
+  useColorModeValue,
+} from "@chakra-ui/react"
+import React from "react"
+import { GrMenu } from "react-icons/gr"
+import { EthGlyphIcon } from "../../icons"
+
+interface IProps extends BoxProps {}
+export const Web3App: React.FC<IProps> = ({ children, ...boxProps }) => (
+  <Box h="full" w="full" bg="background.highlight" {...boxProps}>
+    <Box p={1} bg={useColorModeValue("#e8e8e8", "#171717")}>
+      <Text textAlign="center" fontSize="xs" m={0}>
+        Example.com
+      </Text>
+    </Box>
+    <Flex justify="space-between" p={6} fontSize="4xl">
+      <Icon as={EthGlyphIcon} />
+      <Icon as={GrMenu} ms="auto" />
+    </Flex>
+    <>{children}</>
+  </Box>
+)
