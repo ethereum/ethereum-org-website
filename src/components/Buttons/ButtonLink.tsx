7d037f8b4a033b0deb3cc6e1edb0f2c1679bdda7
Pablo Pettinari <pettinarip@gmail.com>
2023-09-29 14:56:56 +0200

    fix types

--- src/components/Buttons/ButtonLink.tsx
+++ src/components/Buttons/ButtonLink.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 
 import type { IProps as IButtonProps } from "./Button"
-import { BaseLink, IBaseProps as ILinkProps } from "../Link"
+import { BaseLink, LinkProps } from "../Link"
 import Button from "./Button"
 
-export interface IProps extends ILinkProps, Omit<IButtonProps, "toId"> {}
+export type ButtonLinkProps = LinkProps & Omit<IButtonProps, "toId">
 
-const ButtonLink: React.FC<IProps> = (props) => {
+const ButtonLink: React.FC<ButtonLinkProps> = (props) => {
   return <Button as={BaseLink} activeStyle={{}} {...props} />
 }
 
