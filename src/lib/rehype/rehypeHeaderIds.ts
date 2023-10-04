4d6f089faec0a79c5cc0cf3f487d2e4d327c6548
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-10-04 12:36:02 -0700

    fix type issues for rehypeHeaderIds

--- src/lib/rehype/rehypeHeaderIds.ts
+++ src/lib/rehype/rehypeHeaderIds.ts
@@ -1,28 +1,31 @@
 import { visit } from "unist-util-visit"
 import type { Plugin } from "unified"
-import type { Element, Root } from "hast"
+import type { ElementContent, Root } from "hast"
 import { getCustomId, trimmedTitle } from "@/lib/utils/toc"
 
-interface Options {
-  dir: string
-  srcPath: string
-}
+interface Options {}
 
 const setMarkdownHeaderIds: Plugin<[Options], Root> = () => (tree, _file) => {
-  visit(tree, "element", (node: Element) => {
+  visit(tree, "element", (node) => {
     // Ignore non-heading elements
     const headingElements = ["h1", "h2", "h3", "h4", "h5", "h6"]
     if (!headingElements.includes(node.tagName as string)) return
-    // TODO: Fix type issue
     const concatenated: string = node.children.reduce(
-      (acc, { value }) => acc + value,
+      (acc: string, child: ElementContent) =>
+        ("value" in child ? acc + child.value : acc) as string,
       ""
     )
     node.properties.id = getCustomId(concatenated)
     const lastIndex = node.children.length - 1
-    const lastChild: string = node.children[lastIndex].value
-    node.children[lastIndex].value = trimmedTitle(lastChild)
-    // TODO: Add pseudoelement to heading to allow for anchor links
+    const lastChild = node.children[lastIndex] as ElementContent
+    if ("value" in lastChild) {
+      const lastChildValue: string = lastChild.value as string
+      node.children[lastIndex] = {
+        type: "text",
+        value: trimmedTitle(lastChildValue),
+      }
+    }
+    // TODO: Add pseudo-element to heading to allow for anchor links
   })
 }
 
