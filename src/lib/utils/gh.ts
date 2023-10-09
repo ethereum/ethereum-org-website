4a5b45545fb3fa084b760c5291d4538b25c0eb4c
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-10-09 20:50:34 -0300

    utils: update  util to get proper date for translated content

--- src/lib/utils/gh.ts
+++ src/lib/utils/gh.ts
@@ -1,8 +1,12 @@
 import { join } from "path"
 
-import { LAST_COMMIT_BASE_URL, OLD_CONTENT_DIR } from "../constants"
+import {
+  LAST_COMMIT_BASE_URL,
+  OLD_CONTENT_DIR,
+  OLD_TRANSLATIONS_DIR,
+} from "../constants"
 
-export const getLastModifiedDate = async (filePath: string) => {
+export const getLastModifiedDate = async (filePath: string, locale: string) => {
   const headers = new Headers({
     // About personal access tokens https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#about-personal-access-tokens
     Authorization: "Token " + process.env.GITHUB_TOKEN_READ_ONLY,
@@ -10,7 +14,18 @@ export const getLastModifiedDate = async (filePath: string) => {
   const url = new URL(LAST_COMMIT_BASE_URL)
   // TODO: swap `OLD_CONTENT_DIR` for new `CONTENT_DIR` constant value before deploying site to prod
   // as we're currently fetching last commit date from canonical repo
-  url.searchParams.set("path", join(OLD_CONTENT_DIR, filePath, "index.md"))
+
+  // TODO: swap `OLD_TRANSLATIONS_DIR` for new `TRANSLATIONS_DIR` constant value before deploying site to prod
+  // as we're currently fetching last commit date from canonical repo
+
+  if (locale === "en") {
+    url.searchParams.set("path", join(OLD_CONTENT_DIR, filePath, "index.md"))
+  } else {
+    url.searchParams.set(
+      "path",
+      join(OLD_TRANSLATIONS_DIR, locale, filePath, "index.md")
+    )
+  }
   url.searchParams.set("page", "1")
   url.searchParams.set("per_page", "1")
 
