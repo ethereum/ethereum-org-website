a3fb687f33dded27dd1070a74c0f0beb5a3860fe
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-10-10 11:54:11 -0300

    chore: remove  util

--- src/lib/utils/i18n.ts
+++ src/lib/utils/i18n.ts
@@ -1,10 +1,5 @@
 import { join } from "path"
 
-import i18nConfig from "../../../i18n.config.json"
-
-// Return a sorted list of supported locales codes, defined in `i18n.config.json`
-export const getLocalesCodes = () => i18nConfig.map((lang) => lang.code).sort()
-
 // If content is not translated, read it from english path as fallback
 export const getFallbackEnglishPath = (path: string) => {
   const splittedPath = path.split("translations/")
