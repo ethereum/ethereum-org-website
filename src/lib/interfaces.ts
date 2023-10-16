a74686944c5385ec355408728d57c94a40197213
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-10-16 12:32:25 -0300

    chore: add Root interface

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -56,6 +56,13 @@ export interface ToCItem {
 /**
  * Layout interface
  */
+
+export interface Root {
+  children: JSX.Element
+  contentIsOutdated: boolean
+  contentNotTranslated: boolean
+}
+
 export interface PageContent {
   slug: string
   content: string
