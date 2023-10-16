4b5b2c8dcc65b9c1017c48a22c028c68f9527d84
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-10-16 00:01:10 -0300

    chore: add TranslationBanner & TranslationBannerLegal to RootLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,17 +1,65 @@
 import { Container } from "@chakra-ui/react"
+import { useRouter } from "next/router"
+import { join } from "path"
 
 import Nav from "@/components/Nav"
 import Footer from "@/components/Footer"
+import TranslationBanner from "@/components/TranslationBanner"
+import TranslationBannerLegal from "@/components/TranslationBannerLegal"
+
+import { isLangRightToLeft } from "@/lib/utils/translations"
+
+import { Lang } from "@/lib/types"
+
+import { DEFAULT_LOCALE } from "@/lib/constants"
 
 import { lightTheme as oldTheme } from "../theme"
 
-export const RootLayout = ({ children }) => (
-  <Container mx="auto" maxW={oldTheme.variables.maxPageWidth}>
-    {/* TODO: get proper path value after setting i18n */}
-    <Nav path="" />
+export const RootLayout = ({
+  children,
+  contentIsOutdated,
+  contentNotTranslated,
+}) => {
+  const { locale, asPath } = useRouter()
 
-    {children}
+  const isLegal =
+    asPath.includes(`/cookie-policy/`) ||
+    asPath.includes(`/privacy-policy/`) ||
+    asPath.includes(`/terms-of-use/`) ||
+    asPath.includes(`/contributing/`) ||
+    asPath.includes(`/style-guide/`)
 
-    <Footer />
-  </Container>
-)
+  const isPageTranslationOutdated = contentIsOutdated ?? false
+  const isPageContentEnglish = contentNotTranslated
+  const isPageLanguageEnglish = locale === DEFAULT_LOCALE
+  const shouldShowTranslationBanner =
+    (isPageTranslationOutdated ||
+      (isPageContentEnglish && !isPageLanguageEnglish)) &&
+    !isLegal
+  const isPageRightToLeft = isLangRightToLeft(locale as Lang)
+  const originalPagePath = join(DEFAULT_LOCALE, asPath)
+
+  return (
+    <Container mx="auto" maxW={oldTheme.variables.maxPageWidth}>
+      {/* TODO: get proper path value after setting i18n */}
+      <Nav path="" />
+
+      <TranslationBanner
+        shouldShow={shouldShowTranslationBanner}
+        isPageContentEnglish={isPageContentEnglish}
+        isPageRightToLeft={isPageRightToLeft}
+        originalPagePath={originalPagePath}
+      />
+
+      <TranslationBannerLegal
+        shouldShow={isLegal}
+        isPageRightToLeft={isPageRightToLeft}
+        originalPagePath={originalPagePath}
+      />
+
+      {children}
+
+      <Footer />
+    </Container>
+  )
+}
