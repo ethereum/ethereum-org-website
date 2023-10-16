9f4b10bc4414cd1653b0b6682382730dd0a5a685
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-10-16 12:06:38 -0700

    migrate and implement scrollIntoView util

---
+++ src/lib/utils/scrollIntoView.ts
@@ -0,0 +1,8 @@
+export const scrollIntoView = (
+  toId: string,
+  options: ScrollIntoViewOptions = { behavior: "smooth", block: "start" }
+): void => {
+  const element = document.getElementById(toId)
+  if (!element) return
+  element.scrollIntoView(options)
+}
