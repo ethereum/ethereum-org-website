79c2f0e7f0a6d2f674c64174da987c3e0dbd0d0b
Nico <nh.quiroz@gmail.com>
2023-10-16 18:56:15 -0300

    Update src/lib/utils/scrollIntoView.ts

--- src/lib/utils/scrollIntoView.ts
+++ src/lib/utils/scrollIntoView.ts
@@ -3,6 +3,8 @@ export const scrollIntoView = (
   options: ScrollIntoViewOptions = { behavior: "smooth", block: "start" }
 ): void => {
   const element = document.getElementById(toId)
+
   if (!element) return
+  
   element.scrollIntoView(options)
 }
