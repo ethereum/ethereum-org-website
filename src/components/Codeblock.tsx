498ef3b1cdf23209ad68196736f8eebe9b59b4f3
Corwin Smith <cssmittys@gmail.com>
2023-10-19 13:01:55 -0600

    Update src/components/Codeblock.tsx
    Co-authored-by: Paul Wackerow <54227730+wackerow@users.noreply.github.com>

--- src/components/Codeblock.tsx
+++ src/components/Codeblock.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { Box, BoxProps, Flex, useColorModeValue } from "@chakra-ui/react"
 import Highlight, {
   defaultProps,
