8efa09e747ac4e772c468cc8168d4aeb389c3d4d
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-10-20 13:47:33 -0700

    revert removal of React import
    fixes React.Method usage

--- src/components/Codeblock.tsx
+++ src/components/Codeblock.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Box, BoxProps, Flex, useColorModeValue } from "@chakra-ui/react"
 import Highlight, {
   defaultProps,
