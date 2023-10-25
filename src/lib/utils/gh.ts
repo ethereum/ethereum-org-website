4cc1d19a58d36bd827aa166cff20e5d8e4e7499d
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-10-25 13:56:50 -0700

    implement getLastTagDate using git log
    deprecate getLastDeployDate GitHub API fetch

--- src/lib/utils/gh.ts
+++ src/lib/utils/gh.ts
@@ -2,12 +2,7 @@ import fs from "fs"
 import { join } from "path"
 import { execSync } from "child_process"
 
-import {
-  CONTENT_DIR,
-  DEFAULT_LOCALE,
-  LAST_DEPLOY_BASE_URL,
-  TRANSLATIONS_DIR,
-} from "../constants"
+import { CONTENT_DIR, DEFAULT_LOCALE, TRANSLATIONS_DIR } from "../constants"
 
 // This util filters the git log to get the file last commit info, and then the commit date (last update)
 export const getLastModifiedDate = async (slug: string, locale: string) => {
@@ -37,29 +32,7 @@ export const getLastModifiedDate = async (slug: string, locale: string) => {
   return new Date(lastCommitDate).toISOString()
 }
 
-export const getLastDeployDate = async () => {
-  const headers = new Headers({
-    // About personal access tokens https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#about-personal-access-tokens
-    Authorization: "Token " + process.env.GITHUB_TOKEN_READ_ONLY,
-  })
-
-  try {
-    // Fetch data from closed PRs made to the master branch
-    const response = await fetch(LAST_DEPLOY_BASE_URL, { headers })
-    const pullRequests = await response.json()
-
-    // Get the `merged_at` date (deployment date) from last deploy PR made to the master branch
-    const deploys = pullRequests.filter(({ title }: { title: string }) =>
-      title.toLowerCase().includes("deploy")
-    )
-
-    if (deploys.length <= 0) throw new Error("No deploy PRs found")
-    const lastDeployDate: string = deploys[0].merged_at
-
-    return lastDeployDate
-  } catch (err) {
-    console.error(err)
-    // If API fetch fails, use current date as fallback date
-    return new Date().toISOString()
-  }
+export const getLastTagDate = () => {
+  const cmd = "git log -1 --format=%ai $(git describe --tags --abbrev=0)"
+  return new Date(execSync(cmd).toString()).toISOString()
 }
