562af534702e9535f76f5a42adae0b1e8d7ad92a
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-10-30 14:19:30 +0100

    feat: update Translation component

--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -1,8 +1,6 @@
-import React from "react"
-// TODO: enable these deps after i18n is set up
-// import { useTranslation } from "gatsby-plugin-react-i18next"
+import { useTranslation } from "next-i18next"
 // import { TOptions } from "i18next"
-// import htmr from "htmr"
+import htmr from "htmr"
 
 import InlineLink from "./Link"
 
@@ -20,14 +18,12 @@ const transform = {
 // Renders the translation string for the given translation key `id`. It
 // fallback to English if it doesn't find the given key in the current language
 const Translation = ({ id, options }: Props) => {
-  // const { t } = useTranslation()
-
-  // const translatedText = t(id, options)
+  const { t } = useTranslation()
+  const translatedText = t(id, options)
 
   // Use `htmr` to parse html content in the translation text
   // @ts-ignore
-  // return <>{htmr(translatedText, { transform })}</> TODO: add this return later
-  return null
+  return <>{htmr(translatedText, { transform })}</>
 }
 
 export default Translation
