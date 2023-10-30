8b084bbf2eca2bb2612b2cbec0d28eb5f72f2a46
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-10-30 16:45:52 -0700

    remove redundant config
    Co-Authored-By: Pablo Pettinari <pettinarip@gmail.com>

--- src/lib/utils/getCrowdinCode.ts
+++ src/lib/utils/getCrowdinCode.ts
@@ -3,7 +3,7 @@ import type { I18nLocale } from "@/lib/types"
 
 async function getCrowdinCode(langCode: string): Promise<string> {
   try {
-    const data = await fs.promises.readFile("i18n/config.json", "utf-8")
+    const data = await fs.promises.readFile("i18n.config.json", "utf-8")
     const langs: I18nLocale[] = JSON.parse(data)
     const lang = langs.find((lang) => lang.code === langCode)
 
@@ -22,7 +22,7 @@ export async function getLangCodeFromCrowdinCode(
   crowdinCode: string
 ): Promise<string> {
   try {
-    const data = await fs.promises.readFile("i18n/config.json", "utf-8")
+    const data = await fs.promises.readFile("i18n.config.json", "utf-8")
     const locales: I18nLocale[] = JSON.parse(data)
     const locale = locales.find((item) => item.crowdinCode === crowdinCode)
 
