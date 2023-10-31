41776b60105c383ff9a1a2c645939c837421e7ad
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-10-31 16:36:12 +0100

    i18n: updates Translation component to load required namespaces per page

--- src/components/Translation.tsx
+++ src/components/Translation.tsx
@@ -1,12 +1,13 @@
+import { useEffect, useState } from "react"
 import { useTranslation } from "next-i18next"
-// import { TOptions } from "i18next"
 import htmr from "htmr"
+import { useRouter } from "next/router"
 
 import InlineLink from "./Link"
 
 interface Props {
   id: string
-  options?: any // TODO: change to TOptions later
+  options?: any
 }
 
 // Custom components mapping to be used by `htmr` when parsing the translation
@@ -18,7 +19,36 @@ const transform = {
 // Renders the translation string for the given translation key `id`. It
 // fallback to English if it doesn't find the given key in the current language
 const Translation = ({ id, options }: Props) => {
-  const { t } = useTranslation()
+  const [requiredNamespaces, setRequiredNamespaces] = useState<string[]>([])
+  const { asPath } = useRouter()
+
+  useEffect(() => {
+    if (asPath.startsWith("/community")) {
+      setRequiredNamespaces([...requiredNamespaces, "page-community"])
+    }
+
+    if (asPath.startsWith("/energy-consumption")) {
+      setRequiredNamespaces([
+        ...requiredNamespaces,
+        "page-about",
+        "page-what-is-ethereum",
+      ])
+    }
+
+    if (asPath.startsWith("/history")) {
+      setRequiredNamespaces([...requiredNamespaces, "page-history"])
+    }
+
+    if (asPath.startsWith("/nft")) {
+      setRequiredNamespaces([...requiredNamespaces, "learn-quizzes"])
+    }
+
+    if (asPath.startsWith("/staking")) {
+      setRequiredNamespaces([...requiredNamespaces, "page-staking"])
+    }
+  }, [requiredNamespaces])
+
+  const { t } = useTranslation(requiredNamespaces)
   const translatedText = t(id, options)
 
   // Use `htmr` to parse html content in the translation text
