43cfe25b83609d085f36484504bf903aa3dcbefa
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-10-31 16:28:06 +0100

    i18n: enable Translation for Callout

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -1,7 +1,6 @@
 import { Flex, type FlexProps } from "@chakra-ui/react"
+import { useTranslation } from "next-i18next"
 
-// TODO: Re-enable once i18n is implemented
-// import Translation from "./Translation"
 import Emoji from "@/components/Emoji"
 import Text from "@/components/OldText"
 import OldHeading from "@/components/OldHeading"
@@ -28,52 +27,53 @@ const Callout: React.FC<IProps> = ({
   children,
   className,
   ...rest
-}) => (
-  <Flex
-    as="aside"
-    direction="column"
-    bgGradient="linear-gradient(
+}) => {
+  const { t } = useTranslation("common")
+
+  return (
+    <Flex
+      as="aside"
+      direction="column"
+      bgGradient="linear-gradient(
     49.21deg,
     rgba(127, 127, 213, 0.2) 19.87%,
     rgba(134, 168, 231, 0.2) 58.46%,
     rgba(145, 234, 228, 0.2) 97.05%
   )"
-    p={6}
-    m={4}
-    mt={32}
-    mb={{ base: 16, lg: 4 }}
-    borderRadius="base"
-    className={className}
-    {...rest}
-  >
-    {image && (
-      <Image
-        src={image}
-        alt={alt || ""}
-        mt={-40}
-        alignSelf="center"
-        w={263}
-        h={200}
-        maxW="263px"
-        minH="200px"
-      />
-    )}
-    <Flex direction="column" justify="space-between" mt={10} h="full">
-      <div>
-        {emoji && <Emoji text={emoji} fontSize="5xl" />}
-        <OldHeading as="h3" fontSize="2xl" lineHeight={1.4}>
-          {/* TODO: Re-enable after i18n implemented and remove placeholders */}
-          {/* <Translation id={titleKey} /> */}
-          {titleKey}
-        </OldHeading>
-        <Text color="text200" fontSize="xl" lineHeight="140%">
-          {/* <Translation id={descriptionKey} /> */}
-          {descriptionKey}
-        </Text>
-      </div>
-      {children}
+      p={6}
+      m={4}
+      mt={32}
+      mb={{ base: 16, lg: 4 }}
+      borderRadius="base"
+      className={className}
+      {...rest}
+    >
+      {image && (
+        <Image
+          src={image}
+          alt={alt || ""}
+          mt={-40}
+          alignSelf="center"
+          w={263}
+          h={200}
+          maxW="263px"
+          minH="200px"
+        />
+      )}
+      <Flex direction="column" justify="space-between" mt={10} h="full">
+        <div>
+          {emoji && <Emoji text={emoji} fontSize="5xl" />}
+          <OldHeading as="h3" fontSize="2xl" lineHeight={1.4}>
+            {t(titleKey)}
+          </OldHeading>
+          <Text color="text200" fontSize="xl" lineHeight="140%">
+            {t(descriptionKey)}
+          </Text>
+        </div>
+        {children}
+      </Flex>
     </Flex>
-  </Flex>
-)
+  )
+}
 
 export default Callout
