aaae2726a9a6be15f0e5d8376e95533cb9e6ee98
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-10-31 05:04:59 +0100

    Update src/scripts/updatePublishDate.sh

--- src/scripts/updatePublishDate.sh
+++ src/scripts/updatePublishDate.sh
@@ -3,5 +3,5 @@
 # Intended to be run as a `preversion` lifecycle hook as part of `yarn version`
 # 1. Update `published.json` file with the current date (UTC)
 echo "{\"date\":\"$(date -u +%Y-%m-%d)\"}" > src/data/published.json
-# 2. Stage changes before being committed during `version` livecycle hook
+# 2. Stage changes before being committed during `version` lifecycle hook
 git add src/data/published.json
