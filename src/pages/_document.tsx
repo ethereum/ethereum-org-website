c17481daff9790d4a7d4090792b7b53886e2da14
Nicolás Quiroz <nh.quiroz@gmail.com>
2023-11-02 22:43:58 +0100

    theme: add ColorModeScript to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,14 @@
+import { ColorModeScript } from "@chakra-ui/react"
 import { Html, Head, Main, NextScript } from "next/document"
+
+import theme from "@/@chakra-ui/theme"
 
 export default function Document() {
   return (
     <Html>
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
