86f32f24500543e35a63c2ad4048513a044391b5
Pablo Pettinari <pettinarip@gmail.com>
2023-11-03 15:42:03 +0100

    remove unnecessary styles

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode } from "react"
 import {
-  Box,
   Center,
   Popover,
   PopoverArrow,
@@ -17,52 +16,23 @@ export interface IProps extends PopoverProps {
 
 const Tooltip: React.FC<IProps> = ({ content, children, ...rest }) => {
   return (
-    <Box display="inline-block" ml={2}>
-      <Popover
-        placement="top"
-        trigger="hover"
-        strategy="fixed"
-        gutter={6}
-        {...rest}
-      >
-        <PopoverTrigger>
-          <Center
-            as="span"
-            tabIndex={0}
-            p={1}
-            cursor="pointer"
-            color="body.base"
-            _hover={{
-              color: "primary.base",
-            }}
-            _active={{
-              color: "primary.hover",
-            }}
-            _focusVisible={{
-              outline: "none",
-              color: "primary.base",
-              border: "2px",
-              borderColor: "primary.hover",
-              borderRadius: "full",
-            }}
-            sx={{
-              svg: {
-                w: 3,
-                h: 3,
-                ml: 0,
-                color: "inherit",
-              },
-            }}
-          >
-            {children}
-          </Center>
-        </PopoverTrigger>
-        <PopoverContent minW={192}>
-          <PopoverArrow />
-          <PopoverBody>{content}</PopoverBody>
-        </PopoverContent>
-      </Popover>
-    </Box>
+    <Popover
+      placement="top"
+      trigger="hover"
+      strategy="fixed"
+      gutter={6}
+      {...rest}
+    >
+      <PopoverTrigger>
+        <Center as="span" tabIndex={0}>
+          {children}
+        </Center>
+      </PopoverTrigger>
+      <PopoverContent minW={192}>
+        <PopoverArrow />
+        <PopoverBody>{content}</PopoverBody>
+      </PopoverContent>
+    </Popover>
   )
 }
 
