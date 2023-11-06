021d842fab67eb87cb6b5c9e18896c2e0353ac3d
Pablo Pettinari <pettinarip@gmail.com>
2023-11-06 10:19:41 +0100

    move nextconfig type next to the export

--- next.config.js
+++ next.config.js
@@ -1,21 +1,25 @@
-/** @type {import('next').NextConfig} */
-
 const { PHASE_DEVELOPMENT_SERVER } = require("next/dist/shared/lib/constants")
 const i18nConfig = require("./i18n.config.json")
 
 const BUILD_LOCALES = process.env.BUILD_LOCALES
 // Supported locales defined in `i18n.config.json`
-const locales = BUILD_LOCALES ? BUILD_LOCALES.split(",") : i18nConfig.map(({ code }) => code)
-const experimental = (process.env.LIMIT_CPU || "").toLowerCase() === "true" ? {
-  // This option could be enabled in the future when flagged as stable, to speed up builds
-  // (see https://nextjs.org/docs/pages/building-your-application/configuring/mdx#using-the-rust-based-mdx-compiler-experimental)
-  // mdxRs: true,
+const locales = BUILD_LOCALES
+  ? BUILD_LOCALES.split(",")
+  : i18nConfig.map(({ code }) => code)
+const experimental =
+  (process.env.LIMIT_CPU || "").toLowerCase() === "true"
+    ? {
+        // This option could be enabled in the future when flagged as stable, to speed up builds
+        // (see https://nextjs.org/docs/pages/building-your-application/configuring/mdx#using-the-rust-based-mdx-compiler-experimental)
+        // mdxRs: true,
 
-  // Reduce the number of cpus and disable parallel threads in prod envs to consume less memory
-  workerThreads: false,
-  cpus: 2,
-} : {}
+        // Reduce the number of cpus and disable parallel threads in prod envs to consume less memory
+        workerThreads: false,
+        cpus: 2,
+      }
+    : {}
 
+/** @type {import('next').NextConfig} */
 module.exports = (phase, { defaultConfig }) => {
   let nextConfig = {
     ...defaultConfig,
