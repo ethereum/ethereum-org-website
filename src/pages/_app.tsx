4919cea21c4c9bfb9b1e4bf938ed14040dc117ca
Corwin Smith <cssmittys@gmail.com>
2023-11-19 17:45:43 +0300

    contentIsOutdated -> isOutdated. Fix the frontmatter prop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,7 +27,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
       </style>
       <ChakraProvider theme={theme}>
         <RootLayout
-          contentIsOutdated={pageProps.frontmatter.contentIsOutdated}
+          contentIsOutdated={!!pageProps.frontmatter?.isOutdated}
           contentNotTranslated={pageProps.contentNotTranslated}
           lastDeployDate={pageProps.lastDeployDate}
         >
