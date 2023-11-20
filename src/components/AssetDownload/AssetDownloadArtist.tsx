1b030ec59f786760cba014343cfe95e958e94ac2
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-11-20 18:55:23 +0300

    update AssetDownloadArtist component

--- src/components/AssetDownload/AssetDownloadArtist.tsx
+++ src/components/AssetDownload/AssetDownloadArtist.tsx
@@ -1,35 +1,35 @@
-import React from "react"
 import { Flex } from "@chakra-ui/react"
 
-import Emoji from "../Emoji"
-import Link from "../Link"
-import Text from "../OldText"
-import Translation from "../Translation"
+import Emoji from "@/components/Emoji"
+import Link from "@/components/Link"
+import Text from "@/components/OldText"
+import Translation from "@/components/Translation"
 
-interface Props {
+type AssetDownloadArtistProps = {
   artistName: string
   artistUrl?: string
 }
 
-const AssetDownloadArtist = ({ artistName, artistUrl }: Props) => {
-  return (
-    <Flex
-      mb={4}
-      border="1px"
-      borderTop="none"
-      borderColor="white700"
-      py={2}
-      px={4}
-      borderRadius="0 0 4px 4px"
-    >
-      <Flex mr={2} fontSize="md" textColor="text300">
-        <Emoji text=":artist_palette:" mr={2} fontSize="2xl" />
-        <Translation id="page-assets-download-artist" />
-      </Flex>
-      {artistUrl && <Link to={artistUrl}>{artistName}</Link>}
-      {!artistUrl && <Text m={0}>{artistName}</Text>}
+const AssetDownloadArtist = ({
+  artistName,
+  artistUrl,
+}: AssetDownloadArtistProps) => (
+  <Flex
+    mb={4}
+    border="1px"
+    borderTop="none"
+    borderColor="white700"
+    py={2}
+    px={4}
+    borderRadius="0 0 4px 4px"
+  >
+    <Flex me={2} fontSize="md" textColor="text300">
+      <Emoji text=":artist_palette:" me={2} fontSize="2xl" />
+      <Translation id="page-assets-download-artist" />
     </Flex>
-  )
-}
+    {artistUrl && <Link to={artistUrl}>{artistName}</Link>}
+    {!artistUrl && <Text m={0}>{artistName}</Text>}
+  </Flex>
+)
 
 export default AssetDownloadArtist
