27a13ae62926f50571644b2c409169aab778fff3
Pablo Pettinari <pettinarip@gmail.com>
2023-11-24 14:59:52 +0100

    migrate 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import type { GetStaticProps, NextPage } from "next"
+import type { SSRConfig } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import { Box, Flex, Heading, Text } from "@chakra-ui/react"
+
+import { getRequiredNamespacesForPath } from "@/lib/utils/translations"
+
+import InlineLink from "../components/Link"
+import Translation from "../components/Translation"
+
+type Props = SSRConfig
+
+export const getStaticProps = (async (context) => {
+  const { locale } = context
+
+  // load i18n required namespaces for the given page
+  const requiredNamespaces = getRequiredNamespacesForPath("/")
+
+  return {
+    props: await serverSideTranslations(locale!, requiredNamespaces),
+  }
+}) satisfies GetStaticProps<Props>
+
+const NotFoundPage: NextPage<Props> = () => (
+  <Flex flexDir="column" align="center" w="full" mt={16} mb={0} mx="auto">
+    <Box py={4} px={8} w="full">
+      <Heading as="h1" size="2xl" my={8}>
+        <Translation id="we-couldnt-find-that-page" />
+      </Heading>
+      <Text mb={8}>
+        <Translation id="try-using-search" />{" "}
+        <InlineLink href="/">
+          <Translation id="return-home" />
+        </InlineLink>
+        .
+      </Text>
+    </Box>
+  </Flex>
+)
+
+export default NotFoundPage
