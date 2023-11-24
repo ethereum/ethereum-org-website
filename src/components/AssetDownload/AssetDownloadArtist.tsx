9baaf567db87997fca6bf44eec4828986432d938
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-11-23 22:16:53 -0500

    fix i18n namespace for AssetDownloadArtist.tsx

--- src/components/AssetDownload/AssetDownloadArtist.tsx
+++ src/components/AssetDownload/AssetDownloadArtist.tsx
@@ -3,7 +3,7 @@ import { Flex } from "@chakra-ui/react"
 import Emoji from "@/components/Emoji"
 import Link from "@/components/Link"
 import Text from "@/components/OldText"
-import Translation from "@/components/Translation"
+import { useTranslation } from "next-i18next"
 
 type AssetDownloadArtistProps = {
   artistName: string
@@ -13,23 +13,26 @@ type AssetDownloadArtistProps = {
 const AssetDownloadArtist = ({
   artistName,
   artistUrl,
-}: AssetDownloadArtistProps) => (
-  <Flex
-    mb={4}
-    border="1px"
-    borderTop="none"
-    borderColor="white700"
-    py={2}
-    px={4}
-    borderRadius="0 0 4px 4px"
-  >
-    <Flex me={2} fontSize="md" textColor="text300">
-      <Emoji text=":artist_palette:" me={2} fontSize="2xl" />
-      <Translation id="page-assets-download-artist" />
+}: AssetDownloadArtistProps) => {
+  const { t } = useTranslation("page-assets")
+  return (
+    <Flex
+      mb="4"
+      border="1px"
+      borderTop="none"
+      borderColor="white700"
+      py="2"
+      px="4"
+      borderRadius="0 0 4px 4px"
+    >
+      <Flex me="2" fontSize="md" textColor="text300">
+        <Emoji text=":artist_palette:" me="2" fontSize="2xl" />
+        {t("page-assets-download-artist")}
+      </Flex>
+      {artistUrl && <Link to={artistUrl}>{artistName}</Link>}
+      {!artistUrl && <Text m="0">{artistName}</Text>}
     </Flex>
-    {artistUrl && <Link to={artistUrl}>{artistName}</Link>}
-    {!artistUrl && <Text m={0}>{artistName}</Text>}
-  </Flex>
-)
+  )
+}
 
 export default AssetDownloadArtist
