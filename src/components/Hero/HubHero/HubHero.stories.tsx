ede7accfc3efb066cbb9ac0c512586f4b98eee38
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-11-27 12:54:37 -0500

    use next-i18next

--- src/components/Hero/HubHero/HubHero.stories.tsx
+++ src/components/Hero/HubHero/HubHero.stories.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "next-i18next"
 import { Box } from "@chakra-ui/react"
 import { Meta, StoryObj } from "@storybook/react"
 
