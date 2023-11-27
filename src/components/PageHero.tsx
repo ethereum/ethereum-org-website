a87d8500fe790d4cc17ef04f10442b285ae71dd8
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-11-27 09:17:59 -0500

    destructure props

--- src/components/PageHero.tsx
+++ src/components/PageHero.tsx
@@ -10,7 +10,7 @@ import {
   type IButtonProps as ButtonProps,
 } from "@/components/Buttons"
 import { Image, type ImageProps } from "@/components/Image"
-import Text from "./OldText"
+import Text from "@/components/OldText"
 
 export type ButtonLinkType = Omit<ButtonLinkProps, "content"> & {
   content: ReactNode
@@ -45,125 +45,117 @@ function isButtonLink(
 }
 
 const PageHero = ({
-  content,
+  content: { buttons, title, header, subtitle, image, alt },
   isReverse = false,
   children,
   className,
-}: PageHeroProps) => {
-  const { buttons, title, header, subtitle, image, alt } = content
-  return (
-    <Box py="4" px="8" width="full">
-      <Flex
-        justifyContent="space-between"
-        mt="8"
-        px={{ base: 0, lg: 16 }}
-        direction={{ base: isReverse ? "column" : "column-reverse", lg: "row" }}
-        className={className}
+}: PageHeroProps) => (
+  <Box py="4" px="8" width="full">
+    <Flex
+      justifyContent="space-between"
+      mt="8"
+      px={{ base: 0, lg: 16 }}
+      direction={{ base: isReverse ? "column" : "column-reverse", lg: "row" }}
+      className={className}
+    >
+      <Box
+        maxW={{ base: "full", lg: "container.sm" }}
+        pt={{ base: isReverse ? 0 : 8, lg: 32 }}
+        pb={{ base: isReverse ? 8 : 0, lg: 32 }}
+        pl={{ base: 0, lg: 8 }}
+        mr={{ base: 0, lg: 4 }}
       >
-        <Box
-          maxW={{ base: "full", lg: "container.sm" }}
-          pt={{ base: isReverse ? 0 : 8, lg: 32 }}
-          pb={{ base: isReverse ? 8 : 0, lg: 32 }}
-          pl={{ base: 0, lg: 8 }}
-          mr={{ base: 0, lg: 4 }}
+        <Heading
+          as="h1"
+          textTransform="uppercase"
+          fontSize="md"
+          fontWeight="normal"
+          mt={{ base: 0, lg: 8 }}
+          mb="4"
+          color="text300"
+          lineHeight={1.4}
         >
-          <Heading
-            as="h1"
-            textTransform="uppercase"
-            fontSize="md"
-            fontWeight="normal"
-            mt={{ base: 0, lg: 8 }}
-            mb="4"
-            color="text300"
-            lineHeight={1.4}
-          >
-            {title}
-          </Heading>
-          <Heading
-            as="h2"
-            fontWeight="bold"
-            fontSize={{ base: "2.5rem", lg: "5xl" }}
-            maxW="full"
-            mb="0"
-            mt={{ base: 8, lg: 12 }}
-            color="text00"
-            lineHeight={1.4}
-          >
-            {header}
-          </Heading>
-          <Text
-            fontSize={{ base: "xl", lg: "2xl" }}
-            lineHeight={1.4}
-            color="text200"
-            mt="4"
-            mb="8"
-          >
-            {subtitle}
-          </Text>
-          {buttons && (
-            // FIXME: remove the `ul` override once removed the corresponding
-            // global styles in `src/@chakra-ui/gatsby-plugin/styles.ts`
-            <Wrap spacing="2" overflow="visible" sx={{ ul: { m: 0 } }}>
-              {buttons.map((button, idx) => {
-                if (isButtonLink(button)) {
-                  return (
-                    <WrapItem key={idx}>
-                      <ButtonLink
-                        variant={button.variant}
-                        to={button.to}
-                        onClick={() =>
-                          trackCustomEvent({
-                            eventCategory: button.matomo.eventCategory,
-                            eventAction: button.matomo.eventAction,
-                            eventName: button.matomo.eventName,
-                          })
-                        }
-                      >
-                        {button.content}
-                      </ButtonLink>
-                    </WrapItem>
-                  )
-                }
+          {title}
+        </Heading>
+        <Heading
+          as="h2"
+          fontWeight="bold"
+          fontSize={{ base: "2.5rem", lg: "5xl" }}
+          maxW="full"
+          mb="0"
+          mt={{ base: 8, lg: 12 }}
+          color="text00"
+          lineHeight={1.4}
+        >
+          {header}
+        </Heading>
+        <Text
+          fontSize={{ base: "xl", lg: "2xl" }}
+          lineHeight={1.4}
+          color="text200"
+          mt="4"
+          mb="8"
+        >
+          {subtitle}
+        </Text>
+        {buttons && (
+          // FIXME: remove the `ul` override once removed the corresponding
+          // global styles in `src/@chakra-ui/gatsby-plugin/styles.ts`
+          <Wrap spacing="2" overflow="visible" sx={{ ul: { m: 0 } }}>
+            {buttons.map((button, idx) => {
+              if (isButtonLink(button)) {
+                return (
+                  <WrapItem key={idx}>
+                    <ButtonLink
+                      variant={button.variant}
+                      to={button.to}
+                      onClick={() => trackCustomEvent({
+                        eventCategory: button.matomo.eventCategory,
+                        eventAction: button.matomo.eventAction,
+                        eventName: button.matomo.eventName,
+                      })}
+                    >
+                      {button.content}
+                    </ButtonLink>
+                  </WrapItem>
+                )
+              }
 
-                if (button.toId) {
-                  return (
-                    <WrapItem key={idx}>
-                      <Button
-                        variant={button.variant}
-                        toId={button.toId}
-                        onClick={() =>
-                          trackCustomEvent({
-                            eventCategory: button.matomo.eventCategory,
-                            eventAction: button.matomo.eventAction,
-                            eventName: button.matomo.eventName,
-                          })
-                        }
-                      >
-                        {button.content}
-                      </Button>
-                    </WrapItem>
-                  )
-                }
-              })}
-            </Wrap>
-          )}
-          {children}
-        </Box>
-        <Image
-          flex="1 1 50%"
-          alignSelf="center"
-          mt={{ base: 0, lg: 12 }}
-          ml={{ base: 0, lg: 12 }}
-          w="full"
-          maxWidth={{ base: "560px", lg: "624px" }}
-          src={image}
-          style={{ objectFit: "contain" }}
-          alt={alt}
-          loading="eager"
-        />
-      </Flex>
-    </Box>
-  )
-}
+              if (button.toId) {
+                return (
+                  <WrapItem key={idx}>
+                    <Button
+                      variant={button.variant}
+                      toId={button.toId}
+                      onClick={() => trackCustomEvent({
+                        eventCategory: button.matomo.eventCategory,
+                        eventAction: button.matomo.eventAction,
+                        eventName: button.matomo.eventName,
+                      })}
+                    >
+                      {button.content}
+                    </Button>
+                  </WrapItem>
+                )
+              }
+            })}
+          </Wrap>
+        )}
+        {children}
+      </Box>
+      <Image
+        flex="1 1 50%"
+        alignSelf="center"
+        mt={{ base: 0, lg: 12 }}
+        ml={{ base: 0, lg: 12 }}
+        w="full"
+        maxWidth={{ base: "560px", lg: "624px" }}
+        src={image}
+        style={{ objectFit: "contain" }}
+        alt={alt}
+        loading="eager" />
+    </Flex>
+  </Box>
+)
 
 export default PageHero
