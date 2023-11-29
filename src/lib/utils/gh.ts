93b5597345f47317e1cfc87ea2a0c5e6ecbe8960
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-11-29 13:21:13 -0800

    create getLastModifiedDateByPath using git log

--- src/lib/utils/gh.ts
+++ src/lib/utils/gh.ts
@@ -4,8 +4,25 @@ import { join } from "path"
 
 import { CONTENT_DIR, DEFAULT_LOCALE, TRANSLATIONS_DIR } from "@/lib/constants"
 
+const getGitLogFromPath = (path: string): string => {
+  // git command to show file last commit info
+  const gitCommand = `git log -1 -- ${path}`
+  // Execute git command and parse result to string
+  return execSync(gitCommand).toString()
+}
+
+const extractDateFromGitLogInfo = (logInfo: string): string => {
+  // Filter commit date in log and return date using ISOString format (same that GH API uses)
+  const lastCommitDate = logInfo
+    .split("\n")
+    .filter((x) => x.startsWith("Date: "))[0]
+    .slice("Date:".length)
+    .trim()
+  return new Date(lastCommitDate).toISOString()
+}
+
 // This util filters the git log to get the file last commit info, and then the commit date (last update)
-export const getLastModifiedDate = (slug: string, locale: string) => {
+export const getLastModifiedDate = (slug: string, locale: string): string => {
   const translatedContentPath = join(TRANSLATIONS_DIR, locale, slug, "index.md")
   const contentIsNotTranslated = !fs.existsSync(translatedContentPath)
   let filePath = ""
@@ -18,16 +35,12 @@ export const getLastModifiedDate = (slug: string, locale: string) => {
     filePath = join(TRANSLATIONS_DIR, locale, slug, "index.md")
   }
 
-  // git command to show file last commit info
-  const gitCommand = `git log -1 -- ${filePath}`
-  // Execute git command and parse result to string
-  const logInfo = execSync(gitCommand).toString()
-  // Filter commit date in log and return date using ISOString format (same that GH API uses)
-  const lastCommitDate = logInfo
-    .split("\n")
-    .filter((x) => x.startsWith("Date: "))[0]
-    .slice("Date:".length)
-    .trim()
+  const logInfo = getGitLogFromPath(filePath)
+  return extractDateFromGitLogInfo(logInfo)
+}
 
-  return new Date(lastCommitDate).toISOString()
+export const getLastModifiedDateByPath = (path: string): string => {
+  if (!fs.existsSync(path)) throw new Error(`File not found: ${path}`)
+  const logInfo = getGitLogFromPath(path)
+  return extractDateFromGitLogInfo(logInfo)
 }
