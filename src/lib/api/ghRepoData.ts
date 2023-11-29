e2639a1def160bb2f1e19cbf4cf01971415d57d8
Corwin Smith <cssmittys@gmail.com>
2023-11-29 12:15:42 -0700

    env var

--- src/lib/api/ghRepoData.ts
+++ src/lib/api/ghRepoData.ts
@@ -6,8 +6,8 @@ export const ghRepoData = async (githubUrl: string) => {
   const split = githubUrl.split("/")
   const repoOwner = split[split.length - 2]
   const repoName = split[split.length - 1]
-  const repoData = await axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}`, { headers: { 'Authorization': `Bearer ${process.env.GITHUB_TOKEN}` }}) 
-const languageData = await axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/languages`, { headers: { 'Authorization': `Bearer ${process.env.GITHUB_TOKEN}` }})
+  const repoData = await axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}`, { headers: { 'Authorization': `Bearer ${process.env.GITHUB_TOKEN_READ_ONLY}` }}) 
+const languageData = await axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/languages`, { headers: { 'Authorization': `Bearer ${process.env.GITHUB_TOKEN_READ_ONLY}` }})
     return {
         starCount: repoData.data.stargazers_count,
         languages: Object.keys(languageData.data),
