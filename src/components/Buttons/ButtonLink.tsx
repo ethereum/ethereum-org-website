a9ef71c988668a690f53410c1c3a31366798942d
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-11-30 07:29:13 -0800

    add matomo tracking to ButtonLink

--- src/components/Buttons/ButtonLink.tsx
+++ src/components/Buttons/ButtonLink.tsx
@@ -1,10 +1,28 @@
 import Button, { type ButtonProps } from "@/components/Buttons/Button"
 import { BaseLink, type LinkProps } from "@/components/Link"
 
-export type ButtonLinkProps = LinkProps & Omit<ButtonProps, "toId">
+import { type MatomoEventOptions, trackCustomEvent } from "@/lib/utils/matomo"
 
-const ButtonLink = (props: ButtonLinkProps) => (
-  <Button as={BaseLink} activeStyle={{}} {...props} />
-)
+export type ButtonLinkProps = LinkProps &
+  Omit<ButtonProps, "toId" | "onClick"> & {
+    customEventOptions?: MatomoEventOptions
+  }
+
+const ButtonLink = ({ customEventOptions, ...props }: ButtonLinkProps) => {
+  const matomoEvent = customEventOptions ?? {
+    eventCategory: "Link",
+    eventAction: "clicked",
+    eventName: "ButtonLink clicked",
+    eventValue: (props.to ?? props.href) as string,
+  }
+  return (
+    <Button
+      as={BaseLink}
+      activeStyle={{}}
+      {...props}
+      onClick={() => trackCustomEvent(matomoEvent)}
+    />
+  )
+}
 
 export default ButtonLink
