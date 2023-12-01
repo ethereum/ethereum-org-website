43cb831cb83b0aa26846e8666d5fffcaa73e2497
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-12-01 10:34:05 -0600

    add sitemap transform
    exclude 404, fix changefreq and priority depending on English vs non-English paths

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next-sitemap').IConfig} */
+const i18nConfig = require("./i18n.config.json")
+const locales = i18nConfig.map(({ code }) => code)
 
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://ethereum.org',
   generateRobotsTxt: true,
+  transform: async (_, path) => {
+    const rootPath = path.split("/")[1]
+    if (["404"].includes(rootPath)) return null
+    const isDefaultLocale = !locales.includes(rootPath) || rootPath === "en"
+    return {
+      loc: path,
+      changefreq: isDefaultLocale ? "weekly" : "monthly",
+      priority: isDefaultLocale ? 0.7 : 0.5,
+    }
+  },
 }
