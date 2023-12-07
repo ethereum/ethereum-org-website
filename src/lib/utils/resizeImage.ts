31a68e888d21c069eed1032a541c107f80fe4635
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-12-07 07:57:30 -0600

    Delete resizeImage.ts

--- src/lib/utils/resizeImage.ts
+++
@@ -1,6 +0,0 @@
-import type { ImageProps } from "@/components/Image"
-
-export const resizeImage = (image: ImageProps["src"], width: number): ImageProps["src"] => {
-  if (typeof image === "string") return image
-  return { ...image, width }
-}
