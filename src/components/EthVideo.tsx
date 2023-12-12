eb76fcbe252d15f970cd593d8aadc5c26191640a
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-12-12 13:10:29 -0800

    Update EthVideo props

--- src/components/EthVideo.tsx
+++ src/components/EthVideo.tsx
@@ -3,15 +3,8 @@ import { Box, useColorModeValue } from "@chakra-ui/react"
 import darkVideo from "@/public/ethereum-hero-dark.mp4"
 import lightVideo from "@/public/ethereum-hero-light.mp4"
 
-type EthVideoProps = {
-  className?: string
-  videoSrc?: string
-}
-
-const EthVideo = ({ videoSrc }: EthVideoProps) => {
-  const videoFile = useColorModeValue(lightVideo, darkVideo)
-
-  const src = videoSrc || videoFile
+const EthVideo = () => {
+  const src = useColorModeValue(lightVideo, darkVideo)
 
   return (
     <Box>
