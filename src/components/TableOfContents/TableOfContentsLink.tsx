723faa40d29c97c9b7194d45da10394e45976d22
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-01-20 12:47:25 -0800

    refactor: rm classes, use style props
    Use ButtonLink ("link" variant) to take advantage of `isActive` and `_active` props

--- src/components/TableOfContents/TableOfContentsLink.tsx
+++ src/components/TableOfContents/TableOfContentsLink.tsx
@@ -2,29 +2,22 @@ import { cssVar, SystemStyleObject } from "@chakra-ui/react"
 
 import type { ToCItem } from "@/lib/types"
 
-import { BaseLink } from "@/components/Link"
+import { ButtonLink } from "@/components/Buttons"
 
 import { useRtlFlip } from "@/hooks/useRtlFlip"
 
-export interface IPropsTableOfContentsLink {
+type TableOfContentsLinkProps = {
   depth: number
   item: ToCItem
   activeHash?: string
 }
 
-const Link: React.FC<IPropsTableOfContentsLink> = ({
+const TableOfContentsLink = ({
   depth,
   item: { title, url },
   activeHash,
-}) => {
+}: TableOfContentsLinkProps) => {
   const { flipForRtl } = useRtlFlip()
-  const isActive = activeHash === url
-  const isNested = depth === 2
-
-  const classList: Array<string> = []
-  isActive && classList.push("active")
-  isNested && classList.push("nested")
-  const classes = classList.join(" ")
 
   const $dotBg = cssVar("dot-bg")
 
@@ -45,9 +38,10 @@ const Link: React.FC<IPropsTableOfContentsLink> = ({
   }
 
   return (
-    <BaseLink
+    <ButtonLink
+      isActive={activeHash === url}
+      variant="link"
       href={url}
-      className={classes}
       textDecoration="none"
       display="inline-block"
       position="relative"
@@ -58,33 +52,27 @@ const Link: React.FC<IPropsTableOfContentsLink> = ({
       _hover={{
         ...hoverOrActiveStyle,
       }}
-      sx={{
-        [$dotBg.variable]: "colors.background",
-        "&.active": {
-          [$dotBg.variable]: "colors.primary",
-          ...hoverOrActiveStyle,
-        },
-        "&.nested": {
-          _before: {
-            content: `"⌞"`,
-            opacity: 0.5,
-            display: "inline-flex",
-            position: "absolute",
-            insetInlineStart: -3.5,
-            top: -1,
-            transform: flipForRtl,
-          },
-          "&.active, &:hover": {
-            _after: {
-              insetInlineStart: "-2.29rem",
-            },
-          },
-        },
+      _active={{
+        [$dotBg.variable]: "colors.primary",
+        ...hoverOrActiveStyle,
       }}
+      _before={
+        depth > 2
+          ? {
+              content: `"⌞"`,
+              opacity: 0.5,
+              display: "inline-flex",
+              position: "absolute",
+              insetInlineStart: -3.5,
+              top: -1,
+              transform: flipForRtl,
+            }
+          : undefined
+      }
     >
       {title}
-    </BaseLink>
+    </ButtonLink>
   )
 }
 
-export default Link
+export default TableOfContentsLink
