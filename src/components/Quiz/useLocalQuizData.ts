8546c0b2f0933307cc3955269b751a38ad2c3f29
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-01-24 13:51:42 -0800

    fix: add backwards compatibility for string values

--- src/components/Quiz/useLocalQuizData.ts
+++ src/components/Quiz/useLocalQuizData.ts
@@ -1,6 +1,4 @@
-import { CompletedQuizzes, UserStats } from "@/lib/types"
-
-import allQuizzesData from "@/data/quizzes"
+import { UserStats } from "@/lib/types"
 
 import { USER_STATS_KEY } from "@/lib/constants"
 
@@ -14,6 +12,14 @@ export const INITIAL_USER_STATS: UserStats = {
 
 export const useLocalQuizData = () => {
   const data = useLocalStorage(USER_STATS_KEY, INITIAL_USER_STATS)
+
+  // If the user has an old version of the app, convert the
+  // `completed` value from a string to an object.
+  const [current, setCurrent] = data
+  if (typeof current.completed === "string") {
+    const newCompleted = JSON.parse(current.completed)
+    setCurrent({ ...current, completed: newCompleted })
+  }
 
   return data
 }
