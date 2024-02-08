15c182b2667527dd00fc35ec52b618eb66f61456
Pablo Pettinari <pettinarip@gmail.com>
2024-02-08 13:46:01 +0100

    implement a chakra popover for the feedback widget for a11y improvements and to fix issues with positioning on a sticky positioned parent

--- src/components/FeedbackWidget.tsx
+++ src/components/FeedbackWidget.tsx
@@ -1,17 +1,18 @@
-import { useEffect, useMemo, useRef, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { useRouter } from "next/router"
 import { useTranslation } from "next-i18next"
 import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogCloseButton,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogOverlay,
   Box,
   Button,
   ButtonProps,
+  forwardRef,
+  Popover,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverFooter,
+  PopoverHeader,
+  PopoverTrigger,
   ScaleFade,
   useDisclosure,
 } from "@chakra-ui/react"
@@ -29,48 +30,43 @@ type FixedDotProps = ButtonProps & {
   bottomOffset: number
   isExpanded: boolean
 }
-const FixedDot = ({
-  children,
-  bottomOffset,
-  isExpanded,
-  ...props
-}: FixedDotProps) => {
-  const size = "3rem"
-  return (
-    <Button
-      w={{ base: size, lg: isExpanded ? "15rem" : size }}
-      h={size}
-      borderRadius="full"
-      variant="solid"
-      boxShadow="tableItemBox"
-      position="sticky"
-      bottom={{ base: `${bottomOffset + 1}rem`, lg: 4 }}
-      ms="auto"
-      mt={{ lg: "inherit" }}
-      insetEnd={4}
-      zIndex={98} /* Below the mobile menu */
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      whiteSpace="normal"
-      _hover={{
-        transform: "scale(1.1)",
-        transition: "transform 0.2s ease-in-out",
-      }}
-      transition="transform 0.2s ease-in-out, width 0.25s ease-in-out,
+const FixedDot = forwardRef(
+  ({ children, bottomOffset, isExpanded, ...props }: FixedDotProps, ref) => {
+    const size = "3rem"
+    return (
+      <Button
+        ref={ref}
+        position="absolute"
+        w={{ base: size, lg: isExpanded ? "15rem" : size }}
+        h={size}
+        bottom={{ base: `${bottomOffset + 1}rem`, lg: 4 }}
+        insetEnd={4}
+        borderRadius="full"
+        mt={{ lg: "inherit" }}
+        variant="solid"
+        boxShadow="tableItemBox"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        whiteSpace="normal"
+        _hover={{
+          transform: "scale(1.1)",
+          transition: "transform 0.2s ease-in-out",
+        }}
+        transition="transform 0.2s ease-in-out, width 0.25s ease-in-out,
       border-radius 0.25s linear"
-      {...props}
-    >
-      {children}
-    </Button>
-  )
-}
+        {...props}
+      >
+        {children}
+      </Button>
+    )
+  }
+)
 
 const FeedbackWidget = () => {
   const { t } = useTranslation("common")
   const { asPath, locale } = useRouter()
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const cancelRef = useRef<HTMLButtonElement>(null)
   const [isExpanded, setIsExpanded] = useState(false)
   const [feedbackSubmitted, setFeedbackSubmitted] = useState(false)
 
@@ -141,126 +137,129 @@ const FeedbackWidget = () => {
   if (locale !== DEFAULT_LOCALE) return null
 
   return (
-    <>
-      <FixedDot
-        onClick={handleOpen}
-        bottomOffset={bottomOffset}
-        isExpanded={isExpanded}
-        id="dot"
-      >
-        <Box
-          display="flex"
-          justifyContent="space-evenly"
-          width={{ base: "3rem", lg: isExpanded ? "13.5rem" : "3rem" }}
-          position={{
-            base: "inherit",
-            lg: isExpanded ? "absolute" : "inherit",
-          }}
-        >
-          <FeedbackGlyphIcon color="white" my="11px" />
-          {isExpanded && (
-            <ScaleFade in={isExpanded} delay={0.25}>
-              <Text
-                as="div"
-                color="white"
-                fontWeight="bold"
-                noOfLines={2}
-                height="100%"
-                alignItems="center"
-                display={{ base: "none", lg: isExpanded ? "flex" : "none" }}
-              >
-                {t("feedback-widget-prompt")}
-              </Text>
-            </ScaleFade>
-          )}
-        </Box>
-      </FixedDot>
-
-      <AlertDialog
+    <Box position="sticky" bottom="0">
+      <Popover
         isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
+        onOpen={handleOpen}
         onClose={handleClose}
+        placement="top"
       >
-        <AlertDialogOverlay>
-          <AlertDialogContent
-            position="fixed"
-            insetEnd={{ base: 4, sm: 8 }}
-            insetStart={{ base: 4, sm: "auto" }}
-            bottom={{ base: `${bottomOffset + 5}rem`, lg: 20 }}
-            w={{ base: "auto", sm: "300px" }}
-            bgColor="ednBackground"
-            border="1px"
-            borderColor="buttonColor"
-            boxShadow="tableItemBox"
-            borderRadius="base"
-            py="4"
-            px="2"
+        <PopoverTrigger>
+          <FixedDot
+            bottomOffset={bottomOffset}
+            isExpanded={isExpanded}
+            id="dot"
           >
-            <AlertDialogCloseButton />
-
-            <AlertDialogHeader
-              fontSize="xl"
-              fontWeight="bold"
-              lineHeight="6"
-              textAlign="center"
+            <Box
+              display="flex"
+              justifyContent="space-evenly"
+              width={{ base: "auto", lg: isExpanded ? "13.5rem" : "3rem" }}
+              position={{
+                base: "inherit",
+                lg: isExpanded ? "absolute" : "inherit",
+              }}
             >
-              {feedbackSubmitted
-                ? t("feedback-widget-thank-you-title")
-                : t("feedback-widget-prompt")}
-            </AlertDialogHeader>
+              <FeedbackGlyphIcon color="white" my="11px" />
+              {isExpanded && (
+                <ScaleFade in={isExpanded} delay={0.25}>
+                  <Text
+                    as="div"
+                    color="white"
+                    fontWeight="bold"
+                    noOfLines={2}
+                    height="100%"
+                    alignItems="center"
+                    display={{ base: "none", lg: isExpanded ? "flex" : "none" }}
+                  >
+                    {t("feedback-widget-prompt")}
+                  </Text>
+                </ScaleFade>
+              )}
+            </Box>
+          </FixedDot>
+        </PopoverTrigger>
+        <PopoverContent
+          w={{ base: "auto", sm: "300px" }}
+          bgColor="ednBackground"
+          border="1px"
+          borderColor="buttonColor"
+          boxShadow="tableItemBox"
+          borderRadius="base"
+          me="4"
+          mb="2"
+          py="4"
+          px="2"
+        >
+          <PopoverCloseButton position="absolute" top="2" right="4" />
 
-            {/* Body: */}
-            {feedbackSubmitted && (
+          <PopoverHeader
+            fontSize="xl"
+            fontWeight="bold"
+            lineHeight="6"
+            textAlign="center"
+            py="4"
+            px="6"
+          >
+            {feedbackSubmitted
+              ? t("feedback-widget-thank-you-title")
+              : t("feedback-widget-prompt")}
+          </PopoverHeader>
+
+          {/* Body: */}
+          {feedbackSubmitted && (
+            <>
+              <PopoverBody
+                fontWeight="normal"
+                fontSize="md"
+                lineHeight="5"
+                textAlign="center"
+                py="2"
+                px="6"
+              >
+                {t("feedback-widget-thank-you-subtitle")}
+              </PopoverBody>
+              <PopoverBody
+                fontWeight="bold"
+                fontSize="xs"
+                lineHeight="4"
+                letterSpacing="wide"
+                color="searchBorder"
+                textAlign="center"
+                py="2"
+                px="6"
+              >
+                {t("feedback-widget-thank-you-timing")}
+              </PopoverBody>
+            </>
+          )}
+
+          <PopoverFooter display="flex" gap="6" py="4" px="6">
+            {feedbackSubmitted ? (
+              <Button onClick={handleSurveyOpen} flex={1}>
+                {t("feedback-widget-thank-you-cta")}
+              </Button>
+            ) : (
               <>
-                <AlertDialogBody
-                  fontWeight="normal"
-                  fontSize="md"
-                  lineHeight="5"
-                  textAlign="center"
+                <Button
+                  variant="solid"
+                  onClick={() => handleSubmit(true)}
+                  flex={1}
                 >
-                  {t("feedback-widget-thank-you-subtitle")}
-                </AlertDialogBody>
-                <AlertDialogBody
-                  fontWeight="bold"
-                  fontSize="xs"
-                  lineHeight="4"
-                  letterSpacing="wide"
-                  color="searchBorder"
-                  textAlign="center"
+                  {t("yes")}
+                </Button>
+                <Button
+                  variant="solid"
+                  onClick={() => handleSubmit(false)}
+                  flex={1}
                 >
-                  {t("feedback-widget-thank-you-timing")}
-                </AlertDialogBody>
+                  {t("no")}
+                </Button>
               </>
             )}
-
-            <AlertDialogFooter display="flex" gap="6">
-              {feedbackSubmitted ? (
-                <Button onClick={handleSurveyOpen} flex={1}>
-                  {t("feedback-widget-thank-you-cta")}
-                </Button>
-              ) : (
-                <>
-                  <Button
-                    variant="solid"
-                    onClick={() => handleSubmit(true)}
-                    flex={1}
-                  >
-                    {t("yes")}
-                  </Button>
-                  <Button
-                    variant="solid"
-                    onClick={() => handleSubmit(false)}
-                    flex={1}
-                  >
-                    {t("no")}
-                  </Button>
-                </>
-              )}
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-    </>
+          </PopoverFooter>
+        </PopoverContent>
+      </Popover>
+    </Box>
   )
 }
 
