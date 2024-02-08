9a27db1e91bbe1946616c17afd3e8a52b989f83b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-02-08 15:30:45 -0800

    refactor: explode into individual components

---
+++ src/components/Nav/Mobile/LvlAccordion.tsx
@@ -0,0 +1,120 @@
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Button,
+  chakra,
+  Heading,
+  Text,
+} from "@chakra-ui/react"
+
+import { BaseLink } from "@/components/Link"
+
+import type { Level, NavItem } from "../types"
+
+import ExpandIcon from "./ExpandIcon"
+
+import { useNavMenuColors } from "@/hooks/useNavMenuColors"
+
+type LvlAccordionProps = {
+  lvl: Level
+  items: NavItem[]
+  onToggle: () => void
+}
+
+const LvlAccordion = ({ lvl, items, onToggle }: LvlAccordionProps) => {
+  const menuColors = useNavMenuColors()
+
+  return (
+    <Accordion allowToggle boxShadow="menu.accordion">
+      {items.map(({ label, description, ...actions }) => {
+        if ("href" in actions)
+          return (
+            <AccordionItem key={label}>
+              <Button
+                as={BaseLink}
+                w="full"
+                href={actions.href}
+                onClick={onToggle}
+                variant="ghost"
+                borderRadius="none"
+                borderColor={menuColors.stroke}
+                justifyContent="start"
+                gap="2"
+                ps={(lvl + 2) * 4}
+                py="4"
+                _hover={{
+                  color: menuColors.highlight,
+                }}
+              >
+                <Box flex="1" textAlign="start">
+                  <Text fontWeight="bold" fontSize="md" color={menuColors.body}>
+                    {label}
+                  </Text>
+                  <Text
+                    fontWeight="regular"
+                    fontSize="sm"
+                    color={menuColors.lvl[lvl].subtext}
+                  >
+                    {description}
+                  </Text>
+                </Box>
+              </Button>
+            </AccordionItem>
+          )
+        return (
+          <AccordionItem key={label}>
+            {({ isExpanded }) => (
+              <>
+                <Heading
+                  as={chakra[`h${lvl + 1}`]}
+                  color={menuColors.body}
+                  py="0"
+                  borderColor={menuColors.stroke}
+                >
+                  <AccordionButton
+                    justifyContent="start"
+                    gap="2"
+                    ps={lvl * 4}
+                    pe="4"
+                    py="4"
+                  >
+                    <ExpandIcon isOpen={isExpanded} />
+                    <Box flex="1" textAlign="start">
+                      <Text
+                        fontWeight="bold"
+                        fontSize="md"
+                        color={menuColors.body}
+                      >
+                        {label}
+                      </Text>
+                      <Text
+                        fontWeight="regular"
+                        fontSize="sm"
+                        color={menuColors.lvl[lvl].subtext}
+                      >
+                        {description}
+                      </Text>
+                    </Box>
+                  </AccordionButton>
+                </Heading>
+
+                <AccordionPanel p="0" bg={menuColors.lvl[lvl + 1].background}>
+                  <LvlAccordion
+                    lvl={(lvl + 1) as Level}
+                    items={actions.items}
+                    onToggle={onToggle}
+                  />
+                </AccordionPanel>
+              </>
+            )}
+          </AccordionItem>
+        )
+      })}
+    </Accordion>
+  )
+}
+
+export default LvlAccordion
---
+++ src/components/Nav/Mobile/MenuBody.tsx
@@ -0,0 +1,84 @@
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  DrawerBody,
+  Heading,
+} from "@chakra-ui/react"
+
+import { SECTION_LABELS } from "@/lib/constants"
+
+import type { Level, NavSections } from "../types"
+
+import ExpandIcon from "./ExpandIcon"
+import LvlAccordion from "./LvlAccordion"
+
+import { useNavMenuColors } from "@/hooks/useNavMenuColors"
+
+type MenuBodyProps = {
+  onToggle: () => void
+  linkSections: NavSections
+}
+
+const MenuBody = ({ linkSections, onToggle }: MenuBodyProps) => {
+  const menuColors = useNavMenuColors()
+
+  return (
+    <DrawerBody as="nav" p="0">
+      <Accordion allowToggle>
+        {SECTION_LABELS.map((key) => linkSections[key]).map(
+          ({ label, items }) => (
+            <AccordionItem key={label}>
+              {({ isExpanded }) => (
+                <>
+                  <Heading
+                    as="h2"
+                    color={menuColors.body}
+                    py="0"
+                    bg={
+                      isExpanded
+                        ? menuColors.lvl[1].background
+                        : "background.base"
+                    }
+                    borderBottom={isExpanded ? "1px" : "none"}
+                    borderColor="disabled"
+                  >
+                    <AccordionButton
+                      justifyContent="start"
+                      gap="2"
+                      _hover={{ bg: "none" }}
+                      py="4"
+                    >
+                      <ExpandIcon isOpen={isExpanded} />
+                      <Box
+                        as="span"
+                        flex="1"
+                        textAlign="start"
+                        fontWeight="bold"
+                        fontSize="lg"
+                      >
+                        {label}
+                      </Box>
+                    </AccordionButton>
+                  </Heading>
+
+                  <AccordionPanel p="0" bg={menuColors.lvl[2].background}>
+                    <LvlAccordion
+                      lvl={2 as Level}
+                      items={items}
+                      onToggle={onToggle}
+                    />
+                  </AccordionPanel>
+                </>
+              )}
+            </AccordionItem>
+          )
+        )}
+      </Accordion>
+    </DrawerBody>
+  )
+}
+
+export default MenuBody
