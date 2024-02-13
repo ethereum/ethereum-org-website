03b9bb67a697995d62c94a0c3bfbbe5db8c54921
Pablo Pettinari <pettinarip@gmail.com>
2024-02-13 20:15:46 +0100

    detect browser's locale manually

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,28 @@
 import { NextRequest, NextResponse } from "next/server"
 
-import { DEFAULT_LOCALE, FAKE_LOCALE } from "./lib/constants"
+import { DEFAULT_LOCALE, FAKE_LOCALE, LOCALES_CODES } from "./lib/constants"
 
 const PUBLIC_FILE = /\.(.*)$/
 
+function detectLocale(acceptLanguage: string | null) {
+  if (!acceptLanguage) {
+    return DEFAULT_LOCALE
+  }
+
+  // it comes in the format of `en-US,en;q=0.9,de;q=0.8`
+  const locales = acceptLanguage.split(",")
+
+  const locale = locales
+    .map((localeWeight) => localeWeight.split(";")[0].trim())
+    .find((locale) => {
+      return LOCALES_CODES.includes(locale)
+    })
+
+  return locale
+}
+
+// Middleware required to always display the locale prefix in the URL. It
+// redirects to the default locale if the locale is not present in the URL
 export async function middleware(req: NextRequest) {
   if (
     req.nextUrl.pathname.startsWith("/_next") ||
@@ -14,8 +33,13 @@ export async function middleware(req: NextRequest) {
   }
 
   if (req.nextUrl.locale === FAKE_LOCALE) {
+    // Apparently `localeDetection` does not work when using the faked locale
+    // hack. So, detect the locale manually
+    const localeDetected = detectLocale(req.headers.get("accept-language"))
+    const locale = localeDetected || DEFAULT_LOCALE
+
     return NextResponse.redirect(
-      new URL(`/${DEFAULT_LOCALE}${req.nextUrl.pathname}`, req.url)
+      new URL(`/${locale}${req.nextUrl.pathname}`, req.url)
     )
   }
 }
