a252172a5e8bbbfea890db2757462798c06274ce
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-02-14 14:14:24 -0800

    fix: use internal Emoji component

--- src/components/DevconGrantsBanner.tsx
+++ src/components/DevconGrantsBanner.tsx
@@ -1,7 +1,7 @@
-import Emoji from "react-emoji-render"
 import { Text } from "@chakra-ui/react"
 
 import DismissableBanner from "./Banners/DismissableBanner"
+import Emoji from "./Emoji"
 import Link from "./Link"
 
 interface IProps {
@@ -14,8 +14,8 @@ const DevconGrantsBanner: React.FC<IProps> = ({ pathname }) => {
       <DismissableBanner storageKey="devconGrants">
         <Text m={0}>
           The Road to Devcon Grants support Ethereum education initiatives in
-          and close to Southeast Asia <Emoji text="🌏🦄" />{" "}
-          Learn more{" "}
+          and close to Southeast Asia <Emoji text="🦄" />{" "}
+          <Emoji text="🌏" /> Learn more{" "}
           <Link to="https://esp.ethereum.foundation/devcon-grants">here.</Link>
         </Text>
       </DismissableBanner>
