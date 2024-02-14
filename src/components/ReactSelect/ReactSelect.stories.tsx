c3b9c72d40363f9dc563c888ed293dee09c4b614
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-02-14 16:18:01 -0500

    refactor(ReactSelect.stories.tsx): have only one story with variants

--- src/components/ReactSelect/ReactSelect.stories.tsx
+++ src/components/ReactSelect/ReactSelect.stories.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react"
-import { Box } from "@chakra-ui/react"
+import { Box, HStack } from "@chakra-ui/react"
 import { Meta, StoryObj } from "@storybook/react"
 
-import ReactSelect, { ReactSelectOnChange } from "."
-
-// TODO: Work with closed PR diff: https://github.com/ethereum/ethereum-org-website/pull/10542/files
+import ReactSelect from "."
 
 type ReactSelectType = typeof ReactSelect
 
@@ -24,72 +21,30 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
-type SingleLayerOption = {
-  label: string
-  value: string
-  [x: string]: unknown
-}
-
-const singleLayerOptions: SingleLayerOption[] = [
-  {
-    label: "Option 1",
-    value: "option-1",
-  },
-  {
-    label: "Option 2",
-    value: "option-2",
-  },
-  {
-    label: "Option 3",
-    value: "option-3",
-  },
-]
-
-const SelectWrapper: ReactSelectType = <Option extends { value: string }>(
-  props
-) => {
-  const [value, setValue] = useState<string | null>()
-
-  const handleOnChange: ReactSelectOnChange<Option> = (newVal) => {
-    console.log(newVal)
-
-    setValue(newVal?.value)
-    return
-  }
-
-  return (
-    <>
-      <div>Value selected: {value ?? "Please select an option"}</div>
-      <ReactSelect onChange={handleOnChange} {...props} />
-    </>
-  )
-}
-
-// TODO: There should be multiple stories here. Don't forget about meaningful story names.
-export const SingleLayerSelect: Story = {
-  args: {
-    options: singleLayerOptions,
-  },
-  render: (args) => <SelectWrapper {...args} />,
-}
-
-export const MultiGroupSelect: Story = {
+export const Dropdown: Story = {
   args: {
     options: [
       {
-        label: "GroupOption 1",
         options: [
-          {
-            label: "GroupOption 1 Item 1",
-            value: "g1-option-1",
-          },
-          {
-            label: "GroupOption 1 Item 2",
-            value: "g1-option-2",
-          },
+          { label: "Ethereum", value: "eth" },
+          { label: "Bitcoin", value: "bit" },
+          { label: "Dogecoin", value: "doge" },
+        ],
+      },
+      {
+        label: "Layer2 Options",
+        options: [
+          { label: "Mainnet", value: "mainnet" },
+          { label: "Arbitrum", value: "arbitrum" },
+          { label: "Optimism", value: "optimism" },
         ],
       },
     ],
   },
-  render: (args) => <SelectWrapper {...args} />,
+  render: (args) => (
+    <HStack spacing={4}>
+      <ReactSelect {...args} />
+      <ReactSelect {...args} variant="outline" />
+    </HStack>
+  ),
 }
