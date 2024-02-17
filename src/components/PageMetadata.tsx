7438ac3026191365983289406375ad9007d8597f
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-02-16 19:13:17 -0800

    refactor: extract getOgImage util

--- src/components/PageMetadata.tsx
+++ src/components/PageMetadata.tsx
@@ -4,6 +4,8 @@ import Head from "next/head"
 import { useRouter } from "next/router"
 import { useTranslation } from "next-i18next"
 
+import { getOgImage } from "@/lib/utils/metadata"
+
 import { DEFAULT_LOCALE, SITE_URL } from "@/lib/constants"
 
 type NameMeta = {
@@ -58,41 +60,26 @@ const PageMetadata = ({
   const canonical = canonicalUrl || url
 
   /* Set fallback ogImage based on path */
-  let ogImage = "/home/hero.png"
-
-  if (slug.includes("developers")) {
-    ogImage = "/enterprise-eth.png"
-  }
-
-  if (slug.includes("dapps")) {
-    ogImage = "/doge-computer.png"
-  }
-
-  if (slug.includes("roadmap")) {
-    ogImage = "/upgrades/upgrade_doge.png"
-  }
-
-  if (image) {
-    ogImage = image
-  }
+  const ogImage = image || getOgImage(slug)
 
   const ogImageUrl = new URL(ogImage, SITE_URL).href
   const metadata: Meta[] = [
-    { name: `description`, content: desc },
     { name: `image`, content: ogImageUrl },
-    { property: `og:title`, content: fullTitle },
-    { property: `og:description`, content: desc },
-    { property: `og:type`, content: `website` },
+    { name: `description`, content: desc },
+    { name: `docsearch:description`, content: desc },
     { name: `twitter:card`, content: `summary_large_image` },
     { name: `twitter:creator`, content: author || siteTitle },
     { name: `twitter:site`, content: author || siteTitle },
     { name: `twitter:title`, content: fullTitle },
     { name: `twitter:description`, content: desc },
     { name: `twitter:image`, content: ogImageUrl },
+    { property: `og:title`, content: fullTitle },
+    { property: `og:locale`, content: locale! },
+    { property: `og:description`, content: desc },
+    { property: `og:type`, content: `website` },
     { property: `og:url`, content: url },
     { property: `og:image`, content: ogImageUrl },
     { property: `og:site_name`, content: siteTitle },
-    { name: `docsearch:description`, content: desc },
   ]
 
   return (
