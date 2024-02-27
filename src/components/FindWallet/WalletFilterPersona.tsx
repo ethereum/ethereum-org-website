1df9a03cb6a1fba1b7400d0910ce9176f9b6bc0d
Nicolás Quiroz <nh.quiroz@gmail.com>
2024-02-27 11:34:38 -0300

    chore: update WalletFilterPersona checkbox

--- src/components/FindWallet/WalletFilterPersona.tsx
+++ src/components/FindWallet/WalletFilterPersona.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import {
   Box,
   Checkbox,
+  CheckboxGroup,
   Flex,
   Heading,
   useColorModeValue,
@@ -29,78 +30,77 @@ const WalletFilterPersona = ({
     >
       {personas.map((persona, idx) => {
         return (
-          <Flex
-            key={persona.title}
-            role="group"
-            direction="column"
-            alignItems="flex-start"
-            padding={2}
-            mb={2}
-            bg={filterBg}
-            borderRadius="base"
-            border="1px solid"
-            borderColor={getPersonaBorderColor(selectedPersona, idx)}
-            cursor="pointer"
-            transition="0.5s all"
-            _hover={{
-              borderColor: "primary.hover",
-            }}
-            onClick={() => {
-              if (idx === selectedPersona) {
-                resetFilters()
-                trackCustomEvent({
-                  eventCategory: "UserPersona",
-                  eventAction: `${persona.title}`,
-                  eventName: `${persona.title} false`,
-                })
-              } else {
-                setSelectedPersona(idx)
-                setFilters(persona.presetFilters)
-                trackCustomEvent({
-                  eventCategory: "UserPersona",
-                  eventAction: `${persona.title}`,
-                  eventName: `${persona.title} true`,
-                })
-              }
-            }}
-          >
-            <Flex alignItems="center" gap={2} px={2}>
-              <Checkbox
-                key={persona.title}
-                aria-label={persona.title}
-                // TODO: fix on click check
-                // isChecked={persona.presetFilters[option.filterKey!]}
-                width="full"
-                // onChange={handleClick}
-              >
-                <Heading
-                  as="h3"
-                  ms={0}
-                  fontSize="1.2rem"
-                  py={0}
-                  pe={1}
-                  ps={0}
-                  lineHeight="1.7rem"
-                  color="primary.base"
-                  transition="0.5s all"
-                  _groupHover={{ color: "primary.hover" }}
-                >
-                  {persona.title}
-                </Heading>
-              </Checkbox>
-            </Flex>
-            <Box
-              as="span"
-              p="0.5rem"
-              color={selectedPersona === idx ? "text" : "text200"}
-              fontSize="0.9rem"
-              fontWeight="normal"
+          <CheckboxGroup key={persona.title}>
+            <Flex
+              role="group"
+              direction="column"
+              alignItems="flex-start"
+              padding={2}
+              mb={2}
+              bg={filterBg}
+              borderRadius="base"
+              border="1px solid"
+              borderColor={getPersonaBorderColor(selectedPersona, idx)}
+              cursor="pointer"
               transition="0.5s all"
-              lineHeight={1.3}
+              _hover={{
+                borderColor: "primary.hover",
+              }}
+              onClick={() => {
+                if (idx === selectedPersona) {
+                  resetFilters()
+                  trackCustomEvent({
+                    eventCategory: "UserPersona",
+                    eventAction: `${persona.title}`,
+                    eventName: `${persona.title} false`,
+                  })
+                } else {
+                  setSelectedPersona(idx)
+                  setFilters(persona.presetFilters)
+                  trackCustomEvent({
+                    eventCategory: "UserPersona",
+                    eventAction: `${persona.title}`,
+                    eventName: `${persona.title} true`,
+                  })
+                }
+              }}
             >
-              {persona.description}
-            </Box>
-          </Flex>
+              <Flex alignItems="center" gap={2} px={2}>
+                <Checkbox
+                  key={persona.title}
+                  aria-label={persona.title}
+                  isChecked={selectedPersona === idx}
+                  width="full"
+                >
+                  <Heading
+                    as="h3"
+                    ms={0}
+                    fontSize="1.2rem"
+                    py={0}
+                    pe={1}
+                    ps={0}
+                    lineHeight="1.7rem"
+                    color="primary.base"
+                    transition="0.5s all"
+                    _groupHover={{ color: "primary.hover" }}
+                  >
+                    {persona.title}
+                  </Heading>
+                </Checkbox>
+              </Flex>
+              <Box
+                as="span"
+                p="0.5rem"
+                color={selectedPersona === idx ? "text" : "text200"}
+                fontSize="0.9rem"
+                fontWeight="normal"
+                transition="0.5s all"
+                lineHeight={1.3}
+              >
+                {persona.description}
+              </Box>
+            </Flex>
+          </CheckboxGroup>
         )
       })}
     </Flex>
