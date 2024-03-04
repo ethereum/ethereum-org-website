a2346aaafdcbd41abca527b1f669a912a6948dec
Nicolás Quiroz <nh.quiroz@gmail.com>
2024-03-04 02:52:32 -0300

    chore: use radio button for Persona filter

--- src/components/FindWallet/WalletFilterPersona.tsx
+++ src/components/FindWallet/WalletFilterPersona.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Box, Checkbox, CheckboxGroup, Flex, Heading } from "@chakra-ui/react"
+import { MdCircle } from "react-icons/md"
+import { Box, Flex, Heading, Icon, Text } from "@chakra-ui/react"
 
 import { trackCustomEvent } from "@/lib/utils/matomo"
 import { getPersonaBorderColor } from "@/lib/utils/wallets"
@@ -15,81 +15,98 @@ const WalletFilterPersona = ({
   const personas = useWalletPersonas()
 
   return (
-    <Flex direction={{ base: "column", lg: "row" }} gap={4}>
+    <Flex
+      direction={{ base: "column", lg: "row" }}
+      gap={4}
+      mb={{ base: 2, md: 0 }}
+    >
       {personas.map((persona, idx) => {
         return (
-          <CheckboxGroup key={persona.title}>
-            <Flex
-              role="group"
-              direction="column"
-              alignItems="flex-start"
-              padding={2}
-              mb={2}
-              bg="background.highlight"
-              w="100%"
-              borderRadius="base"
-              border="1px solid"
-              borderColor={getPersonaBorderColor(selectedPersona, idx)}
-              cursor="pointer"
-              transition="0.5s all"
-              _hover={{
-                borderColor: "primary.hover",
-              }}
-              onClick={() => {
-                if (idx === selectedPersona) {
-                  resetFilters()
-                  trackCustomEvent({
-                    eventCategory: "UserPersona",
-                    eventAction: `${persona.title}`,
-                    eventName: `${persona.title} false`,
-                  })
-                } else {
-                  setSelectedPersona(idx)
-                  setFilters(persona.presetFilters)
-                  trackCustomEvent({
-                    eventCategory: "UserPersona",
-                    eventAction: `${persona.title}`,
-                    eventName: `${persona.title} true`,
-                  })
-                }
-              }}
-            >
-              <Flex alignItems="center" gap={2} px={2}>
-                <Checkbox
-                  key={persona.title}
-                  aria-label={persona.title}
-                  isChecked={selectedPersona === idx}
-                  width="full"
-                >
-                  <Heading
-                    as="h3"
-                    ms={0}
-                    fontSize="1.2rem"
-                    py={0}
-                    pe={1}
-                    ps={0}
-                    lineHeight="1.7rem"
-                    color="primary.base"
-                    transition="0.5s all"
-                    _groupHover={{ color: "primary.hover" }}
-                  >
-                    {persona.title}
-                  </Heading>
-                </Checkbox>
-              </Flex>
-              <Box
-                as="span"
-                p="0.5rem"
-                color={selectedPersona === idx ? "text" : "text200"}
-                fontSize="0.9rem"
-                fontWeight="normal"
-                transition="0.5s all"
-                lineHeight={1.3}
-              >
-                {persona.description}
+          <Flex
+            key={persona.title}
+            direction="column"
+            alignItems="flex-start"
+            padding={2}
+            w="100%"
+            border="solid"
+            borderColor={getPersonaBorderColor(selectedPersona, idx)}
+            borderRadius="base"
+            cursor="pointer"
+            transition="0.5s all"
+            _hover={{
+              borderColor: "primary.hover",
+            }}
+            role="group"
+            onClick={() => {
+              if (idx === selectedPersona) {
+                resetFilters()
+                trackCustomEvent({
+                  eventCategory: "UserPersona",
+                  eventAction: `${persona.title}`,
+                  eventName: `${persona.title} false`,
+                })
+              } else {
+                setSelectedPersona(idx)
+                setFilters(persona.presetFilters)
+                trackCustomEvent({
+                  eventCategory: "UserPersona",
+                  eventAction: `${persona.title}`,
+                  eventName: `${persona.title} true`,
+                })
+              }
+            }}
+          >
+            <Flex alignItems="center" gap={2} mb="0.2rem" px={2}>
+              <Box role="checkbox" aria-label={`${persona.title} filter`}>
+                <Icon
+                  as={MdCircle}
+                  borderRadius="full"
+                  boxSize={2.5}
+                  my={0}
+                  mx={1}
+                  fill={
+                    selectedPersona === idx
+                      ? "primary.base"
+                      : "rgba(0, 0, 0, 0)"
+                  }
+                  background={
+                    selectedPersona === idx ? "primary.base" : "transparent"
+                  }
+                  outline="1.5px solid"
+                  outlineColor={
+                    selectedPersona === idx ? "primary.base" : "body.light"
+                  }
+                  outlineOffset="3px"
+                  fontSize={8}
+                  _groupHover={{ outlineColor: "primary.hover" }}
+                />
               </Box>
+
+              <Heading
+                as="h3"
+                ms={0}
+                fontSize="1.2rem"
+                py={0}
+                pe={1}
+                ps={0}
+                lineHeight="1.7rem"
+                color="primary.base"
+              >
+                {persona.title}
+              </Heading>
             </Flex>
-          </CheckboxGroup>
+
+            <Text
+              p="0.4rem"
+              color={selectedPersona === idx ? "text" : "text200"}
+              fontSize="0.9rem"
+              fontWeight="normal"
+              transition="0.5s all"
+              lineHeight={1.3}
+            >
+              {persona.description}
+            </Text>
+          </Flex>
         )
       })}
     </Flex>
