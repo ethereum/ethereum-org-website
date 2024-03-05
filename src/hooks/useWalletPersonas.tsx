1d0ac024bea586f8ab3f5bc03b187b19834feec3
Nicolás Quiroz <nh.quiroz@gmail.com>
2024-03-05 14:03:01 -0300

    chore: remove unused const

--- src/hooks/useWalletPersonas.tsx
+++ src/hooks/useWalletPersonas.tsx
@@ -4,61 +4,6 @@ import { WalletPersonas } from "@/lib/types"
 
 export const useWalletPersonas = () => {
   const { t } = useTranslation("page-wallets-find-wallet")
-
-  const filterLabels = {
-    hardware: {
-      label: t("page-find-wallet-hardware"),
-    },
-    open_source: {
-      label: t("page-find-wallet-open-source"),
-    },
-    non_custodial: {
-      label: t("page-find-wallet-non-custodial"),
-    },
-    hardware_support: {
-      label: t("page-find-wallet-hardware-wallet-support"),
-    },
-    rpc_importing: {
-      label: t("page-find-wallet-rpc-importing"),
-    },
-    nft_support: {
-      label: t("page-find-wallet-nft-support"),
-    },
-    connect_to_dapps: {
-      label: t("page-find-wallet-connect-to-dapps"),
-    },
-    staking: {
-      label: t("page-find-wallet-staking"),
-    },
-    swaps: {
-      label: t("page-find-wallet-swaps"),
-    },
-    layer_2: {
-      label: t("page-find-wallet-layer-2"),
-    },
-    gas_fee_customization: {
-      label: t("page-find-wallet-gas-fee-customization"),
-    },
-    ens_support: {
-      label: t("page-find-wallet-ens-support"),
-    },
-    buy_crypto: {
-      label: t("page-find-wallet-buy-crypto"),
-    },
-    withdraw_crypto: {
-      label: t("page-find-wallet-sell-for-fiat"),
-    },
-    multisig: {
-      label: t("page-find-wallet-multisig"),
-    },
-    social_recovery: {
-      label: t("page-find-wallet-social-recovery"),
-    },
-    erc_20_support: {
-      label: t("page-find-wallet-token-support"),
-    },
-  }
-
   const personas: WalletPersonas[] = [
     {
       title: t("page-find-wallet-new-to-crypto-title"),
