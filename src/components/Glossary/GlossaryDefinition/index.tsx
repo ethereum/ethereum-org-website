3be6bc44d182283c920004e7bc10031ef9b89044
Omkar Kamale <check.omkar@gmail.com>
2024-03-15 00:07:01 +0530

    Fix [#12393]: Missing auto-linked headers on glossary page

--- src/components/Glossary/GlossaryDefinition/index.tsx
+++ src/components/Glossary/GlossaryDefinition/index.tsx
@@ -1,6 +1,6 @@
-import { ComponentProps } from "react"
-import { Box, Text } from "@chakra-ui/react"
-
+import { ComponentProps, useState } from "react"
+import { Box, Text, Link, Flex } from "@chakra-ui/react"
+import { VscLink } from "react-icons/vsc"
 import InlineLink from "@/components/Link"
 import OldHeading from "@/components/OldHeading"
 import Translation from "@/components/Translation"
@@ -24,6 +24,7 @@ const GlossaryDefinition = ({
   size = "md",
   options = { ns: DEFAULT_GLOSSARY_NS },
 }: GlossaryDefinitionProps) => {
+  const [showLink, setShowLink] = useState(false)
   const headingStyles =
     size === "sm"
       ? { fontSize: "md", mt: 0, mb: 2 }
@@ -31,15 +32,39 @@ const GlossaryDefinition = ({
 
   const textStyles = size === "sm" ? { mb: 0 } : {}
 
+  const linkStyles = {
+    color: "inherit",
+    textDecoration: "none",
+  }
+
+  const linkIconStyles = {
+    left: "-1.5em",
+    top: "0.8em",
+    fontSize: "0.5em",
+    color: "var(--eth-colors-primary-base)",
+    display: showLink ? "" : "none",
+    cursor: "pointer",
+  }
+
   return (
-    <Box textAlign="start">
+    <Box textAlign="start" onMouseEnter={() => setShowLink(true)} onMouseLeave={() => setShowLink(false)}>
       <OldHeading as="h3" lineHeight={1.4} id={term} {...headingStyles}>
-        <Translation
-          id={term + "-term"}
-          options={options}
-          transform={components}
-        />
+        <Link href={`#${term}`} {...linkStyles} _hover={{ border: 'none', textDecoration: 'none' }}>
+          <Flex pos="relative">
+            <Box pos="absolute" _hover={{ opacity: '0.8' }} {...linkIconStyles}>
+              <VscLink />
+            </Box>
+            <Box>
+              <Translation
+                id={term + "-term"}
+                options={options}
+                transform={components}
+              />
+            </Box>
+          </Flex>
+        </Link>
       </OldHeading>
+
       <Text {...textStyles}>
         <Translation
           id={term + "-definition"}
