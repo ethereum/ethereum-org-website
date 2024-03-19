ffc48f1a503c83a07cb4434935f57990b1100c8e
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-03-19 13:41:10 +0000

    Refactor and add types

--- src/components/ChatBotModal/index.tsx
+++ src/components/ChatBotModal/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react"
-import { useChat } from "ai/react";
+import { useChat, UseChatHelpers } from "ai/react";
 import { Box, Flex,FormControl, Heading,Input, Text } from '@chakra-ui/react';
 import {
   Modal as ChakraModal,
@@ -11,11 +11,98 @@ import {
   UseDisclosureReturn,
 } from "@chakra-ui/react"
 
+type ChatMessagesProps = {
+  messages: UseChatHelpers["messages"];
+  messagesEndRef: React.RefObject<HTMLDivElement>;
+}
+
+type ChatInputProps = {
+  input: UseChatHelpers["input"];
+  handleInputChange: UseChatHelpers["handleInputChange"];
+  handleSubmit: UseChatHelpers["handleSubmit"];
+}
+
 type ChatBotModalProps = ModalContentProps &
   Pick<ModalProps, "size"> & {
     isOpen: UseDisclosureReturn["isOpen"]
     onClose: UseDisclosureReturn["onClose"]
-    children?: React.ReactNode
+  }
+
+  const ChatHeader = () => {
+    return  (
+      <Box
+      pt={8}
+      px={{ base: 4, sm: 8 }}
+      zIndex="sticky"
+      position="sticky"
+      top="0"
+      >
+        <Flex
+          pb={4}
+          borderBottom="1px"
+          borderColor="body.medium"
+          direction="column"
+          alignItems="center"
+          w="full"
+        >
+          <ModalCloseButton />
+          <Heading as="h2" fontSize={{ base: "xl", md: "xl" }} textAlign="center">
+              Ask Ethereum.org
+          </Heading>
+          <Text fontSize="sm">Ethereum.org AI Prototype</Text>
+        </Flex>
+      </Box>
+    )
+  }
+
+  const ChatMessages = ({ messages, messagesEndRef }: ChatMessagesProps) => {
+    return (
+        <Box
+          flex="1"
+          overflowY="auto"
+          py={4}
+          px={{ base: 4, sm: 8 }}
+        >
+          {/* Messages */}
+          {messages.length > 0 && messages.map(m => (
+              <Box key={m.id} whiteSpace="pre-wrap" mb="32px">
+                  <Text as="span" color="body.medium" fontSize="sm" fontStyle="italic">
+                    {m.role === "user" ? "You" : "Ask ethereum.org"}
+                  </Text>
+                  <Text>{m.content}</Text>
+              </Box>
+          ))}
+          {/* Empty div to scroll to */}
+          <Box ref={messagesEndRef}></Box>
+        </Box>
+    )
+  }
+
+  const ChatInput = ({ input, handleInputChange, handleSubmit }: ChatInputProps) => {
+    return (
+      <FormControl 
+        as="form" 
+        // @ts-ignore
+        onSubmit={handleSubmit}
+        position="sticky"
+        bottom="0"
+        pt={2}
+        pb={8}
+        px={{ base: 4, sm: 8 }}
+        bg="background.base" // Add background color to match modal
+        zIndex="sticky"
+      >
+        <Input
+          borderColor="gray.300"
+          borderRadius="md"
+          shadow="xl"
+          p="2"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </FormControl>
+    )
   }
 
   const Chat = () => {
@@ -35,71 +122,9 @@ type ChatBotModalProps = ModalContentProps &
         w="full"
         h="full"
       >
-        {/* Modal header */}
-        <Box
-          pt={8}
-          px={{ base: 4, sm: 8 }}
-          zIndex="sticky"
-          position="sticky"
-          top="0"
-        >
-          <Flex
-            pb={4}
-            borderBottom="1px"
-            borderColor="body.medium"
-            direction="column"
-            alignItems="center"
-            w="full"
-          >
-            <ModalCloseButton />
-            <Heading as="h2" fontSize={{ base: "xl", md: "xl" }} textAlign="center">
-                Ask Ethereum.org
-            </Heading>
-            <Text fontSize="sm">Ethereum.org AI Prototype</Text>
-          </Flex>
-        </Box>
-        {/* Chat messages */}
-        <Box
-          flex="1"
-          overflowY="auto"
-          py={4}
-          px={{ base: 4, sm: 8 }}
-        >
-          {/* Messages */}
-          {messages.length > 0 && messages.map(m => (
-              <Box key={m.id} whiteSpace="pre-wrap" mb="32px">
-                  <Text as="span" color="body.medium" fontSize="sm" fontStyle="italic">
-                    {m.role === "user" ? "You" : "Ask ethereum.org"}
-                  </Text>
-                  <Text>{m.content}</Text>
-              </Box>
-          ))}
-        {/* Empty div to scroll to */}
-          <Box ref={messagesEndRef}></Box>
-        </Box>
-        {/* Chat input */}
-        <FormControl 
-          as="form" 
-          // @ts-ignore
-          onSubmit={handleSubmit}
-          position="sticky"
-          bottom="0"
-          pt={2}
-          pb={8}
-          px={{ base: 4, sm: 8 }}
-          bg="background.base" // Add background color to match modal
-          zIndex="sticky"
-        >
-          <Input
-            borderColor="gray.300"
-            borderRadius="md"
-            shadow="xl"
-            p="2"
-            value={input}
-            placeholder="Say something..."
-            onChange={handleInputChange}
-          />
-        </FormControl>
+        <ChatHeader />
+        <ChatMessages messages={messages} messagesEndRef={messagesEndRef} />
+        <ChatInput input={input} handleInputChange={handleInputChange} handleSubmit={handleSubmit} />
       </Flex>
     );
   }
