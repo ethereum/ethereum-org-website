52abd4faaea91d707c6716e1830cc26276ea2d6c
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-03-19 13:07:50 +0000

    Implement prototype designs

--- src/components/ChatBotModal/index.tsx
+++ src/components/ChatBotModal/index.tsx
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import { useChat } from "ai/react";
-import { Box, Flex,FormControl,Heading,Input, Text } from '@chakra-ui/react';
+import { Box, Flex,FormControl, Heading,Input, Text } from '@chakra-ui/react';
 import {
   Modal as ChakraModal,
   ModalCloseButton,
@@ -22,39 +22,85 @@ type ChatBotModalProps = ModalContentProps &
     'use client';
 
     const { messages, input, handleInputChange, handleSubmit } = useChat();
-    console.log(messages)
+    const messagesEndRef = useRef<null | HTMLDivElement>(null)
+
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
 
     return (
       <Flex
         direction="column"
         justifyContent="space-between"
-        mx="auto"
         w="full"
-        maxW="md"
         h="full"
-        paddingTop="18"
       >
-      <Box overflowY="auto">
+        {/* Modal header */}
+        <Box
+          pt={8}
+          px={{ base: 4, sm: 8 }}
+          zIndex="sticky"
+          position="sticky"
+          top="0"
+        >
+          <Flex
+            pb={4}
+            borderBottom="1px"
+            borderColor="body.medium"
+            direction="column"
+            alignItems="center"
+            w="full"
+          >
+            <ModalCloseButton />
+            <Heading as="h2" fontSize={{ base: "xl", md: "xl" }} textAlign="center">
+                Ask Ethereum.org
+            </Heading>
+            <Text fontSize="sm">Ethereum.org AI Prototype</Text>
+          </Flex>
+        </Box>
+        {/* Chat messages */}
+        <Box
+          flex="1"
+          overflowY="auto"
+          py={4}
+          px={{ base: 4, sm: 8 }}
+        >
+          {/* Messages */}
           {messages.length > 0 && messages.map(m => (
-              <Box key={m.id} whiteSpace="pre-wrap">
-                  <Text as="span">{m.role === "user" ? "User: " : "AI: "}</Text>
-                  {m.content}
+              <Box key={m.id} whiteSpace="pre-wrap" mb="32px">
+                  <Text as="span" color="body.medium" fontSize="sm" fontStyle="italic">
+                    {m.role === "user" ? "You" : "Ask ethereum.org"}
+                  </Text>
+                  <Text>{m.content}</Text>
               </Box>
           ))}
-      </Box>
-      {/* @ts-ignore */}
-      <FormControl as="form" onSubmit={handleSubmit} my="4">
+        {/* Empty div to scroll to */}
+          <Box ref={messagesEndRef}></Box>
+        </Box>
+        {/* Chat input */}
+        <FormControl 
+          as="form" 
+          // @ts-ignore
+          onSubmit={handleSubmit}
+          position="sticky"
+          bottom="0"
+          pt={2}
+          pb={8}
+          px={{ base: 4, sm: 8 }}
+          bg="background.base" // Add background color to match modal
+          zIndex="sticky"
+        >
           <Input
-              borderColor="gray.300"
-              borderRadius="md"
-              shadow="xl"
-              p="2"
-              value={input}
-              placeholder="Say something..."
-              onChange={handleInputChange}
+            borderColor="gray.300"
+            borderRadius="md"
+            shadow="xl"
+            p="2"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
           />
-      </FormControl>
-  </Flex>
+        </FormControl>
+      </Flex>
     );
   }
 
@@ -74,28 +120,24 @@ export const ChatBotModal = ({
     >
       <ModalOverlay bgColor="blackAlpha.700" />
       <ModalContent
-        py={8}
-        px={{ base: 4, sm: 8 }}
+        bgColor="background.base"
         shadow="md"
         border="1px"
         borderColor="border"
         borderRadius="md"
-        overflowY="auto"
         overflowX="hidden"
         minH="unset"
         h="100%"
         maxH={{
           base: "calc(100vh - 1rem)",
-          md: "min(calc(100vh - 2rem), 792px)",
+          md: "min(calc(100vh - 2rem), 790px)",
         }}
         maxW={{
           base: "calc(100vw - 1rem)",
-          md: "min(calc(100vw - 2rem), 1000px)",
+          md: "min(calc(100vw - 2rem), 679px)",
         }}
         {...restProps}
       >
-        <ModalCloseButton />
-        <Heading as="h2" fontSize={{ base: "xl", md: "2xl" }} textAlign={"center"}>Ethereum.org AI</Heading>
         <Chat />
       </ModalContent>
     </ChakraModal>
