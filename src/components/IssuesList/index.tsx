de5d3f7a499ea435657d8e3e8e9d9272330b619c
Pablo Pettinari <pettinarip@gmail.com>
2024-03-29 16:10:55 +0100

    change tag colors based on color mode

--- src/components/IssuesList/index.tsx
+++ src/components/IssuesList/index.tsx
@@ -1,15 +1,19 @@
 import Emoji from "react-emoji-render"
 import {
   Avatar,
+  ColorMode,
   Flex,
   HStack,
   SimpleGrid,
   SimpleGridProps,
   Stack,
   Text,
+  useColorMode,
 } from "@chakra-ui/react"
 
 import type { GHIssue } from "@/lib/types"
+
+import { getContrastYIQ } from "@/lib/utils/getContrastYIQ"
 
 import InlineLink from "../Link"
 import Tag from "../Tag"
@@ -18,7 +22,23 @@ type IssuesListProps = SimpleGridProps & {
   issues: GHIssue[]
 }
 
+function pickTagColorsByColorMode(color: string, colorMode: ColorMode) {
+  if (colorMode === "dark") {
+    return {
+      color: `#${color}`,
+      bgColor: `#${color}22`,
+    }
+  } else {
+    return {
+      color: getContrastYIQ(`#${color}`),
+      bgColor: `#${color}`,
+    }
+  }
+}
+
 const IssuesList = ({ issues, ...props }: IssuesListProps) => {
+  const { colorMode } = useColorMode()
+
   return (
     <SimpleGrid columns={[1, null, 2]} spacing={4} {...props}>
       {issues.map((issue) => (
@@ -51,8 +71,7 @@ const IssuesList = ({ issues, ...props }: IssuesListProps) => {
                 <Tag
                   key={label.id}
                   label={<Emoji text={label.name} />}
-                  color={`#${label.color}`}
-                  bgColor={`#${label.color}22`}
+                  {...pickTagColorsByColorMode(label.color, colorMode)}
                 />
               )
             })}
