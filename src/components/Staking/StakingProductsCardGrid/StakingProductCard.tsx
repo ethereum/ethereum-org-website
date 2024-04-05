e2db1fdebd0d9bf68263d77e81ecdbac43c71200
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-04-05 11:15:59 -0400

    chore(StakingProductCard): add eslint disable line comment

--- src/components/Staking/StakingProductsCardGrid/StakingProductCard.tsx
+++ src/components/Staking/StakingProductsCardGrid/StakingProductCard.tsx
@@ -29,8 +29,7 @@ const getIconFromName = (
 ): ComponentType<SVGProps<SVGElement>> => {
   const {
     [imageName + "GlyphIcon"]: Icon,
-    // ! This throws a hidden type error
-    // "Require statement not part of import statement." `@typescript-eslint/no-var-requires`
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
   } = require("@/components/icons/staking")
   return Icon
 }
