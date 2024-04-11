42a2c4f0025774e01e49ec0418834b9d8d7fcb34
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-04-10 17:40:24 -0700

    refactor: postLangPRs to TS, remove shell version
    update get-translations.yml workflow

---
+++ src/scripts/crowdin/translations/postLangPRs.ts
@@ -0,0 +1,60 @@
+import { execSync } from "child_process"
+import { readFileSync, unlinkSync } from "fs"
+
+import i18n from "../../../../i18n.config.json"
+
+import { SUMMARY_PATH } from "./constants"
+
+const LANGUAGES = i18n.map((lang) => lang.code)
+const MONTH = new Date()
+  .toLocaleString("default", { month: "long" })
+  .toLowerCase()
+const TIMESTAMP = new Date().toISOString().replace(/[^0-9]/g, "")
+const STARTING_BRANCH = execSync("git rev-parse --abbrev-ref HEAD", {
+  encoding: "utf-8",
+}).trim()
+
+for (let lang of LANGUAGES) {
+  const gitStatus = execSync(`git status -s | grep -E "/${lang}/" | wc -l`, {
+    encoding: "utf-8",
+  }).trim()
+  if (+gitStatus === 0) continue
+
+  const BRANCH_NAME = `${MONTH}-${lang}-${TIMESTAMP}`
+  const MESSAGE = `chore: import translations for ${lang}`
+
+  execSync(`git checkout -b ${BRANCH_NAME}`)
+  execSync("git reset .")
+  execSync(`git add public/content/translations/${lang}`)
+  execSync(`git add src/intl/${lang}`)
+  execSync(`git commit -m "${MESSAGE}"`)
+  // Uncomment the following line when ready to push branches to GitHub
+  // execSync(`git push origin ${BRANCH_NAME}`)
+
+  type Lang = string
+  type QASummary = Record<Lang, string[]>
+  const summaryJson: QASummary = JSON.parse(readFileSync(SUMMARY_PATH, "utf-8"))
+  const qaResults = summaryJson[lang]
+    ? summaryJson[lang].join("\n")
+    : "No QA issues found"
+  const prBody = `## Description
+This PR was automatically created to import Crowdin translations.
+This workflows runs on the first of every month at 16:20 (UTC).
+
+Thank you to everyone contributing to translate ethereum.org ❤️
+
+## Markdown QA checker alerts
+${qaResults}`
+
+  // Uncomment the following lines when ready to create the PRs
+  // execSync(`gh auth login --with-token ${process.env.GITHUB_TOKEN}`)
+  // execSync(`gh pr create \
+  //   --base dev \
+  //   --head ${BRANCH_NAME} \
+  //   --title ${MESSAGE} \
+  //   --body "${prBody}"`)
+
+  execSync(`git checkout ${STARTING_BRANCH}`)
+}
+
+unlinkSync(SUMMARY_PATH)
