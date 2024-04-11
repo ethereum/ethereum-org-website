692953f141ec880b111110ad2c68b77e06804870
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-04-10 18:48:40 -0700

    debug: use --body-file for PR body

--- src/scripts/crowdin/translations/postLangPRs.ts
+++ src/scripts/crowdin/translations/postLangPRs.ts
@@ -1,4 +1,5 @@
 import { execSync } from "child_process"
+import fs from "fs"
 import { readFileSync, unlinkSync } from "fs"
 
 import i18n from "../../../../i18n.config.json"
@@ -37,6 +38,7 @@ for (let lang of TEST_LANGUAGE_SET /* LANGUAGES */) {
   const qaResults = summaryJson[lang]
     ? summaryJson[lang].join("\n")
     : "No QA issues found"
+
   const prBody = `## Description
 This PR was automatically created to import Crowdin translations.
 This workflows runs on the first of every month at 16:20 (UTC).
@@ -46,11 +48,15 @@ Thank you to everyone contributing to translate ethereum.org ❤️
 ## Markdown QA checker alerts
 ${qaResults}`
 
+  fs.writeFileSync("body.txt", prBody)
+
   execSync(`gh pr create \
-    --base dev \
-    --head ${BRANCH_NAME} \
-    --title ${MESSAGE} \
-    --body "${prBody}"`)
+  --base dev \
+  --head ${BRANCH_NAME} \
+  --title ${MESSAGE} \
+  --body-file body.txt"`)
+
+  fs.unlinkSync("body.txt")
 
   execSync(`git checkout ${STARTING_BRANCH}`)
 }
