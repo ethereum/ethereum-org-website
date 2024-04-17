e7a4de4564c8c0ebe2ab0f1d2c39bc79a922a34b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-04-17 11:26:16 -0700

    debug: json format

--- src/scripts/crowdin/translations/postLangPRs.ts
+++ src/scripts/crowdin/translations/postLangPRs.ts
@@ -9,12 +9,13 @@ import { processLocale } from "./utils"
 const locales = i18n.map((lang) => lang.code)
 
 function postLangPRs() {
-  const bucketsListData = fs.readFileSync(BUCKETS_PATH, "utf-8")
-  const bucketsList: BucketsList = JSON.parse(bucketsListData)
+  const bucketsListRead = fs.readFileSync(BUCKETS_PATH, "utf-8")
+  const bucketsList = JSON.parse(bucketsListRead) as BucketsList
+
   if (!bucketsList) throw new Error("Failed to read buckets list.")
 
   for (const locale of locales) {
-    if (!bucketsList[locale]) return
+    if (!bucketsList[locale]) continue
     processLocale(locale, bucketsList[locale])
   }
 }
