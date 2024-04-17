c1cbc87d00dc07bb415702fa9eca71818ffc4ec1
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-04-17 10:52:28 -0700

    fix: guard check for no match

--- src/scripts/crowdin/translations/postLangPRs.ts
+++ src/scripts/crowdin/translations/postLangPRs.ts
@@ -8,10 +8,12 @@ import { processLocale } from "./utils"
 const locales = i18n.map((lang) => lang.code)
 
 function postLangPRs() {
-  const bucketsList = fs.readFileSync(BUCKETS_PATH, "utf-8")
+  const bucketsListData = fs.readFileSync(BUCKETS_PATH, "utf-8")
+  const bucketsList = JSON.parse(bucketsListData)
   if (!bucketsList) throw new Error("Failed to read buckets list.")
 
   for (const locale of locales) {
+    if (!bucketsList[locale]) return
     processLocale(locale, bucketsList[locale])
   }
 }
