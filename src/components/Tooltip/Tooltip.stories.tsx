cd46b7f626b51a625eaa0be64c783e37beb8d18f
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-04-23 14:10:12 -0400

    fix(Tooltip.stories): fix open popover snapshot

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { RiInformationLine } from "react-icons/ri"
 import { Box } from "@chakra-ui/react"
 import { Meta, StoryObj } from "@storybook/react"
-import { expect, fireEvent, waitFor, within } from "@storybook/test"
+import { expect, fireEvent, userEvent, waitFor, within } from "@storybook/test"
 
 import InlineLink from "../Link"
 import Translation from "../Translation"
@@ -47,6 +47,13 @@ const meta = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <Box position="relative">
+        <Story />
+      </Box>
+    ),
+  ],
 } satisfies Meta<TooltipType>
 
 export default meta
@@ -64,9 +71,8 @@ export const OnOpen: Story = {
 
     const tooltipIcon = canvas.getByTestId("tooltip-icon")
 
-    fireEvent.mouseOver(tooltipIcon)
-
     await waitFor(async () => {
+      await userEvent.hover(tooltipIcon)
       await expect(canvasParent.getByTestId("tooltip-popover")).toBeVisible()
     })
   },
