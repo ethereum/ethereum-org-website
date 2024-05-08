546ce9b9043bbbf3c4f598df1a3a1d7a96b35f69
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-05-08 15:21:59 -0700

    feat: rm exportApprovedOnly, check if build done
    Repeat calls could produce the same build ID. `import "dotenv/config"` not needed when called from an action

--- src/scripts/crowdin/translations/triggerBuild.ts
+++ src/scripts/crowdin/translations/triggerBuild.ts
@@ -1,14 +1,17 @@
 import crowdin from "../api-client/crowdinClient"
 
-import "dotenv/config"
-
 async function triggerBuild() {
   const projectId = Number(process.env.CROWDIN_PROJECT_ID) || 363359
 
   try {
-    await crowdin.translationsApi.buildProject(projectId, {
-      exportApprovedOnly: true,
-    })
+    const response = await crowdin.translationsApi.buildProject(projectId)
+    const { id, status } = response.data
+    const isAlreadyFinished = status === "finished"
+    console.log(
+      `Build ${isAlreadyFinished ? "already finished" : "triggered"} id:`,
+      id
+    )
+    console.log(`::set-output name=buildId::${id}`)
   } catch (error: unknown) {
     console.error((error as Error).message)
   }
