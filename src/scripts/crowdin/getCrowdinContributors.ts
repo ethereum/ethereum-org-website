6d618e4fcb51b86dc2ca70389ba0752f81995b4d
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-05-08 14:50:50 -0700

    revert: removal of fetchAndSaveDirectories from getCrowdinContributors

--- src/scripts/crowdin/getCrowdinContributors.ts
+++ src/scripts/crowdin/getCrowdinContributors.ts
@@ -1,10 +1,12 @@
 import { getTranslatedMarkdownPaths } from "../markdownChecker"
 
 import getTranslationCostsReports from "./reports/getTranslationCostsReports"
+import getAndSaveDirectories from "./source-files/fetchAndSaveDirectories"
 import fetchAndSaveFileIds from "./source-files/fetchAndSaveFileIds"
 import getDirectoryIds from "./utils"
 
 async function main() {
+  await getAndSaveDirectories()
   const directoryIds = getDirectoryIds()
   await fetchAndSaveFileIds(directoryIds)
   const translatedMarkdownPaths = await getTranslatedMarkdownPaths()
