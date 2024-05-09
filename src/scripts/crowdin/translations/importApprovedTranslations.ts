549210667c47c7d763d611c0f8e279422c1f18c1
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-05-09 11:54:54 -0700

    chore: rename functions

--- src/scripts/crowdin/translations/importApprovedTranslations.ts
+++ src/scripts/crowdin/translations/importApprovedTranslations.ts
@@ -1,26 +1,26 @@
 import fs from "fs"
 
 import { checkMarkdown } from "../../markdownChecker"
-import crowdinImport from "../import/main"
+import importBuckets from "../import/main"
 import type { BucketsList } from "../import/types"
 
 import { BUCKETS_PATH, SUMMARY_PATH } from "./constants"
 import getApprovedBuckets from "./getApprovedBuckets"
 
-async function main() {
+async function importApprovedTranslations() {
   // Get approved buckets from Crowdin
   const buckets = (await getApprovedBuckets()) as BucketsList
 
   // Save buckets for use in PR body later
   fs.writeFileSync(BUCKETS_PATH, JSON.stringify(buckets, null, 2))
 
-  // Run Crowdin import script with buckets from Notion
-  crowdinImport(buckets)
+  // Run Crowdin import script with approved buckets
+  importBuckets(buckets)
 
   // Check markdown
   checkMarkdown(SUMMARY_PATH)
 }
 
-main()
+importApprovedTranslations()
 
-export default main
+export default importApprovedTranslations
