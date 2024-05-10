82e32800b7e19a140b6349e8f5ef87594beaea65
Nicolás Quiroz <nh.quiroz@gmail.com>
2024-05-10 17:14:50 -0300

    chore: update ButtonLink

--- src/components/Buttons/ButtonLink.tsx
+++ src/components/Buttons/ButtonLink.tsx
@@ -4,12 +4,18 @@ import { BaseLink, type LinkProps } from "@/components/Link"
 import { type MatomoEventOptions, trackCustomEvent } from "@/lib/utils/matomo"
 
 export type ButtonLinkProps = LinkProps &
-  Omit<ButtonProps, "toId"> & {
+  Omit<ButtonProps, "toId" | "onClick"> & {
     customEventOptions?: MatomoEventOptions
   }
 
-const ButtonLink = ({ ...props }: ButtonLinkProps) => {
-  return <Button as={BaseLink} activeStyle={{}} {...props} />
+const ButtonLink = ({ customEventOptions, ...props }: ButtonLinkProps) => {
+  const handleClick = () => {
+    customEventOptions && trackCustomEvent(customEventOptions)
+  }
+
+  return (
+    <Button as={BaseLink} activeStyle={{}} {...props} onClick={handleClick} />
+  )
 }
 
 export default ButtonLink
