bc8fdc96faa3353b117872e383f17214114af316
Pablo Pettinari <pettinarip@gmail.com>
2024-05-13 18:17:48 +0200

    disable nextjs compression

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ module.exports = (phase, { defaultConfig }) => {
     images: {
       deviceSizes: [640, 750, 828, 1080, 1200, 1504, 1920],
     },
-    compress: true,
+    compress: false,
   }
 
   if (phase !== PHASE_DEVELOPMENT_SERVER) {
