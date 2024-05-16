6a052a3b55540bd9187be8d0318dc56bd699d18d
Nicolás Quiroz <nh.quiroz@gmail.com>
2024-05-16 09:02:53 -0300

    feat: add breakpoints to EnergyConsumptionChart

--- src/components/EnergyConsumptionChart.tsx
+++ src/components/EnergyConsumptionChart.tsx
@@ -22,11 +22,6 @@ import type { Lang } from "@/lib/types"
 import { wrapLabel } from "@/lib/utils/charts"
 import { isLangRightToLeft } from "@/lib/utils/translations"
 
-import {
-  ENERGY_CONSUMPTION_CHART_COLORS,
-  ENERGY_CONSUMPTION_CHART_DATA,
-} from "@/lib/constants"
-
 // ChartDataLabels required to display y-labels on top of bars
 ChartJS.register(
   CategoryScale,
@@ -41,29 +36,130 @@ const EnergyConsumptionChart = () => {
   const { locale } = useRouter()
   const isRtl = isLangRightToLeft(locale as Lang)
 
-  // chart labels
-  const labels = [
-    t("energy-consumption-chart-global-data-centers-label"),
-    t("energy-consumption-chart-btc-pow-label"),
-    t("energy-consumption-gold-mining-cbeci-label"),
-    t("energy-consumption-chart-gaming-us-label"),
-    t("energy-consumption-chart-eth-pow-label"),
-    "Google",
-    t("energy-consumption-chart-netflix-label"),
-    t("energy-consumption-chart-paypal-label"),
-    t("energy-consumption-chart-airbnb-label"),
-    t("energy-consumption-chart-eth-pos-label"),
-  ]
+  // chart rawData, according to different breakpoints
+  const rawData = useBreakpointValue({
+    base: [
+      {
+        name: t("energy-consumption-chart-global-data-centers-label"),
+        amount: 190,
+        color: "#FF0000",
+      },
+      {
+        name: t("energy-consumption-chart-btc-pow-label"),
+        amount: 149,
+        color: "#F2A900",
+      },
+      {
+        name: t("energy-consumption-chart-gaming-us-label"),
+        amount: 34,
+        color: "#71BB8A",
+      },
+      {
+        name: t("energy-consumption-chart-eth-pow-label"),
+        amount: 21,
+        color: "#C1B6F5",
+      },
+      {
+        name: t("energy-consumption-chart-eth-pos-label"),
+        amount: 0.0026,
+        color: "#C1B6F5",
+      },
+    ],
+    sm: [
+      {
+        name: t("energy-consumption-chart-global-data-centers-label"),
+        amount: 190,
+        color: "#FF0000",
+      },
+      {
+        name: t("energy-consumption-chart-btc-pow-label"),
+        amount: 149,
+        color: "#D7B14A",
+      },
+      {
+        name: t("energy-consumption-gold-mining-cbeci-label"),
+        amount: 131,
+        color: "#F2A900",
+      },
+      {
+        name: t("energy-consumption-chart-eth-pow-label"),
+        amount: 21,
+        color: "#C1B6F5",
+      },
+      {
+        name: t("energy-consumption-chart-netflix-label"),
+        amount: 0.457,
+        color: "#E50914",
+      },
+      {
+        name: t("energy-consumption-chart-eth-pos-label"),
+        amount: 0.0026,
+        color: "#C1B6F5",
+      },
+    ],
+    md: [
+      {
+        name: t("energy-consumption-chart-global-data-centers-label"),
+        amount: 190,
+        color: "#FF0000",
+      },
+      {
+        name: t("energy-consumption-chart-btc-pow-label"),
+        amount: 149,
+        color: "#D7B14A",
+      },
+      {
+        name: t("energy-consumption-gold-mining-cbeci-label"),
+        amount: 131,
+        color: "#D7B14A",
+      },
+      {
+        name: t("energy-consumption-chart-gaming-us-label"),
+        amount: 34,
+        color: "#71BB8A",
+      },
+      {
+        name: t("energy-consumption-chart-eth-pow-label"),
+        amount: 21,
+        color: "#C1B6F5",
+      },
+      {
+        name: "Google",
+        amount: 19,
+        color: "#E50914",
+      },
+      {
+        name: t("energy-consumption-chart-netflix-label"),
+        amount: 0.457,
+        color: "#E50914",
+      },
+      {
+        name: t("energy-consumption-chart-paypal-label"),
+        amount: 0.26,
+        color: "#C1B6F5",
+      },
+      {
+        name: t("energy-consumption-chart-airbnb-label"),
+        amount: 0.02,
+        color: "#E50914",
+      },
+      {
+        name: t("energy-consumption-chart-eth-pos-label"),
+        amount: 0.0026,
+        color: "#C1B6F5",
+      },
+    ],
+  })
 
   const chartOptions = {
     // chart styles
     barThickness: 38,
     borderRadius: 4,
     aspectRatio: 1.1,
-    responsive: true,
-    maintainAspectRatio: true,
+    // responsive: true,
+    // maintainAspectRatio: true,
     hover: { mode: null } as any, // casted to avoid TS issue
-    backgroundColor: ENERGY_CONSUMPTION_CHART_COLORS as any, // casted to avoid TS issue
+    backgroundColor: rawData?.map((item) => item.color) as any, // casted to avoid TS issue
     plugins: {
       // required plugin to display labels on top
       datalabels: {
@@ -130,132 +226,21 @@ const EnergyConsumptionChart = () => {
   }
 
   const chartData = {
-    labels: labels.map((l) => wrapLabel(l)),
+    labels: rawData?.map((item) => wrapLabel(item.name)),
     datasets: [
       {
         label: t("page-what-is-ethereum-energy-consumption-chart-legend"),
-        data: ENERGY_CONSUMPTION_CHART_DATA,
+        data: rawData?.map((item) => item.amount),
       },
     ],
   }
 
-  // TODO: add mobile breakpoints
-  // const data = useBreakpointValue<Data>({
-  //   base: [
-  //     {
-  //       name: t("energy-consumption-chart-global-data-centers-label"),
-  //       amount: 190,
-  //       color: "#FF0000",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-btc-pow-label"),
-  //       amount: 149,
-  //       color: "#F2A900",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-gaming-us-label"),
-  //       amount: 34,
-  //       color: "#71BB8A",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-eth-pow-label"),
-  //       amount: 21,
-  //       color: "#C1B6F5",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-eth-pos-label"),
-  //       amount: 0.0026,
-  //       color: "#C1B6F5",
-  //     },
-  //   ],
-  //   sm: [
-  //     {
-  //       name: t("energy-consumption-chart-global-data-centers-label"),
-  //       amount: 190,
-  //       color: "#FF0000",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-btc-pow-label"),
-  //       amount: 149,
-  //       color: "#D7B14A",
-  //     },
-  //     {
-  //       name: t("energy-consumption-gold-mining-cbeci-label"),
-  //       amount: 131,
-  //       color: "#F2A900",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-eth-pow-label"),
-  //       amount: 21,
-  //       color: "#C1B6F5",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-netflix-label"),
-  //       amount: 0.457,
-  //       color: "#E50914",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-eth-pos-label"),
-  //       amount: 0.0026,
-  //       color: "#C1B6F5",
-  //     },
-  //   ],
-  //   md: [
-  //     {
-  //       name: t("energy-consumption-chart-global-data-centers-label"),
-  //       amount: 190,
-  //       color: "#FF0000",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-btc-pow-label"),
-  //       amount: 149,
-  //       color: "#D7B14A",
-  //     },
-  //     {
-  //       name: t("energy-consumption-gold-mining-cbeci-label"),
-  //       amount: 131,
-  //       color: "#D7B14A",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-gaming-us-label"),
-  //       amount: 34,
-  //       color: "#71BB8A",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-eth-pow-label"),
-  //       amount: 21,
-  //       color: "#C1B6F5",
-  //     },
-  //     {
-  //       name: "Google",
-  //       amount: 19,
-  //       color: "#E50914",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-netflix-label"),
-  //       amount: 0.457,
-  //       color: "#E50914",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-paypal-label"),
-  //       amount: 0.26,
-  //       color: "#C1B6F5",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-airbnb-label"),
-  //       amount: 0.02,
-  //       color: "#E50914",
-  //     },
-  //     {
-  //       name: t("energy-consumption-chart-eth-pos-label"),
-  //       amount: 0.0026,
-  //       color: "#C1B6F5",
-  //     },
-  //   ],
-  // })
+  // FIX: height on mobile
+  // FIX: cramped bars on resizing
 
   return (
     <Center w="full">
+      {/* height:40vh; width:80vw" */}
       <Box maxW="500px" w="full">
         {/* TODO: isRtl ? data?.reverse() : data */}
         <Bar options={chartOptions} data={chartData} />
