a5b842b96025cae004d0fbe470415caf78336a71
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-05-17 09:13:18 -0400

    chore(.storybook/manager): ignore favicon type error

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,5 @@
 import { addons } from "@storybook/manager-api"
 import theme from "./theme"
-// @ts-ignore
 import favicon from "../public/favicon.png"
 
 addons.setConfig({
@@ -10,5 +9,6 @@ addons.setConfig({
 // In order to override the default favicon, and inject a data hash link to the png
 const link = document.createElement("link")
 link.setAttribute("rel", "shortcut icon")
+// @ts-ignore
 link.setAttribute("href", favicon)
 document.head.appendChild(link)
