5cb52accaaa468c9f6ede75753ae22d0dbe37c21
Nicolás Quiroz <nh.quiroz@gmail.com>
2024-06-07 15:07:16 -0300

    chore: comment out code not being used temporally until banner logix is fixed

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,7 +1,7 @@
-import { join } from "path"
+// import { join } from "path"
 
 import dynamic from "next/dynamic"
-import { useRouter } from "next/router"
+// import { useRouter } from "next/router"
 import { Container } from "@chakra-ui/react"
 
 import type { Root } from "@/lib/types"
@@ -9,42 +9,40 @@ import type { Root } from "@/lib/types"
 import Footer from "@/components/Footer"
 import Nav from "@/components/Nav"
 import { SkipLink } from "@/components/SkipLink"
-import TranslationBanner from "@/components/TranslationBanner"
-import TranslationBannerLegal from "@/components/TranslationBannerLegal"
 
-import { toPosixPath } from "@/lib/utils/relativePath"
-
-import { DEFAULT_LOCALE } from "@/lib/constants"
-
+// import TranslationBanner from "@/components/TranslationBanner"
+// import TranslationBannerLegal from "@/components/TranslationBannerLegal"
+// import { toPosixPath } from "@/lib/utils/relativePath"
+// import { DEFAULT_LOCALE } from "@/lib/constants"
 import { lightTheme as oldTheme } from "../theme"
 
 export const RootLayout = ({
   children,
-  contentIsOutdated,
-  contentNotTranslated,
+  // contentIsOutdated,
+  // contentNotTranslated,
   lastDeployDate,
 }: Root) => {
-  const { locale, asPath } = useRouter()
+  // const { locale, asPath } = useRouter()
 
-  const CONTRIBUTING = "/contributing/"
-  const isUntranslatedContributingPage =
-    asPath.includes(CONTRIBUTING) &&
-    !(asPath.endsWith(CONTRIBUTING) || asPath.includes("/translation-program/"))
+  // const CONTRIBUTING = "/contributing/"
+  // const isUntranslatedContributingPage =
+  //   asPath.includes(CONTRIBUTING) &&
+  //   !(asPath.endsWith(CONTRIBUTING) || asPath.includes("/translation-program/"))
 
-  const isLegal =
-    isUntranslatedContributingPage ||
-    asPath.includes(`/cookie-policy/`) ||
-    asPath.includes(`/privacy-policy/`) ||
-    asPath.includes(`/terms-of-use/`) ||
-    asPath.includes(`/style-guide/`)
+  // const isLegal =
+  //   isUntranslatedContributingPage ||
+  //   asPath.includes(`/cookie-policy/`) ||
+  //   asPath.includes(`/privacy-policy/`) ||
+  //   asPath.includes(`/terms-of-use/`) ||
+  //   asPath.includes(`/style-guide/`)
 
-  const isPageLanguageEnglish = locale === DEFAULT_LOCALE
+  // const isPageLanguageEnglish = locale === DEFAULT_LOCALE
 
-  const shouldShowTranslationBanner =
-    (contentIsOutdated || (contentNotTranslated && !isPageLanguageEnglish)) &&
-    !isLegal
-  const shouldShowLegalTranslationBanner = isLegal && !isPageLanguageEnglish
-  const originalPagePath = toPosixPath(join(DEFAULT_LOCALE, asPath))
+  // const shouldShowTranslationBanner =
+  //   (contentIsOutdated || (contentNotTranslated && !isPageLanguageEnglish)) &&
+  //   !isLegal
+  // const shouldShowLegalTranslationBanner = isLegal && !isPageLanguageEnglish
+  // const originalPagePath = toPosixPath(join(DEFAULT_LOCALE, asPath))
 
   const FeedbackWidget = dynamic(() => import("@/components/FeedbackWidget"), {
     ssr: false,
