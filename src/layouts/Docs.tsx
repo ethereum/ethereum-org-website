60c818df592243672e39de925b21d727fea6b132
Nicolás Quiroz <nh.quiroz@gmail.com>
2024-06-12 10:16:15 -0300

    chore: update Docs layout to use lastEditLocaleTimestamp prop

--- src/layouts/Docs.tsx
+++ src/layouts/Docs.tsx
@@ -197,11 +197,11 @@ type DocsLayoutProps = Pick<
   MdPageContent,
   | "slug"
   | "tocItems"
-  | "lastUpdatedDate"
+  | "lastEditLocaleTimestamp"
   | "contributors"
   | "contentNotTranslated"
 > &
-  Required<Pick<MdPageContent, "lastUpdatedDate">> &
+  MdPageContent &
   ChildOnlyProp & {
     frontmatter: DocsFrontmatter
   }
@@ -211,7 +211,7 @@ export const DocsLayout = ({
   frontmatter,
   slug,
   tocItems,
-  lastUpdatedDate,
+  lastEditLocaleTimestamp,
   contributors,
   contentNotTranslated,
 }: DocsLayoutProps) => {
@@ -233,7 +233,7 @@ export const DocsLayout = ({
           <H1 id="top">{frontmatter.title}</H1>
           <FileContributors
             contributors={contributors}
-            lastEdit={lastUpdatedDate}
+            lastEditLocaleTimestamp={lastEditLocaleTimestamp}
           />
           <TableOfContents
             slug={slug}
