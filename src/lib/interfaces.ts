e601a0c3256038e95c5269f0082f65077790ced2
Nicolás Quiroz <nh.quiroz@gmail.com>
2024-06-19 17:51:40 -0300

    chore: update interfaces

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -1,7 +1,6 @@
 import type { StaticImageData } from "next/image"
 
 import type {
-  CrowdinContributor,
   FileContributor,
   Frontmatter,
   Lang,
@@ -163,4 +162,27 @@ export interface IGetInvolvedCard {
   emoji: string
   title: string
   description: string
+}
+
+/**
+ * TitleCardList
+ */
+
+export interface ITitleCardItem {
+  title: string
+  description: string
+  caption?: string
+  link?: string
+  image?: string
+  alt?: string
+  id?: number
+}
+
+/**
+ * Codeblock
+ */
+
+export interface CodeExample extends ITitleCardItem {
+  codeLanguage: string
+  code: string
 }
