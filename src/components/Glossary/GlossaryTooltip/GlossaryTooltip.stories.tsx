e2f1d4cbef58fe77fe2919ca277722e263592e22
Pablo Pettinari <pettinarip@gmail.com>
2024-06-19 18:58:14 +0200

    fix heading styles in glossary definition and tooltip + add story for the glossary tooltip open state

--- src/components/Glossary/GlossaryTooltip/GlossaryTooltip.stories.tsx
+++ src/components/Glossary/GlossaryTooltip/GlossaryTooltip.stories.tsx
@@ -5,13 +5,21 @@ import GlossaryTooltipComponent from "."
 const meta = {
   title: "Molecules / Overlay Content / Glossary Tooltip",
   component: GlossaryTooltipComponent,
+  args: {
+    termKey: "big-endian",
+    children: "big-endian",
+  },
 } satisfies Meta<typeof GlossaryTooltipComponent>
 
 export default meta
 
-export const GlossaryTooltip: StoryObj<typeof meta> = {
+type Story = StoryObj<typeof meta>
+
+export const Basic: Story = {}
+
+// for chromatic story snapshot showing the rendered popover
+export const OnOpen: Story = {
   args: {
-    termKey: "big-endian",
-    children: "big-endian",
+    isOpen: true,
   },
 }
--- src/components/Glossary/GlossaryTooltip/index.tsx
+++ src/components/Glossary/GlossaryTooltip/index.tsx
@@ -1,30 +1,58 @@
 import React, { ReactNode } from "react"
 import { useRouter } from "next/router"
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Text, VStack } from "@chakra-ui/react"
 
-import GlossaryDefinition from "@/components/Glossary/GlossaryDefinition"
-import Tooltip from "@/components/Tooltip"
+import Heading from "@/components/Heading"
+import InlineLink from "@/components/Link"
+import Tooltip, { type TooltipProps } from "@/components/Tooltip"
+import Translation from "@/components/Translation"
 
 import { trackCustomEvent } from "@/lib/utils/matomo"
 import { cleanPath } from "@/lib/utils/url"
 
-type GlossaryTooltipProps = {
+type GlossaryTooltipProps = Omit<TooltipProps, "content"> & {
   children: ReactNode
   termKey: string
 }
 
-const GlossaryTooltip = ({ children, termKey }: GlossaryTooltipProps) => {
+const components = {
+  a: InlineLink,
+}
+
+const GlossaryTooltip = ({
+  children,
+  termKey,
+  ...props
+}: GlossaryTooltipProps) => {
   const { asPath } = useRouter()
 
   return (
     <Box as="span" display="inline-block">
       <Tooltip
+        {...props}
         content={
-          <GlossaryDefinition
-            term={termKey}
-            size="sm"
-            options={{ ns: "glossary-tooltip" }}
-          />
+          <VStack spacing={2} align="stretch" textAlign="start">
+            <Heading as="h6">
+              <Translation
+                id={termKey + "-term"}
+                options={{ ns: "glossary-tooltip" }}
+                transform={components}
+              />
+            </Heading>
+            {/**
+             * `as="span"` prevents hydration warnings for strings that contain
+             * elements that cannot be nested inside `p` tags, like `ul` tags
+             * (found in some Glossary definition).
+             * TODO: Develop a better solution to handle this case.
+             */}
+            <Text as="span">
+              <Translation
+                id={termKey + "-definition"}
+                options={{ ns: "glossary-tooltip" }}
+                transform={components}
+              />
+            </Text>
+          </VStack>
         }
         onBeforeOpen={() => {
           trackCustomEvent({
