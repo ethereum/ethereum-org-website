2876f53abe863ca3bd73f6191ed1254c898cb60a
tomasgrusz <tomas@grusz.dev>
2024-06-28 00:29:47 +0200

    feat: add Logo story

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -1,1 +1,19 @@
-export default null
+import * as React from "react"
+import { Meta, StoryObj } from "@storybook/react"
+
+import LogoComponent from "."
+
+const meta = {
+  title: "Atoms / Media & Icons / Logo",
+  component: LogoComponent,
+} satisfies Meta<typeof LogoComponent>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Logo: Story = {
+    render: () => (
+        <LogoComponent />
+    ),
+}
