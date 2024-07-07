7fcb2553ace598b10c4541232e5d72cb05fb27f8
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-07-07 02:49:33 -0400

    chore(tailwind/.storybook): update breakpoint set for viewports

--- tailwind/.storybook/preview.tsx
+++ tailwind/.storybook/preview.tsx
@@ -8,6 +8,16 @@ import i18n, { baseLocales } from "./i18next"
 import "../../src/styles/global.css"
 import "../../src/styles/main.css"
 
+export const breakpointSet: [token: string, value: string][] = [
+  ["base", "375px"],
+  ["sm", "640px"],
+  ["md", "768px"],
+  ["lg", "1024px"],
+  ["xl", "1280px"],
+  ["2xl", "1536px"],
+
+]
+
 
 const preview: Preview = {
   globals: {
@@ -50,56 +60,18 @@ const preview: Preview = {
     layout: "centered",
     // Modify viewport selection to match Chakra breakpoints (or custom breakpoints)
     viewport: {
-      viewports: {
-        base: {
-          name: "base",
-          styles: {
-            width: '375px',
-            height: "800px"
-          },
-          type: "mobile"
-        },
-        sm: {
-          name: "sm",
-          styles: {
-            width: '640px',
-            height: "800px"
-          },
-          type: "mobile"
-        },
-        md: {
-          name: "md",
-          styles: {
-            width: '768px',
-            height: "800px"
-          },
-          type: "tablet"
-        },
-        lg: {
-          name: "lg",
-          styles: {
-            width: '1024px',
-            height: "800px"
-          },
-          type: "desktop"
-        },
-        xl: {
-          name: "xl",
-          styles: {
-            width: '1280px',
-            height: "800px"
-          },
-          type: "desktop"
-        },
-        "2xl": {
-          name: "2xl",
-          styles: {
-            width: '1536px',
-            height: "800px"
-          },
-          type: "desktop"
-        },
-      },
+      viewports: breakpointSet.reduce<{ [token: string]: { name: string, styles: Record<'width' | 'height', string>} }>((arr, [token, value]) => {
+        return {
+          ...arr,
+          [token]: {
+            name: token,
+            styles: {
+              width: value,
+              height: "800px"
+            }
+          }
+        }
+      }, {})
     },
   },
 }
