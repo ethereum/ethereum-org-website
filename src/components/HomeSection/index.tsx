a2c9bb6d08cf54c272205c35fcce4a562d812c45
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-07-27 14:28:53 -0700

    create: HomeSection component

---
+++ src/components/HomeSection/index.tsx
@@ -0,0 +1,47 @@
+import NextImage, { type StaticImageData } from "next/image"
+import type { ReactNode } from "react"
+
+import { cn } from "@/lib/utils/cn"
+
+type HomeSectionProps = {
+  tag: string
+  title: ReactNode
+  imgSrc: StaticImageData
+  reverse?: boolean
+  className?: string
+  children?: ReactNode
+}
+
+const HomeSection = ({
+  tag,
+  title,
+  imgSrc,
+  reverse,
+  className,
+  children,
+}: HomeSectionProps) => {
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 gap-x-32 gap-y-8 px-6 lg:grid-cols-3",
+        reverse && "md:flex-row-reverse",
+        className
+      )}
+    >
+      <NextImage
+        className="min-h-full rounded-4xl object-cover object-center"
+        src={imgSrc}
+        alt="Ethereum artwork"
+      />
+      <div className="lg:col-span-2">
+        <div className="w-fit rounded-full bg-primary-low-contrast px-4 py-0 text-sm uppercase text-primary">
+          {tag}
+        </div>
+        <h2 className="mt-2 text-5xl font-black">{title}</h2>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default HomeSection
