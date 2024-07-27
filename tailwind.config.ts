2622945fdb37cc80a0ac1ed2f37529f5b5fc50b8
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-07-27 14:28:38 -0700

    theme: add 4xl rounded (2rem, 32px)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -127,6 +127,10 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      // Add custom border-radius tailwinds extension for "4xl" as "2rem"
+      borderRadius: {
+        "4xl": "2rem" /* 32px */,
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
