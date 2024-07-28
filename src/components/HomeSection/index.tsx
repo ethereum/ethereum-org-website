8875bffc4e2da93f60c21d740d5f6c89e464b76e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-07-27 20:34:14 -0700

    update: HomeSection component

--- src/components/HomeSection/index.tsx
+++ src/components/HomeSection/index.tsx
@@ -7,7 +7,6 @@ type HomeSectionProps = {
   tag: string
   title: ReactNode
   imgSrc?: StaticImageData // TODO: Revert to required before prod
-  imageLast?: boolean
   className?: string
   children?: ReactNode
 }
@@ -16,33 +15,17 @@ const HomeSection = ({
   tag,
   title,
   imgSrc,
-  imageLast,
   className,
   children,
 }: HomeSectionProps) => {
   return (
-    <div
-      className={cn(
-        "grid grid-flow-dense grid-cols-1 lg:grid-cols-2 xl:grid-cols-3",
-        "gap-8 xl:gap-16",
-        className
-      )}
-    >
+    <div className={cn("flex flex-col gap-8 lg:flex-row xl:gap-16", className)}>
       <NextImage
-        className={cn(
-          "col-span-1",
-          "col-span-1 min-h-full rounded-4xl object-cover object-center",
-          imageLast && "md:col-start-3"
-        )}
+        className="min-h-full w-full rounded-4xl object-cover object-center lg:max-w-128"
         src={imgSrc || ""}
         alt=""
       />
-      <div
-        className={cn(
-          "col-span-1 xl:col-span-2",
-          imageLast && "md:col-start-3"
-        )}
-      >
+      <div>
         <div className="w-fit rounded-full bg-primary-low-contrast px-4 py-0 text-sm uppercase text-primary">
           {tag}
         </div>
