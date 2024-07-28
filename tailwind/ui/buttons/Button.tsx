eb748de3e6a313a5ffe52131c8f8b25262b970e4
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-07-28 14:25:36 -0700

    theme: update button outline to use primary

--- tailwind/ui/buttons/Button.tsx
+++ tailwind/ui/buttons/Button.tsx
@@ -15,7 +15,8 @@ const buttonVariants = cva(
       variant: {
         solid:
           "!text-background bg-primary border-transparent disabled:bg-disabled disabled:text-background hover:text-background hover:bg-primary-hover hover:shadow-button-hover active:shadow-none",
-        outline: "hover:shadow-button-hover active:shadow-none",
+        outline:
+          "border-primary !text-primary fill-primary hover:shadow-button-hover active:shadow-none",
         ghost: "border-transparent",
         link: "border-transparent font-bold underline py-0 px-1 active:text-primary",
       },
