2ea7d873664a9366a8349e55d6307a59c57d7fad
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-07-28 20:19:10 -0700

    chore: add icon animation

--- src/components/Buttons/SvgButtonLink.tsx
+++ src/components/Buttons/SvgButtonLink.tsx
@@ -21,7 +21,7 @@ const SvgButtonLink = ({
   <a className={cn("group", className)} {...props}>
     <div className="flex flex-col items-center gap-3.5 lg:flex-row">
       <div className="relative grid size-[5em] place-items-center rounded-2xl border shadow-lg">
-        <Svg />
+        <Svg className="group-hover:animate-pulse" />
       </div>
       <div className="text-center lg:text-start">
         {label && <p className="text-xl font-bold">{label}</p>}
