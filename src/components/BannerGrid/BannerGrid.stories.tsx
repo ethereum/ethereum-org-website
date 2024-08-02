940776600956605b3ac6d86cca594a1ce7eb4054
baystef <daramola.adebayo@yahoo.com>
2024-08-02 18:45:17 +0100

    chore(update): run yarn install

--- src/components/BannerGrid/BannerGrid.stories.tsx
+++ src/components/BannerGrid/BannerGrid.stories.tsx
@@ -8,16 +8,17 @@ import Text from "@/components/OldText"
 import { getTranslation } from "@/storybook-utils"
 
 import { langViewportModes } from "../../../.storybook/modes"
+import { ContentContainer } from "../MdComponents"
 
 import {
-  Banner as BannerComponent, BannerBody as BannerBodyComponent,
+  Banner as BannerComponent,
+  BannerBody as BannerBodyComponent,
   BannerGrid as BannerGridComponent,
   BannerGridCell as BannerGridCellComponent,
   BannerImage as BannerImageComponent,
 } from "."
 
 import stats from "@/public/images/upgrades/newrings.png"
-
 
 const meta = {
   title: "Molecules / Display Content / Banner",
@@ -33,17 +34,17 @@ const meta = {
   decorators: [
     (Story) => (
       <Box position="relative" w="full">
-        {/* <ContentContainer> */}
+        <ContentContainer>
           <Story />
-        {/* </ContentContainer> */}
+        </ContentContainer>
       </Box>
-    )
-  ]
+    ),
+  ],
 } satisfies Meta<typeof BannerComponent>
 
 export default meta
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>
 
 export const Banner: Story = {
   render: () => {
@@ -52,7 +53,7 @@ export const Banner: Story = {
         <div>What </div>
       </BannerComponent>
     )
-  }
+  },
 }
 
 export const BannerBody: Story = {
@@ -62,7 +63,7 @@ export const BannerBody: Story = {
         <div>What </div>
       </BannerBodyComponent>
     )
-  }
+  },
 }
 
 export const BannerImage: Story = {
@@ -72,7 +73,7 @@ export const BannerImage: Story = {
         <Image src={stats} alt="" width={400} />
       </BannerImageComponent>
     )
-  }
+  },
 }
 
 export const BannerGrid: Story = {
@@ -80,21 +81,26 @@ export const BannerGrid: Story = {
     const PAGE_WHAT_IS_ETH = "page-what-is-ethereum"
     return (
       <BannerGridComponent>
-        {Array.from({ length: 6 }, (_, i) => i + 1).map(item => <BannerGridCellComponent key={item}>
-          <Box fontSize="5xl" mb={4} lineHeight={1}>
-            {item}k+
-          </Box>
-          <Box fontSize="md" color="text200">
-            {getTranslation("page-what-is-ethereum-ethereum-in-numbers-stat-1-desc", PAGE_WHAT_IS_ETH)}
-            <Text as="span" whiteSpace="nowrap">
-              &nbsp;
-              <Box as="span">
-                <Icon as={MdInfoOutline} verticalAlign="middle" />
-              </Box>
-            </Text>
-          </Box>
-        </BannerGridCellComponent>)}
+        {Array.from({ length: 6 }, (_, i) => i + 1).map((item) => (
+          <BannerGridCellComponent key={item}>
+            <Box fontSize="5xl" mb={4} lineHeight={1}>
+              {item}k+
+            </Box>
+            <Box fontSize="md" color="text200">
+              {getTranslation(
+                "page-what-is-ethereum-ethereum-in-numbers-stat-1-desc",
+                PAGE_WHAT_IS_ETH
+              )}
+              <Text as="span" whiteSpace="nowrap">
+                &nbsp;
+                <Box as="span">
+                  <Icon as={MdInfoOutline} verticalAlign="middle" />
+                </Box>
+              </Text>
+            </Box>
+          </BannerGridCellComponent>
+        ))}
       </BannerGridComponent>
     )
-  }
+  },
 }
