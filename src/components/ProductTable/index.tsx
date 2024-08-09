02f2bb9d523d7d126694fa7706b7f4172051a131
Corwin Smith <cssmittys@gmail.com>
2024-08-08 23:53:10 -0600

    typing

--- src/components/ProductTable/index.tsx
+++ src/components/ProductTable/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-import type { TPresetFilters } from "@/lib/types"
+import type { ProductTablePresetFilters, TPresetFilters } from "@/lib/types"
 
 import PresetFilters from "@/components/ProductTable/PresetFilters"
 
@@ -8,7 +8,9 @@ interface ProductTableProps<TData> {
   presetFilters: TPresetFilters<TData>[]
 }
 
-const ProductTable = ({ presetFilters }: ProductTableProps<object>) => {
+const ProductTable = ({
+  presetFilters,
+}: ProductTableProps<ProductTablePresetFilters>) => {
   const [activePresetIndex, setActivePresetIndex] = useState(NaN)
 
   return (
@@ -24,7 +26,7 @@ const ProductTable = ({ presetFilters }: ProductTableProps<object>) => {
       ) : (
         <></>
       )}
-      <div className="flex">
+      <div className="flex gap-6 px-4 pb-6 pt-4 2xl:px-0">
         <div>Filters</div>
         <div>Table</div>
       </div>
