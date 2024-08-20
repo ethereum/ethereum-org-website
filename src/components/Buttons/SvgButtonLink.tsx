e82e8228a4b67b6d6a8ab9b0a83c4fc8a3345bf0
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-08-20 13:05:36 -0400

    fix: SvgButtonLink styling

--- src/components/Buttons/SvgButtonLink.tsx
+++ src/components/Buttons/SvgButtonLink.tsx
@@ -20,14 +20,14 @@ const SvgButtonLink = ({
 }: SvgButtonLinkProps) => (
   <a
     className={cn(
-      "group rounded-2xl focus:outline focus:outline-4 focus:outline-offset-8",
+      "group rounded-2xl no-underline focus:outline focus:outline-4 focus:outline-offset-8",
       className
     )}
     {...props}
   >
-    <div className="flex flex-col items-center gap-3.5 lg:flex-row">
-      <div className="relative grid size-[5em] place-items-center rounded-2xl border shadow-lg group-hover:shadow-2xl group-hover:transition-shadow group-hover:duration-100 group-focus:shadow-2xl group-focus:transition-shadow group-focus:duration-100">
-        <Svg className="group-hover:animate-pulse group-focus:animate-pulse" />
+    <div className="flex flex-col items-center gap-3.5 lg:flex-row lg:items-center">
+      <div className="relative grid aspect-square size-[5em] shrink-0 place-items-center rounded-2xl border shadow-lg group-hover:shadow-2xl group-hover:transition-shadow group-hover:duration-100 group-focus:shadow-2xl group-focus:transition-shadow group-focus:duration-100 lg:self-start">
+        <Svg className="text-4xl group-hover:animate-pulse group-focus:animate-pulse" />
       </div>
       <div className="text-center lg:text-start">
         {label && <p className="text-xl font-bold">{label}</p>}
