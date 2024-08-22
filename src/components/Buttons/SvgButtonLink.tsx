177f45c9c53d346d00a897dd1aa7a58d552ac948
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-08-21 18:41:19 -0700

    feat: hover and focus styling

--- src/components/Buttons/SvgButtonLink.tsx
+++ src/components/Buttons/SvgButtonLink.tsx
@@ -29,8 +29,20 @@ const SvgButtonLink = ({
     {...props}
   >
     <div className="flex flex-col items-center gap-3.5 lg:flex-row lg:items-center">
-      <div className="relative grid aspect-square size-[5em] shrink-0 place-items-center rounded-2xl border shadow-lg group-hover:shadow-2xl group-hover:transition-shadow group-hover:duration-100 group-focus:shadow-2xl group-focus:transition-shadow group-focus:duration-100 lg:self-start">
-        <Svg className="text-4xl group-hover:animate-pulse group-focus:animate-pulse" />
+      <div
+        className={cn(
+          "relative grid aspect-square size-[5em] shrink-0 place-items-center lg:self-start",
+          "rounded-2xl border shadow-svg-button-link transition-shadow duration-200",
+          "group-hover:shadow-svg-button-link-hover group-hover:transition-shadow group-hover:duration-200",
+          "group-focus:shadow-none group-focus:transition-shadow group-focus:duration-200"
+        )}
+      >
+        <Svg
+          className={cn(
+            "group-hover:rotate-3 group-hover:scale-110",
+            "text-4xl duration-200 group-hover:duration-200 group-focus:rotate-3"
+          )}
+        />
       </div>
       <div className="text-center lg:text-start">
         {label && <p className="text-xl font-bold">{label}</p>}
