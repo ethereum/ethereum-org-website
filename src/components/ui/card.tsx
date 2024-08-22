1d05798f8ecfe0939664cda325cf7a1a640bbd3e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-08-22 15:12:28 -0700

    style: update base `card` components and variants

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,20 +1,50 @@
 import * as React from "react"
+import { cva, VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils/cn"
 
-const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "bg-card text-card-foreground rounded-lg border shadow-sm",
-      className
-    )}
-    {...props}
-  />
-))
+import { BaseLink } from "./Link"
+
+const titleVariants = cva("", {
+  variants: {
+    variant: {
+      normal: "font-normal text-lg",
+      strong: "text-2xl font-bold",
+    },
+  },
+  defaultVariants: {
+    variant: "normal",
+  },
+})
+
+type CardProps = React.HTMLAttributes<HTMLDivElement> & {
+  href?: string
+}
+const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, href, ...props }, ref) => {
+    if (href) {
+      return (
+        <BaseLink
+          href={href}
+          className={cn(
+            "group rounded-2xl text-body no-underline hover:text-body",
+            className
+          )}
+          hideArrow
+        >
+          <div ref={ref} {...props} />
+        </BaseLink>
+      )
+    }
+    return (
+      <div
+        ref={ref}
+        className={cn("group rounded-2xl", className)}
+        {...props}
+      />
+    )
+  }
+)
 Card.displayName = "Card"
 
 const CardHeader = React.forwardRef<
@@ -29,16 +59,29 @@ const CardHeader = React.forwardRef<
 ))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement>
+const CardBanner = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <h3
+  <div
     ref={ref}
     className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
+      "h-48 w-full self-stretch overflow-hidden rounded-2xl",
+      "[&_img]:size-full [&_img]:object-cover [&_img]:outline [&_img]:duration-200 group-hover:[&_img]:scale-110 group-hover:[&_img]:duration-200",
       className
     )}
+    {...props}
+  />
+))
+CardBanner.displayName = "CardBanner"
+
+const CardTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement> & VariantProps<typeof titleVariants>
+>(({ className, variant, ...props }, ref) => (
+  <h3
+    ref={ref}
+    className={cn(titleVariants({ variant, className }))}
     {...props}
   />
 ))
@@ -50,7 +93,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn("text-muted-foreground text-sm", className)}
+    className={cn("text-sm text-body-medium", className)}
     {...props}
   />
 ))
@@ -60,7 +103,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("space-y-2.5 p-2.5", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
@@ -76,4 +119,12 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
+export {
+  Card,
+  CardBanner,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+}
