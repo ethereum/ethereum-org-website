cb2a25709172b0b0fa4d381287b5b3e85c58845d
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-08-28 15:21:29 -0700

    feat: add pagination/nav to mobile PostsSwiper

--- src/components/PostsSwiper/index.tsx
+++ src/components/PostsSwiper/index.tsx
@@ -1,4 +1,5 @@
-import { Keyboard /* , Pagination */ } from "swiper/modules"
+import { MdChevronLeft, MdChevronRight } from "react-icons/md"
+import { Navigation, Pagination } from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react"
 
 import type { ClassNameProp, RSSItem } from "@/lib/types"
@@ -8,8 +9,8 @@ import { cn } from "@/lib/utils/cn"
 import PostPreviewCard from "../Homepage/PostPreviewCard"
 
 import "swiper/css"
-// import "swiper/css/navigation"
-// import "swiper/css/pagination"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
 
 type PostsSwiperProps = ClassNameProp & {
   items: RSSItem[]
@@ -17,22 +18,25 @@ type PostsSwiperProps = ClassNameProp & {
 const PostsSwiper = ({ className, items }: PostsSwiperProps) => (
   <div className={cn("h-fit gap-8", className)}>
     <Swiper
-      cssMode
-      // navigation
-      // pagination
+      // cssMode
+      navigation={{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }}
+      pagination={{ clickable: true }}
       keyboard
       spaceBetween={32}
-      modules={[
-        // Navigation,
-        // Pagination,
-        Keyboard,
-      ]}
+      modules={[Navigation, Pagination]}
     >
       {items.map((item) => (
         <SwiperSlide key={item.title}>
           <PostPreviewCard {...item} />
         </SwiperSlide>
       ))}
+
+      <MdChevronLeft className="swiper-button-prev" />
+      <div className="swiper-pagination" />
+      <MdChevronRight className="swiper-button-next" />
     </Swiper>
   </div>
 )
