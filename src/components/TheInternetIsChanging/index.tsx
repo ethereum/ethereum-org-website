8a91198acd364bd01fd2ee264296b572cf267845
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-09-16 12:08:14 -0700

    feat: import finalized copy

--- src/components/TheInternetIsChanging/index.tsx
+++ src/components/TheInternetIsChanging/index.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "next-i18next"
 import { FaCheck } from "react-icons/fa"
-import { Portal } from "@radix-ui/react-portal"
+
+import Tooltip from "@/components/Tooltip"
 
 import { cn } from "@/lib/utils/cn"
 
@@ -10,70 +11,121 @@ import {
   SectionHeader,
   SectionTag,
 } from "../ui/section"
-import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip"
 
 import { usePrefersReducedMotion } from "@/hooks/usePrefersReducedMotion"
 
 type Item = {
-  oldLabel: string
-  newLabel: string
-  explanation: string[]
+  label: string
+  content: string[]
+}
+
+type Pairing = {
+  legacy: Item
+  ethereum: Item
 }
 
 // TODO: Extract strings for intl
-// TODO: Clean up and finalize copy; currently WIP
-const items: Item[] = [
+const pairings: Pairing[] = [
   {
-    oldLabel: "Discriminatory",
-    newLabel: "Equal Access",
-    explanation: [
-      "Not everyone has the same opportunities or ability to use certain resources, services, or information.",
-      "Some people might have more or better access, while others are left out or given less.",
-      "Anyone in the world should be able to participate as a user, observer or developer, on a maximally equal access. Participation should be permissionless and open to everyone.",
-      "Ethereum protocol is designed to be fair and impartial. It only focuses on whether the process is valid, not who’s behind it.",
-    ],
+    legacy: {
+      label: "Restricted ownership",
+      content: [
+        "With a regular bank or social media platform, your assets and data are managed by the organization. You rely on them for access and control.",
+        "They may use your data in ways you might not agree with, based on their policies.",
+      ],
+    },
+    ethereum: {
+      label: "Direct ownership",
+      content: [
+        "With Ethereum, only you have access and control. Nobody else should ever be able to use your assets. You can decide who to grant that permission.",
+      ],
+    },
   },
   {
-    oldLabel: "Censored",
-    newLabel: "Censorship Resistant",
-    explanation: [
-      "Services decide who gets to speak or participate online, which can silence voices unfairly.",
-      "It also means you can lose access to your following or communities without any say or way to appeal.",
-      "Nobody should be able to stop anyone from using Ethereum apps or doing what they want. Bad actors should be addressed at a different layer.",
-    ],
+    legacy: {
+      label: "Discriminatory",
+      content: [
+        "Today, not everyone has the same access to financial services. Some people may face barriers to access due to their location or nationality.",
+      ],
+    },
+    ethereum: {
+      label: "Equal Access",
+      content: [
+        "We believe everyone should be allowed to benefit from a global system. That is why Ethereum grants equal access to all worldwide, regardless of who you are or where you come from.",
+      ],
+    },
   },
   {
-    oldLabel: "Opaque",
-    newLabel: "Transparent",
-    explanation: [
-      "Anyone should be able to check whether Ethereum and its apps are working correctly and are following the rules their creators claim the apps have, even if you don't trust the creators.",
-    ],
+    legacy: {
+      label: "No privacy",
+      content: [
+        "We cannot expect governments, corporations, or other large, faceless organizations to grant us privacy out of their beneficence.",
+        "Most apps gather as much of your personal information as possible so that they can target you with tailored marketing.",
+      ],
+    },
+    ethereum: {
+      label: "Privacy oriented",
+      content: [
+        "Ethereum community respects privacy. You you have the right to use apps without revealing yourself or your contact information.",
+      ],
+    },
   },
   {
-    oldLabel: "Centralized",
-    newLabel: "Decentralized",
-    explanation: [
-      "Centralization gives too much power to one group, which can lead to censorship or control over users.",
-      "It also creates a single point of failure, making the system more vulnerable to hacks or outages.",
-      "Ethereum is not owned or controlled by any particular entity, even the Ethereum source code is not produced by a single company.",
-      "Ethereum exists thanks to thousands of independent individuals around the world working together for Ethereum’s independence.",
-    ],
+    legacy: {
+      label: "Integrated",
+      content: [
+        "Most apps push you to create separate accounts, making it hard to remember all your login details and registrations.",
+      ],
+    },
+    ethereum: {
+      label: "Fragmented",
+      content: [
+        "With Ethereum you can reuse one account in all apps instead. No individual registrations are needed.",
+      ],
+    },
   },
   {
-    oldLabel: "Monopolistic",
-    newLabel: "Distributed",
-    explanation: [
-      "Some companies and individuals have grown so strong they control most of the resources, gaining significant power over others.",
-      "This allows them to influence decisions and outcomes to their benefit while limiting opportunities for others.",
-    ],
+    legacy: {
+      label: "Centralized",
+      content: [
+        "Companies are owned by private entrepreneurs and shareholders. They alone exert control over the company and benefit the most from its success.",
+      ],
+    },
+    ethereum: {
+      label: "Decentralized",
+      content: [
+        "Just like the internet itself, Ethereum doesn't belong to anyone. It’s shared and shaped equally with all. There is no single owner who could control it.",
+      ],
+    },
   },
   {
-    oldLabel: "Coercive",
-    newLabel: "Opt-in",
-    explanation: [
-      "Current monopolies try to capture and trap you inside a walled ecosystem.",
-      "Ethereum community builds tools that do their task but otherwise freely integrate with a wider open ecosystem instead.",
-    ],
+    legacy: {
+      label: "Censorable",
+      content: [
+        "Modern platforms and its rules often change. They can be influenced by stakeholders, company management or even oppressive regimes.",
+      ],
+    },
+    ethereum: {
+      label: "Censorship resistant",
+      content: [
+        "Resistance to oppression is key principal of Ethereum. Its functionality should always stay fair and impartial.",
+        "Ethereum cannot be controlled by any nation state, company or individual.",
+      ],
+    },
+  },
+  {
+    legacy: {
+      label: "Closed to most",
+      content: [
+        "Companies protect their intellectual property and don’t share. No one outside the company can see how things work, fix problems, or make improvements. Its hard for people to create new tools or customize.",
+      ],
+    },
+    ethereum: {
+      label: "Open to all",
+      content: [
+        "Ethereum is public to all. Anyone can see, use, and improve the code, making it better for everyone.",
+      ],
+    },
   },
 ]
 
@@ -83,36 +135,22 @@ type ItemProps = React.HTMLAttributes<HTMLButtonElement> & {
   icon?: React.ReactNode
 }
 
-const Item = ({
-  children,
-  explanation,
-  className,
-  separatorClass,
-  icon,
-}: ItemProps) => (
+const Item = ({ children, explanation, separatorClass, icon }: ItemProps) => (
   <>
-    <Tooltip>
-      <TooltipTrigger>
-        <button
-          className={cn(
-            "flex h-fit items-center gap-x-1 text-nowrap rounded-full px-4 py-1 text-xs font-bold uppercase md:text-md",
-            className
-          )}
-        >
-          {icon}
-          {children}
-        </button>
-      </TooltipTrigger>
-      <Portal>
-        <TooltipContent className="max-w-sm space-y-2 rounded-lg border-body-light text-sm md:text-md">
+    <Tooltip
+      content={
+        <>
           <h3 className="text-md uppercase text-body-medium">{children}</h3>
           {explanation.map((line) => (
             <p key={line} className="text-sm">
               {line}
             </p>
           ))}
-        </TooltipContent>
-      </Portal>
+        </>
+      }
+    >
+      {icon}
+      {children}
     </Tooltip>
     <div
       className={cn(
@@ -131,7 +169,7 @@ const TheInternetIsChanging = () => {
   }
 
   return (
-    <Section id="values">
+    <Section id="values" className="!my-64">
       <SectionContent className="flex flex-col items-center text-center">
         <SectionTag>{t("page-index:page-index-values-tag")}</SectionTag>
         <SectionHeader>
@@ -141,7 +179,7 @@ const TheInternetIsChanging = () => {
           {t("page-index:page-index-values-description")}
         </p>
       </SectionContent>
-      <div className="mt-19 overflow-hidden max-xl:-mx-4 xl:rounded-2xl">
+      <div className="mt-19 overflow-hidden max-2xl:-mx-4 2xl:rounded-2xl">
         <div className="group bg-blue-50 dark:bg-blue-600">
           <div
             className="flex max-w-full overflow-hidden motion-reduce:overflow-auto"
@@ -152,12 +190,12 @@ const TheInternetIsChanging = () => {
               .map((_, idx) => (
                 <div
                   key={idx}
-                  className="group-hover:animate-pause flex min-w-fit animate-scroll-left items-center space-x-10 p-6 motion-reduce:w-full motion-reduce:animate-none motion-reduce:justify-center"
+                  className="group-hover:animate-pause flex min-w-fit animate-scroll-left items-center space-x-10 p-6 ring motion-reduce:w-full motion-reduce:animate-none motion-reduce:justify-center"
                 >
-                  {items.map(({ newLabel: label, explanation }) => (
+                  {pairings.map(({ ethereum: { label, content } }) => (
                     <Item
                       key={label}
-                      explanation={explanation}
+                      explanation={content}
                       separatorClass="bg-accent-a"
                       className="group/item bg-blue-100 text-blue-600 hover:bg-blue-600 hover:text-white dark:hover:bg-blue-700"
                       icon={
@@ -184,10 +222,10 @@ const TheInternetIsChanging = () => {
                   key={idx}
                   className="group-hover:animate-pause flex animate-scroll-right items-center space-x-10 p-6 motion-reduce:w-full motion-reduce:animate-none motion-reduce:justify-center"
                 >
-                  {items.map(({ oldLabel: label, explanation }) => (
+                  {pairings.map(({ legacy: { label, content } }) => (
                     <Item
                       key={label}
-                      explanation={explanation}
+                      explanation={content}
                       className="bg-gray-200/20 text-body-medium hover:bg-gray-600 hover:text-white dark:bg-gray-950 dark:text-body"
                       separatorClass="bg-gray-200 dark:bg-gray-950"
                     >
