e667e98684032ad6ca5039a1935bbca74c18a67d
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-09-16 19:31:32 -0400

    refactor(icons/quiz): migrate GreenTickIcon to react-icons base

--- src/components/icons/quiz/GreenTickIcon.tsx
+++ src/components/icons/quiz/GreenTickIcon.tsx
@@ -1,10 +1,8 @@
 import * as React from "react"
-import { createIcon } from "@chakra-ui/react"
+import { IconBase, type IconType } from "react-icons/lib"
 
-export const GreenTickIcon = createIcon({
-  displayName: "GreenTickIcon",
-  viewBox: "0 0 16 16",
-  path: (
+export const GreenTickIcon: IconType = (props) => (
+  <IconBase viewBox="0 0 16 16" {...props}>
     <g>
       <rect width={16} height={16} fill="#c8f7d8" rx={8} />
       <path
@@ -12,5 +10,5 @@ export const GreenTickIcon = createIcon({
         d="M7.446 11.641 4 8.567l1.214-1.539 2.232 1.539 3.408-3.848 1.646.989-5.054 5.933Z"
       />
     </g>
-  ),
-})
+  </IconBase>
+)
