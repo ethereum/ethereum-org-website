615864736f7d1dd21ef8a26312c242aa876a1aa4
Joshua <62268199+minimalsm@users.noreply.github.com>
2024-09-23 14:47:58 +0100

    Add locale to tracking and add English keys to pairing groups

--- src/components/Homepage/useValuesMarquee.ts
+++ src/components/Homepage/useValuesMarquee.ts
@@ -1,6 +1,7 @@
 import { useTranslation } from "next-i18next"
 
 type Item = {
+  key: string
   label: string
   content: string[]
 }
@@ -11,10 +12,13 @@ export type Pairing = {
 }
 
 export const useValuesMarquee = () => {
-  const { t } = useTranslation("page-index")
+  const { t, i18n } = useTranslation("page-index")
+  const locale = i18n.language
+
   const pairings: Pairing[] = [
     {
       legacy: {
+        key: "ownership-legacy",
         label: t("page-index-values-ownership-legacy-label"),
         content: [
           t("page-index-values-ownership-legacy-content-0"),
@@ -22,22 +26,26 @@ export const useValuesMarquee = () => {
         ],
       },
       ethereum: {
+        key: "ownership-ethereum",
         label: t("page-index-values-ownership-ethereum-label"),
         content: [t("page-index-values-ownership-ethereum-content-0")],
       },
     },
     {
       legacy: {
+        key: "fairness-legacy",
         label: t("page-index-values-fairness-legacy-label"),
         content: [t("page-index-values-fairness-legacy-content-0")],
       },
       ethereum: {
+        key: "fairness-ethereum",
         label: t("page-index-values-fairness-ethereum-label"),
         content: [t("page-index-values-fairness-ethereum-content-0")],
       },
     },
     {
       legacy: {
+        key: "privacy-legacy",
         label: t("page-index-values-privacy-legacy-label"),
         content: [
           t("page-index-values-privacy-legacy-content-0"),
@@ -45,36 +53,43 @@ export const useValuesMarquee = () => {
         ],
       },
       ethereum: {
+        key: "privacy-ethereum",
         label: t("page-index-values-privacy-ethereum-label"),
         content: [t("page-index-values-privacy-ethereum-content-0")],
       },
     },
     {
       legacy: {
+        key: "integration-legacy",
         label: t("page-index-values-integration-legacy-label"),
         content: [t("page-index-values-integration-legacy-content-0")],
       },
       ethereum: {
+        key: "integration-ethereum",
         label: t("page-index-values-integration-ethereum-label"),
         content: [t("page-index-values-integration-ethereum-content-0")],
       },
     },
     {
       legacy: {
+        key: "decentralization-legacy",
         label: t("page-index-values-decentralization-legacy-label"),
         content: [t("page-index-values-decentralization-legacy-content-0")],
       },
       ethereum: {
+        key: "decentralization-ethereum",
         label: t("page-index-values-decentralization-ethereum-label"),
         content: [t("page-index-values-decentralization-ethereum-content-0")],
       },
     },
     {
       legacy: {
+        key: "censorship-legacy",
         label: t("page-index-values-censorship-legacy-label"),
         content: [t("page-index-values-censorship-legacy-content-0")],
       },
       ethereum: {
+        key: "censorship-ethereum",
         label: t("page-index-values-censorship-ethereum-label"),
         content: [
           t("page-index-values-censorship-ethereum-content-0"),
@@ -84,15 +99,17 @@ export const useValuesMarquee = () => {
     },
     {
       legacy: {
+        key: "open-legacy",
         label: t("page-index-values-open-legacy-label"),
         content: [t("page-index-values-open-legacy-content-0")],
       },
       ethereum: {
+        key: "open-ethereum",
         label: t("page-index-values-open-ethereum-label"),
         content: [t("page-index-values-open-ethereum-content-0")],
       },
     },
   ]
 
-  return { t, pairings }
+  return { t, pairings, locale }
 }
