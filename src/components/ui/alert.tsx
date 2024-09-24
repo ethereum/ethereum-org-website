5b7a31857169d3582520482142eb0db75a4b5152
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-09-24 12:59:28 -0700

    theme: use 16px font for alert descriptions

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -67,7 +67,7 @@ const AlertDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    className={cn("[&_p]:leading-relaxed", className)}
     {...props}
   />
 ))
