87c9123dfd741726eac6ca0e7caded32ea99da91
Corwin Smith <cssmittys@gmail.com>
2024-09-25 16:49:32 -0600

    setup work for layer 2 hub page

---
+++ src/pages/layer-2/index.tsx
@@ -0,0 +1,39 @@
+import { GetStaticProps } from "next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+
+import type { BasePageProps, Lang } from "@/lib/types"
+
+import { existsNamespace } from "@/lib/utils/existsNamespace"
+import { getLastDeployDate } from "@/lib/utils/getLastDeployDate"
+import { getLocaleTimestamp } from "@/lib/utils/time"
+import { getRequiredNamespacesForPage } from "@/lib/utils/translations"
+
+export const getStaticProps = (async ({ locale }) => {
+  const lastDeployDate = getLastDeployDate()
+  const lastDeployLocaleTimestamp = getLocaleTimestamp(
+    locale as Lang,
+    lastDeployDate
+  )
+
+  const requiredNamespaces = getRequiredNamespacesForPage("/layer-2")
+
+  const contentNotTranslated = !existsNamespace(locale!, requiredNamespaces[2])
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale!, requiredNamespaces)),
+      contentNotTranslated,
+      lastDeployLocaleTimestamp,
+    },
+  }
+}) satisfies GetStaticProps<BasePageProps>
+
+const Layer2Hub = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default Layer2Hub
