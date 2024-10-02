8cea986bb4e7f334605c104540835513729d9ed9
Pablo Pettinari <pettinarip@gmail.com>
2024-10-02 15:27:16 +0200

    replace old buttons with new ones and refactor code

--- src/components/ProductTable/MobileFilters.tsx
+++ src/components/ProductTable/MobileFilters.tsx
@@ -5,7 +5,6 @@ import { IoClose } from "react-icons/io5" // Add this import
 
 import { FilterOption, TPresetFilters } from "@/lib/types"
 
-import Button from "@/components/Buttons/Button"
 import { FilterBurgerIcon } from "@/components/icons/wallets"
 import Filters from "@/components/ProductTable/Filters"
 import PresetFilters from "@/components/ProductTable/PresetFilters"
@@ -18,6 +17,8 @@ import {
 } from "@/components/ui/drawer"
 
 import { trackCustomEvent } from "@/lib/utils/matomo"
+
+import { Button } from "../ui/buttons/Button"
 
 interface MobileFiltersProps {
   filters: FilterOption[]
@@ -49,82 +50,64 @@ const MobileFilters = ({
   const { t } = useTranslation("table")
 
   return (
-    <>
-      <Drawer
-        direction="left"
-        open={mobileFiltersOpen}
-        onOpenChange={(open) => {
-          setMobileFiltersOpen(open)
-          trackCustomEvent({
-            eventCategory: "MobileFilterToggle",
-            eventAction: "Tap MobileFilterToggle",
-            eventName: `show mobile filters ${open}`,
-          })
-        }}
-      >
-        <DrawerTrigger className="px-4">
-          <Button
-            rightIcon={<FilterBurgerIcon />}
-            variant="outline"
-            border="none"
-            ps={0}
-            gap={4}
-            sx={{
-              svg: {
-                boxSize: 8,
-                line: { stroke: "primary.base" },
-                circle: { stroke: "primary.base" },
-              },
-            }}
-          >
-            <div className="flex flex-col text-left">
-              <p>{t("table-filters")}</p>
-              <p className="text-body-medium">{` ${activeFiltersCount} ${t("table-active")}`}</p>
-            </div>
-          </Button>
-        </DrawerTrigger>
-        <DrawerContent className="flex h-full flex-col p-2">
-          <div className="sticky top-0 mt-12 flex items-center justify-end p-2">
-            <DrawerClose>
-              <Button variant="ghost" size="icon">
-                <IoClose size={24} />
+    <Drawer
+      direction="left"
+      open={mobileFiltersOpen}
+      onOpenChange={(open) => {
+        setMobileFiltersOpen(open)
+        trackCustomEvent({
+          eventCategory: "MobileFilterToggle",
+          eventAction: "Tap MobileFilterToggle",
+          eventName: `show mobile filters ${open}`,
+        })
+      }}
+    >
+      <DrawerTrigger className="px-4" asChild>
+        <Button variant="outline" className="gap-4 border-0 ps-0">
+          <div className="flex flex-col text-left">
+            <p>{t("table-filters")}</p>
+            <p className="text-body-medium">{` ${activeFiltersCount} ${t("table-active")}`}</p>
+          </div>
+          <FilterBurgerIcon className="h-8 w-8 stroke-primary" />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent className="flex h-full flex-col p-2">
+        <div className="sticky top-0 mt-12 flex items-center justify-end p-2">
+          <DrawerClose asChild>
+            <Button variant="ghost">
+              <IoClose className="text-2xl" />
+            </Button>
+          </DrawerClose>
+        </div>
+        <div className="flex-1 overflow-y-auto">
+          <PresetFilters
+            presets={presets}
+            activePresets={activePresets}
+            handleSelectPreset={handleSelectPreset}
+            showMobileSidebar={true}
+          />
+          <Filters
+            filters={filters}
+            setFilters={setFilters}
+            resetFilters={resetFilters}
+            activeFiltersCount={activeFiltersCount}
+          />
+        </div>
+        <DrawerFooter>
+          <div className="grid grid-cols-2">
+            <div>
+              <Button variant="ghost" className="gap-1" onClick={resetFilters}>
+                <BsArrowCounterclockwise />
+                {t("table-reset-filters")}
               </Button>
+            </div>
+            <DrawerClose className="text-center" asChild>
+              <Button className="w-full">{`${mobileFiltersLabel} (${dataCount})`}</Button>
             </DrawerClose>
           </div>
-          <div className="flex-1 overflow-y-auto">
-            <PresetFilters
-              presets={presets}
-              activePresets={activePresets}
-              handleSelectPreset={handleSelectPreset}
-              showMobileSidebar={true}
-            />
-            <Filters
-              filters={filters}
-              setFilters={setFilters}
-              resetFilters={resetFilters}
-              activeFiltersCount={activeFiltersCount}
-            />
-          </div>
-          <DrawerFooter>
-            <div className="flex w-full">
-              <div className="w-1/2">
-                <Button
-                  variant="ghost"
-                  className="gap-1"
-                  onClick={resetFilters}
-                >
-                  <BsArrowCounterclockwise size={16} />
-                  {t("table-reset-filters")}
-                </Button>
-              </div>
-              <DrawerClose className="w-1/2 text-center">
-                <Button width="100%">{`${mobileFiltersLabel} (${dataCount})`}</Button>
-              </DrawerClose>
-            </div>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   )
 }
 
