cfede5f60ac3b6c9dd250f730e3a7078f24f4b48
Pablo Pettinari <pettinarip@gmail.com>
2024-10-04 13:43:07 +0200

    adjust colors to match DS

--- src/components/GhostCard.tsx
+++ src/components/GhostCard.tsx
@@ -15,10 +15,10 @@ const GhostCard: React.FC<GhostCardProps> = ({
 }) => {
   return (
     <div className={cn("relative self-stretch", className)}>
-      <div className="absolute bottom-2 left-2 -z-10 h-full w-full rounded-sm border-[1px] border-border bg-background-highlight dark:bg-gray-400" />
+      <div className="absolute bottom-2 left-2 z-hide h-full w-full rounded-sm border-border-high-contrast bg-background-medium" />
       <Card
         className={cn(
-          "text-card-foreground z-10 h-full w-full rounded-sm border-[1px] border-border bg-white p-6 text-left dark:bg-gray-600"
+          "text-card-foreground z-10 h-full w-full rounded-sm border bg-background-highlight p-6 text-left"
         )}
         {...props}
       >
