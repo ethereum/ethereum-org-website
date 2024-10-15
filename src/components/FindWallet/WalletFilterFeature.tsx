ce1f2e9dba9366818c101e8bb39be5e4c8bdb11a
Pablo Pettinari <pettinarip@gmail.com>
2024-10-15 16:05:55 +0200

    fix typo

--- src/components/FindWallet/WalletFilterFeature.tsx
+++ src/components/FindWallet/WalletFilterFeature.tsx
@@ -182,7 +182,7 @@ const WalletFilterFeature = ({
                           }
                         >
                           <GridItem>
-                            <LabelIcon className="mt-0. size-7" aria-hidden />
+                            <LabelIcon className="mt-0.5 size-7" aria-hidden />
                           </GridItem>
 
                           <GridItem as="span" lineHeight="1.1rem">
