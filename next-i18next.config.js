495ab1cf427490d48ba28935f5c2f328c30d5e60
Pablo Pettinari <pettinarip@gmail.com>
2024-10-16 19:02:04 +0200

    final final test

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const i18nConfig = require("./i18n.config.json")
 
 const BUILD_LOCALES = process.env.BUILD_LOCALES
