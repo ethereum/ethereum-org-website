02910bb4b9da3ddb5e1471f81cd1411b4a38dfad
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-19 23:33:56 -0700

    refactor: use question bank config

--- src/data/quizzes/index.ts
+++ src/data/quizzes/index.ts
@@ -5,16 +5,21 @@ const quizzes = {
   "what-is-ethereum": {
     title: "what-is-ethereum",
     questions: [
-      "ethereum-1",
-      "ethereum-2",
-      "ethereum-3",
-      "ethereum-4",
-      "ethereum-5",
+      "what-is-ethereum-1",
+      "what-is-ethereum-2",
+      "what-is-ethereum-3",
+      "what-is-ethereum-4",
+      "what-is-ethereum-5",
     ],
   },
   "what-is-ether": {
     title: "what-is-ether",
-    questions: ["ether-1", "ether-2", "ether-3", "ether-4"],
+    questions: [
+      "what-is-ether-1",
+      "what-is-ether-2",
+      "what-is-ether-3",
+      "what-is-ether-4",
+    ],
   },
   web3: {
     title: "web3",
@@ -50,16 +55,16 @@ const quizzes = {
     title: "DAOs",
     questions: ["daos-1", "daos-2", "daos-3", "daos-4", "daos-5"],
   },
-  "solo-staking": {
+  "staking-solo": {
     title: "solo",
     questions: [
-      "staking-1",
-      "staking-2",
-      "staking-4",
-      "staking-5",
-      "staking-6",
-      "staking-7",
-      "staking-8",
+      "staking-solo-1",
+      "staking-solo-2",
+      "staking-solo-4",
+      "staking-solo-5",
+      "staking-solo-6",
+      "staking-solo-7",
+      "staking-solo-8",
     ],
   },
   scaling: {
@@ -150,10 +155,10 @@ export const usingEthereumQuizzes: QuizzesSection[] = [
   {
     id: "scaling",
     level: "advanced",
-    next: "solo-staking",
+    next: "staking-solo",
   },
   {
-    id: "solo-staking",
+    id: "staking-solo",
     level: "advanced",
   },
 ]
