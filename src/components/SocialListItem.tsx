299c4aa6b24450704a1414700f5f3aa03c064ce4
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-22 16:18:09 -0700

    revert: removal of color class mapping
    use `[&svg]:` to style SVG from parent as-needed

--- src/components/SocialListItem.tsx
+++ src/components/SocialListItem.tsx
@@ -9,6 +9,14 @@ import {
 
 import { cn } from "@/lib/utils/cn"
 
+const socialColorClasses = {
+  reddit: "text-[#ff4301]",
+  twitter: "text-[#1da1f2]",
+  youtube: "text-[#ff0000]",
+  discord: "text-[#7289da]",
+  stackExchange: "text-[#48a2da]",
+}
+
 const icons = {
   reddit: FaRedditAlien,
   twitter: FaXTwitter,
@@ -30,7 +38,7 @@ const SocialListItem = ({
   const Icon = icons[socialIcon]
   return (
     <div className={cn("flex w-full items-center px-0 py-2", className)}>
-      <Icon className={cn("size-10 pe-3 text-body")} />
+      <Icon className={cn("size-10 pe-3", socialColorClasses[socialIcon])} />
       <div className="font-normal italic">{children}</div>
     </div>
   )
