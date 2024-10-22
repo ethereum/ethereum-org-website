4056c575cb00fee763e6911260c98072b2e58207
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-10-22 15:27:13 -0700

    deprecate: neutral tokens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -189,7 +189,6 @@ const config = {
         success: {
           DEFAULT: "hsla(var(--success))",
           light: "hsla(var(--success-light))",
-          neutral: "hsla(var(--success-neutral))",
           dark: "hsla(var(--success-dark))",
           border: "hsla(var(--success-border))",
         },
@@ -199,7 +198,6 @@ const config = {
           light: "hsla(var(--error-light))",
           dark: "hsla(var(--error-dark))",
           border: "hsla(var(--error-border))",
-          neutral: "hsla(var(--error-neutral))",
         },
 
         warning: {
