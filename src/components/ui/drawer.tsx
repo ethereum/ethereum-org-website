490bee091741f19cda1506d135612cef4fe101f2
Corwin Smith <cssmittys@gmail.com>
2024-10-22 21:34:23 -0600

    fix rounded drawer border

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -41,7 +41,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-modal mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-modal mt-24 flex h-auto flex-col border bg-background",
         className
       )}
       {...props}
