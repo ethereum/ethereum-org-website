6f121ee1ec5f25514c8328ac14548f1a9851ed20
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-10-29 10:04:05 -0400

    fix(Icon.stories.tsx): fix icon set

--- src/components/icons/quiz/GreenTickIcon.tsx
+++ src/components/icons/quiz/GreenTickIcon.tsx
@@ -1,8 +1,9 @@
-import * as React from "react"
-import { IconBase, type IconType } from "react-icons/lib"
+import { createIconBase } from "../icon-base"
 
-export const GreenTickIcon: IconType = (props) => (
-  <IconBase viewBox="0 0 16 16" {...props}>
+export const GreenTickIcon = createIconBase({
+  displayName: "GreenTickIcon",
+  viewBox: "0 0 16 16",
+  children: (
     <g>
       <rect width={16} height={16} fill="#c8f7d8" rx={8} />
       <path
@@ -10,5 +11,5 @@ export const GreenTickIcon: IconType = (props) => (
         d="M7.446 11.641 4 8.567l1.214-1.539 2.232 1.539 3.408-3.848 1.646.989-5.054 5.933Z"
       />
     </g>
-  </IconBase>
-)
+  ),
+})
