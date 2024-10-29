baf8f23affe597c6c20800d1fe674dd73f12bc37
tylerapfledderer <tyler.pfledderer@gmail.com>
2024-10-29 10:12:03 -0400

    refactor(icons/quiz): update to use `createIconBase`

--- src/components/icons/quiz/CorrectIcon.tsx
+++ src/components/icons/quiz/CorrectIcon.tsx
@@ -1,11 +1,10 @@
-import { IconBase, type IconBaseProps } from "react-icons/lib"
+import { createIconBase } from "../icon-base"
 
-import { cn } from "@/lib/utils/cn"
-
-export const CorrectIcon = ({ className, ...props }: IconBaseProps) => (
-  <IconBase viewBox="0 0 20 16" className={cn("h-4 w-5", className)} {...props}>
+export const CorrectIcon = createIconBase({
+  displayName: "CorrectIcon",
+  viewBox: "0 0 20 16",
+  className: "h-auto w-5",
+  children: (
     <path d="M8 16L0 9.05375L2.75437 7.06313L7.49875 11.4325L16.8531 0L20 2L8 16Z" />
-  </IconBase>
-)
-
-CorrectIcon.displayName = "CorrectIcon"
+  ),
+})
--- src/components/icons/quiz/IncorrectIcon.tsx
+++ src/components/icons/quiz/IncorrectIcon.tsx
@@ -1,11 +1,10 @@
-import { IconBase, type IconBaseProps } from "react-icons/lib"
+import { createIconBase } from "../icon-base"
 
-import { cn } from "@/lib/utils/cn"
-
-export const IncorrectIcon = ({ className, ...props }: IconBaseProps) => (
-  <IconBase className={cn("size-5", className)} viewBox="0 0 20 20" {...props}>
+export const IncorrectIcon = createIconBase({
+  displayName: "IncorrectIcon",
+  viewBox: "0 0 20 20",
+  className: "h-auto w-5",
+  children: (
     <path d="M17.5002 0L9.99912 7.49934L2.49978 0L0 2.49978L7.49934 9.99911L0 17.4984L2.49978 19.9982L9.99912 12.4989L17.5002 19.9982L20 17.4984L12.5007 9.99911L20 2.49978L17.5002 0Z" />
-  </IconBase>
-)
-
-IncorrectIcon.displayName = "IncorrectIcon"
+  ),
+})
