f912fa7783c3c2efe13e308ad3f4a83e0e14dc5f
Corwin Smith <cssmittys@gmail.com>
2024-10-29 11:55:36 -0600

    setup networks page

---
+++ src/pages/layer-2/networks.tsx
@@ -0,0 +1,36 @@
+import type { GetStaticProps } from "next/types"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+
+import type { BasePageProps, Lang } from "@/lib/types"
+
+import { existsNamespace } from "@/lib/utils/existsNamespace"
+import { getLastDeployDate } from "@/lib/utils/getLastDeployDate"
+import { getLocaleTimestamp } from "@/lib/utils/time"
+import { getRequiredNamespacesForPage } from "@/lib/utils/translations"
+
+export const getStaticProps = (async ({ locale }) => {
+  const lastDeployDate = getLastDeployDate()
+  const lastDeployLocaleTimestamp = getLocaleTimestamp(
+    locale as Lang,
+    lastDeployDate
+  )
+
+  const requiredNamespaces = getRequiredNamespacesForPage("/layer-2")
+
+  const contentNotTranslated = !existsNamespace(locale!, requiredNamespaces[2])
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale!, requiredNamespaces)),
+      contentNotTranslated,
+      lastDeployLocaleTimestamp,
+      locale,
+    },
+  }
+}) satisfies GetStaticProps<BasePageProps>
+
+const Layer2Networks = () => {
+  return <div>Layer2Networks</div>
+}
+
+export default Layer2Networks
