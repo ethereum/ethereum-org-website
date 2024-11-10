bc04462f29ce48ac69c2a2f91cd6fc9725757a2a
Corwin Smith <cssmittys@gmail.com>
2024-11-10 11:32:56 +0700

    fix icon props

--- src/components/icons/layer-2/EmergingIcon.tsx
+++ src/components/icons/layer-2/EmergingIcon.tsx
@@ -11,19 +11,19 @@ export const EmergingIcon = createIconBase({
       <path
         d="M0.640625 18.3252L10.6406 18.3252"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M5.64063 18.8902L5.64063 1.4502"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
 
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M12.4714 6.99996C8.6822 7.09798 5.64062 10.2004 5.64062 14.0132C5.64062 14.5661 5.70459 15.1041 5.82552 15.6201C9.61468 15.5221 12.6562 12.4197 12.6562 8.60688C12.6562 8.05397 12.5923 7.51599 12.4714 6.99996Z"
         fill="currentColor"
       />
