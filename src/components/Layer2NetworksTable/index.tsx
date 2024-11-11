9e2e63f8a145fdebe110a799a3698e62834bac75
Corwin Smith <cssmittys@gmail.com>
2024-11-11 13:07:00 +0700

    filtering

--- src/components/Layer2NetworksTable/index.tsx
+++ src/components/Layer2NetworksTable/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
 import { FilterOption, Lang } from "@/lib/types"
 
@@ -6,6 +6,8 @@ import { useNetworkColumns } from "@/components/Layer2NetworksTable/hooks/useNet
 import { useNetworkFilters } from "@/components/Layer2NetworksTable/hooks/useNetworkFilters"
 import NetworkSubComponent from "@/components/Layer2NetworksTable/NetworksSubcomponent"
 import ProductTable from "@/components/ProductTable"
+
+import { trackCustomEvent } from "@/lib/utils/matomo"
 
 import type { Rollups } from "@/data/layer-2/layer-2"
 
@@ -21,15 +23,22 @@ const Layer2NetworksTable = ({
   const networkFilterOptions = useNetworkFilters()
   const [filters, setFilters] = useState<FilterOption[]>(networkFilterOptions)
 
+  const filteredData = useMemo(() => {
+    return layer2Data.filter((network) => {
+      const maturityFilter = filters[0].items.find(
+        (item) => item.filterKey === network.networkMaturity
+      )
+      return maturityFilter.inputState
+    })
+  }, [layer2Data, filters])
+
   const resetFilters = () => {
     setFilters(networkFilterOptions)
-
-    // TODO: Add event tracking
-    // trackCustomEvent({
-    //   eventCategory: "Layer2NetworksTable",
-    //   eventAction: "Reset button",
-    //   eventName: "reset_click",
-    // })
+    trackCustomEvent({
+      eventCategory: "Layer2NetworksTable",
+      eventAction: "Reset button",
+      eventName: "reset_click",
+    })
   }
 
   return (
@@ -38,7 +47,7 @@ const Layer2NetworksTable = ({
         locale: locale,
       }}
       columns={useNetworkColumns}
-      data={layer2Data}
+      data={filteredData}
       allDataLength={layer2Data.length}
       filters={filters}
       presetFilters={[]}
