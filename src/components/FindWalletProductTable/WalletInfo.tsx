ca96cb6771ebdca6e877411780f06fef5fb4d6d2
Corwin Smith <cssmittys@gmail.com>
2024-11-22 14:41:50 -0700

    type

--- src/components/FindWalletProductTable/WalletInfo.tsx
+++ src/components/FindWalletProductTable/WalletInfo.tsx
@@ -64,7 +64,7 @@ const WalletInfo = ({ wallet, isExpanded }: WalletInfoProps) => {
                       className="-ml-1.5 overflow-hidden rounded-full"
                     >
                       <TwImage
-                        src={chainData?.logo}
+                        src={chainData?.logo || ""}
                         alt=""
                         style={{
                           objectFit: "contain",
@@ -109,7 +109,7 @@ const WalletInfo = ({ wallet, isExpanded }: WalletInfoProps) => {
                     className="-ml-1.5 overflow-hidden rounded-full"
                   >
                     <TwImage
-                      src={chainData?.logo}
+                      src={chainData?.logo || ""}
                       alt=""
                       style={{
                         objectFit: "contain",
