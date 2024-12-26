912f1326d3cf89e0af76c3e0eefd6c484eabdfcf
Pablo <pettinarip@gmail.com>
2024-12-26 19:03:10 +0100

    cleanup

--- src/components/Translatathon/TranslationHubCallout.tsx
+++ src/components/Translatathon/TranslationHubCallout.tsx
@@ -1,12 +1,13 @@
-import { ButtonLink } from "@/components/Buttons"
 import { TwImage } from "@/components/Image"
 import { Center, Flex } from "@/components/ui/flex"
+
+import { ButtonLink } from "../ui/buttons/Button"
 
 import WalkingImage from "@/public/images/translatathon/walking.png"
 
 export const TranslationHubCallout = ({ children }) => {
   return (
-    <Flex className="align-stretch w-full flex-col gap-8 bg-background-highlight p-8 lg:flex-row">
+    <Flex className="w-full flex-col items-stretch gap-8 bg-background-highlight p-8 lg:flex-row">
       <Flex className="w-full flex-col">
         {children}
         <Flex>
@@ -19,8 +20,8 @@ export const TranslationHubCallout = ({ children }) => {
         <TwImage
           src={WalkingImage}
           alt=""
-          width={265}
           style={{ objectFit: "contain" }}
+          className="max-w-[265px]"
         />
       </Center>
     </Flex>
