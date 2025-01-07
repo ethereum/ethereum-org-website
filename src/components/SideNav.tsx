e6dc9e0517dbd5dd89355e023ef1ee323d0f42aa
Ashis Kumar <ashishsahoo389@gmail.com>
2025-01-07 19:51:38 +0530

    Migrated the all the Chakra imports to Shadcn/Tailwind.

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -35,7 +35,7 @@ const innerLinksVariants = {
 
 const LinkContainer = ({ children }: ChildOnlyProp) => {
   return (
-    <HStack className="w-full justify-between py-2 pe-4 ps-8 hover:bg-[ednBackground]">
+    <HStack className="w-full justify-between py-2 pe-4 ps-8 hover:bg-background-highlight">
       {children}
     </HStack>
   )
@@ -98,7 +98,7 @@ const NavLink = ({ item, path, isTopLevel }: NavLinkProps) => {
             variants={dropdownIconContainerVariant}
             animate={isOpen ? "open" : "closed"}
           >
-            <MdChevronRight className="h-6 w-6" color="secondary" />
+            <MdChevronRight className="h-6 w-6 text-body-medium" />
           </motion.div>
         </LinkContainer>
         <motion.div className="ms-4 text-sm font-normal leading-relaxed"
@@ -137,7 +137,7 @@ const SideNav = ({ path }: SideNavProps) => {
 
   return (
     <nav
-      className="sticky top-[4.75rem] pt-8 pb-16 h-[calc(100vh - 80px)] w-[calc((100% - 1448px) / 2 + 256px)] min-w-256 overflow-y-auto transition-transform duration-200 ease bg-[background.base] shadow-[1px_0px_0px_rgba(0,0,0,0.1)] border-e border-e-border lg:block"
+      className="sticky top-[4.75rem] pt-8 pb-16 h-[calc(100vh - 80px)] w-[calc((100% - 1448px) / 2 + 256px)] min-w-256 overflow-y-auto transition-transform duration-200 ease bg-background shadow-[1px_0px_0px_rgba(0,0,0,0.1)] border-e border-e-border lg:block"
       aria-label={t("common:nav-developers-docs")}
     >
       {docLinks.map((item, idx) => (
