2514df07dc26c70fe605946fee1e525d7cb65f00
Fredrik Svantes <fredrik@ethereum.org>
2025-01-08 10:01:21 +0100

    Update index.tsx

--- src/components/Banners/BugBountyBanner/index.tsx
+++ src/components/Banners/BugBountyBanner/index.tsx
@@ -10,7 +10,7 @@ const BugBountyBanner = () => (
     <Center>
       <p>
         The Ethereum Protocol Attackathon is live on Immunefi 
-         until the 25th of January, with up to $1,500,000 in rewards!
+         until the 25th of January, with up to USD 1,500,000 in rewards!
       </p>
     </Center>
   </BannerNotification>
