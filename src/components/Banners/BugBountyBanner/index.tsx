a62015a80ba7f2f6bba26a19cb7128d9f1ab3ae6
Fredrik Svantes <fredrik@ethereum.org>
2025-01-08 09:27:07 +0100

    Update index.tsx

--- src/components/Banners/BugBountyBanner/index.tsx
+++ src/components/Banners/BugBountyBanner/index.tsx
@@ -9,7 +9,7 @@ const BugBountyBanner = () => (
   <BannerNotification shouldShow>
     <Center>
       <p>
-        The Ethereum Protocol Attackathon is live on <a href="https://immunefi.com/audit-competition/ethereum-protocol-attackathon/">Immunefi</a>
+        The Ethereum Protocol Attackathon is live on Immunefi 
          until the 25th of January, with up to $1,500,000 in rewards!
       </p>
     </Center>
