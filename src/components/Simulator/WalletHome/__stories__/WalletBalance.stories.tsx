c0a9e20fad27028598f310ce920375563c689c1c
tylerapfledderer <tyler.pfledderer@gmail.com>
2025-01-31 01:21:44 -0500

    refactor(WalletBalance.stories): create two viewport snapshots

--- src/components/Simulator/WalletHome/__stories__/WalletBalance.stories.tsx
+++ src/components/Simulator/WalletHome/__stories__/WalletBalance.stories.tsx
@@ -1,10 +1,19 @@
+import pickBy from "lodash/pickBy"
 import type { Meta, StoryObj } from "@storybook/react"
 
+import { viewportModes } from "../../../../../.storybook/modes"
 import { WalletBalance as Component } from "../WalletBalance"
 
 const meta = {
   title: "Molecules / Display Content / Simulator / WalletHome / WalletBalance",
   component: Component,
+  parameters: {
+    chromatic: {
+      modes: pickBy(viewportModes, ({ viewport }) =>
+        ["base", "md"].includes(viewport)
+      ),
+    },
+  },
 } satisfies Meta<typeof Component>
 
 export default meta
