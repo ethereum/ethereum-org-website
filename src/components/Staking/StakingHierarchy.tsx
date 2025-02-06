77bc30595500a6817c7e5c2b870ef617964fc44d
Pablo <pettinarip@gmail.com>
2025-02-06 14:20:03 +0100

    migrate staking hierarchy component to tw

--- src/components/Staking/StakingHierarchy.tsx
+++ src/components/Staking/StakingHierarchy.tsx
@@ -1,12 +1,10 @@
-import React from "react"
+import React, { HTMLAttributes } from "react"
 import { useTranslation } from "next-i18next"
 import { IconBase } from "react-icons"
-import { cssVar, Icon, SimpleGrid, Text, useToken } from "@chakra-ui/react"
 
 import { ChildOnlyProp } from "@/lib/types"
 
-import { Center, Flex, VStack } from "@/components/ui/flex"
-
+import { cn } from "@/lib/utils/cn"
 import { trackCustomEvent } from "@/lib/utils/matomo"
 
 import { ButtonLink } from "../Buttons"
@@ -18,236 +16,114 @@ import {
   StakingGlyphTokenWalletIcon,
 } from "../icons/staking"
 import Translation from "../Translation"
+import { Center, Flex, VStack } from "../ui/flex"
 
-type SectionGridProps = ChildOnlyProp & {
-  number: number
-}
+type SectionGridProps = ChildOnlyProp
 
-const $colorVar = cssVar("color")
-const $nextColorVar = cssVar("next-color")
-const $fillColorVar = cssVar("fill-color")
-
-const SectionGrid = ({ number, children }: SectionGridProps) => {
-  const colorValue = () => {
-    switch (number) {
-      case 1:
-        return "colors.stakingGold"
-      case 2:
-        return "colors.stakingGreen"
-      case 3:
-        return "colors.stakingBlue"
-      case 4:
-        return "colors.stakingRed"
-      default:
-        return "#000000"
-    }
-  }
-
-  const nextColorValue = () => {
-    switch (number) {
-      case 1:
-        return "colors.stakingGreen"
-      case 2:
-        return "colors.stakingBlue"
-      case 3:
-        return "colors.stakingRed"
-      case 4:
-        return "#00000000"
-      default:
-        return "#000000"
-    }
-  }
-
-  const fillColorValue = () => {
-    switch (number) {
-      case 1:
-        return "colors.stakingGoldFill"
-      case 2:
-        return "colors.stakingGreenFill"
-      case 3:
-        return "colors.stakingBlueFill"
-      case 4:
-        return "colors.stakingRedFill"
-      default:
-        return "#000000"
-    }
-  }
-
-  const asideScaleVal = 1.05 + number / 70
-  const asideTranslateVal = number + "px"
-
+const SectionGrid = ({ children }: SectionGridProps) => {
   return (
-    <SimpleGrid
-      gap={{ base: 4, md: "0 2rem" }}
-      templateColumns={{ base: "1fr", md: "5rem 1fr 5rem" }}
-      templateAreas={{
-        base: `
-          "ether"
-          "header"
-          "content"
-        `,
-        md: `
-          "ether header glyph"
-          "decorator content content"
-        `,
-      }}
-      position="relative"
-      sx={{
-        [$colorVar.variable]: colorValue(),
-        [$nextColorVar.variable]: nextColorValue(),
-        [$fillColorVar.variable]: fillColorValue(),
-        aside: {
-          _after: {
-            transform: `scale(${asideScaleVal}) translateY(${asideTranslateVal})`,
-          },
-        },
-      }}
-    >
+    <div className="staking-grid-stacked md:staking-grid relative grid grid-cols-1 gap-4 md:grid-cols-[5rem_1fr_5rem] md:gap-x-8 md:gap-y-0">
       {children}
-    </SimpleGrid>
+    </div>
   )
 }
 
 const StyledEtherSvg = ({ className = "size-full" }: { className: string }) => {
   return (
-    <Center
-      style={{ gridArea: "ether" }}
-      className="z-2 mx-auto w-full max-w-20"
-    >
+    <Center className="area-ether z-[2] mx-auto w-full max-w-20">
       <StakingGlyphEtherCircleIcon className={className} />
     </Center>
   )
 }
 
 const Line = () => {
-  // TODO: Remove after completion of the Chakra migration
-  const medBp = useToken("breakpoints", "md")
-
-  return (
-    <aside
-      className={`realtive after:h-[calc.subtract("100%", "50px")] after:start-[calc.subtract("50%", "2px")] after:content-["] after:z-1 after:border-orange col-span-1 row-span-2 hidden after:absolute after:top-[50px] after:border-s-4 md:block`}
-
-      /* 
-        This value needs to be updated.
-      _after={{
-        borderImage: `linear-gradient(to bottom, ${$colorVar.reference}, ${$nextColorVar.reference}) 1 100%`,
-      }} */
-    />
-  )
+  return <aside className="grid-col-1 grid-row-3 relative hidden md:block" />
 }
 
-const Header = ({ children }: ChildOnlyProp) => (
+const Header = ({ children, className }: HTMLAttributes<HTMLDivElement>) => (
   <Flex
-    className="flex-col justify-center gap-2 sm:items-center md:items-start"
-    style={{ gridArea: "header" }}
+    className={cn(
+      "area-header flex-col items-center justify-center gap-2 md:items-start",
+      className
+    )}
   >
     {children}
   </Flex>
 )
 
-const HeadingEl = ({ children }: ChildOnlyProp) => (
-  <h1
-    className="text-($colorVar.reference) text-center text-[2rem] font-semibold leading-[1.4] md:text-start"
-    /* This value needs to be updated across the file.
-     color={$colorVar.reference} */
-  >
-    {children}
-  </h1>
+const HeadingEl = ({
+  children,
+  className,
+}: HTMLAttributes<HTMLHeadingElement>) => (
+  <h2 className={cn("text-center md:text-start", className)}>{children}</h2>
 )
-// Todo: The removed sx prop will be added by creating a separate function for Text like we have HeadingEl. This will also help to migrate the Text component.
-const Pills = ({ children }: ChildOnlyProp) => (
-  <Flex className="flex-wrap gap-1 sm:justify-center md:justify-start">
+
+const Pills = ({ children, className }: HTMLAttributes<HTMLDivElement>) => (
+  <Flex
+    className={cn("flex-wrap justify-center gap-1 md:justify-start", className)}
+  >
     {children}
   </Flex>
 )
 
-const TextEl = ({ children }: ChildOnlyProp) => (
+const Pill = ({
+  children,
+  className,
+}: HTMLAttributes<HTMLParagraphElement>) => (
   <p
-    className={`after:content-["] relative m-0 whitespace-nowrap px-1.5 py-0.5 text-[colorVar.reference] after:absolute after:start-0 after:top-0 after:box-content after:rounded after:bg-[var(colorVar.reference)] after:opacity-[.125]`}
+    className={cn(
+      "relative m-0 whitespace-nowrap rounded-sm px-1 py-[0.125rem]",
+      className
+    )}
   >
     {children}
   </p>
 )
 
-type GlyphProps = { glyphIcon: typeof IconBase }
-const Glyph = ({ glyphIcon }: GlyphProps) => (
-  <Center
-    //Need to set the base styles we've had previously ie. gridArea="base:content"
-    style={{
-      gridArea: "glyph",
-    }}
-  >
-    <Icon
-      as={glyphIcon}
-      boxSize={{ base: "50%", md: "50px" }}
-      color={$colorVar.reference}
-      opacity={{ base: 0.1, md: "initial" }}
+type GlyphProps = { glyphIcon: typeof IconBase; className?: string }
+const Glyph = ({ glyphIcon: GlyphIcon, className }: GlyphProps) => (
+  <Center className="area-content md:area-glyph">
+    <GlyphIcon
+      className={cn(
+        "size-[50%] opacity-10 md:size-[50px] md:opacity-100",
+        className
+      )}
     />
   </Center>
 )
 
 const Content = ({ children }: ChildOnlyProp) => (
-  <Flex
-    className="flex-col gap-4 md:mb-12 md:mt-4"
-    style={{
-      gridArea: "content",
-    }}
-    /* sx={{
-      // For use in markdown files
-      ".gold": {
-        color: "stakingGold",
-        fontWeight: 600,
-      },
-    }} */
-  >
+  <Flex className="area-content flex-col gap-4 md:mb-12 md:mt-4">
     {children}
   </Flex>
 )
 
 const StakingHierarchy = () => {
   const { t } = useTranslation("page-staking")
-  const [stakingGold, stakingGreen, stakingBlue, stakingRed] = useToken(
-    "colors",
-    ["stakingGold", "stakingGreen", "stakingBlue", "stakingRed"]
-  )
 
   return (
-    <VStack className="border-gradient-to-b border-gradient-gold-green-blue-red space-y-16 rounded-none border-l-4 border-r-0 bg-gradient-to-b from-[rgba(237,194,84,0.1)] via-[rgba(75,231,156,0.1)] to-[rgba(231,202,200,0.1)] p-8 md:space-y-0 md:rounded-lg md:border-l-0">
-      <SectionGrid number={1}>
+    <VStack className="gap-16 bg-gradient-staking p-8 md:gap-0 md:rounded-lg">
+      <SectionGrid>
         <StyledEtherSvg className="size-[100%] text-staking-gold" />
         <Line />
-        <Header>
+        <Header className="text-staking-gold">
           <HeadingEl>{t("page-staking-hierarchy-solo-h2")}</HeadingEl>
-          <Pills>
-            <Text
-              className={`after:content-["] relative m-0 whitespace-nowrap px-1.5 py-0.5 text-[$colorVar.reference] after:absolute after:start-0 after:top-0 after:box-content after:rounded after:bg-[$colorVar.reference] after:opacity-[.125]`}
-            >
+          <Pills className="*:bg-staking-gold/10">
+            <Pill>
               <em>{t("page-staking-hierarchy-solo-pill-1")}</em>
-            </Text>
-            <Text
-              className={`after:content-["] relative m-0 whitespace-nowrap px-1.5 py-0.5 text-[$colorVar.reference] after:absolute after:start-0 after:top-0 after:box-content after:rounded after:bg-[$colorVar.reference] after:opacity-[.125]`}
-            >
-              {t("page-staking-hierarchy-solo-pill-2")}
-            </Text>
-            <Text
-              className={`after:content-["] relative m-0 whitespace-nowrap px-1.5 py-0.5 text-[$colorVar.reference] after:absolute after:start-0 after:top-0 after:box-content after:rounded after:bg-[$colorVar.reference] after:opacity-[.125]`}
-            >
-              {t("page-staking-hierarchy-solo-pill-3")}
-            </Text>
-            <Text
-              className={`after:content-["] relative m-0 whitespace-nowrap px-1.5 py-0.5 text-[$colorVar.reference] after:absolute after:start-0 after:top-0 after:box-content after:rounded after:bg-[$colorVar.reference] after:opacity-[.125]`}
-            >
-              {t("page-staking-hierarchy-solo-pill-4")}
-            </Text>
+            </Pill>
+            <Pill>{t("page-staking-hierarchy-solo-pill-2")}</Pill>
+            <Pill>{t("page-staking-hierarchy-solo-pill-3")}</Pill>
+            <Pill>{t("page-staking-hierarchy-solo-pill-4")}</Pill>
           </Pills>
         </Header>
-        <Glyph glyphIcon={StakingGlyphCPUIcon} />
+        <Glyph glyphIcon={StakingGlyphCPUIcon} className="text-staking-gold" />
         <Content>
-          <Text>
+          <p>
             <Translation id="page-staking:page-staking-hierarchy-solo-p1" />
-          </Text>
-          <Text>{t("page-staking-hierarchy-solo-p2")}</Text>
-          <Text>{t("page-staking-hierarchy-solo-p3")}</Text>
+          </p>
+          <p>{t("page-staking-hierarchy-solo-p2")}</p>
+          <p>{t("page-staking-hierarchy-solo-p3")}</p>
           <div>
             <ButtonLink
               href="/staking/solo/"
@@ -265,22 +141,25 @@ const StakingHierarchy = () => {
           </div>
         </Content>
       </SectionGrid>
-      <SectionGrid number={2}>
+      <SectionGrid>
         <StyledEtherSvg className="size-[90%] text-staking-green" />
         <Line />
-        <Header>
+        <Header className="text-staking-green">
           <HeadingEl>{t("page-staking-dropdown-saas")}</HeadingEl>
-          <Pills>
-            <Text>{t("page-staking-hierarchy-saas-pill-1")}</Text>
-            <Text>{t("page-staking-hierarchy-saas-pill-2")}</Text>
-            <Text>{t("page-staking-hierarchy-saas-pill-3")}</Text>
+          <Pills className="*:bg-staking-green/10">
+            <Pill>{t("page-staking-hierarchy-saas-pill-1")}</Pill>
+            <Pill>{t("page-staking-hierarchy-saas-pill-2")}</Pill>
+            <Pill>{t("page-staking-hierarchy-saas-pill-3")}</Pill>
           </Pills>
         </Header>
-        <Glyph glyphIcon={StakingGlyphCloudIcon} />
+        <Glyph
+          glyphIcon={StakingGlyphCloudIcon}
+          className="text-staking-green"
+        />
         <Content>
-          <Text>{t("page-staking-hierarchy-saas-p1")}</Text>
-          <Text>{t("page-staking-hierarchy-saas-p2")}</Text>
-          <Text>{t("page-staking-hierarchy-saas-p3")}</Text>
+          <p>{t("page-staking-hierarchy-saas-p1")}</p>
+          <p>{t("page-staking-hierarchy-saas-p2")}</p>
+          <p>{t("page-staking-hierarchy-saas-p3")}</p>
           <div>
             <ButtonLink
               href="/staking/saas/"
@@ -298,34 +177,37 @@ const StakingHierarchy = () => {
           </div>
         </Content>
       </SectionGrid>
-      <SectionGrid number={3}>
+      <SectionGrid>
         <StyledEtherSvg className="size-[80%] text-staking-blue" />
         <Line />
-        <Header>
+        <Header className="text-staking-blue">
           <HeadingEl>{t("page-staking-dropdown-pools")}</HeadingEl>
-          <Pills>
-            <Text>{t("page-staking-hierarchy-pools-pill-1")}</Text>
-            <Text>{t("page-staking-hierarchy-pools-pill-2")}</Text>
-            <Text>{t("page-staking-hierarchy-pools-pill-3")}</Text>
-            <Text>
+          <Pills className="*:bg-staking-blue/10">
+            <Pill>{t("page-staking-hierarchy-pools-pill-1")}</Pill>
+            <Pill>{t("page-staking-hierarchy-pools-pill-2")}</Pill>
+            <Pill>{t("page-staking-hierarchy-pools-pill-3")}</Pill>
+            <Pill>
               <em>{t("page-staking-hierarchy-pools-pill-4")}</em>
-            </Text>
+            </Pill>
           </Pills>
         </Header>
-        <Glyph glyphIcon={StakingGlyphTokenWalletIcon} />
+        <Glyph
+          glyphIcon={StakingGlyphTokenWalletIcon}
+          className="text-staking-blue"
+        />
         <Content>
-          <Text>
+          <p>
             <Translation id="page-staking:page-staking-hierarchy-pools-p1" />
-          </Text>
-          <Text>
+          </p>
+          <p>
             <Translation id="page-staking:page-staking-hierarchy-pools-p2" />
-          </Text>
-          <Text>
+          </p>
+          <p>
             <Translation id="page-staking:page-staking-hierarchy-pools-p3" />
-          </Text>
-          <Text>
+          </p>
+          <p>
             <Translation id="page-staking:page-staking-hierarchy-pools-p4" />
-          </Text>
+          </p>
           <div>
             <ButtonLink
               href="/staking/pools/"
@@ -343,25 +225,28 @@ const StakingHierarchy = () => {
           </div>
         </Content>
       </SectionGrid>
-      <SectionGrid number={4}>
+      <SectionGrid>
         <StyledEtherSvg className="size-[70%] text-staking-red" />
         <Line />
-        <Header>
+        <Header className="text-staking-red">
           <HeadingEl>{t("page-staking-hierarchy-cex-h2")}</HeadingEl>
-          <Pills>
-            <Text>
+          <Pills className="*:bg-staking-red/10">
+            <Pill>
               <em>{t("page-staking-hierarchy-cex-pill-1")}</em>
-            </Text>
-            <Text>{t("page-staking-hierarchy-cex-pill-2")}</Text>
+            </Pill>
+            <Pill>{t("page-staking-hierarchy-cex-pill-2")}</Pill>
           </Pills>
         </Header>
-        <Glyph glyphIcon={StakingGlyphCentralizedIcon} />
+        <Glyph
+          glyphIcon={StakingGlyphCentralizedIcon}
+          className="text-staking-red"
+        />
         <Content>
-          <Text>{t("page-staking-hierarchy-cex-p1")}</Text>
-          <Text>{t("page-staking-hierarchy-cex-p2")}</Text>
-          <Text>
+          <p>{t("page-staking-hierarchy-cex-p1")}</p>
+          <p>{t("page-staking-hierarchy-cex-p2")}</p>
+          <p>
             <Translation id="page-staking:page-staking-hierarchy-cex-p3" />
-          </Text>
+          </p>
         </Content>
       </SectionGrid>
     </VStack>
