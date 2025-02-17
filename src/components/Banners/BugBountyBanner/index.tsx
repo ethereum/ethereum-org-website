adff7656b57ff267d025202eba6c3067b0587acd
Fredrik Svantes <fredrik@ethereum.org>
2025-02-17 12:38:34 +0100

    Update index.tsx

--- src/components/Banners/BugBountyBanner/index.tsx
+++ src/components/Banners/BugBountyBanner/index.tsx
@@ -9,11 +9,11 @@ const BugBountyBanner = () => (
   <BannerNotification shouldShow>
     <Center>
       <p>
-        The Ethereum Protocol Attackathon is live on{" "}
-        <a href="https://immunefi.com/audit-competition/ethereum-protocol-attackathon/">
-          Immunefi
+        Prague code is now in scope! Join the Prague Audit Competition on {" "}
+        <a href="https://cantina.xyz/competitions/9ab19e33-b73c-4384-83a8-b905f508ce5e">
+          Cantina
         </a>{" "}
-        until the 20th of January, with up to $1,500,000 in rewards!
+        on the 21 of February, with up to $2,000,000 in rewards!
       </p>
     </Center>
   </BannerNotification>
