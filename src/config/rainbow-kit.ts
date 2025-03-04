c4995f4f8126095ea5cc4e64b4c1a1717fab6507
Corwin Smith <cssmittys@gmail.com>
2025-03-04 16:45:35 +0100

    feat: customize RainbowKit wallet selection for new users

--- src/config/rainbow-kit.ts
+++ src/config/rainbow-kit.ts
@@ -1,8 +1,27 @@
 import { mainnet } from "wagmi/chains"
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
+import {
+  coinbaseWallet,
+  mewWallet,
+  oneKeyWallet,
+  rainbowWallet,
+  zerionWallet,
+} from "@rainbow-me/rainbowkit/wallets"
 
 export const rainbowkitConfig = getDefaultConfig({
   appName: "ethereum.org",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
   chains: [mainnet],
+  wallets: [
+    {
+      groupName: "New to crypto",
+      wallets: [
+        coinbaseWallet,
+        rainbowWallet,
+        mewWallet,
+        zerionWallet,
+        oneKeyWallet,
+      ],
+    },
+  ],
 })
