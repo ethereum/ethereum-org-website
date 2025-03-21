93bbf0fff6711a1679931df99223b688325f3495
Pablo <pettinarip@gmail.com>
2025-03-21 18:58:53 +0100

    wrap wallet name with LinkOverlay

--- src/components/StartWithEthereumFlow/DownloadAWallet/index.tsx
+++ src/components/StartWithEthereumFlow/DownloadAWallet/index.tsx
@@ -5,7 +5,8 @@ import { Wallet } from "@/lib/types"
 import { Image } from "@/components/Image"
 import { Button, ButtonLink } from "@/components/ui/buttons/Button"
 import Checkbox from "@/components/ui/checkbox"
-import { LinkBox } from "@/components/ui/link-box"
+import InlineLink from "@/components/ui/Link"
+import { LinkBox, LinkOverlay } from "@/components/ui/link-box"
 import { Tag } from "@/components/ui/tag"
 
 import { trackCustomEvent } from "@/lib/utils/matomo"
@@ -74,15 +75,7 @@ const DownloadAWallet = ({
           {newToCryptoWallets.map((wallet) => (
             <LinkBox
               key={wallet.name}
-              className="flex cursor-pointer flex-col gap-4 border-b border-body-light bg-background p-4 last:border-b-0 hover:bg-background-highlight sm:p-6"
-              onClick={() => {
-                window.open(wallet.url, "_blank")
-                trackCustomEvent({
-                  eventCategory: "start page",
-                  eventAction: "get wallet",
-                  eventName: wallet.name,
-                })
-              }}
+              className="flex flex-col gap-4 border-b border-body-light bg-background p-4 last:border-b-0 hover:bg-background-highlight sm:p-6"
             >
               <div className="flex flex-row items-center justify-between gap-2">
                 <div className="flex w-full max-w-[200px] flex-row items-center gap-4">
@@ -94,8 +87,28 @@ const DownloadAWallet = ({
                       height={30}
                     />
                   </div>
-                  <p className="text-body">{wallet.name}</p>
+                  <p>
+                    <LinkOverlay
+                      className="text-body no-underline hover:text-body"
+                      asChild
+                    >
+                      <InlineLink
+                        href={wallet.url}
+                        hideArrow
+                        onClick={() => {
+                          trackCustomEvent({
+                            eventCategory: "start page",
+                            eventAction: "get wallet",
+                            eventName: wallet.name,
+                          })
+                        }}
+                      >
+                        {wallet.name}
+                      </InlineLink>
+                    </LinkOverlay>
+                  </p>
                 </div>
+
                 <ButtonLink href={wallet.url} variant="outline" size="sm">
                   Get wallet
                 </ButtonLink>
