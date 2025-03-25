48139e74338dbe9fb6d412d01a78d5bbe2e38bb7
tylerapfledderer <tyler.pfledderer@gmail.com>
2025-03-25 10:41:45 -0400

    refactor(resources): add wrapper around main content

--- src/pages/[locale]/resources.tsx
+++ src/pages/[locale]/resources.tsx
@@ -13,6 +13,7 @@ import { ResourceItem, ResourcesContainer } from "@/components/Resources"
 import { useResources } from "@/components/Resources/useResources"
 import Translation from "@/components/Translation"
 import { ButtonLink } from "@/components/ui/buttons/Button"
+import { Stack } from "@/components/ui/flex"
 import Link from "@/components/ui/Link"
 import { Section } from "@/components/ui/section"
 
@@ -114,112 +115,113 @@ const ResourcesPage = ({ txCostsMedianUsd }) => {
         description={t("page-resources-hero-description")}
         heroImg={heroImg}
       />
-
-      <div className="sticky top-1 my-8 flex flex-col items-center gap-3 py-4 text-center md:top-6 md:px-2">
-        <div className="my-2 text-body-medium">
-          {t("page-resources-whats-on-this-page")}
-        </div>
-        <nav className="z-sticky mx-4 flex max-w-full gap-1 overflow-x-auto bg-background p-2 shadow md:max-w-[calc(100%-2rem)] md:rounded-2xl md:border md:p-0.5 md:shadow-lg">
-          {resourceSections.map(({ key, title, icon }) => (
-            <ButtonLink
-              key={key}
-              href={`#${key}`}
-              variant="ghost"
-              isSecondary
-              className={cn(
-                "relative text-nowrap rounded-xl px-4 py-2 text-sm [&_svg]:shrink-0 [&_svg]:text-sm",
-                activeSection === key && "!text-primary"
-              )}
-            >
-              {activeSection === key && (
-                <motion.div
-                  layoutId="active-section-highlight"
-                  className="absolute inset-0 z-0 rounded-xl bg-primary-low-contrast"
-                />
-              )}
-              {icon && <span className="z-10 text-lg">{icon}</span>}
-              <span className="relative z-10">{title}</span>
-            </ButtonLink>
-          ))}
-        </nav>
-      </div>
-
-      {resourceSections.map(({ key, icon, title: sectionTitle, boxes }) => (
-        <section id={key} key={key} className="mb-16 scroll-mt-40">
-          <div className="group flex w-full items-center gap-3 border-b bg-transparent px-4 py-6">
-            <div className="grid size-12 place-items-center rounded-lg border border-border-low-contrast text-2xl [&_svg]:shrink-0">
-              {icon || <StackIcon />}
-            </div>
-            <h2 className="flex-1 text-start font-black">{sectionTitle}</h2>
+      <Stack className="gap-4 px-2 py-4 md:gap-8 md:px-4 lg:px-8 xl:gap-11">
+        <div className="sticky top-1 my-8 flex flex-col items-center gap-3 py-4 text-center md:top-6 md:px-2">
+          <div className="my-2 text-body-medium">
+            {t("page-resources-whats-on-this-page")}
           </div>
-          <div className="grid grid-cols-1 gap-8 px-4 pb-12 pt-8 md:pb-12 md:pt-8 lg:grid-cols-2">
-            {boxes.map(({ title, metric, items, className }) => (
-              <div
+          <nav className="z-sticky mx-4 flex max-w-full gap-1 overflow-x-auto bg-background p-2 shadow md:max-w-[calc(100%-2rem)] md:rounded-2xl md:border md:p-0.5 md:shadow-lg">
+            {resourceSections.map(({ key, title, icon }) => (
+              <ButtonLink
+                key={key}
+                href={`#${key}`}
+                variant="ghost"
+                isSecondary
                 className={cn(
-                  "overflow-hidden rounded-2xl border shadow-lg",
-                  className
+                  "relative text-nowrap rounded-xl px-4 py-2 text-sm [&_svg]:shrink-0 [&_svg]:text-sm",
+                  activeSection === key && "!text-primary"
                 )}
-                key={title}
               >
-                <div className="border-b bg-[#ffffff] px-6 py-4 font-bold dark:bg-[#171717]">
-                  {title}
-                </div>
-                <div className="h-full bg-background bg-gradient-to-br from-white to-primary/10 px-2 py-6 dark:from-transparent dark:to-primary/10">
-                  {metric && metric}
-                  <ResourcesContainer>
-                    {items.map((item) => (
-                      <ResourceItem item={item} key={item.title} />
-                    ))}
-                  </ResourcesContainer>
-                </div>
-              </div>
+                {activeSection === key && (
+                  <motion.div
+                    layoutId="active-section-highlight"
+                    className="absolute inset-0 z-0 rounded-xl bg-primary-low-contrast"
+                  />
+                )}
+                {icon && <span className="z-10 text-lg">{icon}</span>}
+                <span className="relative z-10">{title}</span>
+              </ButtonLink>
             ))}
-          </div>
-        </section>
-      ))}
-
-      <div className="mb-20 text-center">
-        <Translation id="page-resources:page-resources-find-more" />
-      </div>
-
-      <Section
-        id="contribute"
-        className={cn(
-          "before:absolute before:-inset-px before:bottom-0 before:z-hide before:rounded-[calc(theme(borderRadius.4xl)+1px)] before:content-['']", // Border/gradient positioning
-          "before:bg-gradient-to-b before:from-primary-hover/[0.24] before:to-primary-hover/[0.08] before:dark:from-primary-hover/40 before:dark:to-primary-hover/20", // Border/gradient coloring
-          "relative inset-0 rounded-4xl bg-background" // Paint background color over card portion
-        )}
-      >
-        <div className="mb-12 flex flex-col gap-y-8 rounded-4xl bg-radial-a px-8 py-12 lg:mb-32 xl:mb-36">
-          <div className="flex flex-col gap-y-4 text-center">
-            <h2>{t("page-resources-contribute-title")}</h2>
-            <p>{t("page-resources-contribute-description")}</p>
-          </div>
-          <div className="mx-auto grid grid-cols-1 gap-16 md:grid-cols-2">
-            {/* TODO: Add issue template for resource listing and redirect to new template */}
-            <ButtonLink
-              href={new URL(
-                "issues/new?template=feature_request.yaml",
-                GITHUB_REPO_URL
-              ).toString()}
-              variant="outline"
-              isSecondary
-            >
-              {t("page-resources-suggest-resource")}
-            </ButtonLink>
-            <ButtonLink
-              href={new URL(
-                "issues/new?template=bug_report.yaml",
-                GITHUB_REPO_URL
-              ).toString()}
-              variant="outline"
-              isSecondary
-            >
-              <FaGithub /> {t("page-resources-found-bug")}
-            </ButtonLink>
-          </div>
+          </nav>
         </div>
-      </Section>
+
+        {resourceSections.map(({ key, icon, title: sectionTitle, boxes }) => (
+          <section id={key} key={key} className="mb-16 scroll-mt-40">
+            <div className="group flex w-full items-center gap-3 border-b bg-transparent px-4 py-6">
+              <div className="grid size-12 place-items-center rounded-lg border border-border-low-contrast text-2xl [&_svg]:shrink-0">
+                {icon || <StackIcon />}
+              </div>
+              <h2 className="flex-1 text-start font-black">{sectionTitle}</h2>
+            </div>
+            <div className="grid grid-cols-1 gap-8 px-4 pb-12 pt-8 md:pb-12 md:pt-8 lg:grid-cols-2">
+              {boxes.map(({ title, metric, items, className }) => (
+                <div
+                  className={cn(
+                    "overflow-hidden rounded-2xl border shadow-lg",
+                    className
+                  )}
+                  key={title}
+                >
+                  <div className="border-b bg-[#ffffff] px-6 py-4 font-bold dark:bg-[#171717]">
+                    {title}
+                  </div>
+                  <div className="h-full bg-background bg-gradient-to-br from-white to-primary/10 px-2 py-6 dark:from-transparent dark:to-primary/10">
+                    {metric && metric}
+                    <ResourcesContainer>
+                      {items.map((item) => (
+                        <ResourceItem item={item} key={item.title} />
+                      ))}
+                    </ResourcesContainer>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
+        ))}
+
+        <div className="mb-20 text-center">
+          <Translation id="page-resources:page-resources-find-more" />
+        </div>
+
+        <Section
+          id="contribute"
+          className={cn(
+            "before:absolute before:-inset-px before:bottom-0 before:z-hide before:rounded-[calc(theme(borderRadius.4xl)+1px)] before:content-['']", // Border/gradient positioning
+            "before:bg-gradient-to-b before:from-primary-hover/[0.24] before:to-primary-hover/[0.08] before:dark:from-primary-hover/40 before:dark:to-primary-hover/20", // Border/gradient coloring
+            "relative inset-0 rounded-4xl bg-background" // Paint background color over card portion
+          )}
+        >
+          <div className="mb-12 flex flex-col gap-y-8 rounded-4xl bg-radial-a px-8 py-12 lg:mb-32 xl:mb-36">
+            <div className="flex flex-col gap-y-4 text-center">
+              <h2>{t("page-resources-contribute-title")}</h2>
+              <p>{t("page-resources-contribute-description")}</p>
+            </div>
+            <div className="mx-auto grid grid-cols-1 gap-16 md:grid-cols-2">
+              {/* TODO: Add issue template for resource listing and redirect to new template */}
+              <ButtonLink
+                href={new URL(
+                  "issues/new?template=feature_request.yaml",
+                  GITHUB_REPO_URL
+                ).toString()}
+                variant="outline"
+                isSecondary
+              >
+                {t("page-resources-suggest-resource")}
+              </ButtonLink>
+              <ButtonLink
+                href={new URL(
+                  "issues/new?template=bug_report.yaml",
+                  GITHUB_REPO_URL
+                ).toString()}
+                variant="outline"
+                isSecondary
+              >
+                <FaGithub /> {t("page-resources-found-bug")}
+              </ButtonLink>
+            </div>
+          </div>
+        </Section>
+      </Stack>
     </MainArticle>
   )
 }
