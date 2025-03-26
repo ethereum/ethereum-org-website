74a4067dad2843d27dcb2a03665326b9908698c9
JJOptimist <optimistkum888@gmail.com>
2025-03-26 01:30:57 +0100

    Fix: Remove duplicate 'minute read' text in tutorial metadata

--- src/components/TutorialMetadata.tsx
+++ src/components/TutorialMetadata.tsx
@@ -79,7 +79,7 @@ const TutorialMetadata = ({
         )}
         <div>
           <Emoji className="me-2 text-sm" text=":stopwatch:" />
-          {timeToRead} {t("comp-tutorial-metadata-minute-read")} minute read
+          {timeToRead} {t("comp-tutorial-metadata-minute-read")}
         </div>
       </Flex>
       {address && (
