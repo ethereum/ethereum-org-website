e95c61834754b9e1d6fe67af73ea9139e27671a9
tylerapfledderer <tyler.pfledderer@gmail.com>
2025-03-25 23:57:40 -0400

    refactor(resources): remove padding and margin from sub nav section

--- src/pages/[locale]/resources.tsx
+++ src/pages/[locale]/resources.tsx
@@ -116,7 +116,7 @@ const ResourcesPage = ({ txCostsMedianUsd }) => {
         heroImg={heroImg}
       />
       <Stack className="gap-4 px-2 py-4 md:gap-8 md:px-4 lg:px-8 xl:gap-11">
-        <div className="sticky top-1 my-8 flex flex-col items-center gap-3 py-4 text-center md:top-6 md:px-2">
+        <div className="sticky top-5 flex flex-col items-center gap-3 text-center md:top-6 md:px-2">
           <div className="my-2 text-body-medium">
             {t("page-resources-whats-on-this-page")}
           </div>
