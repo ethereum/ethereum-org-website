46bb2fbcc3c67540f494fff3b1e68657e84d1010
Corwin Smith <cssmittys@gmail.com>
2025-04-07 14:37:54 -0600

    hero

--- app/[locale]/roadmap/_components/roadmap.tsx
+++ app/[locale]/roadmap/_components/roadmap.tsx
@@ -1,13 +1,28 @@
 import BannerNotification from "@/components/Banners/BannerNotification"
+import { HubHero } from "@/components/Hero"
+import type { HubHeroProps } from "@/components/Hero/HubHero"
 import MainArticle from "@/components/MainArticle"
 
+import { useTranslation } from "@/hooks/useTranslation"
+import roadmapHeroImg from "@/public/images/heroes/roadmap-hub-hero.jpg"
+
 const RoadmapPage = () => {
+  const { t } = useTranslation("page-roadmap")
+
+  const heroContent: HubHeroProps = {
+    title: "",
+    header: t("page-roadmap-meta-title"),
+    description: t("page-roadmap-meta-description"),
+    heroImg: roadmapHeroImg,
+  }
+
   return (
     <MainArticle>
       <BannerNotification shouldShow>
         <p>Ethereum’s development is community-driven and subject to change.</p>
       </BannerNotification>
-      {/* TODO: PAGE HERO */}
+
+      <HubHero {...heroContent} />
       {/* TODO: ROADMAP CAROUSAL */}
       {/* TODO: WHAT CHANGES ARE COMING UP */}
       {/* TODO: WHY DOES ETHEREUM NEED ROADMAP */}
