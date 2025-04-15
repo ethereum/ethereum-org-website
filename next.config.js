43081d20a3fd20ac30798485ff0d03cecffc75dc
Pablo <pettinarip@gmail.com>
2025-04-15 09:40:49 +0200

    remove cpu limit

--- next.config.js
+++ next.config.js
@@ -9,20 +9,6 @@ const createNextIntlPlugin = require("next-intl/plugin")
 
 const withNextIntl = createNextIntlPlugin()
 
-const LIMIT_CPUS = Number(process.env.LIMIT_CPUS ?? 2)
-
-const experimental = LIMIT_CPUS
-  ? {
-      // This option could be enabled in the future when flagged as stable, to speed up builds
-      // (see https://nextjs.org/docs/pages/building-your-application/configuring/mdx#using-the-rust-based-mdx-compiler-experimental)
-      // mdxRs: true,
-
-      // Reduce the number of cpus and disable parallel threads in prod envs to consume less memory
-      workerThreads: false,
-      cpus: LIMIT_CPUS,
-    }
-  : {}
-
 /** @type {import('next').NextConfig} */
 module.exports = (phase, { defaultConfig }) => {
   let nextConfig = {
@@ -114,7 +100,6 @@ module.exports = (phase, { defaultConfig }) => {
     nextConfig = {
       ...nextConfig,
       experimental: {
-        ...experimental,
         outputFileTracingExcludes: {
           "*": [
             /**
