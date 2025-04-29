6bbae03091ef34787535d068b6a2d4e06c54e206
Corwin Smith <cssmittys@gmail.com>
2025-04-29 09:43:56 -0600

    fish breaking build

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -196,7 +196,7 @@ CarouselItem.displayName = "CarouselItem"
 const CarouselPrevious = React.forwardRef<
   HTMLButtonElement,
   React.ComponentProps<typeof Button>
->(({ className, variant = "outline", size = "xs", ...props }, ref) => {
+>(({ className, variant = "outline", size = "sm", ...props }, ref) => {
   const { orientation, scrollPrev, canScrollPrev } = useCarousel()
 
   return (
@@ -225,7 +225,7 @@ CarouselPrevious.displayName = "CarouselPrevious"
 const CarouselNext = React.forwardRef<
   HTMLButtonElement,
   React.ComponentProps<typeof Button>
->(({ className, variant = "outline", size = "xs", ...props }, ref) => {
+>(({ className, variant = "outline", size = "sm", ...props }, ref) => {
   const { orientation, scrollNext, canScrollNext } = useCarousel()
 
   return (
