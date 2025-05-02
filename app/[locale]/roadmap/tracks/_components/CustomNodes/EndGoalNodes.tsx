0afbfd9bcec1669319b54c81065b22ea95522594
Corwin Smith <cssmittys@gmail.com>
2025-05-02 15:42:35 -0600

    style tweaking

--- app/[locale]/roadmap/tracks/_components/CustomNodes/EndGoalNodes.tsx
+++ app/[locale]/roadmap/tracks/_components/CustomNodes/EndGoalNodes.tsx
@@ -8,8 +8,10 @@ type EndGoalNodesProps = {
 
 const EndGoalNodes = ({ data }: EndGoalNodesProps) => {
   return (
-    <div className="flex max-h-36 max-w-36 items-center justify-center rounded-full bg-primary p-8 text-center">
-      <p className="font-inter text-sm text-body-inverse">{data.label}</p>
+    <div className="flex h-36 max-h-36 w-36 max-w-36 items-center justify-center rounded-full bg-primary p-4 text-center">
+      <p className="font-inter text-[14px] font-bold leading-[160%] text-body-inverse">
+        {data.label}
+      </p>
     </div>
   )
 }
