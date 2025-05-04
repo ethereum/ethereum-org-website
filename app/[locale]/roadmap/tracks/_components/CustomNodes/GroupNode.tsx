b768f9d1094b94f6c8839c83a6dfa481dea5e0bb
Corwin Smith <cssmittys@gmail.com>
2025-05-04 16:21:37 -0600

    test

--- app/[locale]/roadmap/tracks/_components/CustomNodes/GroupNode.tsx
+++ app/[locale]/roadmap/tracks/_components/CustomNodes/GroupNode.tsx
@@ -7,7 +7,7 @@ type GroupNodeProps = {
 const GroupNode = ({ data }: GroupNodeProps) => {
   return (
     <div className="relative h-full w-full border-primary">
-      <p className="absolute left-3 top-2 m-0 font-bold">{data.label}</p>
+      <p className="absolute m-0 font-bold">{data.label}</p>
     </div>
   )
 }
