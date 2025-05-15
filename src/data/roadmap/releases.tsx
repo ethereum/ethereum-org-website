d1c56f71b1d80e7f00c4d2e092742732fbcaeabf
Corwin Smith <cssmittys@gmail.com>
2025-05-14 19:09:12 -0600

    Update src/data/roadmap/releases.tsx

--- src/data/roadmap/releases.tsx
+++ src/data/roadmap/releases.tsx
@@ -170,7 +170,7 @@ export const releasesData: Release[] = [
         </ul>
       </div>
     ),
-    href: "https://eips.ethereum.org/EIPS/eip-7594",
+    href: "https://eips.ethereum.org/EIPS/eip-7607",
   },
   {
     image: GuidesHubHeroImage,
