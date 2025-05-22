d2f17acc0c5e944da03de3eca716edef765b087d
Corwin Smith <cssmittys@gmail.com>
2025-05-22 15:35:59 -0600

    A decade of transforming the world section

--- app/[locale]/10-year-anniversary/page.tsx
+++ app/[locale]/10-year-anniversary/page.tsx
@@ -13,6 +13,7 @@ import MainArticle from "@/components/MainArticle"
 import { getMetadata } from "@/lib/utils/metadata"
 import { getRequiredNamespacesForPage } from "@/lib/utils/translations"
 
+import CountDown from "./_components/CountDown"
 import TenYearHero from "./_components/TenYearHero"
 
 const Page = async ({ params }: { params: Promise<{ locale: Lang }> }) => {
@@ -29,8 +30,32 @@ const Page = async ({ params }: { params: Promise<{ locale: Lang }> }) => {
 
   return (
     <I18nProvider locale={locale} messages={messages}>
-      <MainArticle className="mx-auto flex w-full flex-col items-center">
+      <MainArticle className="mx-auto flex w-full flex-col items-center gap-16">
         <TenYearHero />
+
+        <div className="flex w-full flex-col gap-16 px-8 py-8 md:flex-row">
+          <div className="flex flex-1 flex-col gap-5">
+            <div>
+              <h1 className="text-4xl font-bold">
+                A decade of Transforming the World
+              </h1>
+              <p className="text-2xl font-bold">One block at a time</p>
+            </div>
+            <div className="flex flex-1 flex-col gap-4">
+              <p className="text-lg">
+                On July 30, 2015, at 3:44 p.m. UTC, the first block of the
+                Ethereum blockchain came to life. The moment—when the Genesis
+                block was mined—marked the beginning of a revolutionary journey
+                that would forever change how we think about the internet,
+                finance, and digital ownership.
+              </p>
+              <p className="text-lg">Ten years down, infinity to go! 🚀</p>
+            </div>
+          </div>
+          <div className="flex flex-1 flex-row items-center justify-center">
+            <CountDown />
+          </div>
+        </div>
       </MainArticle>
     </I18nProvider>
   )
