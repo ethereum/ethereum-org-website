db4a3f19e215ceb783a1defdab3d5fab7501d643
Matthias Seidl <matthias@orbal-analytics.com>
2025-05-30 10:32:15 +0200

    Update useStatsBoxGrid.tsx

--- src/components/StatsBoxGrid/useStatsBoxGrid.tsx
+++ src/components/StatsBoxGrid/useStatsBoxGrid.tsx
@@ -109,13 +109,13 @@ export const useStatsBoxGrid = ({
     },
     {
       apiProvider: "growthepie",
-      apiUrl: "https://www.growthepie.xyz/fundamentals/transaction-costs",
+      apiUrl: "https://www.growthepie.com/fundamentals/transaction-costs",
       label: t("page-index-network-stats-tx-cost-description"),
       state: medianTxCost,
     },
     {
       apiProvider: "growthepie",
-      apiUrl: "https://www.growthepie.xyz/fundamentals/transaction-count",
+      apiUrl: "https://www.growthepie.com/fundamentals/transaction-count",
       label: t("page-index-network-stats-tx-day-description"),
       state: txs,
     },
