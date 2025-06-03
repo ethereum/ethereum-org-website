cb21041da65758807953178ef21fbd7956a5cf80
Pablo <pettinarip@gmail.com>
2025-06-03 17:47:36 +0200

    enable chromatic<>playwright instegration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,23 @@
+import type { ChromaticConfig } from "@chromatic-com/playwright"
 import { defineConfig, devices } from "@playwright/test"
 
 import "dotenv/config"
 
-export default defineConfig({
+export default defineConfig<ChromaticConfig>({
   testDir: "./tests/e2e",
   outputDir: "./tests/e2e/__results__",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI
-    ? "dot"
-    : [["html", { outputFolder: "./tests/e2e/__report__" }]],
+  reporter: [["html", { outputFolder: "./tests/e2e/__report__" }]],
   use: {
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000",
     trace: "on-first-retry",
     screenshot: "only-on-failure",
+
+    // chromatic settings
+    disableAutoSnapshot: true,
   },
   projects: [
     /* Test against desktop browsers */
