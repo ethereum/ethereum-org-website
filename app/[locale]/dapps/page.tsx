0bcac61792bd985892d55087952760a0c80d1446
Corwin Smith <cssmittys@gmail.com>
2025-06-10 21:38:15 -0600

    /dapps placeholder

--- app/[locale]/dapps/page.tsx
+++ app/[locale]/dapps/page.tsx
@@ -5,11 +5,14 @@ import {
   setRequestLocale,
 } from "next-intl/server"
 
+import { HubHero } from "@/components/Hero"
 import I18nProvider from "@/components/I18nProvider"
 import MainArticle from "@/components/MainArticle"
 
 import { getMetadata } from "@/lib/utils/metadata"
 import { getRequiredNamespacesForPage } from "@/lib/utils/translations"
+
+import DappsHeroImage from "@/public/images/dapps/dapps-hero.png"
 
 const Page = async ({ params }: { params: { locale: string } }) => {
   const { locale } = await params
@@ -22,8 +25,25 @@ const Page = async ({ params }: { params: { locale: string } }) => {
   const messages = pick(allMessages, requiredNamespaces)
   return (
     <I18nProvider locale={locale} messages={messages}>
-      <MainArticle>
-        <h1>Dapps</h1>
+      <HubHero
+        title="Use"
+        header="Dapp store"
+        description="Discover the best decentralized applications on Ethereum"
+        heroImg={DappsHeroImage}
+      />
+
+      <MainArticle className="flex flex-col gap-10 py-10">
+        <div className="flex flex-col px-4 md:px-8">
+          <h2>Highlights</h2>
+        </div>
+
+        <div className="flex flex-col px-4 md:px-8">
+          <h2>Staff picks</h2>
+        </div>
+
+        <div className="flex flex-col px-4 md:px-8">
+          <h2>All dapps</h2>
+        </div>
       </MainArticle>
     </I18nProvider>
   )
