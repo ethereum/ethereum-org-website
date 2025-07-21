7b94e137ed9c85d0b4e07e82c921023ae5bf6618
Alberto Gualis <agualis@gmail.com>
2025-07-21 12:03:31 +0200

    feat: add playwright test for start page [fixes #15896]

--- src/config/rainbow-kit.ts
+++ src/config/rainbow-kit.ts
@@ -9,21 +9,37 @@ import {
   zerionWallet,
 } from "@rainbow-me/rainbowkit/wallets"
 
+import { mockWallet } from "@/config/mockWallet"
+
+const walletGroups = [
+  {
+    groupName: "New to crypto",
+    wallets: [
+      coinbaseWallet,
+      rainbowWallet,
+      metaMaskWallet,
+      zerionWallet,
+      oneKeyWallet,
+      walletConnectWallet,
+    ],
+  },
+]
+
+const isLocalhost =
+  typeof window !== "undefined" && window.location.hostname === "localhost"
+
+// Add mock wallet only when running on localhost
+if (isLocalhost) {
+  walletGroups.push({
+    groupName: "Test",
+    wallets: [mockWallet],
+  })
+}
+
 export const rainbowkitConfig = getDefaultConfig({
   appName: "ethereum.org",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
   chains: [mainnet],
-  wallets: [
-    {
-      groupName: "New to crypto",
-      wallets: [
-        coinbaseWallet,
-        rainbowWallet,
-        metaMaskWallet,
-        zerionWallet,
-        oneKeyWallet,
-        walletConnectWallet,
-      ],
-    },
-  ],
+  wallets: walletGroups,
+  ssr: isLocalhost,
 })
