ccf2661b7af1f12c06e5397d84c6e7ae6ee2e08e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-07-25 13:45:52 -0700

    feat: add ui/textarea

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils/cn"
 
 const textareaVariants = cva(
-  "focus-visible:ring-ring flex min-h-[200px] w-full rounded border border-body bg-background px-3 py-2 text-base ring-offset-background placeholder:text-disabled focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+  "focus-visible:outline focus-visible:outline-primary-hover focus-visible:outline-[3px] focus-visible:-outline-offset-2 flex min-h-[200px] w-full rounded border border-body bg-background px-3 py-2 text-base ring-offset-background placeholder:text-disabled disabled:cursor-not-allowed disabled:opacity-50 hover:not-disabled:border-primary-hover",
   {
     variants: {
       size: {
