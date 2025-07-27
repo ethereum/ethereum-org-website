057e5e7eea8d7fc68cb6a41d67a094bffa741081
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-07-26 19:24:31 -0700

    temp: switch email destination for testing

--- app/api/enterprise-contact/route.ts
+++ app/api/enterprise-contact/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 import { CONTACT_FORM_CHAR_MIN } from "../../[locale]/enterprise/constants"
 
-const ENTERPRISE_EMAIL = "enterprise@ethereum.org"
+const ENTERPRISE_EMAIL = "website@ethereum.org" // TODO: Testing only; revert to enterprise
 const RATE_LIMIT_WINDOW_MS = 60 * 1000 // 1 minute
 const MAX_REQUESTS_PER_WINDOW = 3
 
