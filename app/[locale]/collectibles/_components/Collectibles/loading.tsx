7ccd2e0ac23a7f1bd6e793ac388a33d87d257984
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-07-28 20:11:46 -0700

    fix: simple loading states

--- app/[locale]/collectibles/_components/Collectibles/loading.tsx
+++ app/[locale]/collectibles/_components/Collectibles/loading.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 const Loading = () => (
   <div className="flex w-full flex-col gap-x-8 gap-y-6 xl:flex-row">
-    <Skeleton className="h-64 flex-1" />
-    <Skeleton className="h-64 flex-[3]" />
+    <Skeleton className="h-64 w-full max-w-screen-sm rounded-2xl" />
+    <Skeleton className="h-64 w-full rounded-2xl" />
   </div>
 )
 
--- app/[locale]/collectibles/_components/CollectiblesContent/lazy.tsx
+++ app/[locale]/collectibles/_components/CollectiblesContent/lazy.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic"
 
-import Loading from "./loading"
+import Loading from "../Collectibles/loading"
 
 export default dynamic(() => import("."), { ssr: false, loading: Loading })
