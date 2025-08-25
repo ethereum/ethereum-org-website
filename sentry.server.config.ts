2d075708945b28833cf29a8d60cc9c218bd17193
Pablo <pettinarip@gmail.com>
2025-08-25 15:53:04 +0200

    adjust tracesSampleRate

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,7 +4,7 @@ const environment = process.env.NEXT_PUBLIC_CONTEXT || "development"
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
   debug: environment === "development",
   environment,
 })
