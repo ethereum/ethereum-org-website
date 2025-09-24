df7fb757a9978bed161119a65480db96334647be
Pablo Pettinari <pettinarip@gmail.com>
2025-09-24 13:12:29 +0200

    fix styles and spacing in devconnect banner

--- src/components/DevconnectBanner/Variation1/index.tsx
+++ src/components/DevconnectBanner/Variation1/index.tsx
@@ -11,25 +11,25 @@ const DevconnectBannerVariation1 = () => {
   return (
     <div className="relative w-full">
       <Banner className="h-[66px] w-full" />
-      <div className="absolute inset-0 flex h-[66px] w-full items-center justify-between px-3 md:px-6">
-        <div className="flex flex-col gap-0 md:flex-row md:items-center md:gap-4">
-          <Image
-            src={DevconnectLogo}
-            alt="Devconnect"
-            className="h-[27px] w-auto md:h-[50px]"
-            sizes="(max-width: 768px) 77px, 142px"
-          />
-          <div className="hidden sm:block">
-            <p className="text-xs/6xs text-black">
-              <strong>17 - 22 Nov</strong> 2025
+      <div className="absolute inset-0 flex h-[66px] w-full items-center justify-between gap-4 px-3 md:px-6">
+        <div className="flex flex-col gap-1 md:flex-row md:items-center md:gap-4">
+          <div className="min-w-[80px]">
+            <Image
+              src={DevconnectLogo}
+              alt="Devconnect"
+              className="h-[27px] w-auto object-contain md:h-[50px]"
+              sizes="(max-width: 768px) 77px, 142px"
+            />
+          </div>
+          <div className="hidden text-2xs/6xs font-bold text-black sm:block md:text-xs/6xs">
+            <p className="whitespace-nowrap">
+              17 - 22 Nov <span className="font-normal">2025</span>
             </p>
-            <p className="text-xs/6xs font-bold text-black">
-              Buenos Aires, Argentina
-            </p>
+            <p className="whitespace-nowrap">Buenos Aires, Argentina</p>
           </div>
         </div>
-        <div className="max-w-[200px] text-wrap text-center md:max-w-[287px] lg:max-w-[474px]">
-          <p className="text-xs font-bold text-black md:text-sm lg:text-xl">
+        <div className="text-wrap text-center">
+          <p className="text-xs font-extrabold text-black sm:text-sm md:text-xl">
             {t(
               "page-index-devconnect-banner-join-the-biggest-ethereum-event-of-the-year"
             )}
@@ -38,8 +38,9 @@ const DevconnectBannerVariation1 = () => {
         <ButtonLink
           href="https://devconnect.org/"
           size="sm"
+          variant="ghost"
           hideArrow
-          className="rounded-none border-b-2 border-b-[#215D93] bg-[#74ACDF] uppercase text-2xs text-white hover:bg-[#215D93] md:text-md"
+          className="min-h-0 whitespace-nowrap rounded-none border-b-2 border-b-[#215D93] bg-[#74ACDF] text-xs/none font-bold uppercase text-white hover:bg-[#215D93] hover:!text-white active:bg-[#215D93] active:!text-white md:text-md/none"
         >
           {t("page-index-devconnect-banner-get-tickets")}
         </ButtonLink>
