4f08673e4eb783134626b8f16ea2db7d0047169c
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-10-21 15:24:21 -0700

    patch: spacing

--- app/[locale]/gas/_components/gas.tsx
+++ app/[locale]/gas/_components/gas.tsx
@@ -17,7 +17,7 @@ import MainArticle from "@/components/MainArticle"
 import PageHero from "@/components/PageHero"
 import { StandaloneQuizWidget } from "@/components/Quiz/QuizWidget"
 import Translation from "@/components/Translation"
-import { Alert, AlertTitle } from "@/components/ui/alert"
+import { Alert, AlertContent, AlertTitle } from "@/components/ui/alert"
 import { ButtonLink } from "@/components/ui/buttons/Button"
 import { Divider } from "@/components/ui/divider"
 import { Flex, FlexProps } from "@/components/ui/flex"
@@ -144,16 +144,16 @@ const GasPage = ({
         <Flex className="w-full flex-col items-center lg:flex-row lg:items-start">
           <div className="me-auto w-full flex-[60%] lg:me-2">
             <Alert variant="update" className="mb-8">
-              <div>
+              <AlertContent>
                 <AlertTitle className="mb-6">
                   {t("page-gas-summary-title")}
                 </AlertTitle>
-                <UnorderedList>
+                <UnorderedList className="mb-0">
                   <ListItem>{t("page-gas-summary-item-1")}</ListItem>
                   <ListItem>{t("page-gas-summary-item-2")}</ListItem>
                   <ListItem>{t("page-gas-summary-item-3")}</ListItem>
                 </UnorderedList>
-              </div>
+              </AlertContent>
             </Alert>
             <H2 className="mt-0" id="what-is-gas">
               {t("page-gas-what-are-gas-fees-header")}
