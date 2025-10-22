9893a95988cfa2e2c2aa75616f8ac794eda5b5cf
Daniel Anomfueme <anomfuemedaniel@gmail.com>
2025-10-22 13:23:03 +0100

    Feat: Added Client Diversity chart

--- src/components/MdComponents/index.tsx
+++ src/components/MdComponents/index.tsx
@@ -4,6 +4,7 @@ import type { ChildOnlyProp } from "@/lib/types"
 
 import ContributorsQuizBanner from "@/components/Banners/ContributorsQuizBanner"
 import Card from "@/components/Card"
+import ClientDiversityChart from "@/components/ClientDiversityChart"
 import BrowseApps from "@/components/Content/what-are-apps/BrowseApps"
 import WhatAreAppsStories from "@/components/Content/what-are-apps/WhatAreAppsStories"
 import Contributors from "@/components/Contributors"
@@ -17,6 +18,7 @@ import MarkdownImage from "@/components/Image/MarkdownImage"
 import IssuesList from "@/components/IssuesList"
 import LocaleDateTime from "@/components/LocaleDateTime"
 import MainArticle from "@/components/MainArticle"
+import { PieChart } from "@/components/PieChart"
 import { StandaloneQuizWidget } from "@/components/Quiz/QuizWidget"
 import TooltipLink from "@/components/TooltipLink"
 import { ButtonLink } from "@/components/ui/buttons/Button"
@@ -165,6 +167,7 @@ export const ContentContainer = (props: ComponentProps<"article">) => {
 export const reactComponents = {
   ...AlertComponents,
   BrowseApps,
+  ClientDiversityChart,
   ButtonLink,
   Card,
   ContentContainer,
@@ -177,6 +180,7 @@ export const reactComponents = {
   FeaturedText,
   GlossaryTooltip,
   Page,
+  PieChart,
   QuizWidget: StandaloneQuizWidget,
   IssuesList,
   Tag,
