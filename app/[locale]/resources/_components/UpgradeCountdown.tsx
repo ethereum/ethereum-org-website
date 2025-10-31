1dd199b7fe40fc286ca68d0b8ee4b161e821d952
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-10-30 18:27:23 -0700

    patch: typo

--- app/[locale]/resources/_components/UpgradeCountdown.tsx
+++ app/[locale]/resources/_components/UpgradeCountdown.tsx
@@ -83,7 +83,7 @@ const UpgradeCountdown = () => {
   return (
     <>
       <BaseLink
-        href={`/roadmap/${upgrade}}/`}
+        href={`/roadmap/${upgrade}/`}
         className="text-5xl font-bold text-body no-underline hover:text-primary"
       >
         {upgrade.slice(0, 1).toUpperCase() + upgrade.slice(1)}
