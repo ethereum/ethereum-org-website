a779d30e04bbc7ceedee12dae1f1c345558c834b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-11-04 16:19:16 -0600

    patch: banner resolution

--- src/components/CommunityHubsList.tsx
+++ src/components/CommunityHubsList.tsx
@@ -8,7 +8,12 @@ const CommunityHubsList = () => {
     <div className="mt-12 grid grid-cols-1 gap-12 md:grid-cols-2 md:gap-8">
       {communityHubs.map((hub) => (
         <div key={hub.location} className="flex flex-col gap-4">
-          <Image src={hub.banner} alt={hub.location} className="w-full" />
+          <Image
+            src={hub.banner}
+            alt={hub.location}
+            className="w-full rounded-xl"
+            sizes="368px"
+          />
           <div className="p-2">
             <div className="flex flex-col gap-2 text-center">
               <h3 className="text-xl font-bold md:text-2xl">Community Hub</h3>
