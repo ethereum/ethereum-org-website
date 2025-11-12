de9bc6056de6c7a798a7fe1c3d245f140a6824cf
Corwin Smith <cssmittys@gmail.com>
2025-11-11 20:25:38 -0700

    Update FusakaHero component layout and styling for better readability and user experience, including adjustments to text sizes and hover effects on links.

--- src/components/Hero/FusakaHero/index.tsx
+++ src/components/Hero/FusakaHero/index.tsx
@@ -24,25 +24,29 @@ const FusakaHero = async () => {
       </div>
 
       <LinkBox className="duration-50 -mt-1 rounded-none border border-[rgba(0,0,0,0.08)] bg-[#333369]/95 p-4 text-center text-white transition-shadow lg:mx-8 lg:-mt-14 lg:rounded-2xl lg:border-[#C8B2F5] lg:shadow-[0_10px_10px_0_rgba(108,30,210,0.51)] lg:hover:shadow-[0_20px_10px_5px_rgba(108,30,210,0.51)]">
-        <div className="flex flex-col items-center justify-between lg:flex-row lg:gap-16">
+        <div className="flex flex-col items-center justify-between gap-2 lg:flex-row lg:gap-16">
           <div className="flex flex-col items-center justify-center">
-            <p className="text-4xl font-extrabold uppercase !leading-none lg:text-5xl">
+            <p className="text-xl font-extrabold uppercase !leading-none lg:text-5xl">
               FUSAKA
             </p>
-            <p className="text-xs font-bold uppercase text-purple-100">
+            <p className="text-sm font-bold uppercase text-purple-100">
               Network upgrade
             </p>
           </div>
-          <p className="">
+          <p className="text-xs text-white lg:text-sm">
             For a faster, safer, and more user-friendly Ethereum network |{" "}
-            <LinkOverlay href="/roadmap/fusaka" className="text-white">
+            <LinkOverlay
+              href="/roadmap/fusaka"
+              className="text-white hover:text-purple-300"
+            >
               Read more
             </LinkOverlay>
             .
           </p>
-          <div className="mt-4 flex flex-row items-center justify-center gap-4 lg:mt-0 lg:flex-col lg:gap-0">
+          <div className="flex flex-row items-center justify-center gap-4 lg:mt-0 lg:flex-col lg:gap-0">
             <p className="text-xs font-bold uppercase text-gray-200">
-              Going live in
+              Going
+              <br className="lg:hidden" /> live in
             </p>
             <FusakaCountdown />
           </div>
