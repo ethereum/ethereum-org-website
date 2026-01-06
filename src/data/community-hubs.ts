a162ba0a408d564fd06f3c9fa3d7e7b9203bb90f
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2026-01-06 15:59:14 -0800

    update: community hub data

--- src/data/community-hubs.ts
+++ src/data/community-hubs.ts
@@ -2,6 +2,7 @@ import { StaticImageData } from "next/image"
 
 import BerlinHubBanner from "@/public/images/community/hubs/berlin-hub-banner.png"
 import DubaiHubBanner from "@/public/images/community/hubs/dubai-hub-banner.png"
+import LagosHubBanner from "@/public/images/community/hubs/lagos-hub-banner.png"
 import LondonHubBanner from "@/public/images/community/hubs/london-hub-banner.png"
 
 export type CommunityHub = {
@@ -26,7 +27,7 @@ const communityHubs: CommunityHub[] = [
     coworkingSignupUrl: "https://hub.encode.club/ethereum",
     meetupUrl: "https://luma.com/user/ethldn",
     banner: LondonHubBanner,
-    bgColor: "bg-[#E8E0F0]",
+    bgColor: "bg-[#FEFAF3]",
     logoBgColor: "bg-[#7B5AA6]",
   },
   {
@@ -39,7 +40,7 @@ const communityHubs: CommunityHub[] = [
       "https://docs.google.com/forms/d/e/1FAIpQLScRgO-0OMUXOu30F5s2DYpImMKb4qgTp5pn-4Y6wjiCeY7bYQ/viewform",
     meetupUrl: "https://www.meetup.com/berlin-ethereum-meetup/",
     banner: BerlinHubBanner,
-    bgColor: "bg-[#E8DDD4]",
+    bgColor: "bg-[#F3E8F6]",
     logoBgColor: "bg-[#9B7B5A]",
   },
   {
@@ -51,7 +52,7 @@ const communityHubs: CommunityHub[] = [
     coworkingSignupUrl: "https://forms.gle/G4PthfvMMy476QmZ6",
     meetupUrl: "https://luma.com/HadronFC?k=c",
     banner: DubaiHubBanner,
-    bgColor: "bg-[#F5E6C8]",
+    bgColor: "bg-[#F4E0BB]",
     logoBgColor: "bg-[#D4A84B]",
   },
   {
@@ -62,8 +63,8 @@ const communityHubs: CommunityHub[] = [
     cta: "Join for everyday co-working and regular events.",
     coworkingSignupUrl: "#",
     meetupUrl: "#",
-    banner: DubaiHubBanner, // Placeholder - needs proper Lagos banner
-    bgColor: "bg-[#E8DDD4]",
+    banner: LagosHubBanner,
+    bgColor: "bg-[#E6F3F2]",
     logoBgColor: "bg-[#8B6914]",
   },
 ]
