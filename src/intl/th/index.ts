614ccdf06e63ca0540d625771eff924ccb1cf017
Pum482930 <223752569+Pum482930@users.noreply.github.com>
2026-04-11 17:00:27 +0700

    Create th/index.ts

---
+++ src/intl/th/index.ts
@@ -0,0 +1,16 @@
+import { Translation } from "../types"
+
+const th: Translation = {
+  "common": {
+    "ok": "ตกลง",
+    "cancel": "ยกเลิก",
+    "save": "บันทึก",
+    "search": "ค้นหา"
+  },
+  "page-index": {
+    "title": "ยินดีต้อนรับสู่ Ethereum",
+    "description": "Ethereum เป็นเทคโนโลยีสำหรับการสร้างแอปพลิเคชันและองค์กร การถือครองสินทรัพย์ การทำธุรกรรม และการสื่อสารโดยไม่มีใครควบคุม"
+  }
+}
+
+export default th
