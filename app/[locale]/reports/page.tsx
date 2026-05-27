b723b45e0776ba526979137e7b3f91ea757741f0
myelinated-wackerow <263208946+myelinated-wackerow@users.noreply.github.com>
2026-05-27 06:06:11 -0700

    chore: remove unnecessary wraper div
    Co-Authored-By: wackerow <54227730+wackerow@users.noreply.github.com>

--- app/[locale]/reports/page.tsx
+++ app/[locale]/reports/page.tsx
@@ -50,97 +50,95 @@ const Page = async (props: { params: Promise<PageParams> }) => {
         reports={reports}
       />
 
-      <div>
-        <ContentHero
-          breadcrumbs={{ slug: "research/reports", startDepth: 1 }}
-          heroImg={heroImg}
-          title={t("page-reports-title")}
-          description={t("page-reports-description")}
-        />
-        <MainArticle className="relative space-y-16 px-4 py-16 md:space-y-20 md:px-10 md:py-20">
-          <Section id="reports" className="space-y-12">
-            <div className="max-w-3xl space-y-[0.25lh]">
-              <h2>{t("page-reports-heading")}</h2>
-              <p>{t("page-reports-intro")}</p>
-            </div>
+      <ContentHero
+        breadcrumbs={{ slug: "research/reports", startDepth: 1 }}
+        heroImg={heroImg}
+        title={t("page-reports-title")}
+        description={t("page-reports-description")}
+      />
+      <MainArticle className="relative space-y-16 px-4 py-16 md:space-y-20 md:px-10 md:py-20">
+        <Section id="reports" className="space-y-12">
+          <div className="max-w-3xl space-y-[0.25lh]">
+            <h2>{t("page-reports-heading")}</h2>
+            <p>{t("page-reports-intro")}</p>
+          </div>
 
-            <div className="grid grid-cols-fill-4 gap-4">
-              {reports.map(
-                ({
-                  slug,
-                  title,
-                  publisher,
-                  dateIso,
-                  href,
-                  internal,
-                  imgSrc,
-                  fileSizeBytes,
-                }) => (
-                  <Card key={slug} href={href}>
-                    <CardHeader>
-                      <CardBanner size="full" fit="contain">
-                        <Image
-                          src={imgSrc}
-                          alt=""
-                          sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 384px"
-                        />
-                      </CardBanner>
-                    </CardHeader>
-                    <CardContent>
-                      <div className="flex flex-wrap gap-2 font-bold uppercase">
-                        <Tag status="warning" size="small">
-                          {formatDate(dateIso, locale, {
-                            year: "numeric",
-                            month: "short",
-                            timeZone: "UTC",
-                            day: undefined,
+          <div className="grid grid-cols-fill-4 gap-4">
+            {reports.map(
+              ({
+                slug,
+                title,
+                publisher,
+                dateIso,
+                href,
+                internal,
+                imgSrc,
+                fileSizeBytes,
+              }) => (
+                <Card key={slug} href={href}>
+                  <CardHeader>
+                    <CardBanner size="full" fit="contain">
+                      <Image
+                        src={imgSrc}
+                        alt=""
+                        sizes="(max-width: 768px) 100vw, (max-width: 1280px) 50vw, 384px"
+                      />
+                    </CardBanner>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="flex flex-wrap gap-2 font-bold uppercase">
+                      <Tag status="warning" size="small">
+                        {formatDate(dateIso, locale, {
+                          year: "numeric",
+                          month: "short",
+                          timeZone: "UTC",
+                          day: undefined,
+                        })}
+                      </Tag>
+                      {fileSizeBytes ? (
+                        <Tag status="accent-a" size="small">
+                          {t("page-reports-pdf-size", {
+                            size: numberFormat(locale, {
+                              style: "unit",
+                              unit: "megabyte",
+                              unitDisplay: "short",
+                              maximumFractionDigits: 1,
+                            }).format(fileSizeBytes / 2 ** 20),
                           })}
                         </Tag>
-                        {fileSizeBytes ? (
-                          <Tag status="accent-a" size="small">
-                            {t("page-reports-pdf-size", {
-                              size: numberFormat(locale, {
-                                style: "unit",
-                                unit: "megabyte",
-                                unitDisplay: "short",
-                                maximumFractionDigits: 1,
-                              }).format(fileSizeBytes / 2 ** 20),
-                            })}
-                          </Tag>
-                        ) : !internal ? (
-                          <Tag status="accent-b" size="small">
-                            {t("page-reports-web-article")}
-                          </Tag>
-                        ) : null}
-                      </div>
-                      <CardTitle>
-                        {title}
-                        {(isExternal(href) || isFile(href)) && (
-                          <ExternalLinkIcon />
-                        )}
-                      </CardTitle>
-                      <CardParagraph>{publisher}</CardParagraph>
-                    </CardContent>
-                  </Card>
-                )
-              )}
-            </div>
-          </Section>
+                      ) : !internal ? (
+                        <Tag status="accent-b" size="small">
+                          {t("page-reports-web-article")}
+                        </Tag>
+                      ) : null}
+                    </div>
+                    <CardTitle>
+                      {title}
+                      {(isExternal(href) || isFile(href)) && (
+                        <ExternalLinkIcon />
+                      )}
+                    </CardTitle>
+                    <CardParagraph>{publisher}</CardParagraph>
+                  </CardContent>
+                </Card>
+              )
+            )}
+          </div>
+        </Section>
 
-          <Section id="suggest" className="space-y-[1.5lh]">
-            <div className="max-w-3xl space-y-[0.25lh]">
-              <h2>{t("page-reports-suggest-heading")}</h2>
-              <p>{t("page-reports-suggest-body")}</p>
-            </div>
-            <ButtonLink
-              href="https://github.com/ethereum/ethereum-org-website/issues/new/choose"
-              variant="outline"
-            >
-              {t("page-reports-suggest-cta")}
-            </ButtonLink>
-          </Section>
-        </MainArticle>
-      </div>
+        <Section id="suggest" className="space-y-[1.5lh]">
+          <div className="max-w-3xl space-y-[0.25lh]">
+            <h2>{t("page-reports-suggest-heading")}</h2>
+            <p>{t("page-reports-suggest-body")}</p>
+          </div>
+          <ButtonLink
+            href="https://github.com/ethereum/ethereum-org-website/issues/new/choose"
+            variant="outline"
+          >
+            {t("page-reports-suggest-cta")}
+          </ButtonLink>
+        </Section>
+      </MainArticle>
     </>
   )
 }
