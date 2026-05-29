2e092270629df4b33e96b6941c83e5c8cc7f9d4c
myelinated-wackerow <263208946+myelinated-wackerow@users.noreply.github.com>
2026-05-29 03:33:29 -0700

    deprecate(ui): SimpleHero, in lieu of PageHero
    Co-Authored-By: wackerow <54227730+wackerow@users.noreply.github.com>

--- src/components/Hero/index.ts
+++ src/components/Hero/index.ts
@@ -2,8 +2,3 @@ export { default as HomeHero } from "./HomeHero"
 export { default as HubHero } from "./HubHero"
 export { default as MdxHero, type MdxHeroProps } from "./MdxHero"
 export { default as PageHero, type PageHeroProps } from "./PageHero"
-export {
-  default as SimpleHero,
-  type SimpleHeroButton,
-  type SimpleHeroProps,
-} from "./SimpleHero"
