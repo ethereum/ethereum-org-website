d4ed937611bc1485960a855b7b76969ed57143fa
myelinated-wackerow <263208946+myelinated-wackerow@users.noreply.github.com>
2026-06-11 06:59:11 -0700

    refactor(ui): move actions outside of article
    - Moves actions at end-of-page for dev Docs.tsx layout outside of main `article` element
    - Wraps main central column in semantic `main` container
    - Refactor back-to-top to simple `Link` block element, remove one-off component abstraction
    - Keeps FileContributors inside `article`
    
    Co-Authored-By: wackerow <54227730+wackerow@users.noreply.github.com>

--- src/layouts/Docs.tsx
+++ src/layouts/Docs.tsx
@@ -21,20 +21,12 @@ import Translation from "@/components/Translation"
 import { Alert } from "@/components/ui/alert"
 import { ButtonLink } from "@/components/ui/buttons/Button"
 import { Divider } from "@/components/ui/divider"
-import InlineLink from "@/components/ui/Link"
+import Link from "@/components/ui/Link"
 import { mdxTableComponents } from "@/components/ui/mdx-table-components"
 import YouTube from "@/components/YouTube"
 
 import { getEditPath } from "@/lib/utils/editPath"
 import { addSlashes } from "@/lib/utils/url"
-
-const BackToTop = (props: ChildOnlyProp) => (
-  <div className="display-none mt-12 flex border-t pt-8" {...props}>
-    <InlineLink href="#top">
-      <Translation id="page-developers-docs:back-to-top" /> ↑
-    </InlineLink>
-  </div>
-)
 
 export const docsComponents = {
   ...mdxTableComponents,
@@ -86,33 +78,40 @@ export const DocsLayout = ({
         dir={contentNotTranslated ? "ltr" : "unset"}
       >
         <SideNav path={addSlashes(slug)} />
-        <MainArticle className="flow min-w-0 flex-1 px-8 pt-8 pb-8 md:px-16 md:pt-12 md:pb-16">
-          <h1 id="top">{frontmatter.title}</h1>
-          <PageActions
-            slug={slug}
-            isTranslated={!contentNotTranslated}
-            editPath={absoluteEditPath}
-            hideEditButton={!!frontmatter.hideEditButton}
-            className="-ms-2 mb-10 lg:mb-12"
-          />
-          <TableOfContents
-            items={tocItems}
-            maxDepth={frontmatter.sidebarDepth!}
-            isMobile
-          />
-          {children}
-          {isPageIncomplete && <CallToContribute editPath={absoluteEditPath} />}
-          {!frontmatter.hideEditButton && (
-            <FileContributors
-              className="my-10 border-t"
-              contributors={contributors}
-              lastEditLocaleTimestamp={lastEditLocaleTimestamp}
+        <main className="min-w-0 flex-1 p-8 wrap-break-word xl:p-16">
+          <MainArticle className="flow">
+            <h1 id="top">{frontmatter.title}</h1>
+            <PageActions
+              slug={slug}
+              isTranslated={!contentNotTranslated}
+              editPath={absoluteEditPath}
+              hideEditButton={!!frontmatter.hideEditButton}
+              className="-ms-2 mb-10 lg:mb-12"
             />
-          )}
-          <BackToTop />
+            <TableOfContents
+              items={tocItems}
+              maxDepth={frontmatter.sidebarDepth!}
+              isMobile
+            />
+            {children}
+            {!frontmatter.hideEditButton && (
+              <FileContributors
+                className="my-10 border-t"
+                contributors={contributors}
+                lastEditLocaleTimestamp={lastEditLocaleTimestamp}
+              />
+            )}
+          </MainArticle>
+
+          {/* End-of-page actions */}
+          <Link href="#top">
+            <Translation id="page-developers-docs:back-to-top" /> ↑
+          </Link>
+          {isPageIncomplete && <CallToContribute editPath={absoluteEditPath} />}
           <ContentFeedback isArticle />
           <DocsNav contentNotTranslated={contentNotTranslated} />
-        </MainArticle>
+        </main>
+
         {tocItems && (
           <TableOfContents
             className={isPageIncomplete ? "pt-20" : "pt-12"}
