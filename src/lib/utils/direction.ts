5750f82e0df4d2c7e44c7ff4ce4daa8236ac12af
Alejandro Rodríguez <alerodriargui@gmail.com>
2026-06-30 12:06:39 +0200

    fix flipForRtl api

--- src/lib/utils/direction.ts
+++ src/lib/utils/direction.ts
@@ -3,8 +3,6 @@ import type { Lang } from "@/lib/types"
 import { isLangRightToLeft } from "@/lib/utils/translations"
 
 type DirectionData = {
-  /** @deprecated */
-  flipForRtl: "scaleX(-1)" | undefined // transform (deprecated)
   twFlipForRtl: "rtl:-scale-x-100" // className
   isRtl: boolean
   direction: "ltr" | "rtl"
@@ -14,12 +12,11 @@ type DirectionData = {
  * Determines the direction and transformation for right-to-left (RTL) languages.
  * @param locale The language locale
  * @returns An object containing the Tailwind className, RTL flag, and direction.
- * @example const { twFlipForRtl } = getDirectionData('ar');
+ * @example const { twFlipForRtl } = getDirection('ar');
  */
 export const getDirection = (locale: Lang): DirectionData => {
   const isRtl = isLangRightToLeft(locale)
   return {
-    flipForRtl: isRtl ? "scaleX(-1)" : undefined, // transform (deprecated)
     twFlipForRtl: "rtl:-scale-x-100", // className (preferred)
     isRtl,
     direction: isRtl ? "rtl" : "ltr",
